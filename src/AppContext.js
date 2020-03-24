5189dfe446721583a216d1f4f8d253f2deaa096f
iamzishan78 <zishannaseeb528@gmail.com>
2020-03-24 00:15:10 +0000

    graphQL updates

--- src/AppContext.js
+++ src/AppContext.js
@@ -7,7 +7,7 @@ const AppContext = createContext([{}, () => {}]);
 const AppProvider = props => {
   const [stateApp, setStateApp] = useState({
     selectedRoute: "/",
-    apolloClientEndpoint:'https://m1graph.azurewebsites.net/api/m1graphql?code=0mQgwjKYS3TOX7DvDmJUNbUpCqnP4OQragGaaZVqkFdQVvkz8hRbVQ==',
+    apolloClientEndpoint:'https://m1graphql.azurewebsites.net/api/m1neral?code=kNAzP9HYSsEwdWhlLa55AIGeKj2iiFFOpXaTMRh9IuTODWpNobIX3g==',
     user: null,
     wellCount: 500,
     wells: null,
