9d0bdcbe9db2449324a80a8584fa576d019768f5
iamzishan78 <zishannaseeb528@gmail.com>
2020-04-07 15:10:30 -0500

    fix styles coornidates , comment out title opinion tabs

--- src/components/TitleOpinion/components/TitleOpinionsTaps.js
+++ src/components/TitleOpinion/components/TitleOpinionsTaps.js
@@ -74,9 +74,9 @@ export default function TitleOpinionsTaps() {
         >
           <Tab label="MOR" {...a11yProps(0)} />
           <Tab label="Runsheet" {...a11yProps(1)} />
-          <Tab label="Division Order" {...a11yProps(2)} />
+          {/* <Tab label="Division Order" {...a11yProps(2)} />
           <Tab label="Associated Documents" {...a11yProps(3)} />
-          <Tab label="Internal Note" {...a11yProps(4)} />
+          <Tab label="Internal Note" {...a11yProps(4)} /> */}
         </Tabs>
       </AppBar>
       <TabPanel value={value} index={0} className="MORDiv">
