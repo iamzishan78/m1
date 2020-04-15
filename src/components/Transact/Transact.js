d9566c2a0ffcf1fa7bef90c84d2e2302f837162e
iamzishan78 <zishannaseeb528@gmail.com>
2020-04-15 14:48:35 -0500

    transact

--- src/components/Transact/Transact.js
+++ src/components/Transact/Transact.js
@@ -1,108 +1,152 @@
-import React, { useContext } from 'react';
+import React, { useContext } from "react";
 import Board from "react-trello";
 import { makeStyles } from "@material-ui/core/styles";
 import { TransactContext } from "./TransactContext";
 
-
-
 const boardStyle = {
-  backgroundColor: ' #eeeeee',
+  backgroundColor: " #eeeeee",
   //overflow: 'scroll',
   //height: '100vh !important',
   //width: '100vw !important',
-
 };
 
+// const laneStyle = {
+//   // fontFamily: "Roboto, Helvetica, Arial, sans-serif"
+//   fontFamily: "Poppins",
+// };
 
-
-const laneStyle = {
- // fontFamily: "Roboto, Helvetica, Arial, sans-serif"
- fontFamily: "Poppins"
-};
-
-const cardStyle = {
-  //fontFamily: "Roboto, Helvetica, Arial, sans-serif"
-  fontFamily: "Poppins"
-};
+// const cardStyle = {
+//   //fontFamily: "Roboto, Helvetica, Arial, sans-serif"
+//   fontFamily: "Poppins",
+// };
 
 const data_file = {
   lanes: [
     {
-      id: 'lane1',
-      title: 'Offer Preparation',
+      id: "lane1",
+      title: "Offer Preparation",
       //label: '2/2',
-      style: laneStyle,
-      cardStyle: cardStyle,
+      // style: laneStyle,
+      // cardStyle: cardStyle,
       cards: [
-        {id: 'Card1', 
-        title: 'THORNTON, CHARLES T', 
-        description: 'API: 4230130541\nWell Name: CHRISTOPHER -38- 4\nNRI: 0.017857\nTax Value: $10,860' , label: '$103,100'}
-        
-        
-      ]
+        {
+          id: "Card1",
+          title: "THORNTON, CHARLES T",
+          description:
+            "API: 4230130541\nWell Name: CHRISTOPHER -38- 4\nNRI: 0.017857\nTax Value: $10,860",
+          label: "$103,100",
+        },
+      ],
     },
     {
-      id: 'lane2',
-      title: 'Offer Extended',
+      id: "lane2",
+      title: "Offer Extended",
       //label: '0/0',
-      style: laneStyle,
-      cardStyle: cardStyle,
+      // style: laneStyle,
+      // cardStyle: cardStyle,
       cards: [
-        {id: 'Card3', title: 'SMITH, JAMES E', description: 'Location: WARD, TX\nRoyalty: 20%\nAcreage: 4.83 NMA (7.728 NRA)\nPrice Per NMA: $15,000', label: '$115,900'},
-        {id: 'Card2', title: 'CHRISTOPHER, EDITH', description: 'API: 4230130541\nWell Name: CHRISTOPHER -38- 4\nNRI: 0.005952\nTax Value: $3,620', label: '$54,739'}
-      ]
+        {
+          id: "Card3",
+          title: "SMITH, JAMES E",
+          description:
+            "Location: WARD, TX\nRoyalty: 20%\nAcreage: 4.83 NMA (7.728 NRA)\nPrice Per NMA: $15,000",
+          label: "$115,900",
+        },
+        {
+          id: "Card2",
+          title: "CHRISTOPHER, EDITH",
+          description:
+            "API: 4230130541\nWell Name: CHRISTOPHER -38- 4\nNRI: 0.005952\nTax Value: $3,620",
+          label: "$54,739",
+        },
+      ],
     },
 
     {
-      id: 'lane3',
-      title: 'Accepted - Due Diligence',
+      id: "lane3",
+      title: "Accepted - Due Diligence",
       //label: '0/0',
-      style: laneStyle,
-      cardStyle: cardStyle,
+      // style: laneStyle,
+      // cardStyle: cardStyle,
       cards: [
-        {id: 'Card4', title: 'JONES, MICHAEL F', description: 'Location: UPTON, TX\nRoyalty: 12.5%\nAcreage: 10.2 NMA (10.2 NRA)\nPrice Per NMA: $32,000', label: '$326,400'}
-      ]
+        {
+          id: "Card4",
+          title: "JONES, MICHAEL F",
+          description:
+            "Location: UPTON, TX\nRoyalty: 12.5%\nAcreage: 10.2 NMA (10.2 NRA)\nPrice Per NMA: $32,000",
+          label: "$326,400",
+        },
+      ],
     },
 
     {
-      id: 'lane4',
-      title: 'Deal Closed',
+      id: "lane4",
+      title: "Deal Closed",
       //label: '0/0',
-      style: laneStyle,
-      cardStyle: cardStyle,
+      // style: laneStyle,
+      // cardStyle: cardStyle,
       cards: [
-        {id: 'Card5', title: 'MOUSSEAU, VICKI L', description: 'Location: LEA, NM\nRoyalty: 18.75%\nAcreage: 6.7 NMA (10.05 NRA)\nPrice Per NMA: $18,000', label: '$180,900'},
-        {id: 'Card6', title: 'CANON, MICHAEL J', description: 'Location: REEVES, TX\nRoyalty: 15.625%\nAcreage: 43 NMA (53.75 NRA)\nPrice Per NMA: $32,000', label: '$571,094'}
-            ]
+        {
+          id: "Card5",
+          title: "MOUSSEAU, VICKI L",
+          description:
+            "Location: LEA, NM\nRoyalty: 18.75%\nAcreage: 6.7 NMA (10.05 NRA)\nPrice Per NMA: $18,000",
+          label: "$180,900",
+        },
+        {
+          id: "Card6",
+          title: "CANON, MICHAEL J",
+          description:
+            "Location: REEVES, TX\nRoyalty: 15.625%\nAcreage: 43 NMA (53.75 NRA)\nPrice Per NMA: $32,000",
+          label: "$571,094",
+        },
+      ],
     },
     {
-      id: 'lane5',
-      title: 'Offer Rejected',
+      id: "lane5",
+      title: "Offer Rejected",
       //label: '0/0',
-      style: laneStyle,
-      cardStyle: cardStyle,
+      // style: laneStyle,
+      // cardStyle: cardStyle,
       cards: [
-        {id: 'Card7', title: 'SCARBOROUGH, KATHRYN', description: 'Location: LOVING, TX\nRoyalty: 12.5%\nAcreage: 3 NMA (3 NRA)\nPrice Per NMA: $19,000', label: '$57,000'},
-        {id: 'Card8', title: 'TRAYLOR, MARY ELIZABETH', description: 'API: 4230133032\nWell Name: PISTOL 24-24 2H\nNRI: 0.046743\nTax Value: $215,690', label: '$943,291'},
-        {id: 'Card9', title: 'KING, JACOB B', description: 'Location: REEVES, TX\nRoyalty: 20%\nAcreage: 150 NMA (240 NRA)\nPrice Per NMA: $9,500', label: '$2,280,000'},
-      ]
-       
-    }
-  ]
-}
+        {
+          id: "Card7",
+          title: "SCARBOROUGH, KATHRYN",
+          description:
+            "Location: LOVING, TX\nRoyalty: 12.5%\nAcreage: 3 NMA (3 NRA)\nPrice Per NMA: $19,000",
+          label: "$57,000",
+        },
+        {
+          id: "Card8",
+          title: "TRAYLOR, MARY ELIZABETH",
+          description:
+            "API: 4230133032\nWell Name: PISTOL 24-24 2H\nNRI: 0.046743\nTax Value: $215,690",
+          label: "$943,291",
+        },
+        {
+          id: "Card9",
+          title: "KING, JACOB B",
+          description:
+            "Location: REEVES, TX\nRoyalty: 20%\nAcreage: 150 NMA (240 NRA)\nPrice Per NMA: $9,500",
+          label: "$2,280,000",
+        },
+      ],
+    },
+  ],
+};
 
-
-
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   root: {
     //width: '100vh',
-    //height: '100vh',
-    //overflow: 'auto',  
+    height: "100%",
+    //overflow: 'auto',
   },
   list: {
     //height:'100vh',
     //width: '100vw',
-    overflowX: 'auto !important',
+    overflowX: "auto !important",
+    height: "100%",
+
     //position: 'relative'
     //overflowX:'hidden',
     //overflowY: 'hidden',
@@ -115,41 +159,32 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
-
-
-const handleCardAdd = event => {
-  console.log('card add')
-  console.log(event)
+const handleCardAdd = (event) => {
+  console.log("card add");
+  console.log(event);
 };
 
-const handleDataChange= event => {
-  console.log('card data change')
-  console.log(event)
+const handleDataChange = (event) => {
+  console.log("card data change");
+  console.log(event);
 };
 
-const handleCardDelete = event => {
-  console.log('card delete')
-  console.log(event)
+const handleCardDelete = (event) => {
+  console.log("card delete");
+  console.log(event);
 };
-
 
 export default function Transact() {
   const classes = useStyles();
   const [stateTransact, setStateTransact] = useContext(TransactContext);
 
-
-
-
-
-
-
-    return (
-
-      <div>
-        <Board  className = {classes.list}
+  return (
+    <div  className={classes.root}>
+      <Board
+        className={classes.list}
         style={boardStyle}
         data={data_file}
-        draggable = {true}
+        draggable={true}
         laneDraggable={true}
         cardDraggable={true}
         collapsibleLanes={true}
@@ -175,13 +210,7 @@ export default function Transact() {
         // onLaneClick
         // onLaneScroll
         //onCardMoveAcrossL{handleCardAdd}anes = {handleCardAdd}
-        
-        />
-        
-      </div>
-
-
-    );
-  }
-
-
+      />
+    </div>
+  );
+}
--- src/components/Transact/TransactProvider.js
+++ src/components/Transact/TransactProvider.js
@@ -1,20 +1,20 @@
-import React from 'react'
-import { TransactContextProvider } from './TransactContext'
-import { makeStyles } from '@material-ui/core/styles'
-import Transact from './Transact'
-import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles'
-const useStyles = makeStyles(theme => ({
+import React from "react";
+import { TransactContextProvider } from "./TransactContext";
+import { makeStyles } from "@material-ui/core/styles";
+import Transact from "./Transact";
+import { createMuiTheme, MuiThemeProvider } from "@material-ui/core/styles";
+const useStyles = makeStyles((theme) => ({
   transactWrapper: {
-    width: '100%',
-    height:'100%'
-  }
-}))
+    width: "100%",
+    height: "100%",
+  },
+}));
 
 export default function TransactProvider(props) {
-  let classes = useStyles()
+  let classes = useStyles();
   return (
     <TransactContextProvider>
-        <Transact className={classes.transactWrapper}>{props.children}</Transact>
+      <Transact className={classes.transactWrapper}>{props.children}</Transact>
     </TransactContextProvider>
-  )
+  );
 }
