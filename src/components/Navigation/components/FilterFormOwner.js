202cdc91caff11330626f76fb94e4181260f5ac4
iamzishan78 <zishannaseeb528@gmail.com>
2020-04-18 18:58:00 -0500

    confidence filter shell added

--- src/components/Navigation/components/FilterFormOwner.js
+++ src/components/Navigation/components/FilterFormOwner.js
@@ -4,6 +4,7 @@ import Autocomplete from "@material-ui/lab/Autocomplete";
 import TextField from "@material-ui/core/TextField";
 import { NavigationContext } from "../NavigationContext";
 import FilterOwnerCount from "./FilterOwnerCount";
+import FilterOwnerConfidence from "./FilterOwnerConfidence";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -286,6 +287,7 @@ export default function FilterFormOwner() {
           disableListWrap
         />
       <FilterOwnerCount/>
+      <FilterOwnerConfidence/>
       </div>
     </div>
   );
