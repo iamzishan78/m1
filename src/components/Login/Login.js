432344e1a518d2c6a7a9e799c9d30e1f072531fa
iamzishan78 <zishannaseeb528@gmail.com>
2020-04-26 23:07:49 -0500

    styling and beginnings of footer

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -21,6 +21,7 @@ import CircularProgress from '@material-ui/core/CircularProgress';
 import { Link } from "react-router-dom";
 import { validateData } from "./loginHelpers";
 import InputBase from '@material-ui/core/InputBase';
+import styled from "styled-components";
 
 
 const localStyles = makeStyles(theme => ({
@@ -62,18 +63,18 @@ const localStyles = makeStyles(theme => ({
     justifyContent: "center"
   },
   signInCard: {
-    width: 250,
-    height: '400px',
+    width: '250px',
+    height: '410px',
     marginBottom: 50,
-    border: '1px solid #A9A9A9' ,
+    border: '1px solid #011133' ,
     fontFamily: theme.typography.fontFamily,
     "&:hover": {
-      border: `3px solid ${theme.palette.secondary.main}`,
+      border: `3px solid #f0cfb3`,
     },
   },
   supportCard: {
     width: '425px',
-    height: 500,
+    height: '500px',
     backgroundColor: '#f6fafb',
     display: "flex",
     flexDirection: "column",
@@ -140,10 +141,8 @@ const localStyles = makeStyles(theme => ({
     backgroundColor: "#011133",
     fontFamily: theme.typography.fontFamily
   },
+
 }));
-
-const BackgroundURI = "img/WellsBackgroundlogin.jpg";
-
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -158,6 +157,14 @@ const useStyles = makeStyles(theme => ({
     backgroundColor: "#011133",
     fontFamily: theme.typography.fontFamily
   },
+  buttonLower: {
+    backgroundColor: '#e4a773',
+    color: "#011133",
+    width: "150px",
+    "&:hover" : {
+      backgroundColor: '#f0cfb3',
+    },
+},
   inputs: {
     backgroundColor: theme.palette.background.paper,
     width: "80%",
@@ -397,6 +404,37 @@ const SellersSvg = props => {
 };
 
 
+const M1neralLogoNavNoAuth = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 11320 2490"
+    className={props.className}
+  >
+    <g fill="none" fillRule="evenodd" stroke="none" strokeWidth="1">
+      <path
+        fill="#12ABE0"
+        d="M1396 1823c-201 202-528 202-729 0-15-15-30-31-43-48l-366 366c14 16 29 31 44 47 403 402 1056 402 1459 0 356-356 397-908 124-1309l-379 378c80 188 43 413-110 566zm-839-163c-80-188-43-413 110-566 201-201 528-201 729 0 16 15 30 32 43 48l366-366c-14-16-29-31-44-47L1032 0 302 729c-356 356-397 908-124 1309l379-378zm292-384c101-100 264-100 365 0 101 101 101 264 0 365s-264 101-365 0c-100-101-100-264 0-365z"
+      ></path>
+      <g transform="translate(2687 379)">
+        <path
+          fill="#12ABE0"
+          d="M2703 1686L2703 64 2703 0 2505 64 2072 202 2132 432 2422 351 2422 1686z"
+        ></path>
+        <path fill="white" d="M8354 6L8354 1686 8633 1686 8633 6z"></path>
+        <path
+          fill="white"
+          d="M1324 699c156 0 246 103 246 297v690h279V911c0-297-161-465-426-465-184 0-313 85-412 214-65-129-187-214-362-214-186 0-292 101-370 209V471H0v1215h279v-683c0-189 106-304 260-304s246 106 246 295v692h279v-686c0-195 108-301 260-301zM3099 471v1215h278v-686c0-188 113-301 274-301 166 0 260 108 260 297v690h279V913c0-283-159-467-433-467-189 0-301 99-380 214V471h-278zM5053 446c-347 0-594 285-594 633v4c0 376 272 631 624 631 223 0 382-90 497-228l-163-145c-97 95-194 145-329 145-180 0-320-110-350-308h893c2-28 5-53 5-79 0-349-196-653-583-653zm306 548h-624c26-189 145-320 316-320 184 0 290 140 308 320zM5916 471v1215h279v-462c0-323 170-481 414-481h16V448c-214-9-354 115-430 297V471h-279zM6759 1086c0 345 274 628 644 628 142 0 269-41 373-110v110h279V446h-279v107c-102-68-228-107-368-107-373 0-649 287-649 635v5zm649 386c-216 0-371-179-371-391v-5c0-211 143-386 366-386 219 0 373 177 373 391v5c0 209-142 386-368 386z"
+        ></path>
+      </g>
+    </g>
+  </svg>
+);
+
+const M1neralLogo2 = styled(M1neralLogoNavNoAuth)`
+  width: 200px;
+  padding-top: 50px;
+`;
+
 
 
 
@@ -598,14 +636,8 @@ const Login = props => {
             type="submit"
             style={{
               float: 'left',
-              // marginLeft: "30%",
               marginTop: "35px",
               marginLeft: '65px',
-              // marginRight: "15px",
-              // marginTop: '15px',
-              // color: theme.palette.secondary.contrastText,
-              // alignItems: "center",
-              // justifyItems: "center"
             }}
             color="secondary"
           >
@@ -629,7 +661,7 @@ const Login = props => {
       <div className={localClass.cardContainer}>
         <div className={localClass.signInCardContainer}>
         <Paper 
-        elevation = {1}
+        elevation = {0}
         square={true}
         color="secondary" className={localClass.signInCard}>
           <div>
@@ -643,7 +675,7 @@ const Login = props => {
             <div
               className={localClass.cardTitle}
               style={{ 
-                // color: "white", 
+                color: "#011133", 
                 fontSize: "24px"}}
             >
               LAND OWNERS
@@ -652,6 +684,7 @@ const Login = props => {
           <div className={localClass.cardInputs}>
             <Typography style={{ 
                   textAlign: "center", 
+                  color: '#011133',
                   padding: "11%", 
                   }}>
               For owners and sellers of royalties or minerals looking to learn
@@ -659,16 +692,10 @@ const Login = props => {
             </Typography>
           </div>
           <Button
-            variant="contained"
+            variant="outline"
             disableElevation
-            style={{
-              color: "white",
-              backgroundColor: "rgba(23, 170, 221, 1)",
-              width: "150px",
-              float: 'left',
-              marginLeft: '50px' 
-            }}
-            disabled
+            type = 'submit'
+            className={classes.buttonLower}
           >
             SIGN UP
           </Button>
@@ -677,7 +704,7 @@ const Login = props => {
 
         <div className={localClass.signInCardContainer}>
         <Paper 
-        elevation = {1}
+        elevation = {0}
         square={true}
         color="secondary" className={localClass.signInCard}>
           <div>
@@ -692,7 +719,7 @@ const Login = props => {
             <div
               className={localClass.cardTitle}
               style={{ 
-                // color: "white", 
+                color: "#011133", 
                 fontSize: "24px" }}
             >
               MINERAL BUYERS
@@ -701,6 +728,7 @@ const Login = props => {
           <div className={localClass.cardInputs}>
             <Typography style={{ 
               textAlign: "center", 
+              color: '#011133',
               padding: "10%" }}>
               For buyers seeking potential deals and to streamline acquisition
               workflows
@@ -709,12 +737,7 @@ const Login = props => {
           <Button
             variant="contained"
             disableElevation
-            style={{
-              color: "white",
-              backgroundColor: "rgba(23, 170, 221, 1)",
-              width: "150px",
-              float: 'left',
-              marginLeft: '50px'            }}
+            className={classes.buttonLower}
             onClick={showForm}
           >
             Sign Up
@@ -725,7 +748,7 @@ const Login = props => {
         <div className={localClass.signInCardContainer}>
         <Paper 
         id = "op_card"
-        elevation = {1}
+        elevation = {0}
         square={true}
         color="secondary" className={localClass.signInCard}>
           <div>
@@ -738,15 +761,17 @@ const Login = props => {
             <div
               className={localClass.cardTitle}
               style={{ 
-                // color: "white", 
+                color: "#011133", 
                 fontSize: "24px" }}
             >
               OPERATORS
             </div>
           </div>
           <div className={localClass.cardInputs}>
-            <Typography style={{ textAlign: "center", padding: "10%" }}>
-              For upstream and midstream operating companies looking to streamline
+            <Typography style={{ textAlign: "center", 
+                                  color: '#011133',
+                                  padding: "10%" }}>
+              For operating companies looking to streamline
               land acquisition workflows
             </Typography>
           </div>
@@ -755,16 +780,7 @@ const Login = props => {
             id = "op_button"
             variant="contained"
             disableElevation
-            style={{
-              color: "white",
-              backgroundColor: "rgba(23, 170, 221, 1)",
-              width: "150px",
-              float: 'left',
-              marginLeft: '50px',
-              "&:hover" : {
-                backgroundColor: '#f0cfb3',
-              },
-            }}
+            className={classes.buttonLower}
             onClick={showForm}
           >
             Sign Up
@@ -793,15 +809,12 @@ const Login = props => {
       </div>
 
       <div className={localClass.footer}>
-        <div>M1neral Logo</div>
-        <div>708 Main St.</div>
-        <div>Houston, TX 77002</div>
-        <div>346-242-0408</div>
-        <div>line</div>
-        <div>Sign in</div>
-        <div>Sign up</div>
-        <div>Support</div>
-        <div>copywright</div>
+        <div><M1neralLogo2/></div>
+
+        <Typography variant="h4" className={localClass.headerWords}>
+            (c) 2020 M1neral, LLC
+        </Typography>
+        <div>(c) 2020 M1neral, LLC</div>
         <div>terms of service | Privacy Policy</div>
 
 
