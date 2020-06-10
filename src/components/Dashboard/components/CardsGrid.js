72aa17c47badf5a9b8a132cb912d546220f5e33f
iamzishan78 <zishannaseeb528@gmail.com>
2020-06-09 22:02:49 -0500

    fully responsive card container

--- src/components/Dashboard/components/CardsGrid.js
+++ src/components/Dashboard/components/CardsGrid.js
@@ -24,7 +24,14 @@ const useStyles = makeStyles(() => ({
 const SortableItem = SortableElement(({ content }) => {
   const classes = useStyles();
   return (
-    <Grid item sm={content.size == "2x" ? 8 : 4} className={classes.cgriditem}>
+    <Grid 
+        item 
+        xl={content.size == "2x" ? 9 : 3} 
+        lg={content.size == "2x" ? 9 : 3} 
+        md={content.size == "2x" ? 8 : 4} 
+        sm={content.size == "2x" ? 8 : 4} 
+        xs={content.size == "2x" ? 6 : 6} 
+        className={classes.cgriditem}>
       <Card className={classes.cgridcard}>{content.el}</Card>
     </Grid>
   );
