0d5ae3ed0dbb55a37f03cebccb1c3c643782b0eb
iamzishan78 <zishannaseeb528@gmail.com>
2020-06-11 23:18:30 +0300

    adding studio

--- src/components/Studio/Studio.js
+++ src/components/Studio/Studio.js
@@ -4,14 +4,29 @@ import { StudioContext } from './StudioContext'
 import { MapContext } from '../Map/MapContext'
 import { Container } from '@material-ui/core';
 import Iframe from 'react-iframe';
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    paddingTop: "10px",
+    paddingBottom: "10px",
+    maxHeight: "100%",
+    height:"100%"
+  },
+  iframe: {
+    width: "100%",
+    height: "100%",
+    overflow:"hidden"
+  },
+}));
 
 export default function Studio() {
-
+  const classes = useStyles();
     return (
 
     /// TODO : check for configuration to host url, check if customer width & height is needed.
-      <Container >
-         <Iframe url="https://studio.m1neral.com" /> 
+      <Container maxWidth="xl" className={classes.container}  >
+         <Iframe className={classes.iframe} url="https://studio.m1neral.com"  frameBorder="0" scrolling="no"/> 
       </Container>
 
 
