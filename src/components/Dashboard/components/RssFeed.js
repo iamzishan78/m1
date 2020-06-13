2859796224e33422059c879d2730607dd082ea80
iamzishan78 <zishannaseeb528@gmail.com>
2020-06-13 10:10:47 +0000

    Complete RSSFeed

--- src/components/Dashboard/components/RssFeed.js
+++ src/components/Dashboard/components/RssFeed.js
@@ -16,6 +16,10 @@ import { Grid } from "@material-ui/core";
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
 import Skeleton from "@material-ui/lab/Skeleton";
+import CardMedia from "@material-ui/core/CardMedia";
+import moment from "moment";
+import M1neralIconSvg from "../../Shared/m1neralIconSvg";
+import List from "@material-ui/core/List";
 
 const useStyles = makeStyles((theme) => ({
   header: {
@@ -23,21 +27,24 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: "#011133",
     color: "white",
   },
+  container: {},
   listitem: {
     padding: "10px",
   },
   thumb: {
     height: "16px",
     width: "16px",
-    marginRight: "8px",
   },
   source: {
     fontSize: "8px",
+    marginLeft: "8px",
   },
   title: {
     fontSize: "12px",
     margin: "2px 0",
     fontWeight: "bold",
+    textDecoration: "none",
+    color: "black",
   },
   content: {
     fontSize: "10px",
@@ -63,13 +70,28 @@ const DragHandle = sortableHandle(() => (
 ));
 
 const rsslist = [
-  "https://feeds.feedburner.com/OilGasJournal-GeneralInterest",
-  "https://www.rigzone.com/news/rss/rigzone_latest.aspx",
-  "http://www.oilandgas360.com/feed/",
-  "http://www.cnbc.com/id/10000030/device/rss",
-  "https://www.naturalgasintel.com/rss/1",
-  "https://shalemag.com/feed/",
-  "http://pboilandgasmagazine.com/feed/",
+  {
+    title: "FeedBurner",
+    url: "https://feeds.feedburner.com/OilGasJournal-GeneralInterest",
+    image: "https://base.imgix.net/files/base/pennwell/ogj/logo.png?h=60",
+  },
+  {
+    title: "Rigzone",
+    url: "https://www.rigzone.com/news/rss/rigzone_latest.aspx",
+  },
+  {
+    title: "Oil and Gas 360",
+    url: "http://www.oilandgas360.com/feed/",
+    image:
+      "https://www.oilandgas360.com/wp-content/uploads/2014/09/OAG-Logo-325x90_rev.png",
+  },
+  { title: "CNBC", url: "http://www.cnbc.com/id/10000030/device/rss" },
+  { title: "NGI Shale Daily", url: "https://www.naturalgasintel.com/rss/1" },
+  { title: "Shalemag", url: "https://shalemag.com/feed/" },
+  {
+    title: "PB Oil and Gas Magazine",
+    url: "http://pboilandgasmagazine.com/feed/",
+  },
 ];
 
 const RssFeed = () => {
@@ -78,30 +100,52 @@ const RssFeed = () => {
 
   useEffect(() => {
     const proxy = "https://api.rss2json.com/v1/api.json?rss_url=";
-    // const newArticles = [];
-    const fetchRes = async (url) => {
+    const fetchRss = async ({ url, title, image }) => {
       try {
         const res = await fetch(`${proxy}${url}`);
         const data = await res.json();
         const { feed, items } = data;
-        // console.log({ feed, articles: items });
-        return [
-          { feed, article: items[0] },
-          { feed, article: items[1] },
-        ];
+        return items.map((item, i) => ({
+          source: title,
+          feed,
+          article: item,
+          image,
+        }));
+        // return [
+        //   { source: title, feed, article: items[0], image },
+        //   { source: title, feed, article: items[1], image },
+        //   { source: title, feed, article: items[2], image },
+        //   { source: title, feed, article: items[3], image },
+        //   { source: title, feed, article: items[4], image },
+        // ];
       } catch (error) {
         console.log(error);
       }
     };
-    Promise.all(rsslist.map((url) => fetchRes(url))).then((articles) => {
-      let newArticle = [];
-      articles.forEach((a) => !!a && newArticle.push(...a));
-    //   console.log(
-    //     newArticle.sort((a, b) => a.article.pubDate < b.article.pubDate)
-    //   );
-      setNews([...news, ...newArticle]);
+    Promise.all(rsslist.map((source) => fetchRss(source))).then((articles) => {
+      let newArticles = [];
+      articles.forEach((a) => !!a && newArticles.push(...a));
+      const sorted = newArticles.sort((a, b) =>
+        a.article.pubDate > b.article.pubDate ? -1 : 1
+      );
+      console.log(sorted);
+      setNews([...news, ...sorted]);
     });
   }, []);
+
+  const truncate = (str, num) => {
+    if (str.length <= num) {
+      return str;
+    }
+    return str.slice(0, num) + "...";
+  };
+
+  const cleanedText = (text) => {
+    const el = document.createElement("div");
+    el.innerHTML = text;
+    const sanitized = el.textContent;
+    return sanitized;
+  };
 
   return (
     <Fragment>
@@ -111,41 +155,65 @@ const RssFeed = () => {
         className={classes.header}
       />
 
-      {[...news].map((_, i) => (
-        <Paper key={i} className={classes.paper}>
-          <Grid
-            container
-            direction="row"
-            justify="space-between"
-            alignItems="center"
-            className={classes.listitem}
-            spacing={1}
-          >
-            <Grid item xs={9} zeroMinWidth>
-              <Grid container alignItems="center">
-                <Skeleton variant="rect" className={classes.thumb} />
-                <Typography noWrap className={classes.source}>
-                  {"BBC News"}
-                </Typography>{" "}
+      <List style={{ maxHeight: "calc(100% - 40px)", overflow: "auto" }}>
+        {news.map(({ feed, article, source, image }, i) => (
+          <Paper key={i} className={classes.paper}>
+            <Grid
+              container
+              direction="row"
+              justify="space-between"
+              alignItems="center"
+              className={classes.listitem}
+              spacing={1}
+            >
+              <Grid item xs={9} zeroMinWidth>
+                <Grid container alignItems="center">
+                  {!!(article.enclosure.link || feed.image) ? (
+                    <CardMedia
+                      className={classes.thumb}
+                      component={"img"}
+                      image={feed.image || article.enclosure.link}
+                      title="thumbnail"
+                    />
+                  ) : (
+                    <M1neralIconSvg size={{ height: "16px", width: "16px" }} />
+                  )}
+                  {/* <Skeleton variant="rect" className={classes.thumb} /> */}
+                  <Typography noWrap className={classes.source}>
+                    {cleanedText(feed.title)}
+                  </Typography>{" "}
+                </Grid>
+                <Typography
+                  component="a"
+                  href={article.link}
+                  variant="h2"
+                  className={classes.title}
+                >
+                  {truncate(article.title, 50)}
+                </Typography>
+                <Typography className={classes.content}>
+                  {truncate(cleanedText(article.content), 100)}
+                </Typography>
+                <Typography noWrap className={classes.date}>
+                  {moment.utc(article.pubDate).local().fromNow()}
+                </Typography>
               </Grid>
-              <Typography variant="h2" className={classes.title}>
-                Natural Gas Inventories as of June 5, 2020
-              </Typography>
-              <Typography className={classes.content}>
-                The <a href="http://ir.eia.gov/ngs/ngs.html">EIA</a> has
-                released its natural gas inventory report, showing a net
-                increa...
-              </Typography>
-              <Typography noWrap className={classes.date}>
-                {"2 days ago"}
-              </Typography>
+              <Grid item xs={3} style={{ textAlign: "-webkit-center" }}>
+                {!!(article.enclosure.link || feed.image) ? (
+                  <CardMedia
+                    className={classes.image}
+                    component={"img"}
+                    image={article.enclosure.link || feed.image}
+                    title="Image"
+                  />
+                ) : (
+                  <M1neralIconSvg />
+                )}
+              </Grid>
             </Grid>
-            <Grid item xs={3} style={{ textAlign: "-webkit-center" }}>
-              <Skeleton variant="rect" className={classes.image} />
-            </Grid>
-          </Grid>
-        </Paper>
-      ))}
+          </Paper>
+        ))}
+      </List>
     </Fragment>
   );
 };
