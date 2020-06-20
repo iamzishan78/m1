e2bc6df729c6f277c7203fe9cb339f695be73cb5
iamzishan78 <zishannaseeb528@gmail.com>
2020-06-20 11:01:28 -0500

    studio update

--- src/components/Studio/Studio.js
+++ src/components/Studio/Studio.js
@@ -36,8 +36,8 @@ export default function Studio() {
          
          <Iframe 
               className={classes.iframe} 
-              // url="https://studio.m1neral.com"  
-              url="https://m1studio-dev.azurewebsites.net/"
+              url="https://studio.m1neral.com"  
+              // url="https://m1studio-dev.azurewebsites.net/"
               frameBorder="0" 
               scrolling="no"
               /> 
