af6bec966cec5da6544e514297ae702d55b1eb17
iamzishan78 <zishannaseeb528@gmail.com>
2020-07-04 02:18:21 +0500

    fix: update deals on updating transactions

--- src/components/ContactDetailCard/ContactDetailCard.js
+++ src/components/ContactDetailCard/ContactDetailCard.js
@@ -19,13 +19,11 @@ import Tooltip from "@material-ui/core/Tooltip";
 import FieldContent from "./components/FieldContent";
 import LocationCityIcon from "@material-ui/icons/LocationCity";
 import { CONTACT } from "../../graphQL/useQueryContact";
-import { TRANSACTIONDATA } from "../../graphQL/useQueryTransactionData";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import { useLazyQuery } from "@apollo/react-hooks";
 import ConfirmationDialog from "./components/ConfirmationDialog";
 import Activities from "../Shared/Activities";
 import Deals from "../Shared/Deals";
-import { AppContext } from "../../AppContext";
 
 const useStyles = makeStyles((theme) => ({
   Contacts: {
@@ -150,11 +148,7 @@ const useStyles = makeStyles((theme) => ({
 export default function ContactDetailCard(props) {
   const classes = useStyles();
   const [openDialog, setOpenDialog] = useState(false);
-  const [stateApp] = useContext(AppContext);
-  const [transactData, setTransactData] = useState();
-  const [transactId, setTransactId] = useState();
   const [getContact, { loading, data }] = useLazyQuery(CONTACT);
-  const [getTransactionData, { data: tData, tLoading }] = useLazyQuery(TRANSACTIONDATA);
 
   useEffect(() => {
     if (props.contactId) {
@@ -165,24 +159,6 @@ export default function ContactDetailCard(props) {
       });
     }
   }, [props.contactId]);
-
-  
-  useEffect(() => {
-    if (stateApp.user && stateApp.user.mongoId) {
-      getTransactionData({
-        variables: {
-          userId: stateApp.user.mongoId,
-        },
-      });
-    }
-  }, [stateApp.user]);
-
-  useEffect(() => {
-    if (tData && tData.transactionData && tData.transactionData.allData) {
-      setTransactData(tData.transactionData.allData);
-      setTransactId(tData.transactionData._id);
-    }
-  }, [tData, tLoading]);
 
   return data && data.contact && !loading ? (
     <Grid container spacing={0} className={classes.mainGridContainer}>
@@ -643,11 +619,7 @@ export default function ContactDetailCard(props) {
 
             <Grid item xs={12}>
               <Paper className={classes.paper}>
-                <Deals
-                  contact={data.contact}
-                  transactData={transactData}
-                  transactId={transactId}
-                />
+                <Deals contact={data.contact} />
               </Paper>
               <Paper className={classes.paper}>
                 <Comments
