db1ce8b9f45b19562da4466306e7340567c41b83
iamzishan78 <zishannaseeb528@gmail.com>
2020-08-01 14:09:28 +0000

    one-liner for debugging against local graphql

--- src/App.js
+++ src/App.js
@@ -204,6 +204,8 @@ function App() {
       };
       if (token) {
         apolloConfig.headers["X-ZUMO-AUTH"] = token;
+        //uncomment to run against local 
+        //apolloConfig.uri = "http://localhost:7071/api/m1graph"
       }
 
       let apolloClient = new ApolloClient(apolloConfig);
