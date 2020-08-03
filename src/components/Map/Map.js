220471871f2305cf3a0d5f9cb767c1b573376f5f
iamzishan78 <zishannaseeb528@gmail.com>
2020-08-03 23:35:24 +0000

    perf stuff

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -107,13 +107,13 @@ export default function Map() {
   const [lng, Lng] = useState();
   const setLng = (state) => {
     if(lng != state) {
-      // Lng(state)
+      Lng(state)
     }
   }
   const [lat, Lat] = useState();
   const setLat = (state) => {
     if(lat != state) {
-      // Lat(state)
+      Lat(state)
     }
   }
   const [transform, Transform] = useState("transform: inherit");
@@ -4371,7 +4371,9 @@ export default function Map() {
             deleteSpatialDataAndShape={handleDeleteSpatialDataAndShape}
           />
         )}
-      {mapGridCardActivated && <MapGridCard />}
+      {mapGridCardActivated && <MapGridCard 
+          mapGridCardActivated={mapGridCardActivated}
+        />}
       <div id="tempPopupHolder" className={classes.portal} ref={container} />
       <Portal container={container.current}>
         {stateApp.popupOpen ? (
