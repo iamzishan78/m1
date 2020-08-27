a10667d2d6527049dc8067fec1e4086539095f02
iamzishan78 <zishannaseeb528@gmail.com>
2020-08-27 09:54:32 -0400

    fix the handle close

--- src/components/MapControls/components/ColorPickerDialog.js
+++ src/components/MapControls/components/ColorPickerDialog.js
@@ -39,7 +39,7 @@ export default (props) => {
     setIsOpen(false);
     setStateMapControls((stateMapControls) => ({
       ...stateMapControls,
-      selectedUDLayer: null
+      selectedFileLayer: null
     }));
   }
 
