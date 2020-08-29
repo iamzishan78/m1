56d8ea0aae252b1e828642be6a739403d244b50b
iamzishan78 <zishannaseeb528@gmail.com>
2020-08-29 19:54:10 +0600

    merge master fix

--- src/components/ContactDetailCard/components/FieldContent.js
+++ src/components/ContactDetailCard/components/FieldContent.js
@@ -275,7 +275,7 @@ export default function FieldContent({
           variables: {
             contact: trimmedEditContent,
           },
-          refetchQueries: ["getContacts", "getContactsByOwnerId", "getContact"],
+          refetchQueries: ["getContacts", "getContact"],
           awaitRefetchQueries: true,
         });
       }
