880ce96d86c9a46eee1c538a29daf375270485fd
iamzishan78 <zishannaseeb528@gmail.com>
2020-10-03 22:35:14 +0600

    Adjust interest layer min zoom level

--- src/LayerConfig.js
+++ src/LayerConfig.js
@@ -1631,7 +1631,7 @@ export const defaultLayers = [
         id: "interest",
         sourceProps: "interests_source",
         paintType: "fill",
-        minZoom: 9,
+        minZoom: 7,
         paintProps: {
           "fill-color": "#62a27f",
           "fill-opacity": 0.4,
@@ -1642,7 +1642,7 @@ export const defaultLayers = [
         id: "interest_labels",
         sourceProps: "interest_labels_source",
         paintType: "symbol",
-        minZoom: 12,
+        minZoom: 8,
         symbolProps: {
           "text-allow-overlap": true,
           "text-anchor": "center",
