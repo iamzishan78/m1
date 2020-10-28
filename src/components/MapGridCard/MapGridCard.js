828051c082442801838b5b7bcda580640283ef7b
iamzishan78 <zishannaseeb528@gmail.com>
2020-10-28 13:59:16 +0000

    additional well header index columns

--- src/components/MapGridCard/MapGridCard.js
+++ src/components/MapGridCard/MapGridCard.js
@@ -204,19 +204,25 @@ const wellsColumnHeaders = [
     name: "ApiNumber",
     label: "API",
   },
-
   {
     name: "WellName",
     label: "Well Name",
   },
-
   {
-    name: "Latitude",
-    label: "Latitude",
+    name: "State",
+    label: "State",
   },
   {
-    name: "Longitude",
-    label: "Longitude",
+    name: "County",
+    label: "County",
+  },
+  {
+    name: "WellType",
+    label: "Well Type",
+  },
+  {
+    name: "WellStatus",
+    label: "Well Status",
   },
 ];
 const ownersColumnHeaders = [
