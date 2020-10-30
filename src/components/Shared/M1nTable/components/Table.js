a8842e54f1ba1aae9dd22cfec0621f010fb4f26e
iamzishan78 <zishannaseeb528@gmail.com>
2020-10-30 15:12:33 +0000

    more merge conflicts

--- src/components/Shared/M1nTable/components/Table.js
+++ src/components/Shared/M1nTable/components/Table.js
@@ -59,6 +59,28 @@ import TransactDialog from "../../../Transact/components/dialog";
 import ParcelScreenIcon from "../../svgIcons/parcelScreen";
 import ParcelsDetailCard from "../../../ParcelsDetailCard/ParcelsDetailCard";
 import { debounce } from 'lodash';
+import AssessmentIcon from "@material-ui/icons/Assessment";
+import { WELLQUERY } from "../../../../graphQL/useQueryWell";
+import { useLazyQuery } from "@apollo/client";
+var ticksToDateString = function (ticks) {
+  var epochTicks = 621355968000000000;
+  var ticksPerMillisecond = 10000; // whoa!
+  var maxDateMilliseconds = 8640000000000000;
+  if (isNaN(ticks)) {
+    //      0001-01-01T00:00:00.000Z
+    return "NANA-NA-NATNA:NA:BA.TMAN";
+  }
+  // convert the ticks into something javascript understands
+  var ticksSinceEpoch = ticks - epochTicks;
+  var millisecondsSinceEpoch = ticksSinceEpoch / ticksPerMillisecond;
+  if (millisecondsSinceEpoch > maxDateMilliseconds) {
+    //      +035210-09-17T07:18:31.111Z
+    return "+WHOAWH-OA-ISTOO:FA:RA.WAYZ";
+  }
+  // output the result in something the human understands
+  var date = new Date(millisecondsSinceEpoch);
+  return date.toISOString();
+};
 
 const useStyles = makeStyles((theme) => ({
   root: {
