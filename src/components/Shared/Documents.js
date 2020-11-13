b782356501649877cccaa128fc6d8ec64e61e559
iamzishan78 <zishannaseeb528@gmail.com>
2020-11-12 18:25:01 -0600

    change doc label to recent docs

--- src/components/Shared/Documents.js
+++ src/components/Shared/Documents.js
@@ -260,7 +260,7 @@ export default function Documents(props) {
     <div className={classes.root} variant="outlined">
       <CardActions style={{ padding: "23px 23px 8px 23px" }}>
         <Grid item xs={12} style={{ minHeight: "35px" }}>
-          <h4 style={{ margin: "0 0 8px 0", float: "left" }}>Documents</h4>
+          <h4 style={{ margin: "0 0 8px 0", float: "left" }}>Recent Documents</h4>
           <h4
             className={classes.viewAll}
             // onClick={(e) => {
