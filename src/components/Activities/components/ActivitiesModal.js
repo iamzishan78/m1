09c91cc8dbd7922deeeac447953e387427d1cf71
iamzishan78 <zishannaseeb528@gmail.com>
2020-11-25 11:10:55 -0600

    update activity name text input width

--- src/components/Activities/components/ActivitiesModal.js
+++ src/components/Activities/components/ActivitiesModal.js
@@ -733,7 +733,7 @@ export default function ActivitiesModal({
                 <span className={classes.rowIcon}></span>
                 <TextField
                   className={clsx(
-                    classes.fieldWidth,
+                   // classes.fieldWidth,
                     classes.inputField,
                     activityName === "" && errors.activityName && classes.error
                   )}
