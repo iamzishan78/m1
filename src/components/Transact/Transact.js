dc53a7949ee4c3a6e96a6372b5b1a246b76fca28
iamzishan78 <zishannaseeb528@gmail.com>
2020-12-17 21:52:40 +0500

    fix: left align lane header

--- src/components/Transact/Transact.js
+++ src/components/Transact/Transact.js
@@ -321,11 +321,13 @@ export default function Transact() {
                 backgroundColor: "#fff",
                 color: "#011133",
                 fontWeight: "bold",
+                textAlign: "left"
               }}
               cardStyle={{
                 boxShadow:
                   "0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)",
                 backgroundColor: "#F2F2F2",
+                textAlign: "center"
               }}
 
               //onCardAdd = {handleCardAdd}
