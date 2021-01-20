4accc2052eb8640a4c33c15c2b1717303ccead3a
iamzishan78 <zishannaseeb528@gmail.com>
2021-01-20 03:52:46 +0000

    Revert "fix: Label needs to be fixed to a centroid point"
    This reverts commit 4fed3827c7b89fde7792fd3d734033d16d0bffae.

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -702,17 +702,6 @@ export default function Map() {
             data: geoJson,
             promoteId: "id",
           });
-
-          let pointSource = geoJson.features.map(feature => {
-            return {...turf.centroid(feature), properties: feature.properties}
-          })
-
-          pointSource = {type: "FeatureCollection", features: [...pointSource]}
-
-          map.addSource(`${sourceId}_point`, {
-            type: "geojson",
-            data: pointSource
-          })
         }
       }
 
