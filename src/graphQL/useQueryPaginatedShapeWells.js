bea29fa50c69c7dbd02c57d744305164b1d56500
iamzishan78 <zishannaseeb528@gmail.com>
2021-02-06 23:56:32 +0600

    paginated shape wells, not finished, not optimized

---
+++ src/graphQL/useQueryPaginatedShapeWells.js
@@ -0,0 +1,56 @@
+import gql from "graphql-tag";
+
+export const SHAPEWELLS = gql`
+  query getPaginatedShapeWells(
+    $polygon: String
+    $pagination: PaginationInput = { first: 25, after: null }
+    $sort: WellSortInput = {}
+    $filters: [FilterInput] = []
+    $search: String = ""
+    $userId: ID
+  ) {
+    paginatedShapeWells(
+      polygon: $polygon
+      pagination: $pagination
+      sort: $sort
+      filters: $filters
+      search: $search
+      userId: $userId
+    ) {
+      totalCount,
+      edges {
+        node {
+          id
+          isTracked
+          commentsCounter
+          tags
+          coordinates
+          wellName
+          api
+          operator
+          wellType
+          latitude
+          longitude
+          wellBoreProfile
+          ownerCount
+
+          wellStatus
+          lastTwelveMonthBOE
+          permitApprovedDate
+          permitApprovedDateReady
+          spudDate
+          spudDateReady
+          completionDate
+          completionDateReady
+          firstProductionDate
+          firstProductionDateReady
+        },
+        cursor
+      },
+      pageInfo {
+        hasNextPage,
+        endCursor
+      },
+    }
+  }
+`;
