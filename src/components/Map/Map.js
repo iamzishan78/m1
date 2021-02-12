6b57670614193211875fb046b82bfdeba2ee6a2e
iamzishan78 <zishannaseeb528@gmail.com>
2021-02-12 19:47:30 +0000

    removeSource crash fixes

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1428,8 +1428,7 @@ export default function Map() {
       const sourceId = prop.sourceProps;
       if (map.getSource(sourceId)) map.removeSource(sourceId);
       if (map.getSource(`${sourceId}_point`)) map.removeSource(`${sourceId}_point`);
-      if (map.getSource(`${sourceId}_filter`))
-        map.removeSource(`${sourceId}_filter`);
+      if (map.getSource(`${sourceId}_filter`)) map.removeSource(`${sourceId}_filter`);
     }
   };
 
@@ -3107,10 +3106,8 @@ export default function Map() {
     //     }
 
     if (map && stateApp.wellSelectedCoordinates) {
-      if (map.getLayer("well-point")) {
-        map.removeLayer("well-point");
-        map.removeSource("well-select-point");
-      }
+      if (map.getLayer("well-point")) map.removeLayer("well-point");
+      if (map.getSource("well-select-point")) map.removeSource("well-select-point");
 
       if (stateApp.wellSelectedCoordinates.length > 0) {
         map.addSource("well-select-point", {
@@ -4862,8 +4859,8 @@ export default function Map() {
     if(map && stateApp.selectedParcel){
       setParcelBoundaryId(stateApp.selectedParcel.id);
     } else if(map) {
-      map.removeLayer('parcelBoundary');
-      map.removeSource('parcelBoundarySource');
+      if (map.getLayer('parcelBoundary')) map.removeLayer('parcelBoundary');
+      if (map.getSource('parcelBoundarySource')) map.removeSource('parcelBoundarySource');
       setParcelBoundaryId(null);
     }
   }, [stateApp.selectedParcel])
