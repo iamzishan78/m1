a8c6aff138c4306911e30718284bf4020e0217a2
iamzishan78 <zishannaseeb528@gmail.com>
2021-03-10 13:25:06 +0500

    :bug: Enable white link icon for contacts with no linked global owner

--- src/components/Shared/LinkWithIcon.js
+++ src/components/Shared/LinkWithIcon.js
@@ -49,84 +49,78 @@ export default function LinkWithIcon(props) {
 
   return (
     <React.Fragment>
-      {
-        getGlobalOwners().length > 0 &&
-        <>
-          <Tooltip
-            title={"Linked Global Owner"}
-            placement="top"
-          >
-            <IconButton
-              size={props.iconZiseSmall ? "small" : "medium"}
-              color="primary"
-              className={`${classes.icons}  ${openDialog || (getGlobalOwners().length > 0)
-                ? classes.iconSelected
-                : ""
-                }`}
-              onClick={() => {
-                setOpenDialog(true);
-              }}
-              aria-label="show linked global owner"
-            >
-              <LinkIcon />
-            </IconButton>
-          </Tooltip>
-          {openDialog && (
-            <AlertDialogSlide open={true}>
-              <Container maxWidth="sm" className={classes.gridWidthScroll}>
-                <div className={classes.dealContainer}>
+      <Tooltip
+        title={"Linked Global Owner"}
+        placement="top"
+      >
+        <IconButton
+          size={props.iconZiseSmall ? "small" : "medium"}
+          color="primary"
+          className={`${classes.icons}  ${openDialog || (getGlobalOwners().length > 0)
+            ? classes.iconSelected
+            : ""
+            }`}
+          onClick={() => {
+            setOpenDialog(true);
+          }}
+          aria-label="show linked global owner"
+        >
+          <LinkIcon />
+        </IconButton>
+      </Tooltip>
+      {openDialog && (
+        <AlertDialogSlide open={true}>
+          <Container maxWidth="sm" className={classes.gridWidthScroll}>
+            <div className={classes.dealContainer}>
 
-                  <Box p={3} pt={1}>
-                    <Grid
-                      container
-                      direction="row"
-                      spacing={4}
-                      justify="space-between"
-                      alignItems="center"
-                    >
-                      <Grid item>
-                        <Typography
-                          className={classes.topHeading}
-                          style={{ fontWeight: "bold" }}
-                          variant="h5"
-                          component="h2"
-                        >
-                          Link Platform Owners
-                    </Typography>
-                      </Grid>
-                      <Grid item>
-                        <IconButton aria-label="delete" color="primary" onClick={() => setOpenDialog(false)}>
-                          <CloseSharp />
-                        </IconButton>
-                      </Grid>
-                    </Grid>
+              <Box p={3} pt={1}>
+                <Grid container direction="row" spacing={4} justify="space-between" alignItems="center">
+                  <Grid item>
+                    <Typography className={classes.topHeading} style={{ fontWeight: "bold" }} variant="h5" component="h2">
+                      Link Platform Owners
+                      </Typography>
+                  </Grid>
+                  <Grid item>
+                    <IconButton aria-label="delete" color="primary" onClick={() => setOpenDialog(false)}>
+                      <CloseSharp />
+                    </IconButton>
+                  </Grid>
+                </Grid>
+                {
+                  data?.linkedGlobalOwners?.data?.length > 0 ?
+                    <>
+                      <Box mt={2}>
+                        <Typography>
+                          The below platform owners are linked to the selected contact:
+                        </Typography>
+                      </Box>
 
+                      <Box pt={3}>
+                        <Typography style={{ fontWeight: "bold" }}>Platform owners</Typography>
+                      </Box>
+                    </> :
                     <Box mt={2}>
                       <Typography>
-                        The below platform owners are linked to the selected contact:
-                  </Typography>
+                        No Platform Owner is linked with this contact yet.
+                      </Typography>
                     </Box>
+                }
+              </Box>
 
-                    <Box pt={3}>
-                      <Typography style={{ fontWeight: "bold" }}>Platform owners</Typography>
-                    </Box>
-                  </Box>
+              {data?.linkedGlobalOwners?.data?.map((row) => (
+                <Grid container direction="row" spacing={2} alignItems="center">
+                  <Grid item md={12}>
+                    <Typography style={{ backgroundColor: "#edfbff" }}>
+                      <Box display='inline' pr={2} pl={2}>{row.id}</Box> <Box display='inline' pr={2}>{row.name}</Box> {row.address1} {row.address2} {row.city}, {row.state} {row.zip}
+                    </Typography>
+                  </Grid>
+                </Grid>
+              ))}
+            </div>
+          </Container>
+        </AlertDialogSlide>
+      )}
 
-                  {data.linkedGlobalOwners.data.map((row) => (
-                    <Grid container direction="row" spacing={2} alignItems="center">
-                      <Grid item md={12}>
-                        <Typography style={{ backgroundColor: "#edfbff" }}>
-                          <Box display='inline' pr={2} pl={2}>{row.id}</Box> <Box display='inline' pr={2}>{row.name}</Box> {row.address1} {row.address2} {row.city}, {row.state} {row.zip}
-                        </Typography>
-                      </Grid>
-                    </Grid>
-                  ))}
-                </div>
-              </Container>
-            </AlertDialogSlide>
-          )}
-        </>
-      }
     </React.Fragment>
   );
 }
