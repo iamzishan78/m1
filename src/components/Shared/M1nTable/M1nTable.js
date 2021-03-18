028153ee5e185cfd50a3ce41656f9e284b48db1c
iamzishan78 <zishannaseeb528@gmail.com>
2021-03-18 03:51:18 +0000

    merge cleanup

--- src/components/Shared/M1nTable/M1nTable.js
+++ src/components/Shared/M1nTable/M1nTable.js
@@ -79,108 +79,7 @@ const useStyles = makeStyles((theme) => ({
 },
 }));
 
-////////////HeadCells begin///////////////////////////////////////////////
 
-const ContactWellHeadCells = [
-  {
-    name: "wellId",
-    options: {
-      display: false,
-      filter: false,
-      searchable: false,
-      sort: false,
-      download: false,
-      print: false,
-      viewColumns: false,
-    },
-  },
-  { name: "wellName", label: "Well" },
-  { name: "api", label: "API" },
-  { name: "leaseId", label: "Lease Id", options: { display: false } },
-  { name: "lease", label: "Lease Name", options: { display: false }  },
-  { name: "leaseDescription", label: "Lease" },
-  { name: "leaseAcres", label: "Lease Acres" },
-  { name: "interestOwner", label: "Interest Owner" },
-  {
-    name: "entity",
-    label: "Entity",
-    options: {
-      display: false
-    }
-  },
-  { name: "type", label: "Type" },
-  { name: "amount", label: "Amount" },
-  { name: "taxValue", label: "Tax Value" },
-  { name: "nra", label: "NRA" },
-  { name: "year", label: "Year", options: { display: false } },
-  { name: "globalLod", label: "Global LOD", options: { display: false } },
-  {
-    name: "tags",
-    label: "Tags ",
-    options: {
-      sort: false,
-      download: false,
-      print: false,
-      filterOptions: {
-        names: [],
-        logic(rowVal, pickedTags) {
-          let containIts = true;
-          pickedTags.map((pickedTag) => {
-            if (rowVal[0].indexOf(pickedTag) === -1) {
-              containIts = false;
-            }
-          });
-          return !containIts;
-        },
-      },
-    },
-  },
-  {
-    name: "commentsCounter",
-    label: " ",
-    options: {
-      filter: false,
-      searchable: false,
-      sort: false,
-      download: false,
-      print: false,
-      viewColumns: false,
-    },
-  },
-  {
-    name: "isTracked",
-    label: "Track",
-    options: {
-      searchable: false,
-      download: false,
-      print: false,
-      filterOptions: {
-        names: ["Tracked", "Untracked"],
-        logic(tracked, filterVal) {
-          return !(
-            (filterVal.indexOf("Tracked") >= 0 && tracked) ||
-            (filterVal.indexOf("Untracked") >= 0 && !tracked)
-          );
-        },
-      },
-      filterType: "dropdown",
-    },
-  },
-  {
-    name: "detailCard",
-    label: " ",
-    options: {
-      filter: false,
-      sort: false,
-      searchable: false,
-      download: false,
-      print: false,
-      viewColumns: false,
-    },
-  },
-];
-
-////////////HeadCells end///////////////////////////////////////////////
 
 function M1nTable(props) {
   const classes = useStyles();
@@ -266,8 +165,6 @@ function M1nTable(props) {
   const [getAbstractWellGeo, { data: abstractWellData }] = useLazyQuery(ABSTRACTWELLGEOQUERY);
   const [getPaginatedWellInterests,{ data: constDataWellInterests },] = useLazyQuery(PAGINATEDWELLINTERESTSQUERY, {fetchPolicy: "cache-and-network",});
   const [getWellInterestsFilterOptions,{ data: dataWellInterestsFilterOptions },] = useLazyQuery(WELLINTERESTSFILTEROPTIONS, {fetchPolicy: "cache-and-network",});
-
-
 
 
   ////////////General begin///////////////////////////////////////////////
