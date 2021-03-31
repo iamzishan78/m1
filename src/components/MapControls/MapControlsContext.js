e4856231f4cb9212b54ca285ff103fd3f6a138a7
iamzishan78 <zishannaseeb528@gmail.com>
2021-03-31 14:22:13 +0500

    shapes action popup design implementationa nd AOI and Parcel actions commented

--- src/components/MapControls/MapControlsContext.js
+++ src/components/MapControls/MapControlsContext.js
@@ -3,35 +3,35 @@ import React, { useState, createContext } from "react";
 const MapControlsContext = createContext([{}, () => {}]);
 
 const layers = [
-	{ name: "Basins", value: "basinLayer" },
-	{ name: "Pipelines", value: "pipelineLayer" },
-	{ name: "Surveys", value: "surveyLayer" },
+  { name: "Basins", value: "basinLayer" },
+  { name: "Pipelines", value: "pipelineLayer" },
+  { name: "Surveys", value: "surveyLayer" },
 ];
 
 const MapControlsContextProvider = (props) => {
-	const [stateMapControls, setStateMapControls] = useState({
-		fileUploadedContent: null,
-		selectedControl: "layer",
-		openSpeedDial: true,
-		anchorEl: null,
-		layers: layers,
-		userData: null,
-		heatmaps: null,
-		selectedBaseMap: "",
-		addLayer: false,
-		editDraw: false,
-		map: null,
-		Draw: null,
-		mapStyleList: [],
-		expandedPanel: false,
-	});
-	return (
-		<MapControlsContext.Provider
-			value={[stateMapControls, setStateMapControls]}
-		>
-			{props.children}
-		</MapControlsContext.Provider>
-	);
+  const [stateMapControls, setStateMapControls] = useState({
+    fileUploadedContent: null,
+    selectedControl: "layer",
+    openSpeedDial: true,
+    anchorEl: null,
+    layers: layers,
+    userData: null,
+    heatmaps: null,
+    selectedBaseMap: "",
+    addLayer: false,
+    editDraw: false,
+    map: null,
+    Draw: null,
+    mapStyleList: [],
+    expandedPanel: false,
+  });
+  return (
+    <MapControlsContext.Provider
+      value={[stateMapControls, setStateMapControls]}
+    >
+      {props.children}
+    </MapControlsContext.Provider>
+  );
 };
 
 export { MapControlsContext, MapControlsContextProvider };
