4224544e94355dda47de52009ce04c50fda27a93
iamzishan78 <zishannaseeb528@gmail.com>
2021-06-03 23:33:40 +0000

    bad sort mapping

--- src/components/Table/Wells/AssociatedWellsParcelTable.js
+++ src/components/Table/Wells/AssociatedWellsParcelTable.js
@@ -17,6 +17,8 @@ import { deepEqualObjects, setStateIfDeepEqual } from "components/Shared/functio
 // Header Schemas 
 import TableHeader from 'components/Table/constants/well-header-schema.js'
 
+// Utilities
+import isEmpty from "lodash/isEmpty";
 import ticksToDateString from "../../Shared/valueformatters/ticks-to-string.js";
 import { getPolygonString } from "components/Shared/functions";
 import { handleTagColumn } from "../helpers/index.js";
@@ -134,16 +136,16 @@ function ShapeGridWellsTable(props) {
                     first: tableState.rowsPerPage,
                     after: null,
                 },
-                sort: tableState.activeColumn
-                    ? {
-                        field: tableState.columns[tableState.activeColumn]?.name,
+                ...(!isEmpty(tableState.sortOrder)) && {
+                    sort:
+                    {
+                        field: tableState.columns.find(el => el.name === tableState.sortOrder?.name)?.name,
                         order:
-                            tableState.columns[tableState.activeColumn]
-                                ?.sortDirection === "asc"
+                            tableState.sortOrder?.direction === "asc"
                                 ? 1
                                 : -1,
                     }
-                    : [],
+                },
 
                 filters: {},
             },
