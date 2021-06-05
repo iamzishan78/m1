656ca569356b7760595bffce8d7fdf8a5a93aaec
iamzishan78 <zishannaseeb528@gmail.com>
2021-06-05 15:01:18 +0500

    [BUG] - after deleting a layer group and associated layers the layer still appears on the map until you refresh app

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1447,7 +1447,7 @@ function Map() {
       const prop = paintProps[i];
 
       // -> remove layer
-      const layerId = prop.id;
+      const layerId = layer.layerType == "file layer" ? layer.identifier : prop.id;
       if (map.getLayer(layerId)) map.removeLayer(layerId);
 
       if (prop.clusterProps) {
