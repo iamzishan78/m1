c932a76b2cf7aafd5c1225a28df1fbfeae6219fd
iamzishan78 <zishannaseeb528@gmail.com>
2021-06-10 13:24:59 +0000

    in-flight

--- src/components/MapControls/components/addALayer.js
+++ src/components/MapControls/components/addALayer.js
@@ -34,6 +34,12 @@ import AccordionSummary from '@material-ui/core/AccordionSummary';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import EditableTextField from "components/Shared/components/Fields/EditableTextField";
 
+import proj4 from 'proj4';
+// proj4.defs("EPSG:4267","+proj=longlat +datum=NAD27 +no_defs");
+// proj4.defs("EPSG:4267","+init=epsg:4267 +proj=longlat +ellps=clrk66 +datum=NAD27" +
+//   " +no_defs +nadgrids=@conus,@alaska,@ntv2_0.gsb,@ntv1_can.dat");
+proj4.defs("EPSG:4267", "+proj=tmerc +lat_0=0 +lon_0=-99 +k=0.9996 +x_0=500000.001016002 +towgs84=-8,161,176 +y_0=0 +ellps=clrk66 +to_meter=0.3048006096012192 +datum=NAD27 +no_defs")
+
 const random_rgb = () => {
   var o = Math.round,
     r = Math.random,
