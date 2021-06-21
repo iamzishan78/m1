6e9a62b67aca4a775cd18272094bce327eb206c1
iamzishan78 <zishannaseeb528@gmail.com>
2021-06-21 22:11:45 +0000

    bug fix from parcel ownership changes

--- src/components/Shared/M1nTable/components/Table.js
+++ src/components/Shared/M1nTable/components/Table.js
@@ -2953,7 +2953,7 @@ function SubTable(props) {
         }));
       }
 
-      if (props.parent === "assocTaxRollInterests" && props.targetLabel === 'wells') {
+      if (props.parent === "assocTaxRollInterests" && props.targetLabel === 'well') {
         let card = { ...rows[dataIndex] };
         setStateApp((stateApp) => ({
           ...stateApp,
@@ -2971,7 +2971,8 @@ function SubTable(props) {
           }));
       }
 
-      if (props.targetLabel === "Parcel Ownership") {
+      if (props.parent === "assocTaxRollInterests" && props.targetLabel === 'parcel' ||
+          props.targetLabel === "Parcel Ownership") {
         if (rows[dataIndex]?._id) {
           setOpenDialog("addOwnerToParcel");
           setSelectedRow(rows[dataIndex]);
