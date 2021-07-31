40ae6caecc63d241b26a4818a959df7c97980341
iamzishan78 <zishannaseeb528@gmail.com>
2021-07-31 12:37:03 +0900

    current button only works when the text or images are clicked. Need to make the button work when whitespace areas are clicked.

--- src/components/ContactDetailCard/components/ParcelsCard.js
+++ src/components/ContactDetailCard/components/ParcelsCard.js
@@ -13,7 +13,7 @@ import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    margin: "23px 23px 0 23px",
+    padding: "23px 23px 0 23px",
     width: "100%"
   },
 
