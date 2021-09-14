a3ecac014bf235bd689868cfe49b7f0535824bda
iamzishan78 <zishannaseeb528@gmail.com>
2021-09-14 14:27:07 +0500

    dropdown selection

--- src/components/ContactDetailCard/components/FieldContent/CopyPurchaseInfo.js
+++ src/components/ContactDetailCard/components/FieldContent/CopyPurchaseInfo.js
@@ -1,29 +1,118 @@
-import React from "react";
-import { IconButton } from "@material-ui/core";
+import React, { useState, Fragment } from "react";
+import { IconButton, Menu, InputAdornment } from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
+import TextField from "@material-ui/core/TextField";
 import Tooltip from "@material-ui/core/Tooltip";
-import MoreVertIcon from '@material-ui/icons/MoreVert';
-import useStyles from 'components/ContactDetailCard/components/FieldContent/style'
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import { makeStyles } from "@material-ui/core/styles";
 
-function CopyPurchaseInfo({
-    contactId,
-    content,
-}) {
-    const classes = useStyles();
-    return (
-        <React.Fragment>
-            <Tooltip title={"Copy Purchased data"} placement="top">
-                <IconButton
-                    size="small"
-                    className={classes.mergeIcon}
-                    onClick={(e) => {
-                        e.preventDefault()
-                    }}
-                >
-                    <MoreVertIcon id="mergeTypeIcon" className={classes.pencilIcon} />
-                </IconButton>
-            </Tooltip>
-        </React.Fragment>
-    );
+import {
+  getBasicInfoContent,
+  getBasicInfoExpContent,
+} from "components/ContactDetailedInfo/helper";
+
+const useStyles = makeStyles((theme) => ({
+    menuItem:{
+        padding: "7px 20px",
+        fontSize: "15px",
+        cursor: "pointer",
+    },
+    mergeIcon: {
+        position: "absolute",
+        right: "19px"
+    },
+    pencilIcon: {
+        fontSize: "22px",
+      },
+  }));
+  
+
+function CopyPurchaseInfo({ contactId, content }) {
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [search, setSearch] = useState("");
+  const fields = { ...getBasicInfoContent(), ...getBasicInfoExpContent() };
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <Fragment>
+      <Tooltip title={"Copy Purchased data"} placement="top">
+        <IconButton
+          size="small"
+          className={classes.mergeIcon}
+          onClick={(e) => {
+            e.preventDefault();
+            handleClick(e);
+          }}
+        >
+          <MoreVertIcon
+            id="mergeTypeIcon"
+            className={classes.pencilIcon}
+            aria-controls={contactId}
+            aria-haspopup="true"
+          />
+        </IconButton>
+      </Tooltip>
+      <Menu
+        id={contactId}
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "center",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "center",
+        }}
+      >
+        <TextField
+          type="text"
+          className={classes.menuItem}
+          placeholder="Search contact field"
+          value={search}
+          onChange={(e) => {
+            e.stopPropagation();
+            setSearch(e.target.value);
+          }}
+          InputProps={{
+            startAdornment: (
+              <>
+                <InputAdornment position="start">
+                  <SearchIcon htmlColor="#757575" />
+                </InputAdornment>
+              </>
+            ),
+          }}
+        />
+        {Object.entries(fields).map(([key, row]) => {
+          return key.toLowerCase().includes(search.toLowerCase()) ? (
+            <div
+              className={classes.menuItem}
+              onClick={(e) => {
+                  console.log('Data', content, key, row)
+                // setSelectedField()
+                handleClose();
+              }}
+            >
+              {key}
+            </div>
+          ) : (
+              <></>
+          );
+        })}
+      </Menu>
+    </Fragment>
+  );
 }
 
-export default CopyPurchaseInfo
+export default CopyPurchaseInfo;
