1654002ed6fb776c71d61c1f7c41704ca1160a19
iamzishan78 <zishannaseeb528@gmail.com>
2021-09-25 16:37:33 -0500

    profile and user mgmt updates

--- src/components/Shared/M1nTable/components/Table.js
+++ src/components/Shared/M1nTable/components/Table.js
@@ -1183,7 +1183,7 @@ function SubTable(props) {
                 ...column.options,
                 customBodyRender: (value) => {
                   return (
-                    <span style={{ padding: 10 }}>{value ? moment(value).format("MM/DD/YYYY HH:MM A") : ""}</span>
+                    <span style={{ padding: 10 }}>{value ? moment(value).format('lll') : ""}</span>
                   );
                 },
               };
