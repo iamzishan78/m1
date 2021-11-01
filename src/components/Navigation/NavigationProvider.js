0d0d62c07dd3702ae5c428bd31c334e69d37ed21
iamzishan78 <zishannaseeb528@gmail.com>
2021-11-01 15:17:01 +0500

    unused code removed

--- src/components/Navigation/NavigationProvider.js
+++ src/components/Navigation/NavigationProvider.js
@@ -1,27 +1,16 @@
-import React from 'react'
-import { NavigationContextProvider } from './NavigationContext'
-import { makeStyles } from '@material-ui/core/styles'
-import Navigation from './Navigation'
-const useStyles = makeStyles(theme => ({
-  mapWrapper: {
-    width: '100%',
-    height:'100%'
-  }
-}))
+import React from "react";
+import { NavigationContextProvider } from "./NavigationContext";
+import Navigation from "./Navigation";
 
 export default function NavigationProvider(props) {
-  let classes = useStyles()
-
-  const routes = props.children.map(child => child.props)
-
+  const routes = props.children.map((child) => child.props);
   return (
     <NavigationContextProvider>
       <Navigation>
-        {/* {props.children} */}
-        {React.Children.map(props.children, child => {
-          return React.cloneElement(child, { routes: routes })
+        {React.Children.map(props.children, (child) => {
+          return React.cloneElement(child, { routes: routes });
         })}
       </Navigation>
     </NavigationContextProvider>
-  )
+  );
 }
