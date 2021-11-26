2571d7089915ac329040f0c9213d23fb682de048
iamzishan78 <zishannaseeb528@gmail.com>
2021-11-26 17:30:32 +0500

    contact detail search

--- src/components/ExpandableCard/components/ContactSearch.js
+++ src/components/ExpandableCard/components/ContactSearch.js
@@ -11,11 +11,11 @@ import AutocompEntityNamesVirtualizeList from "../../Shared/M1nTable/components/
 
 const useStyles = makeStyles((theme) => ({
   search: {
-    width: "70%",
-    maxWidth: "400px",
-    "& svg": {
-      fill: "grey",
-    },
+    width: "35%",
+    marginLeft: "5px",
+    // "& svg": {
+    //   fill: "grey",
+    // },
   },
 }));
 
