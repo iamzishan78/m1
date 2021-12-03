c70a82f520f1b0cc47b7c649779d4e97652307bd
iamzishan78 <zishannaseeb528@gmail.com>
2021-12-04 02:30:51 +0500

    send object id in tags

--- src/components/Shared/M1nTable/components/Table.js
+++ src/components/Shared/M1nTable/components/Table.js
@@ -1728,7 +1728,9 @@ function SubTable(props) {
                         ? tableMeta.rowData[1]
                         : props.parent === "ownersPerParcel"
                           ? tableMeta.rowData[1]
-                          : tableMeta.rowData[0];
+                          : props.parent === "RevenueStatementTable"
+                            ? tableMeta.rowData[1].split("_")[1]
+                            : tableMeta.rowData[0];
 
                   if (props.parent === "assocTaxRollInterests" && props.targetLabel === "parcel") {
                     targetSourceId = tableMeta.rowData[15];
