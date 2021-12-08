336f6f83dd50c38357b2a9844dd43f766887a4ab
iamzishan78 <zishannaseeb528@gmail.com>
2021-12-08 14:11:01 +0500

    [ADD] - FE - Unit Detail >Tracts > add tract slideout - search bar at top that searches across existing parcel collections and returns data to slideout

---
+++ src/components/Table/TableAddDialog/Common/TractForm.js
@@ -0,0 +1,58 @@
+import React from "react";
+
+import TextField from "@material-ui/core/TextField";
+import { Controller } from "react-hook-form";
+
+import AutoCompleteShapeLayer from "components/Shared/Forms/Fields/AutoCompleteShapeLayer";
+
+
+function TractForm({ tract, tractValue, setSelectedShapeLayer, register, control, prefix = '' }) {
+
+  return (
+    <>
+      <AutoCompleteShapeLayer value={tractValue} shapeType='parcel' setSelectedShapeLayer={setSelectedShapeLayer} />
+
+      <Controller as={TextField} control={control} variant="outlined" margin="dense" name={`${prefix}state`} label={"State"}
+        InputLabelProps={{ shrink: true }} fullWidth defaultValue={tract?.state || ''} disabled />
+
+      <Controller as={TextField} control={control} variant="outlined" margin="dense" name={`${prefix}county`} label={"County"}
+        InputLabelProps={{ shrink: true }} fullWidth disabled defaultValue={tract?.county || ''} />
+
+      {tract.state !== 'TX' && <>
+        <Controller as={TextField} control={control} variant="outlined" margin="dense" name={`${prefix}meridian`} label={"Meridian"}
+          InputLabelProps={{ shrink: true }} fullWidth disabled />
+
+        <Controller as={TextField} control={control} variant="outlined" margin="dense" name={`${prefix}township`} label={"Township"}
+          InputLabelProps={{ shrink: true }} fullWidth disabled />
+
+        <Controller as={TextField} control={control} variant="outlined" margin="dense" name={`${prefix}range`} label={"Range"}
+          InputLabelProps={{ shrink: true }} fullWidth disabled />
+
+        <Controller as={TextField} control={control} variant="outlined" margin="dense" name={`${prefix}section`} label={"Section"}
+          InputLabelProps={{ shrink: true }} fullWidth disabled />
+      </>}
+
+      {tract.state === 'TX' && <>
+        <Controller as={TextField} control={control} variant="outlined" margin="dense" name={`${prefix}survey`} label={"Survey"}
+          InputLabelProps={{ shrink: true }} fullWidth disabled />
+
+        <Controller as={TextField} control={control} variant="outlined" margin="dense" name={`${prefix}block`} label={"Block"}
+          InputLabelProps={{ shrink: true }} fullWidth disabled />
+
+        <Controller as={TextField} control={control} variant="outlined" margin="dense" name={`${prefix}section`} label={"Section"}
+          InputLabelProps={{ shrink: true }} fullWidth disabled />
+
+        <Controller as={TextField} control={control} variant="outlined" margin="dense" name={`${prefix}abstract`} label={"Abstract"}
+          InputLabelProps={{ shrink: true }} fullWidth disabled />
+
+        <Controller as={TextField} control={control} variant="outlined" margin="dense" name={`${prefix}altSurvey`} label={"Alternate Survey"}
+          InputLabelProps={{ shrink: true }} fullWidth disabled />
+      </>}
+
+      <Controller as={TextField} control={control} variant="outlined" margin="dense" name={`${prefix}legalDescription`} label={"Full Legal Description"}
+        InputLabelProps={{ shrink: true }} multiline rows={4} fullWidth disabled defaultValue={tract?.legalDescription || ''} />
+    </>
+  );
+}
+
+export default TractForm;
