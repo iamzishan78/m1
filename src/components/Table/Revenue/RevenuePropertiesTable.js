0ec00f72a5de9b99e93691f80f4e41a08bd9ed0c
iamzishan78 <zishannaseeb528@gmail.com>
2021-12-11 10:34:24 +0500

    fetched data from ES

--- src/components/Table/Revenue/RevenuePropertiesTable.js
+++ src/components/Table/Revenue/RevenuePropertiesTable.js
@@ -3,6 +3,8 @@ import { Container } from "@material-ui/core";
 import Table from "components/Shared/M1nTable/components/Table";
 import TableHOC from "components/Table/TableHOC";
 import TableHeader from "components/Table/constants/revenue-properties-header-schema";
+import { GET_ES_PAGINATED_LIST } from "graphQL/useQueryESPaginatedList";
+import { useLazyQuery } from "@apollo/client";
 
 // QUERIES
 
@@ -13,15 +15,42 @@ import { usetableStyles } from "../Styles";
 
 function RevenuePropertiesTable(props) {
   const classes = usetableStyles();
+  const [getESPaginatedList, { data: elasticData, loading }] = useLazyQuery(
+    GET_ES_PAGINATED_LIST,
+    {
+      fetchPolicy: "no-cache",
+      onCompleted: () => {
+        console.log("compeleted");
+      },
+    }
+  );
+  console.log("elasticData", elasticData?.getESPaginatedList?.hits);
+  const tableData = elasticData?.getESPaginatedList?.hits?.map((eachRow) => {
+    return {
+      name: eachRow.name,
+      number: eachRow.number,
+      payorName: eachRow?.operator?.name,
+      state: eachRow.state,
+      country: eachRow?.county,
+      source: eachRow?.source,
+      wellApiNumber: eachRow?.well?.apiNumber,
+      wellName: eachRow?.well?.wellName,
+      status: eachRow?.well?.status,
+      checkNumber: eachRow?.lastCheck?.checkNumber,
+      lastChecked: new Date(eachRow?.lastCheck?.checkDate).toLocaleDateString(),
+    };
+  });
 
   // function states
   const [columns] = useState(JSON.parse(JSON.stringify(TableHeader)));
   const [selectedRows] = useState([]);
   const [potentialIssuesList] = useState([]);
+  const esIndex = "properties_flat";
+  const startPaginationAt = 10;
 
   // const count = tableData?.total || 0
   const options = {
-    // rowsPerPageOptions: [10, 25, 50, 100],
+    rowsPerPageOptions: [10, 25, 50, 100],
     searchable: true,
     rowsSelected: selectedRows.map((sR) => sR.dataIndex),
     filter: true,
@@ -29,71 +58,22 @@ function RevenuePropertiesTable(props) {
     serverSide: true,
   };
   // added dummy rows for just display
-  const testRows = [
-    {
-      _id: "61aeb76ee1d4eb43b897354a",
-      propertyName: "LSE 100-100001-L3 JULIA MEINERS",
-      propertyCode: "100-100001",
-      payorName: "Eric Calpton",
-      state: "OH",
-      country: "US",
-      source: "Uniliver",
-      wellApiNumber: "#123556",
-      wellName: "Oliver Foundations",
-      status: "Approved",
-      type: "A1",
-      amount: "120.32",
-      checkNumber: "A13d45tyg",
-      lastCheckDate: "12/12/2020",
-    },
-    {
-      _id: "61aeb76ee1d4eb4ab897354a",
-      propertyName: "LSE 100-100001-L3 JULIA MEINERS",
-      propertyCode: "100-1as00001",
-      payorName: "John Elton",
-      state: "OH",
-      country: "US",
-      source: "Uniliver",
-      wellApiNumber: "#123556",
-      wellName: "Oliver Foundations",
-      status: "Pending",
-      type: "A1",
-      amount: "120.32",
-      checkNumber: "A13d45tyg",
-      lastCheckDate: "12/12/2020",
-    },
-    {
-      _id: "61aeb76ee1d4eb4ab897354a",
-      propertyName: "LSE 100-100001-L3 JULIA MEINERS",
-      propertyCode: "100-1as00001",
-      payorName: "Saad Bin Khalid",
-      state: "OH",
-      country: "US",
-      source: "Uniliver",
-      wellApiNumber: "#123556",
-      wellName: "Oliver Foundations",
-      type: "A1",
-      amount: "120.32",
-      checkNumber: "A13d45tyg",
-      lastCheckDate: "12/12/2020",
-    },
-    {
-      _id: "90aeb76ee1d4eb4ab897354a",
-      propertyName: "LSE 100-100001-L3 JULIA MEINERS",
-      propertyCode: "100-10a0001",
-      payorName: "Saad Bin Khalid",
-      state: "OH",
-      country: "US",
-      source: "Uniliver",
-      wellApiNumber: "#123556",
-      wellName: "Oliver Foundations",
-      status: "Declined",
-      type: "A1",
-      amount: "120.32",
-      checkNumber: "A13d45tyg",
-      lastCheckDate: "12/12/2020",
-    },
-  ];
+
+  // fetaching data
+  React.useEffect(() => {
+    getESPaginatedList({
+      variables: {
+        esIndex,
+        pagination: {
+          first: startPaginationAt,
+          keep_alive: "1micros",
+        },
+        search: ``,
+        sort: [],
+        filter: "",
+      },
+    });
+  }, [getESPaginatedList, props.parent]);
 
   return (
     <Container
@@ -105,11 +85,11 @@ function RevenuePropertiesTable(props) {
         style={{ backgroundColor: "#fff" }}
         header={props.header}
         columns={columns}
-        rows={testRows}
+        rows={tableData}
         total={false}
         potentialIssues={potentialIssuesList}
         addAble={{ type: "RevenueProperties" }}
-        loading={false}
+        loading={loading}
         targetLabel={props.targetLabel}
         uploadIcon={null}
         dense={props.dense ? props.dense : undefined}
