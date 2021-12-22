25a993fe25a3a6ac79dc2f44a6e38f91e695f15b
iamzishan78 <zishannaseeb528@gmail.com>
2021-12-22 17:05:06 +0500

    revenue secondary panel header styling fixed

--- src/components/Revenue/styles.js
+++ src/components/Revenue/styles.js
@@ -45,6 +45,9 @@ export const useStyles = makeStyles((theme) => ({
     "& .MuiDrawer-paperAnchorLeft": {
       left: "52px",
     },
+    "& .MuiDivider-root": {
+      backgroundColor: "#263451",
+    },
   },
   drawerPaper: {
     width: drawerWidth,
@@ -65,7 +68,7 @@ export const useStyles = makeStyles((theme) => ({
     width: "100%",
   },
   header: {
-    paddingLeft: "10px",
+    padding: "10px 5px 15px 10px",
     alignItems: "center",
   },
   pulloutBox: {
