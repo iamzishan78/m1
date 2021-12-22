1aa0e50804492a68b59f9ad83050718c818b9e7c
iamzishan78 <zishannaseeb528@gmail.com>
2021-12-22 16:03:12 +0500

    code cleanup

--- src/components/Land/components/Agreements/index.js
+++ src/components/Land/components/Agreements/index.js
@@ -1,85 +1,67 @@
 import React, { useState, useContext } from "react";
-import { useHistory, useLocation } from "react-router-dom";
-import { makeStyles } from "@material-ui/core/styles";
-import Button from "@material-ui/core/Button";
-import Add from "@material-ui/icons/Add";
 import { AppContext } from "AppContext";
 import AnalyticsCards from "../Common/AnalyticsCards";
-import AgreementsTable from "../../../Table/Agreement/AgreementsTable";
-
-const useStyles = makeStyles((theme) => ({
-}));
+import AgreementsTable from "components/Table/Agreement/AgreementsTable";
 
 function Agreements(props) {
-    const [stateApp] = useContext(AppContext);
-    const history = useHistory();
+  const [stateApp] = useContext(AppContext);
 
-    const [agreementCount, setAgreementCount] = useState(0);
-    const [activeCount, setActiveCount] = useState(0);
-    const [inactiveCount, setInactiveCount] = useState(0);
-    const [approvedCount, setApprovedCount] = useState(0);
-    const [unapprovedCount, setUnapprovedCount] = useState(0);
-    const [openDrawer, setOpenDrawer] = useState(false);
+  const [agreementCount, setAgreementCount] = useState(0);
+  const [activeCount, setActiveCount] = useState(0);
+  const [inactiveCount, setInactiveCount] = useState(0);
+  const [approvedCount, setApprovedCount] = useState(0);
+  const [unapprovedCount, setUnapprovedCount] = useState(0);
 
-    const onAgreementCount = (count) => {
-        setAgreementCount(count);
-      }
+  const onAgreementCount = (count) => {
+    setAgreementCount(count);
+  }
 
-    const onActiveCount = (count) => {
-        setActiveCount(count);
-        setInactiveCount(agreementCount - count);
-      }
+  const onActiveCount = (count) => {
+    setActiveCount(count);
+    setInactiveCount(agreementCount - count);
+  }
 
-      const onApprovedCount = (count) => {
-        setApprovedCount(count);
-        setUnapprovedCount(agreementCount - count);
-      }
+  const onApprovedCount = (count) => {
+    setApprovedCount(count);
+    setUnapprovedCount(agreementCount - count);
+  }
 
-    const handleListItemClick = (path) => {
-        history.push(path);
-        handleDrawerClose();
-      };
+  const cards = [
+    {
+      heading: "Total Agreements",
+      points: agreementCount,
+    },
+    {
+      heading: "Active",
+      points: activeCount,
+    },
+    {
+      heading: "Inactive",
+      points: inactiveCount,
+    },
+    {
+      heading: "Unapproved",
+      points: unapprovedCount,
+      type: "warning",
+    },
+  ];
 
-    const handleDrawerClose = () => {
-    setOpenDrawer(false);
-    };
-
-    const cards = [
-      {
-        heading: "Total Agreements",
-        points: agreementCount,
-      },
-      {
-        heading: "Active",
-        points: activeCount,
-      },
-      {
-        heading: "Inactive",
-        points: inactiveCount,
-      },
-      {
-        heading: "Unapproved",
-        points: unapprovedCount,
-        type: "warning",
-      },
-    ];
-
-    return (
-        <>
-            <AnalyticsCards cards={cards} />
-            <div style={{ padding: 30, paddingTop: 0, overflow: "auto" }}>
-                <AgreementsTable 
-                  header="Agreements"
-                  onAgreementCount={onAgreementCount}
-                  onActiveCount={onActiveCount}
-                  onApprovedCount={onApprovedCount}
-                  parent="AgreementsTable"
-                  targetLabel="agreement"
-                  landSearchQuery={stateApp.landSearchQuery}
-                />
-            </div>
-        </>
-    )
+  return (
+    <>
+      <AnalyticsCards cards={cards} />
+      <div style={{ padding: 30, paddingTop: 0, overflow: "auto" }}>
+        <AgreementsTable
+          header="Agreements"
+          onAgreementCount={onAgreementCount}
+          onActiveCount={onActiveCount}
+          onApprovedCount={onApprovedCount}
+          parent="AgreementsTable"
+          targetLabel="agreement"
+          landSearchQuery={stateApp.landSearchQuery}
+        />
+      </div>
+    </>
+  )
 }
 
 export default Agreements
