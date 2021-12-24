e93afa9d5f868d4d9435196b25f094ced2f69451
iamzishan78 <zishannaseeb528@gmail.com>
2021-12-24 17:56:21 +0500

    table sorting issues has been fixed

--- src/components/Shared/M1nTable/components/Table.js
+++ src/components/Shared/M1nTable/components/Table.js
@@ -2410,6 +2410,19 @@ function SubTable(props) {
               },
             };
             break;
+          case "propertyNumber":
+            column.options = {
+              ...column.options,
+              customBodyRender: (value) => {
+                const splitNumber = value.split("_");
+                return (
+                  <p style={{ fontWeight: 600, color: "#17aadd", cursor: "pointer" }}>
+                    {splitNumber[0]}
+                  </p>
+                );
+              },
+            };
+            break;
           case "checkNumber":
             column.options = {
               ...column.options,
