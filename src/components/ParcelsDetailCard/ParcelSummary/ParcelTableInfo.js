5f71f83a0a31d81aebdab033bd8cb2ad24189c2c
iamzishan78 <zishannaseeb528@gmail.com>
2021-12-25 19:37:07 +0500

    console removed from parcel table info

--- src/components/ParcelsDetailCard/ParcelSummary/ParcelTableInfo.js
+++ src/components/ParcelsDetailCard/ParcelSummary/ParcelTableInfo.js
@@ -222,7 +222,6 @@ export default function ParcelTableInfo({ properties, updateProperties, updateCu
     setTableDataState({})
   }, [properties]);
 
-  console.log("tableTempProperties", tableTempProperties);
 
   useEffect(() => {
     if (search) {
