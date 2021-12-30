a51ad9660d624e6975d62e3e0a1020cf46d5a151
iamzishan78 <zishannaseeb528@gmail.com>
2021-12-30 18:20:49 +0500

    line item index component added

---
+++ src/components/Revenue/components/Statements/LineItem/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import NavHeader from "components/Revenue/components/Common/NavHeader";
+
+export default function LineItem(props) {
+  return (
+    <NavHeader title="94782044-EXXON MOBIL CORP">
+      <div>Will be working on it!!</div>
+    </NavHeader>
+  );
+}
