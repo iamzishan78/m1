b663b08fee536b9fd1f2b8d9192fd2e301066ae5
iamzishan78 <zishannaseeb528@gmail.com>
2021-12-30 20:39:32 +0500

    contact convert first index

--- src/store/sagas/contactSaga.js
+++ src/store/sagas/contactSaga.js
@@ -47,7 +47,7 @@ function* convertTaxOwnerToContact(action) {
     const { uri, id, internalKey } = uploadUri.data.data.getUploadContactUri.job;
     
     yield put(toggleBulkUploadAction(!bulkUpload));
-    const res = yield call(Api.fetchBlob, JSON.stringify([owners[0]]), id, internalKey, uri);
+    const res = yield call(Api.fetchBlob, JSON.stringify(owners), id, internalKey, uri);
     if (res?._response?.status === 201) {
       const jobResponse = yield call(Api.fetch, CREATE_JOB, { jobId: id, sendEmail: false });
       yield call(Api.fetch, UPDATE_JOB, {
