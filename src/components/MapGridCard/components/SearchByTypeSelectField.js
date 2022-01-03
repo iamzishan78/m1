c41c3859e4f454cb86284e995646f0106eff3f9c
iamzishan78 <zishannaseeb528@gmail.com>
2022-01-03 16:19:51 -0600

    hiding snapgrid menu items for use defined data

--- src/components/MapGridCard/components/SearchByTypeSelectField.js
+++ src/components/MapGridCard/components/SearchByTypeSelectField.js
@@ -12,6 +12,8 @@ import ExpandableSearch from "components/Shared/Forms/Fields/ExpandableSearch";
 import { Typography } from "@material-ui/core";
 import { platformDataInitialData, userDefinedInitialData } from "./data";
 
+import { useSelector } from "react-redux";
+
 const StyledMenu = withStyles({
   paper: {
     minWidth: "420px",
@@ -91,6 +93,7 @@ const SearchByTypeSelectField = ({ handleChange, value, backgroundColor, color }
   const classes = useStyles({ backgroundColor, color });
   const [search, setSearch] = useState('');
   const [anchorEl, setAnchorEl] = React.useState(null);
+  const { mapGridCardActivated } = useSelector(({ MapGridCard }) => MapGridCard);
 
   const platformData = useMemo(() => {
     return platformDataInitialData.filter((data) => !search || data.label.toLocaleLowerCase().startsWith(search.toLocaleLowerCase()))
@@ -158,24 +161,26 @@ const SearchByTypeSelectField = ({ handleChange, value, backgroundColor, color }
             </StyledMenuItem>
           })}
 
-          <Grid container className={classes.underlinedHeader} direction="row" justifyContent="space-between" alignItems="center" >
-            <Grid item>
-              <Typography variant="h6">
-                User Defined Data
-              </Typography>
+          {!mapGridCardActivated && <>
+            <Grid container className={classes.underlinedHeader} direction="row" justifyContent="space-between" alignItems="center" >
+              <Grid item>
+                <Typography variant="h6">
+                  User Defined Data
+                </Typography>
+              </Grid>
             </Grid>
-          </Grid>
 
-          {userDefinedData.map((icon) => {
-            const Icon = icon.Icon
-            return <StyledMenuItem key={icon.index} selected={icon.value === value.value} onClick={() => { handleChange(icon); handleClose() }}>
-              <ListItemIcon>
-                <Icon className={classes.icon} fontSize="small" color="#bdc7d1" opacity="1" />
-              </ListItemIcon>
+            {userDefinedData.map((icon) => {
+              const Icon = icon.Icon
+              return <StyledMenuItem key={icon.index} selected={icon.value === value.value} onClick={() => { handleChange(icon); handleClose() }}>
+                <ListItemIcon>
+                  <Icon className={classes.icon} fontSize="small" color="#bdc7d1" opacity="1" />
+                </ListItemIcon>
 
-              <ListItemText primary={icon.label} />
-            </StyledMenuItem>
-          })}
+                <ListItemText primary={icon.label} />
+              </StyledMenuItem>
+            })}
+          </>}
         </Grid>
       </StyledMenu>
     </>
