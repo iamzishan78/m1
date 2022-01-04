2ba7a31ecb36bf45b16278f1003ed8465b279574
iamzishan78 <zishannaseeb528@gmail.com>
2022-01-04 21:52:42 +0500

    date formating

--- src/components/Table/Revenue/RevenueStatementTable.js
+++ src/components/Table/Revenue/RevenueStatementTable.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container } from "@material-ui/core";
 import Table from "components/Shared/M1nTable/components/Table";
 import TableHOC from "components/Table/TableHOC";
+import moment from "moment";
 
 // QUERIES 
 import { useLazyQuery } from "@apollo/client";
@@ -13,7 +14,7 @@ import TableHeader from 'components/Table/constants/revenue-statement-header-sch
 
 // Utilities
 import { usetableStyles } from "../Styles";
-import { GET_ES_PAGINATED_REVENUE_LIST } from "graphQL/useQueryESPaginatedRevenueList";
+import { GET_ES_PAGINATED_LIST } from "graphQL/useQueryESPaginatedList";
 import { GET_ES_FILTER_LIST } from "graphQL/useQueryESFilterList";
 import { GET_ES_POTENTIAL_ISSUES_SUMMARY } from "graphQL/useQueryESSummary";
 import { AutoCompleteFilter } from "../AutoCompleteFilter";
@@ -32,7 +33,7 @@ function RevenueStatementTable(props) {
 
     // queries 
 
-    const [getESPaginatedList, { data: elasticData }] = useLazyQuery(GET_ES_PAGINATED_REVENUE_LIST, {
+    const [getESPaginatedList, { data: elasticData }] = useLazyQuery(GET_ES_PAGINATED_LIST, {
         context: { batch: true },
         fetchPolicy: "no-cache",
     });
@@ -42,7 +43,7 @@ function RevenueStatementTable(props) {
         fetchPolicy: "no-cache",
     });
 
-    const tableData = elasticData?.getESPaginatedRevenueList;
+    const tableData = elasticData?.getESPaginatedList;
     const issues = potentialIssues?.getESPotentialIssuesSummary;
 
     const startPaginationAt = 25;
@@ -93,7 +94,16 @@ function RevenueStatementTable(props) {
 
     useEffect(() => {
         if (tableData?.hits?.length > 0) {
-            let hits = tableData?.hits
+            
+            const hits = tableData?.hits.map((hit) => {
+                hit.checkDate = hit.checkDate
+                  ? moment(new Date(hit.checkDate)).format("MM/DD/YYYY")
+                  : null;
+                hit.depositDate = hit.depositDate
+                  ? moment(new Date(hit.depositDate)).format("MM/DD/YYYY")
+                  : null;
+                return hit;
+            });
             props.onGettingStatements(hits);
             props.setRows(hits);
             let headers = copy(TableHeader)
