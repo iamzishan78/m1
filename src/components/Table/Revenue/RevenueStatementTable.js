41512dbe57b7acfd9882175fca9abd7791b99043
iamzishan78 <zishannaseeb528@gmail.com>
2022-01-05 17:15:28 +0500

    revenue validation column

--- src/components/Table/Revenue/RevenueStatementTable.js
+++ src/components/Table/Revenue/RevenueStatementTable.js
@@ -16,6 +16,7 @@ import TableHeader from 'components/Table/constants/revenue-statement-header-sch
 import { usetableStyles } from "../Styles";
 import { GET_ES_PAGINATED_LIST } from "graphQL/useQueryESPaginatedList";
 import { GET_ES_FILTER_LIST } from "graphQL/useQueryESFilterList";
+import { GET_VALIDATION_CHECK } from "graphQL/useQueryValidationCheck";
 import { GET_ES_POTENTIAL_ISSUES_SUMMARY } from "graphQL/useQueryESSummary";
 import { AutoCompleteFilter } from "../AutoCompleteFilter";
 
@@ -34,6 +35,11 @@ function RevenueStatementTable(props) {
     // queries 
 
     const [getESPaginatedList, { data: elasticData }] = useLazyQuery(GET_ES_PAGINATED_LIST, {
+        context: { batch: true },
+        fetchPolicy: "no-cache",
+    });
+
+    const [getRevenueValidationCheck, { data: validationData }] = useLazyQuery(GET_VALIDATION_CHECK, {
         context: { batch: true },
         fetchPolicy: "no-cache",
     });
@@ -91,6 +97,32 @@ function RevenueStatementTable(props) {
             setPotentialIssuesList([]);
         }
     }, [potentialIssues]);
+
+    useEffect(() => {
+        if(validationData?.getRevenueValidationCheck?.hits){
+            const validation = JSON.parse(JSON.stringify(validationData.getRevenueValidationCheck.hits))
+            const rows = JSON.parse(JSON.stringify(props.rows))
+            for(let i = 0; i < rows.length; i++){
+                if(validation[rows[i]._id]){
+                    rows[i].validation = !(parseFloat(validation[rows[i]._id].checkDetailAmt.value.toFixed(2)) === rows[i].checkAmount)
+                }else{
+                    rows[i].validation = false
+                }
+            }
+            props.setRows(rows)
+        }
+    },[validationData])
+
+    useEffect(() => {
+        if (tableData?.hits?.length > 0) {
+            const objectsIdsArray = tableData.hits.map((check) => check._id);
+            getRevenueValidationCheck({
+              variables: {
+                checkIds: objectsIdsArray,
+              },
+            });
+        }
+    },[tableData])
 
     useEffect(() => {
         if (tableData?.hits?.length > 0) {
