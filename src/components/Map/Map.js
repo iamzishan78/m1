452a8ef08193e596999675a9f151e995357c4490
iamzishan78 <zishannaseeb528@gmail.com>
2022-01-12 23:05:38 +0500

    card open before map

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -408,13 +408,12 @@ function Map({ type, paramId, lati, longi }) {
     const keys = { parcels: "selectedParcel", ...layersWithSelectedShapeKey(), wells: "selectedWell" };
 
     if (type === "wells") {
-      setShowExpandableCard(true);
-      setStateApp({ ...stateApp, selectedWellId: paramId.toLowerCase(), popupOpen: false, expandedCard: true });
       const currentFeature = { ...(await getElasticWell(paramId)) };
       if (currentFeature?.Id) currentFeature.id = currentFeature.Id;
-      setStateApp({ ...stateApp, selectedWell: currentFeature });
-      findBoundsMap([currentFeature.geoJSON], map);
-      drawWellBoundary(map, [currentFeature.Longitude, currentFeature.Latitude]);
+      setStateApp({ ...stateApp, selectedWell: currentFeature, selectedWellId: paramId.toLowerCase(), popupOpen: false, expandedCard: true });
+      setShowExpandableCard(true);
+        findBoundsMap([currentFeature.geoJSON], map);
+        drawWellBoundary(map, [currentFeature.Longitude, currentFeature.Latitude]);
       return;
     }
     if (!stateApp[keys[type]] || paramId !== stateApp[keys[type]]?.id) {
