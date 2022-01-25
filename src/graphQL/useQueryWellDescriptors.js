e15e4e7e359018a57beb037bce9c95801ce7e077
iamzishan78 <zishannaseeb528@gmail.com>
2022-01-25 19:15:33 +0500

    associated wells for property

---
+++ src/graphQL/useQueryWellDescriptors.js
@@ -0,0 +1,7 @@
+import gql from "graphql-tag";
+
+export const GET_WELL_DESCRIPTORS = gql`
+  query getWellsDescriptors($relatedObject: ID) {
+    getWellsDescriptors(relatedObject: $relatedObject)
+  }
+`;
