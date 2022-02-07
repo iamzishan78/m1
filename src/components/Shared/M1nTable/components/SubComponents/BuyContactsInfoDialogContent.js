8464bed45e85502ea40f482b153a923ee1ec2ba1
iamzishan78 <zishannaseeb528@gmail.com>
2022-02-08 00:24:32 +0500

    replace all selected rows count with valid contact information count

--- src/components/Shared/M1nTable/components/SubComponents/BuyContactsInfoDialogContent.js
+++ src/components/Shared/M1nTable/components/SubComponents/BuyContactsInfoDialogContent.js
@@ -140,8 +140,8 @@ export default function BuyContactsInfoDialogContent(props) {
   }, [idiCoreData]);
 
   function loadPersonData() {
-    if (props.rows.length > currentCredits) {
-      dispatch(showErrorMessage(`Your remaning credit count is ${currentCredits} but you are trying to use ${props.rows.length}`));
+    if (validContactData.length > currentCredits) {
+      dispatch(showErrorMessage(`Your remaning credit count is ${currentCredits} but you are trying to use ${validContactData.length}`));
     } else {
 
       setBuyNowClicked(true)
