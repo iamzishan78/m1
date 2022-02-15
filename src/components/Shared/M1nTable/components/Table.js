02b562567f89d87971fdc523e7939650737a97b2
iamzishan78 <zishannaseeb528@gmail.com>
2022-02-16 00:24:54 +0500

    Flyto well added in unit detail card.

--- src/components/Shared/M1nTable/components/Table.js
+++ src/components/Shared/M1nTable/components/Table.js
@@ -721,22 +721,14 @@ function SubTable(props) {
 
   // handlers
   const handleWellFlyTo = (value) => {
-    // setting state to fly to the selected well
+    let unitId = history.location.pathname.split("/");
     setStateApp((stateApp) => ({
       ...stateApp,
-      fitBounds: null,
-      selectedWell: null,
+      selectedShape: null,
       selectedWellId: value.wellId ? value.wellId.toLowerCase() : null,
-      wellSelectedCoordinates: [value.center[0], value.center[1]],
-      wellListFromSearch: [
-        {
-          id: value.wellId,
-          longitude: value.center[0],
-          latitude: value.center[1],
-        },
-      ],
+      wellSelectedCoordinates: [value.center[0], value.center[1]]
     }));
-    stateApp.toggleLayersActivity("Search", true);
+    history.push(`/map/wells/${value?.wellId.toUpperCase()}`, { fromUnitDetail: true, unitId: unitId[unitId.length-1] });
   };
 
   const handleLocationFlyTo = (newValue) => {
