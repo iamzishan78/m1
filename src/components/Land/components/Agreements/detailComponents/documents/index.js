3229496bb1d89eb38d5d5d4367bcbd0f3f7965a6
iamzishan78 <zishannaseeb528@gmail.com>
2022-02-18 14:31:27 +0500

    unused imports removed

--- src/components/Land/components/Agreements/detailComponents/documents/index.js
+++ src/components/Land/components/Agreements/detailComponents/documents/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { makeStyles } from "@material-ui/styles";
 import { Typography, Accordion, AccordionSummary, AccordionDetails, Grid, Chip, IconButton } from "@material-ui/core";
 import { ExpandMore as ExpandMoreIcon, DescriptionOutlined as DescriptionOutlinedIcon } from "@material-ui/icons";
