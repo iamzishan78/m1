e46b843c4787f81a00252f01c77452d663db8295
iamzishan78 <zishannaseeb528@gmail.com>
2022-02-23 01:07:21 +0500

    activity page search bar title

--- src/components/Navigation/components/ActivitySearch.js
+++ src/components/Navigation/components/ActivitySearch.js
@@ -17,6 +17,21 @@ import { GETALLACTIVITIESFORSEARCH } from "../../../graphQL/useQueryGetAllActivi
 import { AppContext } from "../../../AppContext";
 
 const useStyles = makeStyles((theme) => ({
+  barTitle: {
+    alignItems: "center",
+    marginRight: "45px",
+    display: "flex",
+    fontWeight: "bold",
+    "& svg": {
+      fill: "black !important",
+      fontSize: "2rem",
+      marginRight: "5px",
+    },
+    "& .MuiTypography-root": {
+      fontSize: "30px",
+      fontWeight: "bold",
+    },
+  },
   search: {
     position: "relative",
     borderRadius: theme.shape.borderRadius,
@@ -112,103 +127,115 @@ const ActivitySearch = () => {
   );
 
   return (
-    <div style={{ marginLeft: quickActionsPanelState ? "425px" : "0px" }}>
-      <Autocomplete
-        className={classes.search}
-        style={{
-          margin: 0,
-        }}
-        defaultValue={nameAutValue}
-        value={nameAutValue}
-        disableListWrap
-        options={activities}
-        getOptionLabel={(option) => option.name}
-        getOptionSelected={(option, value) => {
-          return option === value;
-        }}
-        renderOption={(option) => {
-          return (
-            <Grid container spacing={0}>
-              <Grid container item xs={12} alignItems="center">
-                <Grid item xs>
-                  <span style={{ fontWeight: 400 }}>{option.name}</span>
+    <Grid
+      container
+      display="flex"
+      direction="row"
+      alignItems="center"
+      style={{ marginLeft: quickActionsPanelState ? "425px" : "0px", width: "55%" }}
+    >
+      <Grid item className={classes.barTitle}>
+        <EventIcon />
+        <Typography color="primary">Activities</Typography>
+      </Grid>
+      <Grid item>
+        <Autocomplete
+          className={classes.search}
+          style={{
+            margin: 0,
+          }}
+          defaultValue={nameAutValue}
+          value={nameAutValue}
+          disableListWrap
+          options={activities}
+          getOptionLabel={(option) => option.name}
+          getOptionSelected={(option, value) => {
+            return option === value;
+          }}
+          renderOption={(option) => {
+            return (
+              <Grid container spacing={0}>
+                <Grid container item xs={12} alignItems="center">
+                  <Grid item xs>
+                    <span style={{ fontWeight: 400 }}>{option.name}</span>
 
-                  <Typography variant="body2" color="textSecondary">
-                    {option.type}
-                  </Typography>
+                    <Typography variant="body2" color="textSecondary">
+                      {option.type}
+                    </Typography>
+                  </Grid>
                 </Grid>
               </Grid>
-            </Grid>
-          );
-        }}
-        onInputChange={onInputChange}
-        onChange={(e, act) => {
-          handleSelectActivity(act?._id);
-          setNameAutValue(act);
-        }}
-        renderInput={(params) => (
-          <TextField
-            margin="dense"
-            {...params}
-            style={{
-              margin: 0,
-            }}
-            className={classes.activitySearchField}
-            placeholder="Search for activities"
-            variant="outlined"
-            InputProps={{
-              ...params.InputProps,
-              startAdornment: (
-                <InputAdornment>
-                  <IconButton size="small">
-                    <SearchIcon htmlColor="grey" />
-                  </IconButton>
-                </InputAdornment>
-              ),
-              endAdornment: (
-                <>
-                  <ButtonGroup variant="text">
-                    <Tooltip title="Clear">
-                      <IconButton
-                        size="small"
-                        htmlColor="#fff"
-                        className={`${classes.toggleBtn} ${stateApp.activityDisplayType === "table" && classes.activeBtn}`}
-                        onClick={() => {
-                          setNameAutValue({ name: "", _id: null });
-                        }}
-                      >
-                        <ClearIcon />
-                      </IconButton>
-                    </Tooltip>
-                    <Tooltip title="List View">
-                      <IconButton
-                        size="small"
-                        htmlColor="#fff"
-                        className={`${classes.toggleBtn} ${stateApp.activityDisplayType === "table" && classes.activeBtn}`}
-                        onClick={() => setActivityDisplayType("table")}
-                      >
-                        <List />
-                      </IconButton>
-                    </Tooltip>
-                    <Tooltip title="Calendar">
-                      <IconButton
-                        size="small"
-                        htmlColor="#fff"
-                        className={`${classes.toggleBtn} ${stateApp.activityDisplayType === "calendar" && classes.activeBtn}`}
-                        onClick={() => setActivityDisplayType("calendar")}
-                      >
-                        <EventIcon />
-                      </IconButton>
-                    </Tooltip>
-                  </ButtonGroup>
-                </>
-              ),
-            }}
-            size="small"
-          />
-        )}
-      />
-    </div>
+            );
+          }}
+          onInputChange={onInputChange}
+          onChange={(e, act) => {
+            handleSelectActivity(act?._id);
+            setNameAutValue(act);
+          }}
+          renderInput={(params) => (
+            <TextField
+              margin="dense"
+              {...params}
+              style={{
+                margin: 0,
+              }}
+              className={classes.activitySearchField}
+              placeholder="Search for activities"
+              variant="outlined"
+              InputProps={{
+                ...params.InputProps,
+                startAdornment: (
+                  <InputAdornment>
+                    <IconButton size="small">
+                      <SearchIcon htmlColor="grey" />
+                    </IconButton>
+                  </InputAdornment>
+                ),
+                endAdornment: (
+                  <>
+                    <ButtonGroup variant="text">
+                      <Tooltip title="Clear">
+                        <IconButton
+                          size="small"
+                          htmlColor="#fff"
+                          className={`${classes.toggleBtn} ${stateApp.activityDisplayType === "table" && classes.activeBtn}`}
+                          onClick={() => {
+                            setNameAutValue({ name: "", _id: null });
+                          }}
+                        >
+                          <ClearIcon />
+                        </IconButton>
+                      </Tooltip>
+                      <Tooltip title="List View">
+                        <IconButton
+                          size="small"
+                          htmlColor="#fff"
+                          className={`${classes.toggleBtn} ${stateApp.activityDisplayType === "table" && classes.activeBtn}`}
+                          onClick={() => setActivityDisplayType("table")}
+                        >
+                          <List />
+                        </IconButton>
+                      </Tooltip>
+                      <Tooltip title="Calendar">
+                        <IconButton
+                          size="small"
+                          htmlColor="#fff"
+                          className={`${classes.toggleBtn} ${stateApp.activityDisplayType === "calendar" && classes.activeBtn}`}
+                          onClick={() => setActivityDisplayType("calendar")}
+                        >
+                          <EventIcon />
+                        </IconButton>
+                      </Tooltip>
+                    </ButtonGroup>
+                  </>
+                ),
+              }}
+              size="small"
+            />
+          )}
+        />
+      </Grid>
+    </Grid>
   );
 };
 
