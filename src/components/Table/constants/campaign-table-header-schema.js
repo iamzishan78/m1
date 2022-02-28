8b3092f2c4491d6e214c8f4ad20a81ddee0432f1
iamzishan78 <zishannaseeb528@gmail.com>
2022-02-28 13:44:30 +0500

    campaign grid integrated

---
+++ src/components/Table/constants/campaign-table-header-schema.js
@@ -0,0 +1,93 @@
+import { history } from "store";
+
+const CampaignsHeadCells = [
+  {
+    name: "_id",
+    options: {
+      display: false,
+      filter: false,
+      searchable: false,
+      sort: false,
+      download: false,
+      print: false,
+      viewColumns: false,
+    },
+  },
+  {
+    name: "name",
+    label: "Campaign Name",
+    esKey: "name.keyword",
+    options: {
+      display: true,
+      sort: true,
+      filter: true,
+      customRender: (value) => {
+        return value ? (
+          <p
+            onClick={(e) => {
+              e.stopPropagation();
+              // history.push(`/revenue/statement/details/${tableMeta.rowData[0]}`);
+            }}
+            style={{ fontWeight: 600, color: "#17aadd", cursor: "pointer" }}
+          >
+            {value}
+          </p>
+        ) : (
+          <p style={{ color: "#898989b0" }}>N/A</p>
+        );
+      },
+    },
+  },
+  {
+    name: "type",
+    label: "Campaign Type",
+    esKey: "type.keyword",
+    options: {
+      display: true,
+      sort: true,
+      filter: true,
+    },
+  },
+  {
+    name: "entityType",
+    label: "Entity Type",
+    esKey: "type.keyword",
+    options: {
+      display: true,
+      sort: true,
+      filter: true,
+    },
+  },
+  {
+    name: "currentEntities",
+    label: "# Current Entities",
+    esKey: "type.keyword",
+    options: {
+      display: true,
+      sort: true,
+      filter: true,
+    },
+  },
+  {
+    name: "type",
+    label: "# Sent",
+    esKey: "type.keyword",
+    options: {
+      display: true,
+      sort: true,
+      filter: true,
+    },
+  },
+  {
+    name: "status",
+    label: "Campaign Status",
+    esKey: "type.keyword",
+    options: {
+      display: true,
+      sort: true,
+      filter: true,
+    },
+  },
+];
+
+export default CampaignsHeadCells;
