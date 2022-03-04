1400dbf7a07be275d2d22cbaf008c61331328b4a
iamzishan78 <zishannaseeb528@gmail.com>
2022-03-04 16:27:19 +0500

    reporting group dropdown change

--- src/components/Revenue/components/Portfolio/index.js
+++ src/components/Revenue/components/Portfolio/index.js
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
-import { Grid, Button, Divider, TextField } from "@material-ui/core";
+import { Grid, Button, Divider } from "@material-ui/core";
 import { makeStyles } from "@material-ui/styles";
-import Autocomplete from "@material-ui/lab/Autocomplete";
 
 import AnalyticsCards from "components/Revenue/components/Common/AnalyticsCards";
 import CustomDates from "components/Revenue/components/Common/CustomDates";
 import DetailTabsSection from "components/Revenue/components/Portfolio/DetailTabsSection";
+import ReportGroupHeader from "components/Shared/ReportGroupHeader";
 
 const useStyles = makeStyles((theme) => ({
   actionBar: {
@@ -55,6 +55,11 @@ export default function Portfolio() {
   const [toDate, setToDate] = React.useState(null);
   const [monthsInterval, setMonths] = useState([]);
 
+  const [filterToggle, setFilterToggle] = React.useState(false);
+  // props to pass in table
+  // const esIndex = "shapes_flat";
+  const [esFilters, setESFilters] = useState([]);
+
   const onChangeDates = (fromDate, toDate) => {
     const months = [];
     if (fromDate && toDate) {
@@ -98,20 +103,8 @@ export default function Portfolio() {
               </Grid>
             </Grid>
           </Grid>
-          <Grid item xs={3}>
-            <Autocomplete
-              size="small"
-              onChange={(event, newValue) => {}}
-              options={["Last Month"]}
-              renderInput={(params) => (
-                <TextField {...params} label="Reporting Groups" variant="outlined" placeholder="" style={{ backgroundColor: "white" }} />
-              )}
-              defaultValue={"Last Month"}
-              disableListWrap
-              style={{ maxWidth: "400px", marginTop: "15px" }}
-            />
-          </Grid>
         </Grid>
+        <ReportGroupHeader type={"Portfolios"} esFilters={esFilters} setESFilters={setESFilters} setFilterToggle={setFilterToggle} />
       </div>
       {/* <AnalyticsCards cards={cards} /> */}
       <Divider className={classes.divider} />
