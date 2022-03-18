cc4f0116dd8e065e42380c19cf79f225488f42e2
iamzishan78 <zishannaseeb528@gmail.com>
2022-03-18 15:05:26 -0500

    p1 stuff

--- src/components/ContactDetailCard/components/DealsNew.js
+++ src/components/ContactDetailCard/components/DealsNew.js
@@ -13,7 +13,7 @@ import Button from "@material-ui/core/Button";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    padding: "23px 23px 0 23px",
+    padding: "10px 0 0 10px",
     cursor: "pointer",
     width: "100%",
   },
