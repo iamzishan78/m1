3604be30dc7dd099387ee770b62896a5e54d8810
iamzishan78 <zishannaseeb528@gmail.com>
2022-04-05 16:09:26 +0500

    Tract Uploader issues fixed

--- src/components/BulkUpload/jobHeaders/TRACTS.js
+++ src/components/BulkUpload/jobHeaders/TRACTS.js
@@ -1,6 +1,6 @@
 const PARCELINTERESTS_FIELDS = require("./PARCELINTERESTS").default
 
-export default [
+const tracts = [
     {
         label: "State",
         mapped_key: "",
@@ -81,3 +81,8 @@ export default [
     },
     ...PARCELINTERESTS_FIELDS
 ];
+
+tracts.find((key) => key.actual_key === 'entityDetail.state').label = 'AddressState'
+// tracts.find((key) => key.actual_key === 'ownerType').label = 'Entity Type'
+console.log(tracts)
+export default tracts
