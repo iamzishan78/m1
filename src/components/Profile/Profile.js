95c7257a184c03bc60f4ea7fccfb66c91a12045a
iamzishan78 <zishannaseeb528@gmail.com>
2022-04-18 23:50:49 +0530

    My Account Popup updated

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,4 +1,5 @@
 import Dialog from "@material-ui/core/Dialog";
+import { Tabs, Tab, Checkbox, FormControlLabel, Box } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import React, { useContext } from "react";
 import { useHistory } from "react-router-dom";
@@ -7,24 +8,49 @@ import ImageModal from "./ImageModal";
 import ProfileActions from "./ProfileActions";
 import ProfileContent from "./ProfileContent";
 import ProfileTitle from "./ProfileTitle";
+import { TramOutlined, TrendingUpOutlined } from "@material-ui/icons";
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
   paper: {
     marginTop: "100px",
     marginLeft: "auto",
     marginBottom: "auto",
     maxHeight: "calc(100% - 72px)",
-    overflow: 'hidden'
+    overflow: "hidden",
+  },
+  tabs: {
+    paddingLeft: theme.spacing(2)
+  },
+  tab: {
+    minWidth: "unset",
   },
 }));
 
 const Profile = () => {
+  const [tab, setTab] = React.useState(0);
+  const [notificationsState, setNotificationState] = React.useState({
+    flowModule: false,
+    newDealsAssigned: false,
+    delasEntersOnAssignedLane: false,
+    mentions: false,
+  });
   const [stateNav, setStateNav] = useContext(NavigationContext);
   const { isProfileOpen } = stateNav;
   const classes = useStyles();
   const handleClose = () => {
     setStateNav({ ...stateNav, isProfileOpen: false });
   };
+
+  const handleTabChange = (event, newValue) => {
+    setTab(newValue);
+  };
+
+  const handleCheckboxChange = (event) => {
+    setNotificationState({
+      ...notificationsState,
+      [event.target.name]: event.target.checked,
+    });
+  }
 
   return (
     <div>
@@ -34,13 +60,99 @@ const Profile = () => {
         open={isProfileOpen}
         classes={{ paper: classes.paper }}
       >
-        {isProfileOpen && <ImageModal/>}
+        {isProfileOpen && <ImageModal />}
         <ProfileTitle />
-        <ProfileContent />
-        <ProfileActions />
+        <Tabs
+          className={classes.tabs}
+          value={tab}
+          onChange={handleTabChange}
+          indicatorColor="primary"
+          textColor="primary"
+        >
+          <Tab label="Profile" className={classes.tab} />
+          <Tab label="Notificatons" className={classes.tab} />
+        </Tabs>
+
+        <TabPanel value={tab} index={0}>
+          <ProfileContent />
+          <ProfileActions />
+        </TabPanel>
+        <TabPanel value={tab} index={1}>
+          <Box p={2}>
+            <p>Send me email notificaitons for:</p>
+            <dl>
+              <dt>
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      checked={notificationsState.flowModule}
+                      onChange={handleCheckboxChange}
+                      name="flowModule"
+                    />
+                  }
+                  label="Flow Module"
+                />
+              </dt>
+              <dd>
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      checked={notificationsState.newDealsAssigned}
+                      onChange={handleCheckboxChange}
+                      name="newDealsAssigned"
+                    />
+                  }
+                  label="New deals assigned to you"
+                />
+              </dd>
+              <dd>
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      checked={notificationsState.delasEntersOnAssignedLane}
+                      onChange={handleCheckboxChange}
+                      name="delasEntersOnAssignedLane"
+                    />
+                  }
+                  label="Deals enters lane assigned to you"
+                />
+              </dd>
+              <dt>
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      checked={notificationsState.mentions}
+                      onChange={handleCheckboxChange}
+                      name="mentions"
+                    />
+                  }
+                  label="Mentions"
+                />
+              </dt>
+            </dl>
+          </Box>
+        </TabPanel>
       </Dialog>
     </div>
   );
 };
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`wrapped-tabpanel-${index}`}
+      aria-labelledby={`wrapped-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        children
+      )}
+    </div>
+  );
+}
 
 export default Profile;
