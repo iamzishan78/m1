a2b1897a27de180a232b06c72176eeba76a01a42
iamzishan78 <zishannaseeb528@gmail.com>
2022-04-20 01:47:45 +0500

    export uni interest,  contacts and contact purchase

--- src/components/Table/Shape/UnitInterestOwnerTable.js
+++ src/components/Table/Shape/UnitInterestOwnerTable.js
@@ -1,48 +1,56 @@
 import React, { useContext, useEffect, useState } from "react";
-// context
+
 import { useHistory } from "react-router-dom";
-import { Container, Button, Tooltip, IconButton } from "@material-ui/core";
 import DeleteIcon from "@material-ui/icons/Delete";
-import { useApolloClient, useMutation } from "@apollo/client";
-import { useDispatch } from "react-redux";
+import CloudUploadIcon from "@material-ui/icons/CloudUpload";
+import { Container, Button, Tooltip, IconButton } from "@material-ui/core";
+import { useMutation } from "@apollo/client";
 
 import { AppContext } from "AppContext";
 import TableESHOC from "components/Table/TableESHOC";
 import Table from "components/Shared/M1nTable/components/Table";
 import { FEATURES } from "components/Shared/FeatureFlag/common";
 import RequestPageIcon from "components/Shared/svgIcons/request_page";
-import { execCommonAsyncExportJobAction } from "store/actions/commonActions";
 import ButtonDropDown from "components/Shared/M1nTable/components/ButtonGroup";
 import { NavigationContext } from "components/Navigation/NavigationContext";
 import FeatureFlag from "components/Shared/FeatureFlag/FeatureFlagComponent";
 import RightDialog from "components/ContactDetailCard/components/RightDialog";
+import ExportOwnersAndContacts from "components/Shared/ExportOwnerAndContacts";
 import AddUnitOwnerDialogContent from "components/Shared/M1nTable/components/SubComponents/AddUnitOwnerDialogContent";
 import BuyContactsInfoDialogContent from "components/Shared/M1nTable/components/SubComponents/BuyContactsInfoDialogContent";
 import DeleteConfirmationDialogContent from "components/Shared/M1nTable/components/SubComponents/DeleteConfirmationDialogContent";
 
-import TableHeader from 'components/Table/constants/ownersperunit-header-schema'
+import TableHeader from "components/Table/constants/ownersperunit-header-schema";
 import { UPDATEPARCELOWNER } from "graphQL/useMutationUpdateParcelOwner";
 import { deepEqualObjects, copy } from "components/Shared/functions";
 import { addTrailingZeros } from "components/Shared/functions";
 import { usetableStyles } from "../Styles";
 
 const genericDataActions = ["tags", "comments", "tracks", "ifAreContacts"];
-const interestKeys = ['working_interest', 'royalty_interest', 'orri', 'nri', 'nra'];
+const interestKeys = [
+  "working_interest",
+  "royalty_interest",
+  "orri",
+  "nri",
+  "nra",
+];
+const startPaginationAt = 25;
 
 function UnitInterestOwnerTable(props) {
   let history = useHistory();
-  const dispatch = useDispatch();
   const classes = usetableStyles();
-  const client = useApolloClient();
-  const [stateApp, setStateApp] = useContext(AppContext);
+  const [selectedRows, setSelectedRows] = useState([]);
+  const [isSelectAll, setIsSelectAll] = useState(false);
+  const [stateApp,] = useContext(AppContext);
   const [stateNav, setStateNav] = useContext(NavigationContext);
-  const { customLayer, esIndex, searchFields, clickedRow } = props;
+  const { customLayer, esIndex, clickedRow } = props;
 
   const [openCustomDialog, setOpenCustomDialog] = useState("");
   const [selectedOwner, setSelectedOwner] = useState(null);
 
   const [updateParcelOwner] = useMutation(UPDATEPARCELOWNER);
 
+  const searchFields= ["contact.entityDetail.name", "_all"];
   const appliedFilters = [
     { field: "shape._id", value: customLayer._id },
     { field: "contact.IsDeleted", value: "false" },
@@ -71,14 +79,13 @@ function UnitInterestOwnerTable(props) {
     });
   };
 
-
   useEffect(() => {
     props.setTableMeta({
-      filters: appliedFilters,
+      filters: appliedFilters || [],
       searchFields,
       TableHeader: copy(TableHeader),
       esIndex,
-      startPaginationAt: 25,
+      startPaginationAt,
       formatHits,
       defaultSort: { field: "_ts", order: "asc" },
       setAppliedFilters: props.filtersChange,
@@ -98,28 +105,6 @@ function UnitInterestOwnerTable(props) {
       setOpenCustomDialog("addOwnerToUnit");
     }
   }, [clickedRow]);
-
-  const onDownload = (e) => {
-    dispatch(
-      execCommonAsyncExportJobAction.STARTED({
-        jobType: "EXPORTCSV",
-        client,
-        setStateApp,
-        userId: stateApp.user.mongoId,
-        requestPayload: {
-          esIndex,
-          filters: [],
-          search: { query: stateApp.activitySearchQuery, fields: searchFields },
-          datasets: {
-            exportActivities: true,
-          },
-          counts: {
-            exportActivities: props.total,
-          },
-        },
-      })
-    );
-  };
 
   const getRows = () => {
     const selectedRows = [];
@@ -143,12 +128,135 @@ function UnitInterestOwnerTable(props) {
       });
     }
   };
+
+  const customOptions = {
+    customToolbar: () => {
+        const options = [
+          {
+            text: "+ ADD OWNER TO UNIT",
+            isShow: false,
+            action: () => setOpenCustomDialog("addOwnerToUnit"),
+          },
+          {
+            text: "Import Interest Owners",
+            isShow: true,
+            action: () => {
+              setStateNav((stateNav) => ({
+                ...stateNav,
+                bulkUploadFromMap: true,
+                bulkUploadParcel: stateApp.selectedParcel,
+              }));
+              history.push("/bulkupload");
+            },
+          },
+        ];
+        return (
+          <div
+            style={{
+              display: "inline",
+              float: "left",
+              marginTop: "5px",
+              marginRight: "5px",
+            }}
+          >
+            <ButtonDropDown options={options} />
+          </div>
+        );
+      },
+      customToolbarSelect: () => {
+        return (
+          <div
+            style={{
+              height: "48px",
+              display: "flex",
+            }}
+          >
+            <div
+              style={{
+                marginTop: "6px",
+                height: "35px",
+                display: "flex",
+              }}
+            >
+              <Button
+                color="secondary"
+                startIcon={<CloudUploadIcon color="white" />}
+                className={classes.multiSelectionTopBarButtons}
+                onClick={() => {
+                  let owners = [];
+                  for (let i in props.selectedRows) {
+                    owners.push(
+                      props.rows[props.selectedRows[i].dataIndex]
+                    );
+                  }
+                  setSelectedRows(owners);
+                  setOpenCustomDialog("exportOwnersAndContact");
+                }}
+              >
+                Export
+              </Button>
+              <FeatureFlag feature={FEATURES.IDICORE}>
+                <Button
+                  color="secondary"
+                  startIcon={<RequestPageIcon color="white" />}
+                  className={classes.multiSelectionTopBarButtons}
+                  disabled={props.selectedRows.length < 1}
+                  onClick={() => setOpenCustomDialog("buyContactsInfoData")}
+                >
+                  Contact Data
+                </Button>
+              </FeatureFlag>
+
+              <Tooltip title={"Delete"}>
+                <IconButton
+                  size="medium"
+                  style={{ margin: "0 5px" }}
+                  onClick={(e) => {
+                    setOpenCustomDialog("deleteOwner");
+                  }}
+                  aria-label="delete"
+                >
+                  <DeleteIcon />
+                </IconButton>
+              </Tooltip>
+            </div>
+          </div>
+        );
+      },
+      onRowSelectionChange: (
+        currentRowsSelected,
+        allRowsSelected,
+        rowsSelected
+      ) => {
+        if (
+          allRowsSelected.length === startPaginationAt ||
+          allRowsSelected.length === props.options.count
+        ) {
+          setIsSelectAll(true);
+        } else {
+          setIsSelectAll(false);
+        }
+      }
+  }
+
   return (
     <Container
       maxWidth={false}
       className={classes.container}
       id={props.id ? props.id : props.parent}
     >
+      {openCustomDialog === "exportOwnersAndContact" && (
+        <ExportOwnersAndContacts
+          onClose={() => setOpenCustomDialog("")}
+          search={props.activeSearchRef.current}
+          filters={[...props.initialFilters, ...appliedFilters]}
+          total={props.options.count}
+          isSelectAll={isSelectAll}
+          rows={selectedRows}
+          esIndex={esIndex}
+          open={true}
+        />
+      )}
       {openCustomDialog === "buyContactsInfoData" && (
         <RightDialog
           open={true}
@@ -165,16 +273,16 @@ function UnitInterestOwnerTable(props) {
       )}
       {openCustomDialog === "addOwnerToUnit" && (
         <AddUnitOwnerDialogContent
-            open={true}
-            width="450px"
-            shapeId={props.customLayer._id}
-            uAcres={props.customLayer?.shapeJson?.properties?.uAcres}
-            shapeType={props.shapeType}
-            selectedRow={selectedOwner}
-            onClose={() => {
-                setSelectedOwner(null);
-                setOpenCustomDialog("");
-            }}
+          open={true}
+          width="450px"
+          shapeId={props.customLayer._id}
+          uAcres={props.customLayer?.shapeJson?.properties?.uAcres}
+          shapeType={props.shapeType}
+          selectedRow={selectedOwner}
+          onClose={() => {
+            setSelectedOwner(null);
+            setOpenCustomDialog("");
+          }}
         />
       )}
       {openCustomDialog === "deleteOwner" && (
@@ -211,82 +319,7 @@ function UnitInterestOwnerTable(props) {
         onTableChange={props.onTableChange}
         options={{
           ...props.options,
-          customToolbar: () => {
-            const options = [
-              {
-                text: "+ ADD OWNER TO UNIT",
-                isShow: false,
-                action: () => setOpenCustomDialog("addOwnerToUnit"),
-              },
-              {
-                text: "Import Interest Owners",
-                isShow: true,
-                action: () => {
-                  setStateNav((stateNav) => ({
-                    ...stateNav,
-                    bulkUploadFromMap: true,
-                    bulkUploadParcel: stateApp.selectedParcel,
-                  }));
-                  history.push("/bulkupload");
-                },
-              },
-            ];
-            return (
-              <div
-                style={{
-                  display: "inline",
-                  float: "left",
-                  marginTop: "5px",
-                  marginRight: "5px",
-                }}
-              >
-                <ButtonDropDown options={options} />
-              </div>
-            );
-          },
-          customToolbarSelect: () => {
-            return (
-              <div
-                style={{
-                  height: "48px",
-                  display: "flex",
-                }}
-              >
-                <div
-                  style={{
-                    marginTop: "6px",
-                    height: "35px",
-                    display: "flex",
-                  }}
-                >
-                  <FeatureFlag feature={FEATURES.IDICORE}>
-                    <Button
-                      color="secondary"
-                      startIcon={<RequestPageIcon color="white" />}
-                      className={classes.multiSelectionTopBarButtons}
-                      disabled={props.selectedRows.length < 1}
-                      onClick={() => setOpenCustomDialog("buyContactsInfoData")}
-                    >
-                      Contact Data
-                    </Button>
-                  </FeatureFlag>
-
-                  <Tooltip title={"Delete"}>
-                    <IconButton
-                      size="medium"
-                      style={{ margin: "0 5px" }}
-                      onClick={(e) => {
-                        setOpenCustomDialog("deleteOwner");
-                      }}
-                      aria-label="delete"
-                    >
-                      <DeleteIcon />
-                    </IconButton>
-                  </Tooltip>
-                </div>
-              </div>
-            );
-          },
+          ...customOptions
         }}
         parent={props.parent}
         setColumnsBase={[]}
@@ -295,7 +328,4 @@ function UnitInterestOwnerTable(props) {
   );
 }
 
-export default React.memo(
-  TableESHOC(UnitInterestOwnerTable),
-  deepEqualObjects
-);
+export default React.memo(TableESHOC(UnitInterestOwnerTable), deepEqualObjects);
