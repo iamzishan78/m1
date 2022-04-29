fc5861d22342a725e4d1c201866b79e6a922f010
iamzishan78 <zishannaseeb528@gmail.com>
2022-04-29 17:23:46 +0530

    Store notification preferences to BE.

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,14 +1,13 @@
-import Dialog from "@material-ui/core/Dialog";
-import { Tabs, Tab, Checkbox, FormControlLabel, Box } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
 import React, { useContext } from "react";
-import { useHistory } from "react-router-dom";
+import Dialog from "@material-ui/core/Dialog";
+import { Tabs, Tab } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 import { NavigationContext } from "../Navigation/NavigationContext";
 import ImageModal from "./ImageModal";
 import ProfileActions from "./ProfileActions";
 import ProfileContent from "./ProfileContent";
 import ProfileTitle from "./ProfileTitle";
-import { TramOutlined, TrendingUpOutlined } from "@material-ui/icons";
+import NotificationSettings from "./NotificationSettings";
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -24,24 +23,14 @@ const useStyles = makeStyles((theme) => ({
   tab: {
     minWidth: "unset",
   },
-  boldCheckboxLabel: {
-    "& > .MuiFormControlLabel-label": {
-      fontWeight: "bold",
-    },
-  },
 }));
 
 const Profile = () => {
   const [tab, setTab] = React.useState(0);
-  const [notificationsState, setNotificationState] = React.useState({
-    flowModule: false,
-    newDealsAssigned: false,
-    delasEntersOnAssignedLane: false,
-    mentions: false,
-  });
   const [stateNav, setStateNav] = useContext(NavigationContext);
   const { isProfileOpen } = stateNav;
   const classes = useStyles();
+
   const handleClose = () => {
     setStateNav({ ...stateNav, isProfileOpen: false });
   };
@@ -49,23 +38,6 @@ const Profile = () => {
   const handleTabChange = (event, newValue) => {
     setTab(newValue);
   };
-
-  const handleCheckboxChange = (event) => {
-    const { name, checked } = event.target;
-    const setObj = {
-      [name]: checked
-    }
-
-    if (name === "flowModule"){
-      setObj.newDealsAssigned = true;
-      setObj.delasEntersOnAssignedLane = true;
-    }
-
-    setNotificationState({
-      ...notificationsState,
-      ...setObj
-    });
-  }
 
   return (
     <div>
@@ -90,65 +62,11 @@ const Profile = () => {
 
         <TabPanel value={tab} index={0}>
           <ProfileContent />
-          <ProfileActions />
         </TabPanel>
         <TabPanel value={tab} index={1}>
-          <Box p={2}>
-            <p>Send me email notificaitons for:</p>
-            <dl>
-              <dt>
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      checked={notificationsState.flowModule}
-                      onChange={handleCheckboxChange}
-                      name="flowModule"
-                    />
-                  }
-                  className={classes.boldCheckboxLabel}
-                  label="Flow Module"
-                />
-              </dt>
-              <dd>
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      checked={notificationsState.newDealsAssigned}
-                      onChange={handleCheckboxChange}
-                      name="newDealsAssigned"
-                    />
-                  }
-                  label="New deals assigned to you"
-                />
-              </dd>
-              <dd>
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      checked={notificationsState.delasEntersOnAssignedLane}
-                      onChange={handleCheckboxChange}
-                      name="delasEntersOnAssignedLane"
-                    />
-                  }
-                  label="Deals enters lane assigned to you"
-                />
-              </dd>
-              <dt>
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      checked={notificationsState.mentions}
-                      onChange={handleCheckboxChange}
-                      name="mentions"
-                    />
-                  }
-                  className={classes.boldCheckboxLabel}
-                  label="Mentions"
-                />
-              </dt>
-            </dl>
-          </Box>
+          <NotificationSettings />
         </TabPanel>
+        <ProfileActions />
       </Dialog>
     </div>
   );
