cdc97115cc65f25778d9f8d887a0e57ebc9028e3
iamzishan78 <zishannaseeb528@gmail.com>
2022-05-31 16:18:11 +0500

    tract and unit api call from frontend commented

--- src/components/BulkUpload/components/M1neralHeaders.js
+++ src/components/BulkUpload/components/M1neralHeaders.js
@@ -12,7 +12,7 @@ import { AppContext } from "../../../AppContext";
 import { anyToDate } from "@amcharts/amcharts4/.internal/core/utils/Utils";
 // queries
 import { useApolloClient } from "@apollo/client";
-import { GET_ES_SIMPLE_SEARCH } from "graphQL/useQueryESSimpleSearch";
+// import { GET_ES_SIMPLE_SEARCH } from "graphQL/useQueryESSimpleSearch";
 
 const useStyles = makeStyles({
   root: {
@@ -219,102 +219,102 @@ export default function M1neralHeaders(props) {
         }
       }
 
-      if (['TRACTS'].includes(stateApp.jobType) &&
-        (!return_obj["landgrid._id"] ||
-          !return_obj["landgrid.name"])) {
-        const { data: landGridGeoms } = await client.query({
-          query: GET_ES_SIMPLE_SEARCH,
-          variables: {
-            index: "platformData:landgrid",
-            filters: [
-              ...[
-                "landgrid.level1Type.State",
-                "landgrid.level2Type.County",
-                "landgrid.level3Type.Survey",
-                "landgrid.level4Type.Block",
-                "landgrid.level5Type.Section",
-                "landgrid.level6Type.Abstract",
-                "landgrid.level3Type.Meridian",
-                "landgrid.level5Type.TownshipRange",
-                "landgrid.level6Type.Section"
-              ].flatMap((key) => {
-                const keyParts = key.split(".");
-                return (return_obj[key]) ?
-                  [
-                    { field: `${keyParts[1]}.keyword`, value: keyParts[2] },
-                    { field: `${keyParts[1].replace("Type", "Name")}.keyword`, value: return_obj[key] }
-                  ] :
-                  []
-              }),
-              { field: "level7Id.keyword", value: undefined },
-              { field: "level8Id.keyword", value: undefined },
-              { field: "level9Id.keyword", value: undefined },
-              { field: "level10Id.keyword", value: undefined }
-            ],
-            sort: [],
-          }
-        });
+      // if (['TRACTS'].includes(stateApp.jobType) &&
+      //   (!return_obj["landgrid._id"] ||
+      //     !return_obj["landgrid.name"])) {
+      //   const { data: landGridGeoms } = await client.query({
+      //     query: GET_ES_SIMPLE_SEARCH,
+      //     variables: {
+      //       index: "platformData:landgrid",
+      //       filters: [
+      //         ...[
+      //           "landgrid.level1Type.State",
+      //           "landgrid.level2Type.County",
+      //           "landgrid.level3Type.Survey",
+      //           "landgrid.level4Type.Block",
+      //           "landgrid.level5Type.Section",
+      //           "landgrid.level6Type.Abstract",
+      //           "landgrid.level3Type.Meridian",
+      //           "landgrid.level5Type.TownshipRange",
+      //           "landgrid.level6Type.Section"
+      //         ].flatMap((key) => {
+      //           const keyParts = key.split(".");
+      //           return (return_obj[key]) ?
+      //             [
+      //               { field: `${keyParts[1]}.keyword`, value: keyParts[2] },
+      //               { field: `${keyParts[1].replace("Type", "Name")}.keyword`, value: return_obj[key] }
+      //             ] :
+      //             []
+      //         }),
+      //         { field: "level7Id.keyword", value: undefined },
+      //         { field: "level8Id.keyword", value: undefined },
+      //         { field: "level9Id.keyword", value: undefined },
+      //         { field: "level10Id.keyword", value: undefined }
+      //       ],
+      //       sort: [],
+      //     }
+      //   });
 
-        if (landGridGeoms?.getESSimpleSearch?.total > 0) {
-          return_obj["landgrid._id"] = landGridGeoms?.getESSimpleSearch?.hits?.[0]?._id;
-          return_obj["landgrid.name"] = landGridGeoms?.getESSimpleSearch?.hits?.[0]?.level6Name;
-        }
+      //   if (landGridGeoms?.getESSimpleSearch?.total > 0) {
+      //     return_obj["landgrid._id"] = landGridGeoms?.getESSimpleSearch?.hits?.[0]?._id;
+      //     return_obj["landgrid.name"] = landGridGeoms?.getESSimpleSearch?.hits?.[0]?.level6Name;
+      //   }
 
-        if (!return_obj["landgrid._id"] ||
-          !return_obj["landgrid.name"]) {
-          filtered_data_to_send.push(null)
-          continue;
-        }
-      }
+      //   if (!return_obj["landgrid._id"] ||
+      //     !return_obj["landgrid.name"]) {
+      //     filtered_data_to_send.push(null)
+      //     continue;
+      //   }
+      // }
 
 
-      if (['UNITS'].includes(stateApp.jobType) &&
-        (!return_obj["landgrid._id"] ||
-          !return_obj["landgrid.name"])) {
-        const { data: landGridGeoms } = await client.query({
-          query: GET_ES_SIMPLE_SEARCH,
-          variables: {
-            index: "platformData:landgrid",
-            filters: [
-              ...[
-                "landgrid.level1Type.State",
-                "landgrid.level2Type.County",
-                "landgrid.level3Type.Survey",
-                "landgrid.level4Type.Block",
-                "landgrid.level5Type.Section",
-                "landgrid.level6Type.Abstract",
-                "landgrid.level3Type.Meridian",
-                "landgrid.level5Type.TownshipRange",
-                "landgrid.level6Type.Section"
-              ].flatMap((key) => {
-                const keyParts = key.split(".");
-                return (return_obj[key]) ?
-                  [
-                    { field: `${keyParts[1]}.keyword`, value: keyParts[2] },
-                    { field: `${keyParts[1].replace("Type", "Name")}.keyword`, value: return_obj[key] }
-                  ] :
-                  []
-              }),
-              { field: "level7Id.keyword", value: undefined },
-              { field: "level8Id.keyword", value: undefined },
-              { field: "level9Id.keyword", value: undefined },
-              { field: "level10Id.keyword", value: undefined }
-            ],
-            sort: [],
-          }
-        });
+      // if (['UNITS'].includes(stateApp.jobType) &&
+      //   (!return_obj["landgrid._id"] ||
+      //     !return_obj["landgrid.name"])) {
+      //   const { data: landGridGeoms } = await client.query({
+      //     query: GET_ES_SIMPLE_SEARCH,
+      //     variables: {
+      //       index: "platformData:landgrid",
+      //       filters: [
+      //         ...[
+      //           "landgrid.level1Type.State",
+      //           "landgrid.level2Type.County",
+      //           "landgrid.level3Type.Survey",
+      //           "landgrid.level4Type.Block",
+      //           "landgrid.level5Type.Section",
+      //           "landgrid.level6Type.Abstract",
+      //           "landgrid.level3Type.Meridian",
+      //           "landgrid.level5Type.TownshipRange",
+      //           "landgrid.level6Type.Section"
+      //         ].flatMap((key) => {
+      //           const keyParts = key.split(".");
+      //           return (return_obj[key]) ?
+      //             [
+      //               { field: `${keyParts[1]}.keyword`, value: keyParts[2] },
+      //               { field: `${keyParts[1].replace("Type", "Name")}.keyword`, value: return_obj[key] }
+      //             ] :
+      //             []
+      //         }),
+      //         { field: "level7Id.keyword", value: undefined },
+      //         { field: "level8Id.keyword", value: undefined },
+      //         { field: "level9Id.keyword", value: undefined },
+      //         { field: "level10Id.keyword", value: undefined }
+      //       ],
+      //       sort: [],
+      //     }
+      //   });
 
-        if (landGridGeoms?.getESSimpleSearch?.total > 0) {
-          return_obj["landgrid._id"] = landGridGeoms?.getESSimpleSearch?.hits?.[0]?._id;
-          return_obj["landgrid.name"] = landGridGeoms?.getESSimpleSearch?.hits?.[0]?.level6Name;
-        }
+      //   if (landGridGeoms?.getESSimpleSearch?.total > 0) {
+      //     return_obj["landgrid._id"] = landGridGeoms?.getESSimpleSearch?.hits?.[0]?._id;
+      //     return_obj["landgrid.name"] = landGridGeoms?.getESSimpleSearch?.hits?.[0]?.level6Name;
+      //   }
 
-        if (!return_obj["landgrid._id"] ||
-          !return_obj["landgrid.name"]) {
-          filtered_data_to_send.push(null)
-          continue;
-        }
-      }
+      //   if (!return_obj["landgrid._id"] ||
+      //     !return_obj["landgrid.name"]) {
+      //     filtered_data_to_send.push(null)
+      //     continue;
+      //   }
+      // }
 
       // if ('CONTACTSWELLINTEREST' === stateApp.jobType &&
       //   (return_obj["well.globalWell"] ||
