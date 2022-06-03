f227bc0998ba31a65d79abdaee2d35f865f53971
iamzishan78 <zishannaseeb528@gmail.com>
2022-06-03 18:01:51 +0530

    Worked on dashboard my tasks

---
+++ src/components/Dashboard/components/Tasks.js
@@ -0,0 +1,282 @@
+import { Grid } from "@material-ui/core";
+import CardHeader from "@material-ui/core/CardHeader";
+import IconButton from "@material-ui/core/IconButton";
+import List from "@material-ui/core/List";
+import Paper from "@material-ui/core/Paper";
+import { useLazyQuery, useMutation } from "@apollo/client";
+import { makeStyles } from "@material-ui/core/styles";
+import DragIndicatorOutlinedIcon from "@material-ui/icons/DragIndicatorOutlined";
+import React, { Fragment, useEffect, useState } from "react";
+import { sortableHandle } from "react-sortable-hoc";
+import Tooltip from "@material-ui/core/Tooltip";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import { CircularProgress } from "@material-ui/core";
+import CheckCircleIcon from "components/Shared/svgIcons/CheckCircleIcon";
+import EventCalendarIcon from "components/Shared/svgIcons/EventCalendarIcon";
+import { GETALLACTIVITIES } from "../../../graphQL/useQueryGetAllActivities";
+import CallIcon from "@material-ui/icons/Call";
+import MeetingIcon from "@material-ui/icons/Group";
+import TaskIcon from "@material-ui/icons/WatchLater";
+import DeadlineIcon from "@material-ui/icons/Flag";
+import EmailIcon from "@material-ui/icons/Email";
+import ContactMailIcon from "@material-ui/icons/ContactMail";
+import DefaultIcon from "@material-ui/icons/Event";
+import moment from "moment";
+import { useHistory } from "react-router-dom";
+import { UPDATEACTIVITY } from "../../../graphQL/useMutationActivity";
+
+const useStyles = makeStyles((theme) => ({
+  header: {
+    padding: "8px 8px 0 16px",
+    backgroundColor: "#FFFFF",
+    color: "black",
+  },
+  listitem: {
+    padding: "10px",
+    backgroundColor: "#F6F8F9",
+    "&:hover": {
+      backgroundColor: "#DDDFE0",
+    },
+    "& .MuiFormControl-marginDense": {
+      margin: "0px !important",
+    },
+    "& .MuiIconButton-root": {
+      padding: "10px !important",
+    },
+    "& .MuiPaper-elevation1": {
+      boxShadow: "none !important",
+    },
+  },
+  thumb: {
+    height: "16px",
+    maxWidth: "16px",
+  },
+  source: {
+    fontSize: "12px",
+    marginLeft: "0px",
+  },
+  title: {
+    fontSize: "15px",
+    marginLeft: "10px",
+    fontWeight: "bold",
+    textDecoration: "none",
+    color: "black",
+    cursor: "pointer",
+    display: "flex",
+    "& svg": {
+      color: "#000000",
+      marginRight: "7px",
+    },
+    "&:hover": {
+      textDecoration: "underline",
+    },
+  },
+  content: {
+    fontSize: "12px",
+    marginBottom: "2px",
+  },
+  date: {
+    fontSize: "10px",
+  },
+  paper: {
+    margin: "12px 8px",
+    cursor: "pointer",
+    boxShadow: 'none !important'
+  },
+  image: {
+    maxHeight: "72px",
+    maxWidth: "65px !important",
+    borderRadius: "4px",
+  },
+  gridStyle: {
+    padding: "8px 0px",
+  },
+  paddingLeft10: {
+    paddingLeft: "10px !important",
+    paddingTop: "3px !important",
+  },
+  bold: {
+    fontWeight: "bold",
+  },
+  commentTime: {
+    marginLeft: "10px",
+    fontSize: "12px",
+  },
+  sysNotification: {
+    marginLeft: 0,
+  },
+  customTabs: {
+    float: "right",
+    paddingRight: "30px",
+    "& .MuiTab-root": {
+      minWidth: "60px",
+    },
+    "& .Mui-selected": {
+      color: "#18AADD",
+    },
+  },
+}));
+
+const DragHandle = sortableHandle(() => (
+  <IconButton aria-label="drag">
+    <DragIndicatorOutlinedIcon fontSize="default" htmlColor="#808080" />
+  </IconButton>
+));
+
+const activityIcons = {
+  call: <CallIcon />,
+  meeting: <MeetingIcon />,
+  task: <TaskIcon />,
+  deadline: <DeadlineIcon />,
+  email: <EmailIcon />,
+  mailer: <ContactMailIcon />,
+};
+
+const Tasks = () => {
+  const history = useHistory();
+  const [getAllActivities, { data, loading }] = useLazyQuery(GETALLACTIVITIES, {
+    fetchPolicy: `network-only`,
+  });
+  const [updateActivityMutation] = useMutation(
+    UPDATEACTIVITY,
+    {
+      refetchQueries: ["getAllActivities"],
+      awaitRefetchQueries: true,
+    }
+  );
+  const classes = useStyles();
+  const [tab, setTab] = useState(0);
+
+  useEffect(() => {
+    getAllActivities({
+      variables: {
+        category: "CRM",
+      },
+    });
+  }, []);
+
+  const completeActivity = async (activityData) => {
+    await updateActivityMutation({
+      variables: {
+        activity: {
+          _id: activityData._id,
+          isClosed: true,
+        },
+      },
+    });
+  };
+
+  const Title = () => {
+    return (
+      <Grid container className={classes.gridStyle}>
+        <Grid item xs={6}>
+          <div>My Tasks</div>
+        </Grid>
+        <Grid item xs={6}>
+          <div className={classes.customTabs}>
+            <Tabs
+              value={tab}
+              textColor="primary"
+              onChange={(e, newValue) => {
+                setTab(newValue);
+              }}
+            >
+              <Tab label="Upcoming" />
+              <Tab label="Overdue" />
+            </Tabs>
+          </div>
+        </Grid>
+      </Grid>
+    );
+  };
+  return (
+    <Fragment>
+      <CardHeader
+        // action={<DragHandle />}
+        title={<Title />}
+        className={classes.header}
+      />
+      {loading ? (
+        <CircularProgress
+          className={classes.progress}
+          size={80}
+          disableShrink
+          color="secondary"
+        ></CircularProgress>
+      ) : (
+        <List style={{ maxHeight: "calc(100% - 48px)", overflow: "auto" }}>
+          {data &&
+            data.activities &&
+            Array.isArray(data.activities) &&
+            data.activities
+              .filter((activity) => !activity.isClosed)
+              .map((activity, i) => {
+                return (
+                  <Paper key={i} className={classes.paper}>
+                    <Grid
+                      container
+                      direction="row"
+                      justify="space-between"
+                      alignItems="center"
+                      className={classes.listitem}
+                      spacing={1}
+                    >
+                      <Grid item xs={10} zeroMinWidth>
+                        <span
+                          className={classes.title}
+                          onClick={() =>
+                            history.push(`/calendar/activities/${activity._id}`)
+                          }
+                        >
+                          {activityIcons[activity.type] || <DefaultIcon />}
+                          {activity?.name || "N/A"}
+                        </span>
+                        <Grid container className={classes.gridStyle}>
+                          <Grid item xs={11} className={classes.paddingLeft10}>
+                            <span>
+                              Contact: {activity?.contactName || "N/A"}
+                            </span>
+                            <br />
+                            <span>
+                              Date:{" "}
+                              {activity.dateTime
+                                ? moment
+                                    .parseZone(new Date(+activity.dateTime))
+                                    .format("mm/DD/YYYY hh:mm:ssa")
+                                : "N/A"}
+                            </span>
+                          </Grid>
+                        </Grid>
+                      </Grid>
+                      <Grid item xs={2} style={{ textAlign: "-webkit-center" }}>
+                        <Tooltip title="Archive notification">
+                          <IconButton
+                            onClick={(e) => {
+                              e.stopPropagation();
+                            }}
+                          >
+                            <EventCalendarIcon />
+                          </IconButton>
+                        </Tooltip>
+                        <Tooltip title="Mark as unread">
+                          <IconButton
+                            onClick={(e) => {
+                              e.stopPropagation();
+                              completeActivity(activity);
+                            }}
+                          >
+                            <CheckCircleIcon />
+                          </IconButton>
+                        </Tooltip>
+                      </Grid>
+                    </Grid>
+                  </Paper>
+                );
+              })}
+        </List>
+      )}
+    </Fragment>
+  );
+};
+export default Tasks;
