92fb1622e0052b97cfd421fd73595c3f1f8f7b8f
iamzishan78 <zishannaseeb528@gmail.com>
2022-06-13 11:48:16 +0500

    internal company autocomplete added for land analyst screen

--- src/components/Shared/Agreement/helpers.js
+++ src/components/Shared/Agreement/helpers.js
@@ -1,3 +1,4 @@
+import hr from "date-fns/esm/locale/hr/index.js";
 import { get } from "lodash";
 
 export const getCustomMetaFields = (agreementDetails, metaDataRes) => {
@@ -20,6 +21,10 @@ export const getCustomMetaFields = (agreementDetails, metaDataRes) => {
     //? Meta data which is attached to this agreement
     metaData.forEach(md => {
         const { isCustom, ...meta } = md;
+        // Checking if meta data key exists in agreement detail
+        // then it should be ignored from meta data
+        // e.g. internal_company
+        if (md.name.replace(/[^a-zA-Z0-9]+(.)/g, (m, chr) => chr.toUpperCase()) in agreementDetails) return;
         if (md.name in get(agreementDetails, "custom_data", [])) {
             attachedMetaData.push(meta);
         } else {
