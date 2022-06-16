d721d06f7c3ac4a67498018ce84e474edd69cb50
iamzishan78 <zishannaseeb528@gmail.com>
2022-06-16 23:24:16 +0500

    unused fragment removed

--- src/components/Document/Document.js
+++ src/components/Document/Document.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, Fragment } from "react";
+import React, { useContext, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 import { isEmpty } from "lodash";
