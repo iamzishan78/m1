c8d5776a9382e4babce7be3f2ff7592b123f1746
iamzishan78 <zishannaseeb528@gmail.com>
2022-06-22 13:38:39 +0500

    agreement breadcrumbs removed when launnched from snapgrid

--- src/components/Table/constants/agreements-header-schema.js
+++ src/components/Table/constants/agreements-header-schema.js
@@ -45,7 +45,7 @@ const AgreementsHeadCells = (isSnapGrid = false) => [
               e.stopPropagation();
               if (isSnapGrid)
                 history.push(`/map/${tableMeta.rowData[18]}s/${tableMeta.rowData[0]}`,
-                  { showAgreementBreadcrumb: true }
+                  { showAgreementBreadcrumb: false }
                 );
               else
                 history.push(`/land/agreement/details/${tableMeta.rowData[0]}`,
