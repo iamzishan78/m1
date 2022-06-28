286db2c9993874a86ddcf6778049800eada820b3
iamzishan78 <zishannaseeb528@gmail.com>
2022-06-28 15:37:07 +0500

    contact detailed selector icons updated

--- src/components/ContactDetailCard/components/ContactDetailSelector/data.js
+++ src/components/ContactDetailCard/components/ContactDetailSelector/data.js
@@ -1,22 +1,19 @@
-// import PersonIcon from "@material-ui/icons/Person";
-// import OwnershipIcon from "components/Shared/svgIcons/ownership";
 import LeaseIcon from "components/Shared/svgIcons/lease";
 import WellIcon from "components/Shared/svgIcons/well";
-import TractIcon from "components/Shared/svgIcons/tract";
 import UnitIcon from "components/Shared/svgIcons/unit";
-import FolderIcon from "@material-ui/icons/Folder";
-import OperatorIcon from "components/Shared/svgIcons/operator";
 import TaxOwnerIcon from "@material-ui/icons/AccountBalance";
-import ContactIcon from "@material-ui/icons/Group";
-import LayersIcon from "@material-ui/icons/Layers";
+import ContactInformationIcon from '@material-ui/icons/ContactPhone';
+import DocumentsIcon from "@material-ui/icons/DescriptionOutlined";
+import DealsIcon from "@material-ui/icons/MonetizationOn";
+import ActivitiesIcon from '@material-ui/icons/Event';
 
 export const contactDetailInitialData = [
-  { index: 4, value: "unitInterests", Icon: LeaseIcon, label: "Unit Interests" },
-  { index: 3, value: "wellInterests", Icon: LayersIcon, label: "Well Interests" },
+  { index: 4, value: "unitInterests", Icon: UnitIcon, label: "Unit Interests" },
+  { index: 3, value: "wellInterests", Icon: WellIcon, label: "Well Interests" },
   { index: 5, value: "parcelInterests", Icon: LeaseIcon, label: "Tract Interests" },
-  { index: 2, value: "taxRollInterests", Icon: OperatorIcon, label: "Tax Roll Interests" },
-  { index: 1, value: "activities", Icon: TaxOwnerIcon, label: "Activities" },
-  { index: 0, value: "contactInformation", Icon: WellIcon, label: "Contact Info" },
-  { index: 7, value: "documents", Icon: ContactIcon, label: "Documents" },
-  { index: 6, value: "deals", Icon: ContactIcon, label: "Deals" },
+  { index: 2, value: "taxRollInterests", Icon: TaxOwnerIcon, label: "Tax Roll Interests" },
+  { index: 1, value: "activities", Icon: ActivitiesIcon, label: "Activities" },
+  { index: 0, value: "contactInformation", Icon: ContactInformationIcon, label: "Contact Info" },
+  { index: 7, value: "documents", Icon: DocumentsIcon, label: "Documents" },
+  { index: 6, value: "deals", Icon: DealsIcon, label: "Deals" },
 ];
