d1e80c622ac581066b591ccc2912b6b942922efa
iamzishan78 <zishannaseeb528@gmail.com>
2022-07-03 15:29:10 -0500

    fix documents sticky column right border

--- src/components/Shared/M1nTable/components/Table.js
+++ src/components/Shared/M1nTable/components/Table.js
@@ -2077,7 +2077,9 @@ function SubTable(props) {
                   const uri = row_line?.fileUrl;
 
                   return (
-                    <div style={{ minWidth: 400 }}>
+                    <div style={{ minWidth: 400,
+                      boxShadow: 'inset -1px 0px 0px 0px lightgrey',
+                    }}>
                       <Grid container spacing={0} direction="row" >
                         {
                           props.parent === 'Documents' && <div style={{ position: 'relative', zIndex: 100 }}>
