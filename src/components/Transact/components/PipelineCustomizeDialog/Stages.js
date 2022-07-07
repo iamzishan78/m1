510058600d06ac6183afa29445c52cd936759d7f
iamzishan78 <zishannaseeb528@gmail.com>
2022-07-07 11:06:09 -0500

    spelling

--- src/components/Transact/components/PipelineCustomizeDialog/Stages.js
+++ src/components/Transact/components/PipelineCustomizeDialog/Stages.js
@@ -236,6 +236,7 @@ export default function LanesInfoPanel({
 
   const handleCloseDeleteDialog = () => setDeleteDialogOpen(false);
   const handleStagesSave = (stageIndex) => {
+    // console.log('DOG BRAIN', stageIndex)
     handleSaveOrUpdate();
     setStageIndex(stageIndex);
   };
@@ -338,6 +339,7 @@ export default function LanesInfoPanel({
                                     />
                                   </TableCell>
 
+                                  {/* {console.log('DOG BRAIN', index)} */}
                                   <TableCell padding="checkbox">
                                     <Tooltip title="Stage Details" placement="top">
                                       <DetailsIcon onClick={() => handleStagesSave(index)} className={classes.settingIcon} />
