f2d2c281fe6020c458d87a0ca65c733d30c202b2
iamzishan78 <zishannaseeb528@gmail.com>
2022-07-22 19:40:36 +0500

    gridview _id set to false for api

--- src/components/Table/TableESHOC.js
+++ src/components/Table/TableESHOC.js
@@ -676,8 +676,12 @@ export const TableESHOC = (Component, shouldGridViewSort = true) => {
         }
 
         const updateGridViewRedux = (tableState) => {
+            debugger
             setTableMeta((tableMeta) => {
-                if (tableMeta?.selectedGridView)
+                if (tableMeta?.selectedGridView) {
+                    if (isFiniteScroll)
+                        tableState.columns[0].display = false
+
                     dispatch(updateUserGridViewSettingAction.STARTED({
                         userGridViewSetting: {
                             module: tableMeta?.typeKeyword?.gridViewCategory,
@@ -689,6 +693,7 @@ export const TableESHOC = (Component, shouldGridViewSort = true) => {
                             user: stateApp.user?.mongoId,
                         }
                     }));
+                }
                 let filters = activeFiltersRef.current;
                 if (props.targetLabel === "well") {
                     filters = filters.filter(f => f.type !== "geo_intersects");
