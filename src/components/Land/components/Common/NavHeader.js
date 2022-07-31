89c91b49da521acc93a1fd6a5e355ac7cb274fb5
iamzishan78 <zishannaseeb528@gmail.com>
2022-07-31 22:09:02 +0500

    campaign header take back fixed

--- src/components/Land/components/Common/NavHeader.js
+++ src/components/Land/components/Common/NavHeader.js
@@ -10,63 +10,63 @@ import Link from "@material-ui/core/Link";
 import ProfileMenu from "components/Profile/ProfileMenu";
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        minHeight: "100vh",
-        backgroundColor: "#f3f3f3",
-        width: "100%",
-    },
-    navSection: {
-        minHeight: 56,
-        padding: "10px 20px",
-        backgroundColor: "#fff",
-    },
-    title: {
-        color: "#18AADD",
-        fontSize: "16px",
-        marginLeft: "5px",
-        cursor: "pointer"
-    }
+  root: {
+    minHeight: "100vh",
+    backgroundColor: "#f3f3f3",
+    width: "100%",
+  },
+  navSection: {
+    minHeight: 56,
+    padding: "10px 20px",
+    backgroundColor: "#fff",
+  },
+  title: {
+    color: "#18AADD",
+    fontSize: "16px",
+    marginLeft: "5px",
+    cursor: "pointer",
+  },
 }));
 
 export default function DetailComponents(props) {
-    const history = useHistory();
-    const classes = useStyles(props);
-    const { title, onClickFunc } = props;
+  const history = useHistory();
+  const classes = useStyles(props);
+  const { title, onClickFunc } = props;
 
-    const { activeModule } = useSelector(({ common }) => common);
+  const { activeModule } = useSelector(({ common }) => common);
 
-    return (
-        <div className={classes.root}>
-            {/**
+  return (
+    <div className={classes.root}>
+      {/**
        * Detail Header
        */}
-            <div className={classes.navSection}>
-                <Grid container alignItems="center" direction="row" display="flex" justify="space-between">
-                    <Grid item>
-                        <Breadcrumbs separator={<NavigateNextIcon fontSize="small" />} aria-label="breadcrumb">
-                            <Link
-                                style={{ marginLeft: "5px", fontSize: "16px", cursor: "pointer", fontWeight: "bold" }}
-                                color="inherit"
-                                onClick={() => history.push(activeModule.link)}
-                            >
-                                {activeModule.title}
-                            </Link>
-                            <Typography
-                                className={classes.title}
-                                onClick={() => {
-                                    if (onClickFunc) onClickFunc();
-                                }}
-                            >
-                                {title}
-                            </Typography>
-                        </Breadcrumbs>
-                    </Grid>
-                    <Grid item>
-                        <ProfileMenu />
-                    </Grid>
-                </Grid>
-            </div>
-            {props.children}
-        </div>
-    );
+      <div className={classes.navSection}>
+        <Grid container alignItems="center" direction="row" display="flex" justify="space-between">
+          <Grid item>
+            <Breadcrumbs separator={<NavigateNextIcon fontSize="small" />} aria-label="breadcrumb">
+              <Link
+                style={{ marginLeft: "5px", fontSize: "16px", cursor: "pointer", fontWeight: "bold" }}
+                color="inherit"
+                onClick={() => history.push("/contacts/campaigns")}
+              >
+                {activeModule.title}
+              </Link>
+              <Typography
+                className={classes.title}
+                onClick={() => {
+                  if (onClickFunc) onClickFunc();
+                }}
+              >
+                {title}
+              </Typography>
+            </Breadcrumbs>
+          </Grid>
+          <Grid item>
+            <ProfileMenu />
+          </Grid>
+        </Grid>
+      </div>
+      {props.children}
+    </div>
+  );
 }
