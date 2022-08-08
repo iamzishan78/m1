998abdfe3296394c71a29d5198fa6b5c415d6c37
iamzishan78 <zishannaseeb528@gmail.com>
2022-08-08 23:02:15 +0530

    Show comment when Unit Pricing is updated

--- src/components/Shared/CommentComponent.js
+++ src/components/Shared/CommentComponent.js
@@ -437,13 +437,13 @@ export default function CommentComponent(props) {
                       >
                         <Grid item style={{ maxWidth: "55px", padding: "0px" }}>
                           <IconButton>
-                            {profilesInfo[eachComment.user.email]?.profileImage ||
+                            {profilesInfo[eachComment?.user?.email]?.profileImage ||
                               eachComment.isNew ? (
                               <Avatar
                                 src={
                                   eachComment.isNew
                                     ? profileImage
-                                    : profilesInfo[eachComment.user.email]
+                                    : profilesInfo[eachComment?.user?.email]
                                       .profileImage
                                 }
                                 size="38"
@@ -451,7 +451,7 @@ export default function CommentComponent(props) {
                               />
                             ) : (
                               <Avatar
-                                name={eachComment.user.name}
+                                name={eachComment?.user?.name}
                                 size="38"
                                 round
                               />
@@ -461,7 +461,7 @@ export default function CommentComponent(props) {
                         <Grid item className={`${classes.paddingLeft10} ${classes.commentContent}`}>
                           <div>
                             <span className={classes.bold}>
-                              {eachComment.user.name}
+                              {eachComment?.user?.name}
                             </span>
                             {
                               <ReactTimeAgo
@@ -476,7 +476,7 @@ export default function CommentComponent(props) {
                               </span>
                             )}
                             {
-                              eachComment.user.email === stateApp.user.email &&
+                              eachComment?.user?.email === stateApp.user.email &&
                               showCommentActionId === eachComment._id &&
                               editCommentId !== eachComment._id && (
                                 <div
