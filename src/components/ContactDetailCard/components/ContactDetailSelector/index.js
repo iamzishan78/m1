fa07323778860d9882677ff516f94842b5fbfd32
iamzishan78 <zishannaseeb528@gmail.com>
2022-08-10 17:56:36 +0500

    contact detail basic table static header

--- src/components/ContactDetailCard/components/ContactDetailSelector/index.js
+++ src/components/ContactDetailCard/components/ContactDetailSelector/index.js
@@ -216,7 +216,7 @@ function MapGridCard(props) {
                   </List>
                 </Grid>
 
-                <Grid item md={10} style={{ padding: "0px 0px", maxHeight: "49.25vh", overflow: "overlay" }}>
+                <Grid item md={10} style={{ padding: "0px" }}>
                   <div style={{ position: "relative" }} classes={classes.gridTables}>
                     <Fragment>
                       {searchTapValue.value === "contactInformation" && (
