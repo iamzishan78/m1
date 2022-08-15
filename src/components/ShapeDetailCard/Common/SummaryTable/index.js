c73445472a4610270545d9c42b4841b1880f6308
iamzishan78 <zishannaseeb528@gmail.com>
2022-08-15 17:41:19 +0500

    agreement map summary date calender changed

--- src/components/ShapeDetailCard/Common/SummaryTable/index.js
+++ src/components/ShapeDetailCard/Common/SummaryTable/index.js
@@ -23,6 +23,7 @@ import { getRoundedNra } from "utils/helper";
 import ReactSelectField from "components/Shared/M1nTable/components/SubComponents/ReactSelectField";
 import { copy } from "components/Shared/functions";
 import { AppContext } from "AppContext";
+import { Clear } from "@material-ui/icons";
 
 function TableTextField({ data, value, onChange, onKeyDown, onBlur, onWheel, showMessage, type, InputProps }) {
   const classes = summaryTableStyles();
@@ -363,31 +364,29 @@ export default function SummartyTableInfo({ tableData, properties, updatePropert
                       />
                     )}
                     {data.type === "date" && (
-                      <KeyboardDatePicker
-                        autoFocus
-                        className={classes.select}
-                        disableToolbar
-                        variant="inline"
-                        fullWidth
-                        inputVariant="outlined"
-                        format="MM/DD/YYYY"
+                      <TextField
+                        autoOk
+                        type="date"
+                        variant="outlined"
                         margin="normal"
-                        id="date-picker-inline"
-                        value={tableTempProperties[data.key] || null}
-                        onOpen={() => {
-                          tableDataState[`${data.key}date`] = true;
-                        }}
-                        onClose={() => {
-                          tableDataState[`${data.key}date`] = false;
-                          setTableDataState({});
-                          setTableTempProperties({ ...tableTempProperties, [data.key]: properties[data.key] });
-                        }}
-                        onKeyDown={(e) => {
-                          if (e.keyCode === 13) {
-                            e.stopPropagation();
-                            onKeyDown(e, data, "value");
+                        fullWidth
+                        value={tableTempProperties?.[data.key] ? moment(tableTempProperties[data.key]).format("yyyy-MM-DD") : ""}
+                        onChange={(event) => {
+
+                          const date = String(event?.target?.value) || null
+                          if (date === null) {
+                            setTableTempProperties({ ...tableTempProperties, [`${data.key}`]: date });
                           }
+                          if (date) {
+                            tableTempProperties[`${data.key}`] = date ? date : null;
+                            setTableTempProperties({ ...tableTempProperties });
+                            if (date?._pf?.overflow === -2 || !date?._strict) {
+                              onKeyDown(null, data, "value");
+                            }
+                          }
+
                         }}
+
                         onBlur={() => {
                           setTimeout(() => {
                             if (!tableDataState[`${data.key}date`]) {
@@ -396,20 +395,71 @@ export default function SummartyTableInfo({ tableData, properties, updatePropert
                             }
                           }, 100);
                         }}
-                        onChange={(date) => {
-                          if (date === null) {
-                            setTableTempProperties({ ...tableTempProperties, [`${data.key}`]: date });
-                          }
-                          if (date && date?._d?.toString() !== "Invalid Date") {
-                            tableTempProperties[`${data.key}`] = date ? String(date["_d"]) : null;
-                            setTableTempProperties({ ...tableTempProperties });
-                            if (date?._pf?.overflow === -2 || !date?._strict) {
-                              onKeyDown(null, data, "value");
-                            }
-                          }
+                        InputLabelProps={{
+                          shrink: true,
                         }}
+                        disableToolbar
                         KeyboardButtonProps={{ "aria-label": "change date" }}
+                        format="MM/DD/YYYY"
+                        PopoverProps={{ disablePortal: false }}
+                        InputProps={{
+                          endAdornment: (
+                            <IconButton >
+                              <Clear style={{ height: 22, width: 22 }} />
+                            </IconButton>
+                          ),
+                          classes: {
+                            root: classes.select
+                          },
+                        }}
                       />
+                      // <KeyboardDatePicker
+                      //   autoFocus
+                      //   className={classes.select}
+                      //   disableToolbar
+                      //   variant="inline"
+                      //   fullWidth
+                      //   inputVariant="outlined"
+                      //   format="MM/DD/YYYY"
+                      //   margin="normal"
+                      //   id="date-picker-inline"
+                      //   value={tableTempProperties[data.key] || null}
+                      //   onOpen={() => {
+                      //     tableDataState[`${data.key}date`] = true;
+                      //   }}
+                      //   onClose={() => {
+                      //     tableDataState[`${data.key}date`] = false;
+                      //     setTableDataState({});
+                      //     setTableTempProperties({ ...tableTempProperties, [data.key]: properties[data.key] });
+                      //   }}
+                      //   onKeyDown={(e) => {
+                      //     if (e.keyCode === 13) {
+                      //       e.stopPropagation();
+                      //       onKeyDown(e, data, "value");
+                      //     }
+                      //   }}
+                      //   onBlur={() => {
+                      //     setTimeout(() => {
+                      //       if (!tableDataState[`${data.key}date`]) {
+                      //         setTableDataState({});
+                      //         setTableTempProperties({ ...tableTempProperties, [data.key]: properties[data.key] });
+                      //       }
+                      //     }, 100);
+                      //   }}
+                      //   onChange={(date) => {
+                      //     if (date === null) {
+                      //       setTableTempProperties({ ...tableTempProperties, [`${data.key}`]: date });
+                      //     }
+                      //     if (date && date?._d?.toString() !== "Invalid Date") {
+                      //       tableTempProperties[`${data.key}`] = date ? String(date["_d"]) : null;
+                      //       setTableTempProperties({ ...tableTempProperties });
+                      //       if (date?._pf?.overflow === -2 || !date?._strict) {
+                      //         onKeyDown(null, data, "value");
+                      //       }
+                      //     }
+                      //   }}
+                      //   KeyboardButtonProps={{ "aria-label": "change date" }}
+                      // />
                     )}
                     {data.type === "autocomplete" && (
                       <>
