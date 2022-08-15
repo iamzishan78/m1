d186433ac7ff5c31920561a0af8c7ffd4bf8efd5
iamzishan78 <zishannaseeb528@gmail.com>
2022-08-15 17:25:34 +0500

    campaign unit and contacts grid hover effeect changes

--- src/components/Shared/M1nTable/components/Table.js
+++ src/components/Shared/M1nTable/components/Table.js
@@ -184,7 +184,7 @@ const useStyles = makeStyles((theme) => ({
     // }),
     "& .MuiTableCell-body": {
       padding: (props) => (props.dense ? "0 !important" : "12px 16px"),
-      backgroundColor: "#fff",
+      // backgroundColor: "#fff",
     },
     // "& .MuiTableCell-paddingCheckbox": {
     //   position: "relative",
@@ -2692,8 +2692,8 @@ function SubTable(props) {
                       {props.parent === "ownersPerParcel" && column.name === "name" && (
                         <FeatureFlag feature={FEATURES.IDICORE}>
                           <span> {tableMeta.rowData[props.columns.findIndex(
-                          (val) => val.name === "isPurchased"
-                        )] && <RequestPageIcon color="grey" fontSize="8px" />}</span>
+                            (val) => val.name === "isPurchased"
+                          )] && <RequestPageIcon color="grey" fontSize="8px" />}</span>
                         </FeatureFlag>
                       )}
 
