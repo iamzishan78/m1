876cb13ffe5e4493ca68411845951e0a1a4c9998
iamzishan78 <zishannaseeb528@gmail.com>
2022-08-18 20:09:57 +0500

    campaign header total nra changes

--- src/components/Contacts/components/campaign/CampaignHeaderSection.js
+++ src/components/Contacts/components/campaign/CampaignHeaderSection.js
@@ -99,7 +99,7 @@ const CampaignHeader = ({ campaign, updateCampaignInformation }) => {
                 Total NRA
               </Typography>
               <Typography variant="h6" component="div" className={classes.cardNumberTypography}>
-                {vf_number(get(campaign, "totalNra", 0))}
+                {get(campaign, "totalNra", 0).toFixed(2)}
               </Typography>
             </CardContent>
           </Card>
