81cfa9575400de4a1ba0cc6c5e4a83c8ad31eeb2
iamzishan78 <zishannaseeb528@gmail.com>
2022-08-23 22:11:26 +0530

    Incremental commit [Find Map] > Slide-out modal > LayerGroup changes

--- src/graphQL/useMutationLayerGroup.js
+++ src/graphQL/useMutationLayerGroup.js
@@ -5,3 +5,15 @@ export const ADD_LAYER_GROUP = gql`
     addLayerGroup(userId: $userId, layerGroup: $layerGroup)
   }
 `;
+
+export const UPDATE_LAYER_GROUP = gql`
+  mutation updateLayerGroupName($layerGroupId: ID, $layerGroupName: String) {
+    updateLayerGroupName(layerGroupId: $layerGroupId, layerGroupName: $layerGroupName)
+  }
+`;
+
+export const REMOVE_LAYER_GROUP = gql`
+  mutation removeLayerGroup($userId: ID, $layerGroupId: ID) {
+    removeLayerGroup(userId: $userId, layerGroupId: $layerGroupId)
+  }
+`;
