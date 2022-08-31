3a435f80c986aeb67ea93cd9c1fe7f63ff0ebda6
iamzishan78 <zishannaseeb528@gmail.com>
2022-08-31 18:18:32 +0500

    campaign unit grid status added

--- src/components/Table/constants/campaign-units-header-schema.js
+++ src/components/Table/constants/campaign-units-header-schema.js
@@ -156,6 +156,15 @@ const unitsColumnHeaders = [
     },
   },
   {
+    name: "uStatus",
+    label: "Status",
+    esKey: "shapeJson.properties.uStatus.keyword",
+    options: {
+      sort: true,
+      filter: true,
+    },
+  },
+  {
     name: "ownersCount",
     label: "Owner Count",
     esKey: "interestSummary.unitInterestCount",
