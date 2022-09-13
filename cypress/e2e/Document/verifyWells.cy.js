d4c9cb443fdda3fb175372080751976068c5857f
iamzishan78 <zishannaseeb528@gmail.com>
2022-09-13 22:26:25 +0500

    verify well test case in proggress 3

--- cypress/e2e/Document/verifyWells.cy.js
+++ cypress/e2e/Document/verifyWells.cy.js
@@ -20,25 +20,47 @@ describe('Verify Well Spec', () => {
             cy.get('table').contains('td', name).next().click();
 
             cy.log('==== STEP: ADD WELL ====')
-            cy.addWell('silver dog')
+            cy.get("#wellIcon").click()
+
+            cy.addWell('black dog')
+
+            cy.get('#closeIcon').click({ force: true })
+
+            cy.get('table').contains('td', name).next().click();
 
             // cy.get('tbody>tr', { timeout: 5000 }).eq(2).children().eq(3).children().eq(1).click({ force: true })
 
-            // cy.get('#attachedDocument').trigger('mouseover')
+            cy.get('#attachedDocument').trigger('mouseover')
 
-            // cy.get('#documentDeleteIcon').click({ force: true })
-            // cy.interceptApi('updateDocument')
+            cy.get('#documentDeleteIcon').click({ force: true })
+            cy.interceptApi('updateDocument')
 
-            // cy.get(".MuiButton-label").contains('Delete').click()
-            // cy.verifyApiResponse('@updateDocumentApi', { responseTimeout: 5000 })
+            cy.get(".MuiButton-label").contains('Delete').click()
+            cy.verifyApiResponse('@updateDocumentApi', { responseTimeout: 5000 })
 
-            // cy.interceptApi('AddDescriptorFile')
-            // cy.get('input[type=file]', { force: true }).selectFile('cypress/files/documentSample.png', {
-            //     force: true
-            // })
-            // cy.verifyApiResponse('@AddDescriptorFileApi', { responseTimeout: 5000 })
+            cy.interceptApi('AddDescriptorFile')
+
+            cy.get('input[type=file]', { force: true }).selectFile('cypress/files/documentSample.png', {
+                force: true
+            })
+
+            cy.verifyApiResponse('@AddDescriptorFileApi', { responseTimeout: 5000 })
+
+
+            cy.wait(1000)
+            cy.interceptApi('updateDocument')
+            cy.get(".MuiButton-label").contains('Save').click()
+            cy.verifyApiResponse('@updateDocumentApi', { responseTimeout: 5000 })
+
+
+
+            cy.get('table').contains('td', name).next().click();
 
         })
+
+
+
+
     })
 
 })
