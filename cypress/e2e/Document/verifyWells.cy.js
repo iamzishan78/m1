bbcd6f4f5630d32c655adf537745b677b9809668
iamzishan78 <zishannaseeb528@gmail.com>
2022-09-13 20:01:03 +0500

    verify well test case in proggress

--- cypress/e2e/Document/verifyWells.cy.js
+++ cypress/e2e/Document/verifyWells.cy.js
@@ -5,29 +5,35 @@ describe('Verify Well Spec', () => {
         cy.viewport(1400, 900)
         cy.visit('http://localhost:3000/documents')
 
+        cy.interceptApi('getESDocuments')
+        cy.visit('http://localhost:3000/documents')
+
         cy.checkAndLogin()
 
         cy.get('#addDocument', { timeout: 50000 }).should('be.visible')
 
-        cy.wait(3000)
-        cy.get('tbody>tr', { timeout: 5000 }).eq(2).children().eq(3).children().eq(1).click({ force: true })
+        cy.verifyApiResponse('@getESDocumentsApi').then((response) => {
+            const hits = response.response.body.data.getESFiles.hits
 
-        cy.get('#attachedDocument').trigger('mouseover')
+            console.log("hits : ", hits)
 
-        cy.get('#documentDeleteIcon').click({ force: true })
-        cy.interceptApi('updateDocument')
+            // cy.get('tbody>tr', { timeout: 5000 }).eq(2).children().eq(3).children().eq(1).click({ force: true })
 
-        cy.get(".MuiButton-label").contains('Delete').click()
-        cy.verifyApiResponse('@updateDocumentApi', { responseTimeout: 5000 })
+            // cy.get('#attachedDocument').trigger('mouseover')
 
-        cy.interceptApi('AddDescriptorFile')
-        cy.get('input[type=file]', { force: true }).selectFile('cypress/files/documentSample.png', {
-            force: true
+            // cy.get('#documentDeleteIcon').click({ force: true })
+            // cy.interceptApi('updateDocument')
+
+            // cy.get(".MuiButton-label").contains('Delete').click()
+            // cy.verifyApiResponse('@updateDocumentApi', { responseTimeout: 5000 })
+
+            // cy.interceptApi('AddDescriptorFile')
+            // cy.get('input[type=file]', { force: true }).selectFile('cypress/files/documentSample.png', {
+            //     force: true
+            // })
+            // cy.verifyApiResponse('@AddDescriptorFileApi', { responseTimeout: 5000 })
+
         })
-        cy.verifyApiResponse('@AddDescriptorFileApi', { responseTimeout: 5000 })
-
-
-
     })
 
 })
