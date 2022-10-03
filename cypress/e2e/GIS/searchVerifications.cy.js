ca99a05043b9cc42451dde6e9ebde8168d433091
iamzishan78 <zishannaseeb528@gmail.com>
2022-10-03 12:27:20 +0500

    searchVerification spec completed

--- cypress/e2e/GIS/searchVerifications.cy.js
+++ cypress/e2e/GIS/searchVerifications.cy.js
@@ -7,8 +7,8 @@ describe('Searches Verifications Spec', () => {
         const { shorTimeout, longTimeout, extraTimeout } = basic_timeouts
 
         const searchStrings = {
-            tractName: "BLK 39T2N SEC 22",
             agreementName: "JA1234-AMEER AGMT",
+            tractName: "BLK 39T2N SEC 22",
             unitName: "SP RR CO A-275"
         }
 
@@ -17,30 +17,44 @@ describe('Searches Verifications Spec', () => {
 
         cy.checkAndLogin('#workSpaceSignin')
 
-        // cy.log(`==== STEP:Search UNIT BY NAME ${searchStrings.tractName} ====`)
-        // cy.interceptApi('getCustomLayer')
-        // cy.searchOnMap('Tracts', searchStrings.tractName)
-        // cy.verifyApiResponse('@getCustomLayerApi', { responseTimeout: longTimeout })
+        cy.log(`==== STEP:Search AGREEMENT BY NAME ${searchStrings.agreementName} ====`)
+        cy.interceptApi('getCustomLayer')
+        cy.searchOnMap('Agreements', searchStrings.agreementName)
+        cy.verifyApiResponse('@getCustomLayerApi', { responseTimeout: longTimeout })
 
-        // cy.visit('http://localhost:3000')
+        cy.visit('http://localhost:3000')
 
-        // cy.log(`==== STEP:Search UNIT BY NAME ${searchStrings.agreementName} ====`)
-        // cy.interceptApi('getCustomLayer')
-        // cy.searchOnMap('Agreements', searchStrings.agreementName)
-        // cy.verifyApiResponse('@getCustomLayerApi', { responseTimeout: longTimeout })
+        cy.log(`==== STEP:Search TRACT BY NAME ${searchStrings.tractName} ====`)
+        cy.interceptApi('getCustomLayer')
+        cy.searchOnMap('Tracts', searchStrings.tractName)
+        cy.verifyApiResponse('@getCustomLayerApi', { responseTimeout: longTimeout })
 
-        // cy.visit('http://localhost:3000')
+        cy.visit('http://localhost:3000')
 
+        cy.log(`==== STEP:Search UNIT BY NAME ${searchStrings.unitName} ====`)
+        cy.interceptApi('getCustomLayer')
+        cy.searchOnMap('Agreements', searchStrings.unitName)
+        cy.verifyApiResponse('@getCustomLayerApi', { responseTimeout: longTimeout })
+
+
+        cy.log('==== STEP: CLICK ON SNAP GRID BUTTON ====')
         cy.get("#snapGridButton", { timeout: longTimeout }).should('be.visible').click()
-
         cy.get('#snapGrid', { timeout: longTimeout }).should('be.visible')
 
+        cy.log(`==== STEP:Search AGREEMENT BY NAME ${searchStrings.agreementName} IN SNAPGRID ====`)
+        cy.get('.MuiTypography-displayBlock').contains("Agreements").click()
+        cy.get("#MapGridAgreementsTable", { timeout: longTimeout }).should('be.visible')
+        cy.gridSearch(searchStrings.agreementName, 'getESSimpleSearch', "#mapGridCardSearch-basic")
+
+        cy.log(`==== STEP:Search TRACT BY NAME ${searchStrings.tractName} IN SNAPGRID ====`)
+        cy.get('.MuiTypography-displayBlock').contains("Tracts").click()
+        cy.get("#MapGridTractsTable", { timeout: longTimeout }).should('be.visible')
+        cy.gridSearch(searchStrings.tractName, 'getESSimpleSearch', "#mapGridCardSearch-basic")
+
+        cy.log(`==== STEP:Search UNIT BY NAME ${searchStrings.unitName} IN SNAPGRID ====`)
         cy.get('.MuiTypography-displayBlock').contains("Units").click()
-
         cy.get("#MapGridUnitTable", { timeout: longTimeout }).should('be.visible')
-
         cy.gridSearch(searchStrings.unitName, 'getESSimpleSearch', "#mapGridCardSearch-basic")
-
 
     })
 
