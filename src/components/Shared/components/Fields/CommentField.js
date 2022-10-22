b7b88aa043bae35563dcfc95607dd89524518f05
iamzishan78 <zishannaseeb528@gmail.com>
2022-10-22 21:31:09 +0500

    comment type segregated component

--- src/components/Shared/components/Fields/CommentField.js
+++ src/components/Shared/components/Fields/CommentField.js
@@ -7,15 +7,7 @@ import IconButton from "@material-ui/core/IconButton";
 import Button from "@material-ui/core/Button";
 import Autocomplete, { createFilterOptions } from "@material-ui/lab/Autocomplete";
 import { makeStyles } from "@material-ui/core/styles";
-import EditNoteIcon from "components/Shared/svgIcons/edit-note";
-import Select from "@material-ui/core/Select";
-import MenuItem from "@material-ui/core/MenuItem";
-import FormControl from "@material-ui/core/FormControl";
-import InputLabel from "@material-ui/core/InputLabel";
-import { GET_COMMENT_TYPES, UPSERTCOMMENTTYPE } from "graphQL/useQueryCommentType";
-import { useMutation, useQuery } from "@apollo/client";
-import { showInfoMessage } from "actions";
-import { useDispatch } from "react-redux";
+import CommentType from "components/Shared/components/Comment/CommentType";
 
 const filter = createFilterOptions();
 
@@ -168,9 +160,6 @@ const useStyles = makeStyles((theme) => ({
     },
   },
 }));
-
-const CategoryList = ["All", "Agreement", "Contact", "Document", "Flow", "Revenue", "Tract", "Unit"];
-
 export default function DealComment({
   comment,
   showActions,
@@ -185,22 +174,12 @@ export default function DealComment({
   ...props
 }) {
   const classes = useStyles({ fieldWidth });
-  const dispatch = useDispatch();
   const [filterValue, setFilterValue] = useState("");
   const [showOptions, setShowOptions] = useState(false);
   const [isSelected, setIsSelected] = useState(false);
   const [nameAutValue, setNameAutValue] = useState({});
-  const [showCommentType, setShowCommentType] = useState(props.showCommentType);
   const [showCommentTypeDialog, setShowCommentTypeDialog] = useState(false);
-  const [selectedTab, setSelectedTab] = useState("Existing");
   const [selectedCommentType, setSelectedCommentType] = useState("General");
-  const [commentTypeData, setCommentTypeData] = useState({
-    commentType: "",
-    category: "",
-  });
-  const { data } = useQuery(GET_COMMENT_TYPES);
-  const [upsertCommentType] = useMutation(UPSERTCOMMENTTYPE);
-  const [commentTypes, setCommentTypes] = useState([]);
 
   (function () {
     var target = $("#colorText");
@@ -228,12 +207,6 @@ export default function DealComment({
     }
     return isActive;
   };
-
-  useEffect(() => {
-    if (data && Array.isArray(data.commentsType)) {
-      setCommentTypes(data.commentsType);
-    }
-  }, [data]);
 
   useEffect(() => {
     let value = JSON.parse(JSON.stringify(comment));
@@ -298,38 +271,6 @@ export default function DealComment({
     for (let i = 0; i < splittedArray.length - 1; i += 1) value += `${splittedArray[i]}${i !== splittedArray.length - 2 ? "@" : ""}`;
     setComment(value + `{{${act._id}}}`);
     setIsSelected(true);
-  };
-
-  const addCommentType = () => {
-    const commentType = (commentTypeData.commentType || "").trim();
-    const category = (commentTypeData.category || "").trim();
-
-    if (!commentType && !category) {
-      return dispatch(showInfoMessage("Comment Type and Category is required"));
-    } else if (!commentType) {
-      return dispatch(showInfoMessage("Comment Type is required"));
-    } else if (!category) {
-      return dispatch(showInfoMessage("Category is required"));
-    }
-
-    upsertCommentType({
-      variables: {
-        commentType: {
-          commentType: commentTypeData.commentType,
-          category: commentTypeData.category,
-        },
-      },
-      refetchQueries: ["getAllCommentsType"],
-      awaitRefetchQueries: false,
-    });
-
-    setShowCommentTypeDialog(false);
-    setSelectedCommentType(commentTypeData.commentType);
-    setSelectedTab("Existing");
-    setCommentTypeData({
-      commentType: "",
-      category: "",
-    });
   };
 
   return (
@@ -408,122 +349,6 @@ export default function DealComment({
           </>
         )}
       />
-      {showCommentTypeDialog && (
-        <div
-          style={{
-            position: "absolute",
-            bottom: selectedTab === "New Comment Type" ? "25px" : "69px",
-            background: "white",
-            zIndex: "9999",
-            boxShadow: "0 10px 40px 0 rgb(0 0 0 / 15%)",
-            padding: "15px",
-            width: "315px",
-            padding: "15px",
-            left: "12px",
-          }}
-        >
-          <Grid item className={classes.headerActions}>
-            <div>
-              <span
-                className={`${classes.tab} ${selectedTab === "Existing" ? classes.selectedTab : ""}`}
-                onClick={() => setSelectedTab("Existing")}
-              >
-                Existing
-              </span>
-              <span
-                className={`${classes.tab} ${selectedTab === "New Comment Type" ? classes.selectedTab : ""}`}
-                onClick={() => setSelectedTab("New Comment Type")}
-              >
-                New Comment Type
-              </span>
-            </div>
-          </Grid>
-          <Grid style={{ marginTop: "25px" }}>
-            {selectedTab === "Existing" && (
-              <Select
-                className={classes.selectCommentType}
-                variant="outlined"
-                value={selectedCommentType}
-                onChange={(e) => {
-                  setSelectedCommentType(e.target.value);
-                  setShowCommentTypeDialog(false);
-                }}
-              >
-                {commentTypes.map((obj) => (
-                  <MenuItem value={obj.commentType}>{obj.commentType}</MenuItem>
-                ))}
-              </Select>
-            )}
-            {selectedTab === "New Comment Type" && (
-              <>
-                <FormControl className={classes.formControlCommentType} variant="outlined" fullWidth>
-                  <TextField
-                    id="demo-simple-select-standard-label"
-                    className={classes.commentTypeInput}
-                    variant="outlined"
-                    value={commentTypeData.commentType}
-                    label="Comment Type"
-                    onChange={(e) => {
-                      setCommentTypeData((prev) => ({ ...prev, commentType: e.target.value }));
-                    }}
-                  />
-                </FormControl>
-                <FormControl className={classes.formControlCommentType} variant="outlined" fullWidth>
-                  <InputLabel className={classes.formLabel} id="demo-simple-select-category-label">
-                    Category
-                  </InputLabel>
-                  <Select
-                    className={classes.selectCommentType}
-                    variant="outlined"
-                    label="Category"
-                    value={commentTypeData.category}
-                    onChange={(e) => {
-                      setCommentTypeData((prev) => ({ ...prev, category: e.target.value }));
-                    }}
-                  >
-                    {CategoryList.map((category) => (
-                      <MenuItem value={category}>{category}</MenuItem>
-                    ))}
-                  </Select>
-                </FormControl>
-              </>
-            )}
-          </Grid>
-          {selectedTab === "New Comment Type" && (
-            <div className={classes.dialogFooter}>
-              <Button
-                variant="contained"
-                color="white"
-                size="medium"
-                disableElevation
-                className={classes.footerButtonCancel}
-                style={{
-                  margin: "0px 15px 0px 0px",
-                }}
-                onClick={() => {
-                  setShowCommentTypeDialog(false);
-                  setSelectedTab("Existing");
-                }}
-              >
-                Cancel
-              </Button>
-
-              <Button
-                variant="contained"
-                color="secondary"
-                size="medium"
-                disableElevation
-                onClick={() => {
-                  addCommentType();
-                }}
-                className={classes.footerButton}
-              >
-                Add
-              </Button>
-            </div>
-          )}
-        </div>
-      )}
       {!isEdit ? (
         <div
           style={{
@@ -531,30 +356,8 @@ export default function DealComment({
             justifyContent: "space-between",
             alignItems: "center",
           }}
-          onClick={() => {
-            if (showCommentTypeDialog) {
-              setShowCommentTypeDialog(false);
-              setSelectedTab("Existing");
-            }
-          }}
         >
-          <div
-            style={{
-              padding: "0px 10px",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              color: "#949494",
-            }}
-            onClick={() => setShowCommentTypeDialog((o) => !o)}
-          >
-            {showCommentType && (
-              <>
-                <EditNoteIcon fill={showCommentTypeDialog ? "black" : ""} />
-                <span style={{ marginLeft: "1px" }}>{selectedCommentType}</span>
-              </>
-            )}
-          </div>
+          <CommentType showCommentType={props.showCommentType} setSelectedCommentType={setSelectedCommentType} />
           {showActions && (
             <Button
               className={classes.commentBtn}
@@ -563,7 +366,6 @@ export default function DealComment({
               onClick={() => {
                 if (!showCommentTypeDialog) {
                   upsertComment({ comment, commentType: selectedCommentType });
-                  setSelectedTab("Existing");
                   setNameAutValue({});
                 }
               }}
