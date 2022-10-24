9d2f018d082504ee5e856c728719a945e8273c95
iamzishan78 <zishannaseeb528@gmail.com>
2022-10-24 13:53:04 +0500

    crash fixed

--- src/components/Land/components/Agreements/detailComponents/DetailsComponent.js
+++ src/components/Land/components/Agreements/detailComponents/DetailsComponent.js
@@ -410,8 +410,7 @@ export function DetailComponents(props) {
       if (getRelativePosition("provisions-div") < 30) activeTab = 2;
       if (getRelativePosition("legal-description-div") < 30) activeTab = 3;
       if (getRelativePosition("related-wells-div") < 30) activeTab = 4;
-      if (getRelativePosition("related-docs-div") < 30) activeTab = 5;
-      if (getRelativePosition("related-agrmt-div") < 30) activeTab = 6;
+      if (getRelativePosition("related-docs-div") < 300) activeTab = 5;
 
       if (tab !== activeTab) setTab(activeTab);
     }
