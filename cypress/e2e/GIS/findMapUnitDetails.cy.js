45e722d83878c419630a9c782ca1b1e59e5712e6
iamzishan78 <zishannaseeb528@gmail.com>
2022-11-25 12:19:40 -0600

    update comments

--- cypress/e2e/GIS/findMapUnitDetails.cy.js
+++ cypress/e2e/GIS/findMapUnitDetails.cy.js
@@ -2,6 +2,11 @@
 
 import { basic_timeouts } from "../../cypressUtils/data"
 
+// Click a unit on the map and open the detail card for Panther 1H (id = 628410d19ec87abf87b956e0)
+// Click the wells tab and scroll the right until the location/flyTo icon appears
+// Click the flyTo icon to ensure the unit detail screen closes and well detail card appears
+// Close out of the well detail card
+
 describe('open well detail card of Unit', () => {
     it('passes', () => {
 
