29e96e72e5d0887c14812a3101610817087d2c60
iamzishan78 <zishannaseeb528@gmail.com>
2022-11-25 14:58:51 +0500

    revenue > properties detail > sales date format fixed - https://app.asana.com/0/1203298095408491/1203414617708259/f

--- src/components/Table/TableESHOC.js
+++ src/components/Table/TableESHOC.js
@@ -839,17 +839,17 @@ export const TableESHOC = (Component) => {
         options.page = page
 
         const onInfiniteScroll = () => {
-            setCurrentRowsLength(currentRowsLength => {
+            setCurrentRowsLength(_currentRowsLength => {
                 Loading((loading) => {
-                    setRows(rows => {
-                        if (rows.length < currentRowsLength && !loading) {
+                    setRows(tableRows => {
+                        if (tableRows.length < _currentRowsLength && !loading) {
                             document.getElementById('pagination-next').click()
                         }
-                        return rows
+                        return tableRows;
                     })
-                    return loading
+                    return loading;
                 })
-                return currentRowsLength
+                return _currentRowsLength;
             })
         }
 
