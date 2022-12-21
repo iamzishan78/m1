f7b893c658ce699c4cd12965fe77bd6d4c8c692d
iamzishan78 <zishannaseeb528@gmail.com>
2022-12-21 17:29:59 +0530

    created related agreements advance search UI

--- src/components/Land/components/Agreements/components/AdvanceSearch/searchComponents.js
+++ src/components/Land/components/Agreements/components/AdvanceSearch/searchComponents.js
@@ -1,3 +1,4 @@
 export { default as ProvisionFilters } from "./Provisions";
 export { default as RemarksTypes } from "./Remarks";
 export { default as RelatedWellsFilters } from "./RelatedWells";
+export { default as RelatedAgreementsFilters } from "./RelatedAgreements";
