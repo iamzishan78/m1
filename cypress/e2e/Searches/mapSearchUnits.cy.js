a2c1d288f387bb39f20787b173380ba0cba9fe65
iamzishan78 <zishannaseeb528@gmail.com>
2023-01-02 18:25:56 +0500

    mapsearchunit  spec working

--- cypress/e2e/Searches/mapSearchUnits.cy.js
+++ cypress/e2e/Searches/mapSearchUnits.cy.js
@@ -6,7 +6,7 @@ describe('Map Search Agreements Spec', () => {
     it('passes', () => {
         const { longTimeout } = basic_timeouts
 
-        const searchStrings = "SP RR CO A-275"
+        const searchStrings = "6499 - MASHBURN"
 
         cy.viewport(1400, 900)
         cy.visit('http://localhost:3000')
@@ -14,6 +14,8 @@ describe('Map Search Agreements Spec', () => {
         cy.checkAndLogin('#workSpaceSignin')
 
         cy.log(`==== STEP:Search UNIT BY NAME ${searchStrings} ====`)
+
+
         cy.interceptApi('getCustomLayer')
         cy.searchOnMap('Units', searchStrings)
         cy.verifyApiResponse('@getCustomLayerApi', { responseTimeout: longTimeout })
