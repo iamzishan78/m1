53e7467081e5dc7c6e50b73a3d629b83ded4e920
iamzishan78 <zishannaseeb528@gmail.com>
2023-01-09 17:57:25 +0530

    integrated Activity modal

--- src/components/Dashboard/components/Tasks.js
+++ src/components/Dashboard/components/Tasks.js
@@ -26,6 +26,8 @@ import moment from "moment";
 import { useHistory } from "react-router-dom";
 import { UPDATEACTIVITY } from "../../../graphQL/useMutationActivity";
 import { AppContext } from "AppContext";
+import AddIcon from '@material-ui/icons/Add';
+import ActivitiesModal from "../../Activities/components/ActivitiesModal"
 
 const useStyles = makeStyles((theme) => ({
   header: {
@@ -110,6 +112,7 @@ const activityIcons = {
 
 const Tasks = () => {
   const history = useHistory();
+  const [stateApp, setStateApp] = useContext(AppContext);
   const [getAllActivities, { data: orginalData, loading }] = useLazyQuery(
     GETALLACTIVITIES,
     {
@@ -122,7 +125,6 @@ const Tasks = () => {
   });
   const classes = useStyles();
   const [tab, setTab] = useState(0);
-  const [stateApp, setStateApp] = useContext(AppContext);
   const [data, setData] = useState([]);
 
   useEffect(() => {
@@ -186,8 +188,13 @@ const Tasks = () => {
   const Title = () => {
     return (
       <Grid container className={classes.gridStyle}>
-        <Grid item xs={6}>
-          <div>My Tasks</div>
+        <Grid item xs={6} >
+          <Grid container alignItems="center">
+            <div>My Tasks</div>
+            <IconButton title="Add new task" onClick={() => setStateApp({...stateApp, activityDialog: true })}>
+              <AddIcon />
+            </IconButton>
+          </Grid>
         </Grid>
         <Grid item xs={6}>
           <div className={classes.customTabs}>
@@ -287,6 +294,8 @@ const Tasks = () => {
           })}
         </List>
       )}
+
+        <ActivitiesModal setSelectedActivityId={() => {}} events={[]} />
     </Fragment>
   );
 };
