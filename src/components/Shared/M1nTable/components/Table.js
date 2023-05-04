35bd7e55804d301e512f96a3a194849206d2ed05
iamzishan78 <zishannaseeb528@gmail.com>
2023-05-04 13:18:58 +0500

    css changes fixed for grid name column

--- src/components/Shared/M1nTable/components/Table.js
+++ src/components/Shared/M1nTable/components/Table.js
@@ -1373,11 +1373,9 @@ function SubTable(props) {
                       >
                         <Grid container spacing={0} direction="row"
                           style={{
-                            display: "flex",
                             justifyContent: "space-between",
-                            // position: 'absolute'  
+                            position: 'absolute'
                           }}
-                            className={classes.agreementNumber}
                         >
                           <Grid item
                             style={{
@@ -1391,9 +1389,7 @@ function SubTable(props) {
                             />
                           </Grid>
                           {column.label !== 'Contact Name' &&
-                            <Grid style={{
-                              marginRight:"20px"
-                            }} item>
+                            <Grid style={{ marginRight: "20px" }} item>
                               <GridComments value={commentValue} targetSourceId={targetSourceId} tableMeta={tableMeta} />
                             </Grid>
                           }
