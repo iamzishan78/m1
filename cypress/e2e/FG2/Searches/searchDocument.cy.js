79f21712b171ce8cee743624f5caee0df9943496
iamzishan78 <zishannaseeb528@gmail.com>
2023-06-04 21:00:39 -0500

    path fix

--- cypress/e2e/FG2/Searches/searchDocument.cy.js
+++ cypress/e2e/FG2/Searches/searchDocument.cy.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
 
-import { basic_timeouts } from "../../cypressUtils/data"
+import { basic_timeouts } from "../../../cypressUtils/data"
 
 describe('Search Grid Spec', () => {
         it('passes', () => {
