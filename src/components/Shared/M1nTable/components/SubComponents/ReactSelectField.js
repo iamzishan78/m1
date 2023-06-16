a8c1f623bcb982ccd0ec3f613892869c001cf28a
iamzishan78 <zishannaseeb528@gmail.com>
2023-06-16 14:57:09 +0500

    add searching functionality

--- src/components/Shared/M1nTable/components/SubComponents/ReactSelectField.js
+++ src/components/Shared/M1nTable/components/SubComponents/ReactSelectField.js
@@ -3,18 +3,10 @@ import { colorPallete } from "components/Table/helpers";
 import CloseIcon from "@material-ui/icons/Close";
 import ArrowDropDownIcon from "@material-ui/lab/es/internal/svg-icons/ArrowDropDown";
 import ArrowDropUpIcon from '@material-ui/icons/ArrowDropUp';
-import EditIcon from "@material-ui/icons/Edit";
 import { makeStyles } from "@material-ui/core/styles";
-import { useInView } from "react-intersection-observer";
-
-import Select from 'react-select';
-import { components } from "react-select";
-import { defaultTheme } from 'react-select';
 import { copy } from "components/Shared/functions";
-import { AppContext } from "AppContext";
-import { Grid, Tooltip, Typography } from "@material-ui/core";
-import Checkbox from "@material-ui/core/Checkbox";
-import { Waypoint } from 'react-waypoint';
+import { Tooltip, Typography } from "@material-ui/core";
+import GnericSelectField from "./GnericSelectField";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -81,18 +73,6 @@ const ReactSelectField = ({
   const wrapperRef = useRef(null);
   const [showIcon, setShowIcon] = useState(showChevron);
 
-  const [options, setOptions] = useState([]);
-  const [dropDownValues, setDropDownValues] = useState([]);
-  const [displayedOptions, setDisplayedOptions] = useState(100);
-  const [, setStateApp] = useContext(AppContext);
-
-  const defaultValue = {
-    label: "--",
-    value: "--",
-  };
-
-  const { colors } = defaultTheme;
-
   useEffect(() => {
     function handleClickOutside(event) {
       if (wrapperRef.current && !wrapperRef.current.contains(event.target)) {
@@ -104,344 +84,6 @@ const ReactSelectField = ({
       document.removeEventListener("mousedown", handleClickOutside);
     };
   }, [wrapperRef]);
-
-  useEffect(() => {
-    onFilterChange("");
-  }, [dropdownOptions]);
-
-  // const Option = (props) => {
-  //   const palleteId = props?.options.find(opt => opt.value === props.value)?.palleteId
-  //   const pallete = colorPallete.find(
-  //     (pallete) => pallete.id === palleteId
-  //   );
-  //   return (
-  //     <components.Option {...props}>
-  //       {props.value === "editOption" ? (
-  //         <Grid
-  //           id={props}
-  //           style={{
-  //             "flex-wrap": "nowrap",
-  //             marginTop: "5px",
-  //             borderTop: "1px solid #959595",
-  //             padding: "8px 6px 2px 6px",
-  //           }}
-  //           container
-  //           spacing={0}
-  //           onClick={() => {
-  //             setIsOpen(false);
-  //             setStateApp((stateApp) => ({
-  //               ...stateApp,
-  //               selectedMeta: column,
-  //               showFieldModal: true,
-  //             }));
-  //           }}
-  //         >
-  //           <Grid
-  //             style={{
-  //               "flex-grow": 1,
-  //               width: "fit-content",
-  //               "max-width": "max-content",
-  //             }}
-  //             container
-  //             item
-  //             xs={2}
-  //             alignItems="center"
-  //           >
-  //             <EditIcon
-  //               style={{ alignSelf: "center", fontSize: 18, marginRight: 5 }}
-  //             />
-  //           </Grid>
-  //           <Grid
-  //             container
-  //             item
-  //             xs={10}
-  //             alignItems="center"
-  //             style={{
-  //               fontSize: 14,
-  //               "white-space": "nowrap",
-  //             }}
-  //           >
-  //             Edit options
-  //           </Grid>
-  //         </Grid>
-  //       ) : (
-  //         <Grid
-  //           style={{
-  //             "flex-grow": 1,
-  //             width: "fit-content",
-  //             "flex-wrap": "nowrap",
-  //           }}
-  //           className={classes.myClass}
-  //           container
-  //           spacing={0}
-  //         >
-  //           <Grid
-  //             style={{
-  //               "flex-grow": 1,
-  //               width: "fit-content",
-  //               "max-width": "max-content",
-  //             }}
-  //             container
-  //             item
-  //             xs={2}
-  //             alignItems="center"
-  //           >
-  //             <Checkbox
-  //               checked={
-  //                 value?.includes(props.value) ||
-  //                 (!value && props.value === defaultValue.label)
-  //               }
-  //               color="default"
-  //               style={{ marginRight: 5 }}
-  //               inputProps={{
-  //                 "aria-label": "checkbox with default color",
-  //               }}
-  //             />
-  //           </Grid>
-  //           <Grid
-  //             style={{
-  //               "flex-grow": 1,
-  //               width: "fit-content" /*"max-width": "max-content"*/,
-  //             }}
-  //             container
-  //             item
-  //             xs={10}
-  //             alignItems="center"
-  //           >
-  //             <Grid style={{ "flex-grow": 1, width: "fit-content" }} item xs>
-  //               <Tooltip title={props.value} placement="top">
-  //                 <Typography style={{
-  //                   width: "100%",
-  //                   fontWeight: 400,
-  //                   backgroundColor: pallete?.color,
-  //                   color: pallete?.textColor,
-  //                   padding: "3px 10px",
-  //                   borderRadius: 26,
-  //                   fontSize: 14,
-  //                   overflow: "hidden",
-  //                   "white-space": "nowrap",
-  //                   "text-overflow": "ellipsis",
-  //                   textOverflow: 'ellipsis',
-  //                   maxWidth: "187px"
-  //                 }}>
-  //                   {props.value
-  //                   }</Typography>
-
-  //               </Tooltip>
-
-  //             </Grid>
-  //           </Grid>
-  //         </Grid>
-  //       )}
-
-  //     </components.Option>
-
-  //   );
-  // };
-
-  const CustomMenuList = (props) => {
-    const handleScroll = () => {
-      if (props.options.length >= dropDownValues.length) {
-        return;
-      }
-
-      const startIndex = props.options.length;
-      const endIndex = Math.min(startIndex + 100, dropDownValues.length);
-      setDisplayedOptions(endIndex)
-      const nextOptions = dropDownValues.slice(startIndex, endIndex);
-
-      const updatedOptions = props.options.concat(nextOptions);
-
-      setOptions(updatedOptions);
-
-      const waypointElement = document.getElementById(`waypoint-${startIndex - 5}`);
-      if (waypointElement) {
-        waypointElement.scrollIntoView();
-      }
-    };
-
-    return (
-      <components.MenuList {...props}>
-        <>
-          {props.options.map((opt, index) => {
-            return (
-              <React.Fragment key={index}>
-                {index === props.options.length - 5 && (
-                  <Waypoint onEnter={handleScroll} />
-                )}
-              <MyOption setValue={props.setValue} opt={opt} index={index}/>
-              </React.Fragment>
-            )
-          })}
-        </>
-      </components.MenuList>
-    )
-  }
-
-
-  const MyOption = ({opt, setValue, index}) => {
-    const [ref, inView] = useInView();
-    const pallete = colorPallete.find((pallete) => pallete.id === opt.palleteId);
-    return (
-      <div ref={ref} id={`waypoint-${index}`}>
-        {inView ? (
-          <>
-            {opt.value === "editOption" ? (
-              <Grid
-                id={opt.value}
-                style={{
-                  "flex-wrap": "nowrap",
-                  marginTop: "5px",
-                  borderTop: "1px solid #959595",
-                  padding: "8px 6px 2px 6px",
-                }}
-                container
-                spacing={0}
-                onClick={() => {
-                  setIsOpen(false);
-                  setStateApp((stateApp) => ({
-                    ...stateApp,
-                    selectedMeta: column,
-                    showFieldModal: true,
-                  }));
-                }}
-              >
-                <Grid
-                  style={{
-                    "flex-grow": 1,
-                    width: "fit-content",
-                    "max-width": "max-content",
-                  }}
-                  container
-                  item
-                  xs={2}
-                  alignItems="center"
-                >
-                  <EditIcon
-                    style={{ alignSelf: "center", fontSize: 18, marginRight: 5 }}
-                  />
-                </Grid>
-                <Grid
-                  container
-                  item
-                  xs={10}
-                  alignItems="center"
-                  style={{
-                    fontSize: 14,
-                    "white-space": "nowrap",
-                  }}
-                >
-                  Edit options
-                </Grid>
-              </Grid>
-            ) : (
-              <Grid
-                style={{
-                  "flex-grow": 1,
-                  width: "fit-content",
-                  "flex-wrap": "nowrap",
-                }}
-                className={classes.myClass}
-                container
-                spacing={0}
-              >
-                <Grid
-                  style={{
-                    "flex-grow": 1,
-                    width: "fit-content",
-                    "max-width": "max-content",
-                  }}
-                  container
-                  item
-                  xs={2}
-                  alignItems="center"
-                >
-                  <Checkbox
-                    checked={
-                      value?.includes(opt.value) ||
-                      (!value && opt.value === defaultValue.label)
-                    }
-                    onChange={(e)=> {
-                      if(e.target.checked){
-                        setValue(opt, "select-option")
-                      }else{
-                        setValue(opt, "deselect-option")
-                      }
-
-                    }}
-                    color="default"
-                    style={{ marginRight: 5 }}
-                    inputProps={{
-                      "aria-label": "checkbox with default color",
-                    }}
-                  />
-                </Grid>
-                <Grid
-                  style={{
-                    "flex-grow": 1,
-                    width: "fit-content" /*"max-width": "max-content"*/,
-                  }}
-                  container
-                  item
-                  xs={10}
-                  alignItems="center"
-                >
-                  <Grid style={{ "flex-grow": 1, width: "fit-content" }} item xs>
-                    <Tooltip title={opt.value} placement="top">
-                      <Typography style={{
-                        width: "100%",
-                        fontWeight: 400,
-                        backgroundColor: pallete?.color,
-                        color: pallete?.textColor,
-                        padding: "3px 10px",
-                        borderRadius: 26,
-                        fontSize: 14,
-                        overflow: "hidden",
-                        "white-space": "nowrap",
-                        "text-overflow": "ellipsis",
-                        textOverflow: 'ellipsis',
-                        maxWidth: "187px"
-                      }}>
-                        {opt.value}
-                      </Typography>
-
-                    </Tooltip>
-
-                  </Grid>
-                </Grid>
-              </Grid>
-            )}
-          </>
-        ): (  <div style={{ height: "30px"}}></div>)}
-      </div>
-    )
-  }
-  // const Option = (props) => {
-  //   // const { onMouseMove, onMouseOver, ...rest } = props.innerProps;
-  //   // const newProps = { ...props, innerProps: rest };
-  //   return (
-  //     <components.Option {...props}>
-  //       {props.children}
-  //     </components.Option>
-  //   );
-  // };
-
-  const onFilterChange = (search) => {
-    const options = JSON.parse(JSON.stringify(dropdownOptions.filter(op => op.value?.toLowerCase()?.includes(search.toLowerCase()))));
-    options.unshift(defaultValue);
-    options.push({ label: "edit", value: "editOption" });
-    setDropDownValues(options)
-    const endIndex = Math.min(displayedOptions, options.length);
-    const initialOptions = options.slice(0, endIndex);
-    setOptions(initialOptions);
-  }
-
-  const handleKeyDown = (e) => {
-    if (e.key === "Escape" && isOpen) {
-      e.stopPropagation();
-      setIsOpen(!isOpen)
-    }
-  }
 
   const Menu = props => {
     const shadow = 'hsla(218, 50%, 10%, 0.1)';
@@ -479,70 +121,16 @@ const ReactSelectField = ({
       {isOpen ? <Blanket onClick={onClose} /> : null}
     </div>
   );
-  const Svg = p => (
-    <svg
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      focusable="false"
-      role="presentation"
-      {...p}
-    />
-  );
-  const DropdownIndicator = () => (
-    <div css={{ color: colors.neutral20, height: 24, width: 32 }}>
-      <Svg>
-        <path
-          d="M16.436 15.085l3.94 4.01a1 1 0 0 1-1.425 1.402l-3.938-4.006a7.5 7.5 0 1 1 1.423-1.406zM10.5 16a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11z"
-          fill="currentColor"
-          fillRule="evenodd"
-        />
-      </Svg>
-    </div>
-  );
 
   const toggleOpen = () => {
 
     setIsOpen(!isOpen)
-  };
-  const onSelectChange = act => {
-    // toggleOpen();
-    if (act?.value !== "editOption" && act?.value !== "search") {
-      if (isSingleSelect) {
-        onCustomKeyChange(act?.value !== defaultValue.value ? act?.value : null);
-        setIsOpen(false)
-      }
-      else {
-        let newValue = value ? copy(value) : [];
-        const selectedValue =
-          act?.value !== defaultValue.value ? act?.value : null;
-        if (!Array.isArray(newValue) || newValue?.length === 0) {
-          newValue = [selectedValue];
-        } else if (newValue.includes(selectedValue)) {
-          const index = newValue.findIndex((v) => v === selectedValue);
-          newValue.splice(index, 1);
-        } else {
-          newValue.push(selectedValue);
-        }
-        onCustomKeyChange(newValue);
-      }
-    }
-
   };
 
   const selectStyles = {
     control: provided => ({ ...provided, minWidth: 240, margin: 8 }),
     menu: () => ({ boxShadow: 'inset 0 1px 0 rgba(0, 0, 0, 0.1)' }),
     menuPortal: base => ({ ...base, zIndex: 9999, backgroundColor: "white", position: "fixed" })
-  };
-
-
-  const filterOptions = (candidate, input
-  ) => {
-    if (candidate.value === "editOption") {
-      return true;
-    }
-    return candidate.value.toLowerCase().includes(input?.toLowerCase())
   };
 
   return (
@@ -603,34 +191,13 @@ const ReactSelectField = ({
 
           }
         >
-          <Select
-            captureMenuScroll={false} 
-            classNamePrefix='react-select'
-            className='react-select-container'
-            autoFocus
-            backspaceRemovesValue={false}
-            controlShouldRenderValue={false}
-            hideSelectedOptions={false}
-            isClearable={false}
-            id="searchForValue"
-            menuIsOpen
-            onKeyDown={handleKeyDown}
-            onChange={(e) => onSelectChange(e)}
-            options={options
-              .filter((op) => typeof op.value === "string")
-              .map((op) => ({
-                ...op,
-                value: op.value,
-                label: op.value,
-              }))}
-            filterOption={filterOptions}
-            components={{ DropdownIndicator, IndicatorSeparator: null, MenuList: CustomMenuList }}
-            placeholder="Search for value"
-            styles={selectStyles}
-            tabSelectsValue={false}
-            value={value}
-            menuPortalTarget={document.body}
-          />
+         <GnericSelectField 
+         dropdownOptions = {dropdownOptions} 
+         value = {value}
+         isSingleSelect = {isSingleSelect}
+         onCustomKeyChange = {onCustomKeyChange}
+         column = {column}
+        />
 
         </Dropdown>
       </div>
