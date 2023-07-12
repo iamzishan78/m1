c4313524aae66f27421965e0bf78b71d4855ee42
iamzishan78 <zishannaseeb528@gmail.com>
2023-07-12 15:20:52 -0500

    remove track icon and comments from ownersperwell

--- src/components/Shared/constants/ownersperwell-header-schema.js
+++ src/components/Shared/constants/ownersperwell-header-schema.js
@@ -79,18 +79,18 @@ const OwnersPerWellHeadCells = [
     },
   },
 
-  {
-    name: "commentsCounter",
-    label: " ",
-    options: {
-      filter: false,
-      searchable: false,
-      sort: false,
-      download: false,
-      print: false,
-      viewColumns: false,
-    },
-  },
+  // {
+  //   name: "commentsCounter",
+  //   label: " ",
+  //   options: {
+  //     filter: false,
+  //     searchable: false,
+  //     sort: false,
+  //     download: false,
+  //     print: false,
+  //     viewColumns: false,
+  //   },
+  // },
   {
     name: "address",
     label: " ",
@@ -103,25 +103,25 @@ const OwnersPerWellHeadCells = [
       viewColumns: false,
     },
   },
-  {
-    name: "isTracked",
-    label: "Track",
-    options: {
-      searchable: false,
-      download: false,
-      print: false,
-      filterOptions: {
-        names: ["Tracked", "Untracked"],
-        logic(tracked, filterVal) {
-          return !(
-            (filterVal.indexOf("Tracked") >= 0 && tracked) ||
-            (filterVal.indexOf("Untracked") >= 0 && !tracked)
-          );
-        },
-      },
-      filterType: "dropdown",
-    },
-  },
+  // {
+  //   name: "isTracked",
+  //   label: "Track",
+  //   options: {
+  //     searchable: false,
+  //     download: false,
+  //     print: false,
+  //     filterOptions: {
+  //       names: ["Tracked", "Untracked"],
+  //       logic(tracked, filterVal) {
+  //         return !(
+  //           (filterVal.indexOf("Tracked") >= 0 && tracked) ||
+  //           (filterVal.indexOf("Untracked") >= 0 && !tracked)
+  //         );
+  //       },
+  //     },
+  //     filterType: "dropdown",
+  //   },
+  // },
   {
     name: "actionMenu",
     label: " ",
