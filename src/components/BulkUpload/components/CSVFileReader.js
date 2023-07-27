5a1d20f80cb6165abd33588575be6a28c0546a1c
iamzishan78 <zishannaseeb528@gmail.com>
2023-07-27 17:50:24 +0530

    Fixed  "Passing multiple tags were getting store as a single string value"

--- src/components/BulkUpload/components/CSVFileReader.js
+++ src/components/BulkUpload/components/CSVFileReader.js
@@ -161,9 +161,31 @@ export default function CSVFileReader(props) {
     };
   }, []);
 
+  const speareValuesWithComas = (data) => {
+    if(!Array.isArray(data)){
+      throw new Error('Passed argument is not an Array');
+    }
+    const newData = [];
+    data.forEach((row) => {
+      const newRow = {};
+      Object.keys(row.data).forEach(key => {
+        if(row.data[key]?.includes?.(',')){
+          newRow[key] = row.data[key].split(',');
+        } else {
+          newRow[key] = row.data[key]
+        }
+      })
+      newData.push({ ...row, data: newRow, })
+    })
+
+    return newData
+  }
+
   let handleOnDrop = (data) => {
     if (!unmounted.current) {
       if (data && data.length <= 10001) {
+
+        data = speareValuesWithComas(data);
         stateNav.bulkUploadFromMap &&
           stateNav.bulkUploadParcel &&
           data.forEach((data) => {
