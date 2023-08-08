1d6c7f5c526f4ff529ecb403a62dde61abf5ec9c
iamzishan78 <zishannaseeb528@gmail.com>
2023-08-08 19:34:14 +0500

    Added virtualized functionality to grid filters .

--- src/components/Table/AutoCompleteFilter.js
+++ src/components/Table/AutoCompleteFilter.js
@@ -4,10 +4,10 @@ import moment from "moment";
 import { AppContext } from "AppContext";
 import { useLazyQuery } from "@apollo/client";
 import uniqBy from "lodash/uniqBy";
-
 import TextField from "@material-ui/core/TextField";
 import Autocomplete from "@material-ui/lab/Autocomplete";
-import CircularProgress from "@material-ui/core/CircularProgress";
+import { VariableSizeList } from 'react-window';
+import PropTypes from 'prop-types';
 import { capitalizeFirstLetter, customStartCaseString } from "components/Shared/functions";
 
 export const AutoCompleteFilter = React.memo(function AutoCompleteFilter({
@@ -132,7 +132,6 @@ export const AutoCompleteFilter = React.memo(function AutoCompleteFilter({
 
   const handleChange = (search) => {
     setSearch(search);
-    getFiltersAction(search);
   };
 
   const getFiltersAction = (search) => {
@@ -156,11 +155,100 @@ export const AutoCompleteFilter = React.memo(function AutoCompleteFilter({
           field: typeof filterKey === "string" ? filterKey : undefined,
           fields: typeof filterKey !== "string" ? filterKey : undefined,
           type: others.aggsType ? others.aggsType : undefined,
-          size: 50,
+          size: 100000,
         },
       },
     });
   };
+
+  // Adapter for react-window
+  const ListboxComponent = React.forwardRef(function ListboxComponent(props, ref) {
+    const { children, ...other } = props;
+    const itemData = [];
+    children.forEach((item) => {
+      itemData.push(item);
+      itemData.push(...(item.children || []));
+    });
+
+    const itemCount = itemData.length;
+    const itemSize = 48;
+
+    const getChildSize = (child) => {
+      if (child.hasOwnProperty('group')) {
+        return 48;
+      }
+
+      return itemSize;
+    };
+
+    const getHeight = () => {
+      if (itemCount > 8) {
+        return 8 * itemSize;
+      }
+      return itemData.map(getChildSize).reduce((a, b) => a + b, 0);
+    };
+
+    const gridRef = useResetCache(itemCount);
+
+    return (
+      <div ref={ref}>
+        <OuterElementContext.Provider value={other}>
+          <VariableSizeList
+            itemData={itemData}
+            height={getHeight() + 2 * LISTBOX_PADDING}
+            width="100%"
+            ref={gridRef}
+            outerElementType={OuterElementType}
+            innerElementType="ul"
+            itemSize={(index) => getChildSize(itemData[index])}
+            overscanCount={5}
+            itemCount={itemCount}
+          >
+            {renderRow}
+          </VariableSizeList>
+        </OuterElementContext.Provider>
+      </div>
+    );
+  });
+
+  ListboxComponent.propTypes = {
+    children: PropTypes.node,
+  };
+
+  const LISTBOX_PADDING = 8; // px
+
+  function renderRow(props) {
+    const { data, index, style } = props;
+
+    if (!data[index]) {
+      return null;
+    }
+
+    return React.cloneElement(data[index], {
+      style: {
+        ...style,
+        top: style.top + LISTBOX_PADDING,
+      },
+    });
+  }
+
+
+  const OuterElementContext = React.createContext({});
+
+  const OuterElementType = React.forwardRef((props, ref) => {
+    const outerProps = React.useContext(OuterElementContext);
+    return <div ref={ref} {...props} {...outerProps} />;
+  });
+
+  function useResetCache(data) {
+    const ref = React.useRef(null);
+    React.useEffect(() => {
+      if (ref.current != null) {
+        ref.current.resetAfterIndex(0, true);
+      }
+    }, [data]);
+    return ref;
+  }
 
   return (
     <Autocomplete
@@ -173,7 +261,8 @@ export const AutoCompleteFilter = React.memo(function AutoCompleteFilter({
       onClose={() => {
         setOpen(false);
       }}
-      disabled={others.disabled || false}
+      disableListWrap
+      ListboxComponent={ListboxComponent}
       value={multiple && !value ? [] : value}
       inputValue={customStartCaseString(search?.toString(), isDate)}
       getOptionSelected={(option, value) => option.key === value.key}
@@ -223,12 +312,6 @@ export const AutoCompleteFilter = React.memo(function AutoCompleteFilter({
           }}
           InputProps={{
             ...params.InputProps,
-            endAdornment: (
-              <React.Fragment>
-                {loading ? <CircularProgress color="inherit" size={20} /> : null}
-                {params.InputProps.endAdornment}
-              </React.Fragment>
-            ),
           }}
         />
       )}
