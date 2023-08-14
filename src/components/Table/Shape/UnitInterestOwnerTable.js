afa9427f83799f03318d3b61f0cb227fd5b19b4c
iamzishan78 <zishannaseeb528@gmail.com>
2023-08-14 19:48:09 +0530

    Added Max Offer Price on Unit Interest owner grid

--- src/components/Table/Shape/UnitInterestOwnerTable.js
+++ src/components/Table/Shape/UnitInterestOwnerTable.js
@@ -88,6 +88,7 @@ function UnitInterestOwnerTable(props) {
     return hits.map((hit) => {
       hit.isPurchased = hit?.contact?.isPurchased;
       hit.contactStatus = hit?.contact?.contactStatus;
+      hit.max_offer_price = hit?.nra * props.customLayer?.shapeJson?.properties?.uMaxUnitPricing;
       Object.keys(hit).forEach((key) => {
         if (interestKeys.includes(key)) {
           if (typeof hit[key] === "number")
