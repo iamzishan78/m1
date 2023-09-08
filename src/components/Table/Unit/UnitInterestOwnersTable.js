bcee4c00a19a210d11da89fda4ce83df9937fce6
iamzishan78 <zishannaseeb528@gmail.com>
2023-09-08 17:31:54 +0500

    CampaignRelatedGrids UnitInterestOwnersTable init filters fixed

--- src/components/Table/Unit/UnitInterestOwnersTable.js
+++ src/components/Table/Unit/UnitInterestOwnersTable.js
@@ -65,6 +65,18 @@ function UnitInterestOwnersTable(props) {
 
   useEffect(() => {
     const search = esExtentedSearch(props.landSearchQuery, searchInput)
+
+    props.setInitialFilters([
+      {
+        field: 'shape.layer.keyword',
+        value: 'unit',
+      },
+      ...(props.campaignName ? [{
+        field: 'campaignName.keyword',
+        value: props.campaignName
+      }] : [])
+    ]);
+
     setTableMeta({
       extendSearchQuery: isNaN(parseFloat(search.replaceAll('*', ''))) ? search : search.replaceAll('*', ''),
       searchFields: [
@@ -91,17 +103,6 @@ function UnitInterestOwnersTable(props) {
       selectedGridView: GridViewModule || defaultView,
       typeKeyword: { gridViewCategory: 'UnitInterest' },
       startPaginationAt: 50,
-      filters: [
-        {
-          field: 'shape.layer.keyword',
-          value: 'unit',
-        },
-        ...(props.campaignName ? [{
-          field: 'campaignName.keyword',
-          value: props.campaignName
-        }] : [])
-      ],
-
       defaultSort: { field: '_ts', order: 'desc' },
       polygon: stateApp?.currentFeature?.geometry && {
         type: 'geo_intersects',
