0985b6d55d48a8ad8683d110a4d83257a080e335
iamzishan78 <zishannaseeb528@gmail.com>
2023-09-14 18:37:48 -0500

    cleanup ui on unit tracts grid

--- src/components/Table/constants/unit-tracts-header-schema.js
+++ src/components/Table/constants/unit-tracts-header-schema.js
@@ -4,13 +4,13 @@ const UnitTractHeadCells = [
     name: "_id", options: { filter: false, display: false, sort: false, viewColumns: false, }
   },
   {
+    name: "name", label: "Tract Name", esKey: 'name.keyword', options: { sort: true, filter: true }, style: { minWidth: 250, maxWidth: 300 }
+  },
+  {
     name: "state", label: "State", esKey: 'state.keyword', options: { sort: true, filter: true }, style: { maxWidth: 70 }
   },
   {
     name: "county", label: "County", esKey: 'county.keyword', options: { sort: true, filter: true }
-  },
-  {
-    name: "name", label: "Tract Name", esKey: 'name.keyword', options: { sort: true, filter: true }, style: { minWidth: 200, maxWidth: 300 }
   },
   {
     name: "meridian", label: "Meridian", esKey: 'meridian.keyword', options: { sort: true, filter: true }
@@ -25,7 +25,7 @@ const UnitTractHeadCells = [
     name: "section", label: "Section", esKey: 'section.keyword', options: { sort: true, filter: true }
   },
   {
-    name: "altSurvey", label: "Alt Survey", esKey: 'altSurvey.keyword', options: { sort: true, filter: true }
+    name: "altSurvey", label: "Alt Survey", esKey: 'altSurvey.keyword', options: { sort: true, filter: true }, style: { minWidth: 200, maxWidth: 300 }
   },
   {
     name: "legalDescription", label: "Full Legal Description", esKey: 'legalDescription.keyword', options: { sort: true, filter: true }
