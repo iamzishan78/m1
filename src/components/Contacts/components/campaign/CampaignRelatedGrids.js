934635919df84cfd41200adf4919f304c519a489
iamzishan78 <zishannaseeb528@gmail.com>
2023-10-06 17:44:14 +0500

    add height in campaing

--- src/components/Contacts/components/campaign/CampaignRelatedGrids.js
+++ src/components/Contacts/components/campaign/CampaignRelatedGrids.js
@@ -87,13 +87,14 @@ function CamapignRelatedGrids({ campaign }) {
         maxHeight: '40%',
       },
     },
-    maxTableHeight: 'calc(100vh - 600px)',
+    // maxTableHeight: 'calc(100vh - 600px)',
+    height: '35vh',
   }), [campaign?.name]);
 
   return (
     <div className={classes.card}>
       <Card className={classes.dockMenu}>
-        <div className={`cancelDraggableEffect ${classes.mainPanelsDiv}`} style={{ position: 'relative' }}>
+        <div className={`cancelDraggableEffect ${searchTapValue.value === 'unitInterests' ? '' : classes.mainPanelsDiv}`} style={{ position: 'relative' }}>
           {/* //// search panel //// */}
           <Grid container direction="row" style={{ height: '100%' }}>
             <Grid item md={2} className={classes.selectorOptions}>
