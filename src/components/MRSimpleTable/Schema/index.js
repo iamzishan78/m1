a1bd80451915f17d82e82b95b0d0b55e2ad3dafd
iamzishan78 <zishannaseeb528@gmail.com>
2023-10-28 20:52:16 +0500

    removed unused code

--- src/components/MRSimpleTable/Schema/index.js
+++ src/components/MRSimpleTable/Schema/index.js
@@ -1,7 +1,5 @@
 import PotentialOwnersMeta from './potential_owners_schema';
-import TractPotentialUnitsMeta from './tract_potential_units_schema';
 
 export const SCHEMA = {
-	TractPotentialUnitsTable: TractPotentialUnitsMeta,
 	PotentialOwners: PotentialOwnersMeta,
 };
