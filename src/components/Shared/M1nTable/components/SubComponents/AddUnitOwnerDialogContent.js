bad9199d2e6cb9c9413c82937b602d8cb2112c87
iamzishan78 <zishannaseeb528@gmail.com>
2023-10-31 12:18:56 +0500

    added 8 decimal restriction

--- src/components/Shared/M1nTable/components/SubComponents/AddUnitOwnerDialogContent.js
+++ src/components/Shared/M1nTable/components/SubComponents/AddUnitOwnerDialogContent.js
@@ -167,10 +167,10 @@ export default function AddUnitOwnerDialogContent({ selectedRow, setSelectedRow,
       } = selectedRow;
       setNameAutValue({ name, _id: ownerEntity });
       const owner = {
-        working_interest: working_interest || null,
-        royalty_interest: royalty_interest || null,
-        orri: orri || null,
-        nri: nri || null,
+        working_interest: working_interest ? parseFloat(working_interest).toFixed(8) : null,
+        royalty_interest: royalty_interest ? parseFloat(royalty_interest).toFixed(8) : null,
+        orri: orri ? parseFloat(orri).toFixed(8) : null,
+        nri: nri ? parseFloat(nri).toFixed(8) : null,
         nra: nra || null,
         seller_asking_price: seller_asking_price || null,
         competitor_offer_price: competitor_offer_price || null,
