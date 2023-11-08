42805ff69d4e3430924aca35a0890c6b0435ebda
iamzishan78 <zishannaseeb528@gmail.com>
2023-11-08 14:52:23 -0600

    add depth restrictions label

--- src/components/MRTTable/TablesOverride/TractInterestOwnerTable/Dialog/AddParcelOwnerDialogContent.js
+++ src/components/MRTTable/TablesOverride/TractInterestOwnerTable/Dialog/AddParcelOwnerDialogContent.js
@@ -876,6 +876,7 @@ export default function AddParcelOwnerDialogContent({ selectedRow, setSelectedRo
               </>
             )}
             <Grid item xs={12}>
+            <h3>Depth Restrictions</h3>
               <RadioGroup
                 row
                 value={parcelOwnersRadioBValue}
