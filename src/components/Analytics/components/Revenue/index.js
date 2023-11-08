49b8ed541286800863a4d71a62f34b291b80a53d
iamzishan78 <zishannaseeb528@gmail.com>
2023-11-08 15:23:12 +0500

    bug fix

--- src/components/Analytics/components/Revenue/index.js
+++ src/components/Analytics/components/Revenue/index.js
@@ -257,7 +257,7 @@ export default function RevenueAnalytics(props) {
       ...(propertiesReportGroup || []),
       ...(filters || []),
     ])
-  }, [propertiesReportGroup, filters])
+  }, [propertiesReportGroup])
 
   useEffect(() => {
     if (!fromDate) return
