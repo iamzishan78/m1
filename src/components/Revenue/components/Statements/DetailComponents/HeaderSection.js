7f2e1e8a60d16617fa2be2e68d902d8e97f92423
iamzishan78 <zishannaseeb528@gmail.com>
2023-11-23 16:44:27 +0500

    date issue resolved

--- src/components/Revenue/components/Statements/DetailComponents/HeaderSection.js
+++ src/components/Revenue/components/Statements/DetailComponents/HeaderSection.js
@@ -276,7 +276,7 @@ function HeaderFunction(props) {
                     }}
                     onBlur={(e) => {
                       handleUpdateCheck({
-                        checkDate: moment(e.target.value).toISOString(),
+                        checkDate: moment.utc(e.target.value).toISOString(),
                       });
                     }}
                     InputLabelProps={{
