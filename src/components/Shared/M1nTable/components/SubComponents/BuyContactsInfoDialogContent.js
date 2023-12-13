93058eed66627fa29262c8b3012b8d106215edfc
iamzishan78 <zishannaseeb528@gmail.com>
2023-12-13 16:05:07 +0500

    add loading in contact data dialog

--- src/components/Shared/M1nTable/components/SubComponents/BuyContactsInfoDialogContent.js
+++ src/components/Shared/M1nTable/components/SubComponents/BuyContactsInfoDialogContent.js
@@ -87,6 +87,7 @@ export default function BuyContactsInfoDialogContent(props) {
   const [buyNowClicked, setBuyNowClicked] = useState(false);
   const [dataFetched, setDataFetched] = useState(false);
   const [currentCredits, setCurrentCredits] = useState(-1);
+  const [rowsLoading, setRowsLoading] = useState(false);
   const modalClass = Modals();
 
   const [getFeatureQuota, { loading, data: quota }] =
@@ -173,8 +174,9 @@ export default function BuyContactsInfoDialogContent(props) {
 
   useEffect(() => {
     if (!props.rows || props.rows.length === 0) {
-      props.onClose();
+      setRowsLoading(true);
     } else {
+      setRowsLoading(false);
       const missingContacts = [];
       let validContacts = [];
       for (const row of props.rows) {
@@ -203,157 +205,164 @@ export default function BuyContactsInfoDialogContent(props) {
 
   return (
     <React.Fragment>
-      <>
-        <DialogTitle
-          id="customized-dialog-title"
-          onClose={props.onClose}
-        >
-          {props.header ? props.header : "Contact Info Purchase"}
-        </DialogTitle>
-        <DialogContent>
-          <Grid container spacing={1}>
-            {props.header === "Contact Data Integration" && (
-              <>
-                <Grid item xs={12}>
-                  <h3
-                    style={{ padding: 0, marginTop: "20px", marginBottom: 0 }}
-                  >
-                    Available search credits
+      {rowsLoading ? (
+        <div className={modalClass.loaderWrapper}>
+          <CircularProgress color="secondary" className={modalClass.loader} size={80} disableShrink />
+        </div>
+      ) : (
+        <>
+          <DialogTitle
+            id="customized-dialog-title"
+            onClose={props.onClose}
+          >
+            {props.header ? props.header : "Contact Info Purchase"}
+          </DialogTitle>
+          <DialogContent>
+            <Grid container spacing={1}>
+              {props.header === "Contact Data Integration" && (
+                <>
+                  <Grid item xs={12}>
+                    <h3
+                      style={{ padding: 0, marginTop: "20px", marginBottom: 0 }}
+                    >
+                      Available search credits
+                    </h3>
+                  </Grid>
+                  <Grid item xs={12} className={modalClass.inputContainer}>
+                    <FormLabel className={modalClass.inputLabel}>
+                      Current Balance
+                    </FormLabel>
+                    <FormLabel className={modalClass.inputContent}>
+                      {currentCredits} Credit
+                      {currentCredits && currentCredits > 1 ? "s" : ""}
+                    </FormLabel>
+                  </Grid>
+                </>
+              )}
+
+              <Grid item xs={12} style={{ marginTop: "50px" }}>
+                <h3 style={{ margin: "0" }}>Contacts missing required information (will be excluded)</h3>
+              </Grid>
+
+              {contactDataMissing &&
+                contactDataMissing.map((row, index) => (
+                  <Grid item xs={12} className={modalClass.inputContainer}>
+                    <FormLabel className={modalClass.inputLabel}>
+                      {`${row.name}`}
+                    </FormLabel>
+                    <FormLabel className={modalClass.inputContent}>
+                      <div className="flex jusifyEnd alignCenter">
+                        {setMissingLabelsFunc(row) && setMissingLabelsFunc(row).map((label, index) => (
+                          <p key={index + 1} className={classes.label}>{label}</p>
+                        ))}
+                      </div>
+
+                    </FormLabel>
+                  </Grid>
+                ))}
+
+              <Grid item xs={12}>
+                <p style={{ margin: "0" }}>Note: contact data enrichment integration requires contacts to have a valid first name, last name and address entered.</p>
+              </Grid>
+
+              <Grid item xs={12} style={{ marginTop: "50px" }}>
+                {validContactData && validContactData.length > 0 && (<h3 style={{ margin: "0" }}>Selected contacts with valid information</h3>)}
+                {validContactData && validContactData.length === 0 && (
+                  <div className="flex justifyStart alignCenter" style={{ margin: "0 0 8px 0" }}>
+                    <WarningRoundedIcon style={{ fill: "#000000" }} />
+                    <h3 style={{ margin: "0 0 0 8px" }}>No selected contacts with valid information</h3>
+                  </div>
+                )}
+              </Grid>
+              {validContactData &&
+                validContactData.map((row, index) => (
+                  <Grid item xs={12} className={modalClass.inputContainer}>
+                    <FormLabel className={modalClass.inputLabel}>
+                      {`${row.firstName} ${row.lastName}`}
+                    </FormLabel>
+                    <FormLabel className={modalClass.inputContent}>
+                      {dataFetched && (
+                        <>
+                          {idiCoreData.getIdiCoreData.data?.find(contact => contact.contactId === row.id) ? (
+                            <span className={classes.iconsSuccess}>
+                              <Tooltip title='Data found for contact' >
+                                <CheckCircleIcon />
+                              </Tooltip>
+                            </span>
+                          ) : (
+                            <span className={classes.iconsError}>
+                              <Tooltip title='No data found for contact' >
+                                <ErrorIcon />
+                              </Tooltip>
+                            </span>
+                          )}
+                        </>
+                      )}
+                      {!dataFetched && (
+                        <DeleteOutlinedIcon
+                          fontSize="small"
+                          style={{ cursor: "pointer", float: "right" }}
+                          onClick={() => {
+                            let reducedRows = [...props.rows];
+                            reducedRows.splice(index, 1);
+                            props.setRows(reducedRows);
+                          }}
+                        />
+                      )}
+                    </FormLabel>
+                  </Grid>
+                ))}
+
+              {validContactData && validContactData.length > 0 && (
+                <Grid item xs={12} className={modalClass.greyedInputContainer}>
+                  <h3 style={{ float: "left", margin: "5px" }}>TOTAL</h3>
+                  <h3 style={{ float: "right", margin: "5px" }}>
+                    {validContactData && validContactData.length ? validContactData.length : ""}{" "}
+                    CREDIT
+                    {validContactData && validContactData.length && validContactData.length > 1
+                      ? "S"
+                      : ""}
                   </h3>
                 </Grid>
-                <Grid item xs={12} className={modalClass.inputContainer}>
-                  <FormLabel className={modalClass.inputLabel}>
-                    Current Balance
-                  </FormLabel>
-                  <FormLabel className={modalClass.inputContent}>
-                    {currentCredits} Credit
-                    {currentCredits && currentCredits > 1 ? "s" : ""}
-                  </FormLabel>
-                </Grid>
-              </>
-            )}
-
-            <Grid item xs={12} style={{ marginTop: "50px" }}>
-              <h3 style={{ margin: "0" }}>Contacts missing required information (will be excluded)</h3>
-            </Grid>
-
-            {contactDataMissing &&
-              contactDataMissing.map((row, index) => (
-                <Grid item xs={12} className={modalClass.inputContainer}>
-                  <FormLabel className={modalClass.inputLabel}>
-                    {`${row.name}`}
-                  </FormLabel>
-                  <FormLabel className={modalClass.inputContent}>
-                    <div className="flex jusifyEnd alignCenter">
-                      {setMissingLabelsFunc(row) && setMissingLabelsFunc(row).map((label, index) => (
-                        <p key={index + 1} className={classes.label}>{label}</p>
-                      ))}
-                    </div>
-
-                  </FormLabel>
-                </Grid>
-              ))}
-
-            <Grid item xs={12}>
-              <p style={{ margin: "0" }}>Note: contact data enrichment integration requires contacts to have a valid first name, last name and address entered.</p>
-            </Grid>
-
-            <Grid item xs={12} style={{ marginTop: "50px" }}>
-              {validContactData && validContactData.length > 0 && (<h3 style={{ margin: "0" }}>Selected contacts with valid information</h3>)}
-              {validContactData && validContactData.length === 0 && (
-                <div className="flex justifyStart alignCenter" style={{ margin: "0 0 8px 0" }}>
-                  <WarningRoundedIcon style={{ fill: "#000000" }} />
-                  <h3 style={{ margin: "0 0 0 8px" }}>No selected contacts with valid information</h3>
-                </div>
               )}
-            </Grid>
-            {validContactData &&
-              validContactData.map((row, index) => (
-                <Grid item xs={12} className={modalClass.inputContainer}>
-                  <FormLabel className={modalClass.inputLabel}>
-                    {`${row.firstName} ${row.lastName}`}
-                  </FormLabel>
-                  <FormLabel className={modalClass.inputContent}>
-                    {dataFetched && (
-                      <>
-                        {idiCoreData.getIdiCoreData.data?.find(contact => contact.contactId === row.id) ? (
-                          <span className={classes.iconsSuccess}>
-                            <Tooltip title='Data found for contact' >
-                              <CheckCircleIcon />
-                            </Tooltip>
-                          </span>
-                        ) : (
-                          <span className={classes.iconsError}>
-                            <Tooltip title='No data found for contact' >
-                              <ErrorIcon />
-                            </Tooltip>
-                          </span>
-                        )}
-                      </>
-                    )}
-                    {!dataFetched && (
-                      <DeleteOutlinedIcon
-                        fontSize="small"
-                        style={{ cursor: "pointer", float: "right" }}
-                        onClick={() => {
-                          let reducedRows = [...props.rows];
-                          reducedRows.splice(index, 1);
-                          props.setRows(reducedRows);
-                        }}
-                      />
-                    )}
-                  </FormLabel>
-                </Grid>
-              ))}
 
-            {validContactData && validContactData.length > 0 && (
-              <Grid item xs={12} className={modalClass.greyedInputContainer}>
-                <h3 style={{ float: "left", margin: "5px" }}>TOTAL</h3>
-                <h3 style={{ float: "right", margin: "5px" }}>
-                  {validContactData && validContactData.length ? validContactData.length : ""}{" "}
-                  CREDIT
-                  {validContactData && validContactData.length && validContactData.length > 1
-                    ? "S"
-                    : ""}
-                </h3>
-              </Grid>
+            </Grid>
+          </DialogContent>
+          <DialogActions className={modalClass.actionButtons}>
+            <Button
+              onClick={() => {
+                props.onClose();
+              }}
+              color="primary"
+            >
+              Cancel
+            </Button>
+            <Button
+              onClick={() => {
+                loadPersonData();
+              }}
+              disabled={buyNowClicked || idiLoading || validContactData.length === 0}
+              color="secondary"
+              variant="contained"
+            >
+              Buy Now
+            </Button>
+            {idiLoading && (
+              <CircularProgress
+                size={24}
+                style={{
+                  position: 'absolute',
+                  marginTop: '2px',
+                  marginRight: '35px',
+                  color: 'green',
+                }}
+              />
             )}
 
-          </Grid>
-        </DialogContent>
-        <DialogActions className={modalClass.actionButtons}>
-          <Button
-            onClick={() => {
-              props.onClose();
-            }}
-            color="primary"
-          >
-            Cancel
-          </Button>
-          <Button
-            onClick={() => {
-              loadPersonData();
-            }}
-            disabled={buyNowClicked || idiLoading || validContactData.length === 0}
-            color="secondary"
-            variant="contained"
-          >
-            Buy Now
-          </Button>
-          {idiLoading && (
-            <CircularProgress
-              size={24}
-              style={{
-                position: 'absolute',
-                marginTop: '2px',
-                marginRight: '35px',
-                color: 'green',
-              }}
-            />
-          )}
-
-        </DialogActions>
-      </>
+          </DialogActions>
+        </>
+      )
+      }
     </React.Fragment>
   );
 }
