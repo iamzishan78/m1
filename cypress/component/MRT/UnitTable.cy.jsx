70ba6545384dcbea232aba67a67768b98d896b6f
iamzishan78 <zishannaseeb528@gmail.com>
2024-02-01 23:54:13 +0500

    interceptAndWait command created

--- cypress/component/MRT/UnitTable.cy.jsx
+++ cypress/component/MRT/UnitTable.cy.jsx
@@ -1,21 +1,16 @@
 /* eslint-disable no-undef */
 import MRTTable from 'components/MRTTable';
-import { basic_timeouts } from '../../cypressUtils/data';
 
 const columns = [{ name: 'M1neral System ID' }];
 
 describe('UnitInterest Table', () => {
   beforeEach(() => {
-    cy.interceptApiByIndex('getESSimpleSearch', 'shapes_flat');
-
-    cy.viewport(1600, 1200).mount(<MRTTable name="UnitTable" />);
+    cy.interceptAndWait(['getESSimpleSearch', 'shapes_flat'], () => {
+      cy.viewport(1600, 1200).mount(<MRTTable name="UnitTable" />);
+    });
   });
 
   it('exports with M1neral System ID', () => {
-    cy.verifyApiResponse('@getESSimpleSearchApiByIndex', {
-      responseTimeout: basic_timeouts.midTimeout,
-    });
-
     cy.mrtExport({ columns });
   });
 });
