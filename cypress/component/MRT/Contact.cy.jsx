2fd7a52558b82489c68fd6289df46ba367cb68b2
iamzishan78 <zishannaseeb528@gmail.com>
2024-02-13 11:10:06 +0500

    checks created at/by and updated at/by fields in contact grid

--- cypress/component/MRT/Contact.cy.jsx
+++ cypress/component/MRT/Contact.cy.jsx
@@ -1,82 +1,110 @@
 /* eslint-disable no-undef */
-import MRTTable from 'components/MRTTable';
-import { basic_timeouts, retries } from '../../cypressUtils/data';
-import moment from 'moment';
+import MRTTable from "components/MRTTable";
+import { basic_timeouts, retries } from "../../cypressUtils/data";
+import moment from "moment";
 
 let responseHits = [];
 
 const columns = [
   {
-    name: 'Last Updated Date',
-    type: 'date',
+    name: "Last Updated Date",
+    type: "date",
   },
   {
-    name: 'Name',
-    type: 'string',
-    selector: 'div > p > div > div > a',
+    name: "Name",
+    type: "string",
+    selector: "div > p > div > div > a",
   },
 ];
 
-describe('Contact Table', () => {
+describe("Contact Table", () => {
   beforeEach(() => {
-    cy.interceptAndWait(['getESSimpleSearch'], (alias) => {
-      cy.viewport(1600, 1200).mount(<MRTTable name="ContactTable" />);
-      cy.wait(alias, { timeout: basic_timeouts.longTimeout }).then((response) => {
-        responseHits = response.response.body.data.getESSimpleSearch.hits;
-      });
-    }, { wait: false });
+    cy.interceptAndWait(
+      ["getESSimpleSearch"],
+      (alias) => {
+        cy.viewport(1600, 1200).mount(<MRTTable name="ContactTable" />);
+        cy.wait(alias, { timeout: basic_timeouts.longTimeout }).then(
+          (response) => {
+            responseHits = response.response.body.data.getESSimpleSearch.hits;
+          }
+        );
+      },
+      { wait: false }
+    );
   });
 
-  it('sorts by Name & Last Updated', () => {
+  it("sorts by Name & Last Updated", () => {
     cy.wait(100);
 
     cy.mrtSortColumn({ column: columns[0] });
     cy.mrtSortColumn({ column: columns[1] });
   });
 
-  it(
-    'Filters by Name & Last Updated Single Select',
-    retries.fiveTries,
-    () => {
-      cy.get('.MuiButtonBase-root[aria-label="Show/Hide filters"]').click();
+  it("Filters by Name & Last Updated Single Select", retries.fiveTries, () => {
+    cy.get('.MuiButtonBase-root[aria-label="Show/Hide filters"]').click();
 
-      cy.mrtSingleSelect({ column: columns[0] });
-      cy.mrtSingleSelect({ column: columns[1] });
-    }
-  );
+    cy.mrtSingleSelect({ column: columns[0] });
+    cy.mrtSingleSelect({ column: columns[1] });
+  });
 
-  it('Filters by Name Multi Select', retries.fiveTries, () => {
+  it("Filters by Name Multi Select", retries.fiveTries, () => {
     cy.get('.MuiButtonBase-root[aria-label="Show/Hide filters"]').click();
     cy.mrtMultiSelect({ column: columns[1] });
   });
 
-  it('Filters by last updated Comparison Check', retries.fiveTries, () => {
-    console.log(responseHits)
+  it("Filters by last updated Comparison Check", retries.fiveTries, () => {
+    console.log(responseHits);
     if (responseHits?.length) {
       const placeholder = "Filter by Last Updated Date";
       cy.get('.MuiButtonBase-root[aria-label="Show/Hide filters"]').click();
 
       const lastUpdateAt = responseHits[0].lastUpdateAt;
       const currentDateForPreviousDay = new Date(lastUpdateAt);
-      currentDateForPreviousDay.setDate(currentDateForPreviousDay.getDate() - 1)
-      const oneDayPriorDate = currentDateForPreviousDay.toISOString().split('T')[0];
+      currentDateForPreviousDay.setDate(
+        currentDateForPreviousDay.getDate() - 1
+      );
+      const oneDayPriorDate = currentDateForPreviousDay
+        .toISOString()
+        .split("T")[0];
       cy.get(`[data-testid="MoreVertIcon"]`).eq(19).click();
       cy.wait(500);
       cy.get('[data-testid="sentinelStart"] + div ul li:nth-child(5)').click();
       cy.wait(500);
-      cy.get('[data-testid="sentinelStart"] + div ul li:nth-child(1):eq(1)').click();
-      cy.mrtComparisonFilterCheck({ column: columns[0], type: "date", value: moment.parseZone(new Date(oneDayPriorDate)).format('MM/DD/YY'), filter: "greaterThanEqualTo", placeholder });
+      cy.get(
+        '[data-testid="sentinelStart"] + div ul li:nth-child(1):eq(1)'
+      ).click();
+      cy.mrtComparisonFilterCheck({
+        column: columns[0],
+        type: "date",
+        value: moment.parseZone(new Date(oneDayPriorDate)).format("MM/DD/YY"),
+        filter: "greaterThanEqualTo",
+        placeholder,
+      });
 
       const currentDateForNextDay = new Date(lastUpdateAt);
-      currentDateForNextDay.setDate(currentDateForNextDay.getDate() + 1)
-      const nextDayDate = currentDateForNextDay.toISOString().split('T')[0];
-      cy.get('[data-testid="MoreVertIcon"]').eq(19).scrollIntoView().click({ force: true });
+      currentDateForNextDay.setDate(currentDateForNextDay.getDate() + 1);
+      const nextDayDate = currentDateForNextDay.toISOString().split("T")[0];
+      cy.get('[data-testid="MoreVertIcon"]')
+        .eq(19)
+        .scrollIntoView()
+        .click({ force: true });
       cy.wait(500);
       cy.get('[data-testid="sentinelStart"] + div ul li:nth-child(5)').click();
       cy.wait(500);
-      cy.get('[data-testid="sentinelStart"] + div ul li:nth-child(2):eq(1)').click();
-      cy.mrtComparisonFilterCheck({ column: columns[0], type: "date", value: moment.parseZone(new Date(nextDayDate)).format('MM/DD/YY'), filter: "lessThanEqualTo", placeholder });
+      cy.get(
+        '[data-testid="sentinelStart"] + div ul li:nth-child(2):eq(1)'
+      ).click();
+      cy.mrtComparisonFilterCheck({
+        column: columns[0],
+        type: "date",
+        value: moment.parseZone(new Date(nextDayDate)).format("MM/DD/YY"),
+        filter: "lessThanEqualTo",
+        placeholder,
+      });
     }
   });
 
+  it("checks created at/by and updated at/by fields in contact grid", () => {
+    cy.VerifyAuthInfoMRT();
+  });
 });
