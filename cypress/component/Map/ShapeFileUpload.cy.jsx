eb4a66d2a35fb26200db4c614d10c074aba2f954
iamzishan78 <zishannaseeb528@gmail.com>
2024-02-13 17:59:09 +0500

    Shapefile grid flyto testcase moved from Map Spec to ShapeFileUpload Spec

--- cypress/component/Map/ShapeFileUpload.cy.jsx
+++ cypress/component/Map/ShapeFileUpload.cy.jsx
@@ -1,6 +1,9 @@
 /* eslint-disable no-undef */
-import MapProvider from 'components/Map/MapProvider';
 import { v4 as uuid } from 'uuid';
+import * as turf from '@turf/turf';
+import MapProvider from 'components/Map/MapProvider';
+import { getUdLayerCardTitle } from 'components/UdLayerCard/UdLayerCard';
+import { popupController } from 'hookstate/popupStateController';
 import { basic_timeouts } from '../../cypressUtils/data';
 
 const { midTimeout, longTimeout, partialLongTimeout } = basic_timeouts;
@@ -50,7 +53,7 @@ describe('Map Component Shape File Upload', () => {
   it('Data exits in dataset grid', () => {
     cy.wait(partialLongTimeout);
 
-    cy.interceptAndWait(['getESSimpleSearch'], () => {
+    cy.interceptAndWait(['getESSimpleSearch', 'shapefile_flat'], () => {
       cy.get(`[id='grid-icon-${sourceName}']`, { timeout: longTimeout })
         .scrollIntoView()
         .click({ force: true });
@@ -62,6 +65,66 @@ describe('Map Component Shape File Upload', () => {
     cy.get('.MuiButtonBase-root[aria-label="Close"]').click();
 
     cy.wait(1000);
+  });
+
+  it('Shapefile grid flyto works', () => {
+    cy.interceptAndWait(['getESSimpleSearch', 'shapefile_flat'], () => {
+      cy.get(`[id='grid-icon-${sourceName}']`, { timeout: longTimeout })
+        .scrollIntoView()
+        .click({ force: true });
+    });
+
+    cy.get('[data-testid="mrt-fly-to-map"]').eq(0).click();
+
+    cy.wait(basic_timeouts.shorTimeout).then(() => {
+      const { selectedShapeFile, selectedUserDefinedLayer } = popupController.getValues([
+        'selectedShapeFile',
+        'selectedUserDefinedLayer',
+      ]);
+
+      expect(!!selectedShapeFile).to.be.equal(true);
+
+      const bounds = window.mapRef.getBounds();
+      const bbox = [
+        bounds.getWest(),
+        bounds.getSouth(),
+        bounds.getEast(),
+        bounds.getNorth(),
+      ];
+      const bboxPolygon = turf.bboxPolygon(bbox);
+
+      let isGeometryWithinBbox = false;
+
+      if (selectedShapeFile.geometry.type === 'MultiPolygon')
+        for (let i = 0; i < selectedShapeFile.geometry.coordinates.length; i++) {
+          let polygon = turf.polygon(selectedShapeFile.geometry.coordinates[i]);
+          if (turf.booleanWithin(polygon, bboxPolygon)) {
+            isGeometryWithinBbox = true;
+            break;
+          }
+        }
+      else
+        isGeometryWithinBbox = turf.booleanWithin(
+          selectedShapeFile.geometry,
+          bboxPolygon
+        );
+
+      expect(isGeometryWithinBbox).to.be.equal(true);
+
+      cy.get('[data-testid="ud-layer-card-header"] .MuiCardHeader-title').contains(
+        getUdLayerCardTitle(selectedUserDefinedLayer)
+      );
+
+      cy.get('[data-testid="ud-layer-card-header"] .MuiCardHeader-subheader').contains(
+        selectedUserDefinedLayer.layer.groupName
+          ? selectedUserDefinedLayer.layer.layerName
+          : ''
+      );
+
+      cy.get('.MuiButtonBase-root[aria-label="Close"]').click();
+
+      cy.wait(1000);
+    });
   });
 
   it('Does not delete the group when a sub dataset is deleted', () => {
