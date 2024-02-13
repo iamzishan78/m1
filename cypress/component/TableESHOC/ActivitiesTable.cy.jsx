8acd0f2e81d4b7b43c0da89a915b9ad681fa23f8
iamzishan78 <zishannaseeb528@gmail.com>
2024-02-13 17:44:17 +0500

    checks created at/by and updated at/by fields in activities grid

---
+++ cypress/component/TableESHOC/ActivitiesTable.cy.jsx
@@ -0,0 +1,28 @@
+/* eslint-disable no-undef */
+import ActivitiesTable from "../../../src/components/Table/Activities/ActivitiesTable";
+
+describe("Activities ESHOC Table", () => {
+  beforeEach(() => {
+    cy.interceptAndWait(["getESSimpleSearch", "activities_flat"], () => {
+      cy.viewport(1600, 1200).mount(
+        <ActivitiesTable
+          activityFilterByType={"all"}
+          activityFilterByTime={"all"}
+          activityFilterByOwner={"all"}
+          esIndex={"activities_flat"}
+          searchFields={["name", "_all"]}
+          filtersChange={() => {}}
+          appliedFilters={null}
+          filterToggle={null}
+          targetLabel={"activitiesDashboard"}
+          header="Activities"
+          parent="Activities"
+        />
+      );
+    });
+  });
+
+  it("checks created at/by and updated at/by fields in activities grid", () => {
+    cy.VerifyAuthInfoECHOC();
+  });
+});
