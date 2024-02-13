968e45fa1b79836cf3ee2aedf4406d657d0e8a49
iamzishan78 <zishannaseeb528@gmail.com>
2024-02-13 17:59:36 +0500

    source remove bug fix

--- src/components/MapControls/components/SourceLayerManager/DeleteSourceAndCategoryConfirmationDialog.js
+++ src/components/MapControls/components/SourceLayerManager/DeleteSourceAndCategoryConfirmationDialog.js
@@ -30,7 +30,11 @@ export default function DeleteSourceAndCategoryConfirmationDialog(props) {
 
   const isSource = !props.actionItem?.category
   const title = isSource ? 'Datasource' : 'Category';
-  const layers = stateApp.layers.filter((layer) => (isSource ? layer.file === props.actionItem.dataset?.file : layer.file === props.actionItem.dataset?.file && layer.layerGeometry === props.actionItem.category.layerGeometry))
+  const layers = stateApp.layers.filter(layer =>
+    isSource
+      ? layer.file === props.actionItem.dataset?.file
+      : layer.file === props.actionItem.dataset?.file && layer.layerShapeName === props.actionItem.category.layerShapeName
+  );
 
   useEffect(() => {
     if (layersDeleted && layersDeleted.updateManyLayer) {
@@ -67,7 +71,7 @@ export default function DeleteSourceAndCategoryConfirmationDialog(props) {
     }
     updateDataset({ variables: { dataset: props.actionItem.dataset } })
     if (layers.length > 0) {
-      if (layers[0].groupId && !props?.actionItem?.dataset?.categories?.length > 2) {
+      if (layers[0].groupId && (isSource || props?.actionItem?.dataset?.categories?.length < 2)) {
         removeLayerGroup({
           variables: {
             userId: stateApp.user.mongoId,
