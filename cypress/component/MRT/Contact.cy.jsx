3fcf2c287dad485ab8665868da884bf08a1afdae
iamzishan78 <zishannaseeb528@gmail.com>
2024-02-26 13:28:13 +0500

    add test case to verify primary address field in export response

--- cypress/component/MRT/Contact.cy.jsx
+++ cypress/component/MRT/Contact.cy.jsx
@@ -2,8 +2,23 @@
 import MRTTable from 'components/MRTTable';
 import { basic_timeouts, retries } from '../../cypressUtils/data';
 import moment from 'moment';
+import ldata from '../../fixtures/ldata.json';
+import { GET_JOBS_STATUS } from 'graphQL/useQueryGetJobStatus';
 
 let responseHits = [];
+const getJobPayload = {
+  operationName: 'getJobsStatus',
+  variables: {
+    "userId": "659ce7cf97935e0ffa857858",
+    "showProgress": true
+  },
+  query: GET_JOBS_STATUS.loc.source.body,
+};
+
+const headers = {
+  'Content-Type': 'application/json',
+  'X-ZUMO-AUTH': ldata.x_zumo_auth,
+};
 
 const columns = [
   {
@@ -17,12 +32,37 @@ const columns = [
   },
 ];
 
+const checkPrimaryAddress = (job) => {
+  cy.wrap(job.resultsPayload.datasets[0].exportResponse[0]["Primary Address"])
+    .should('exist')
+    .and('not.be.empty');
+}
+
+const pollExportJobStatus = (ldata, headers, getJobPayload, jobId, callback) => {
+  cy.request({
+    method: 'POST',
+    url: ldata.url,
+    headers: headers,
+    body: getJobPayload,
+  }).then(response => {
+    const job = response.body.data.getJobsStatus.jobs.find((job) => (job._id === jobId));
+    if (job.status !== "Completed") {
+      cy.wait(5000);
+      pollExportJobStatus(ldata, headers, getJobPayload, jobId, callback);
+    } else {
+      callback(job);
+    }
+  });
+}
+
+
+
 describe('Contact Table', () => {
   beforeEach(() => {
     cy.interceptAndWait(
       ['getESSimpleSearch'],
       alias => {
-        cy.viewport(1600, 1200).mount(<MRTTable name="ContactTable" />);
+        cy.viewport(1600, 1200).mount(<MRTTable name="ContactTable" />, { spec: 'ContactTableSpec' });
         cy.wait(alias, { timeout: basic_timeouts.longTimeout }).then(response => {
           responseHits = response.response.body.data.getESSimpleSearch.hits;
         });
@@ -51,7 +91,6 @@ describe('Contact Table', () => {
   });
 
   it('Filters by last updated Comparison Check', retries.fiveTries, () => {
-    console.log(responseHits);
     if (responseHits?.length) {
       const placeholder = 'Filter by Last Updated';
       cy.get('.MuiButtonBase-root[aria-label="Show/Hide filters"]').click();
@@ -92,5 +131,22 @@ describe('Contact Table', () => {
         placeholder,
       });
     }
+  });
+
+  it('Filters by name and checks is primary address value is exporting', retries.fiveTries, () => {
+    cy.mrtFilterBySearch({
+      value: "CLARK (Cypress do not delete)",
+      columnlabel: "Name",
+      alias: "Name"
+    });
+    cy.get('[data-testid="download-csv"]').click();
+
+    cy.interceptAndWait(['initializeExportJob'], (alias) => {
+      cy.contains('span.MuiButton-label', 'Export').click();
+      cy.wait(alias, { timeout: basic_timeouts.longTimeout }).then((response) => {
+        const jobId = response.response.body.data.initializeExportJob.job._id
+        const exportJob = pollExportJobStatus(ldata, headers, getJobPayload, jobId, checkPrimaryAddress);
+      });
+    }, { wait: false });
   });
 });
--- cypress/component/MRT/commands.js
+++ cypress/component/MRT/commands.js
@@ -317,4 +317,24 @@ Cypress.Commands.add('mrtPurchasedIconCheck', () => {
   });
 });
 
+Cypress.Commands.add('mrtFilterBySearch', ({ value, columnlabel, alias }) => {
+  cy.interceptAndWait(['getESSimpleFilter'], () => {
+    cy.get('.MuiButtonBase-root[aria-label="Show/Hide filters"]').click();
+    cy.get(`[data-testid="single-filter-${columnlabel}"]`).as(`single-filter-${alias}`).click();
+    cy.get(`@single-filter-${alias}`).type(`${value}{enter}`);
+    cy.get('.MuiAutocomplete-option', {
+      timeout: basic_timeouts.midTimeout,
+    })
+      .first()
+      .as(`${columnlabel}-option`)
+      .invoke('text')
+      .then(() => {
+        cy.interceptAndWait(['getESSimpleSearch'], () => {
+          cy.get(`@${columnlabel}-option`).click();
+          cy.wait(5000);
+        }, { wait: false });
+      });
+  });
+});
+
 
