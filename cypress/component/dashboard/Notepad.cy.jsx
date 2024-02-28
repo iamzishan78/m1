90644cc3918020e64ee304880b485470d2e981da
iamzishan78 <zishannaseeb528@gmail.com>
2024-02-28 14:25:53 +0530

    Rename the testcase title

--- cypress/component/dashboard/Notepad.cy.jsx
+++ cypress/component/dashboard/Notepad.cy.jsx
@@ -21,22 +21,20 @@ beforeEach(() => {
 });
 
 
-  it('get notes description from the api and compare with textarea value', () => {
+  it('Get notes description from the API and compare with textarea value', () => {
     cy.wait('@getNoteAlias', { timeout: 10000 }).then((interception) => {
-      // Now you can make assertions on the component or the API response
       const description = interception.response.body.data.getUserNotes.description;
       cy.get('[data-testid="notes-description-text-area"] textarea').invoke('val').then((textareaText) => {
-        // Compare the values
         expect(textareaText).to.equal(description);
       });
     });
   });
 
-  it('Edit description and call save data api', () => {
-    cy.wait(11000); 
+  it('Edit description and call save data API', () => {
+    cy.wait(10000); 
     cy.get('[data-testid="notes-description-text-area"]').type('Edit Notes description');
     cy.get('[data-testid="notes-description-text-area"]').find('textarea').blur();  
-    cy.wait('@createNote', { timeout: 10000 }).then((interception) => {
+    cy.wait('@createNote', { timeout: 12000 }).then((interception) => {
       const description = interception.response.body.data.createNote.description;
       cy.get('[data-testid="notes-description-text-area"] textarea').invoke('val').then((textareaText) => {
         expect(textareaText).to.equal(description);
@@ -44,8 +42,8 @@ beforeEach(() => {
     });
   });
 
-  it('revert changes', () => {
-    cy.wait(12000); // Wait for 1 second
+  it('Revert changes', () => {
+    cy.wait(12000); 
     cy.get('[data-testid="notes-description-text-area"] textarea').type('{selectall}{backspace}');
     cy.get('[data-testid="notes-description-text-area"]').type('Test  Notes description');
     cy.get('[data-testid="notes-description-text-area"]').find('textarea').blur();
