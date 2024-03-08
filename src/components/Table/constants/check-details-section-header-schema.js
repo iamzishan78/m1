f07f693371dd346a10826d1cff1bd5ce3dbaad3b
iamzishan78 <zishannaseeb528@gmail.com>
2024-03-08 13:12:08 +0500

    add check details grid updates

--- src/components/Table/constants/check-details-section-header-schema.js
+++ src/components/Table/constants/check-details-section-header-schema.js
@@ -1,5 +1,7 @@
 
 import ColumnWithLink from "components/Shared/M1nTable/components/SubComponents/ColumnWithLink";
+import { vf_currency_to_fixed } from "components/Shared/valueformatters/vf_currency";
+import vf_number from "components/Shared/valueformatters/vf_number";
 import { GlobalStickyStyles } from "GlobalSettings";
 
 const RevenueStatementHeadCells = [
@@ -43,10 +45,10 @@ const RevenueStatementHeadCells = [
         },
     },
     {
-        name: "number", label: "Property #", esKey: 'property.number.keyword', options: { display: false, sort: false, filter: true, style: { minWidth: 250 }, }
+        name: "propertyName", label: "Property", esKey: 'property.name.keyword', options: { sort: true, filter: true }
     },
     {
-        name: "purchaser", label: "Purchaser", esKey: 'check.payor.name.keyword', options: { sort: true, filter: true }
+        name: "purchaser", label: "Payor", esKey: 'check.payor.name.keyword', options: { sort: true, filter: true }
     },
     {
         name: "checkDate", label: "Check Date", esKey: 'check.checkDate', custom: { key_as_string: true, isDate: true }, options: { sort: true, filter: true }
@@ -61,7 +63,16 @@ const RevenueStatementHeadCells = [
         name: "depositDate", label: "Deposit Date", esKey: 'check.depositDate', custom: { key_as_string: true, isDate: true }, options: { sort: true, filter: true }
     },
     {
-        name: "checkAmount", label: "Check Amount", esKey: 'check.checkAmount', options: { sort: true, filter: true }
+        name: "checkAmount",
+        label: "Check Amount",
+        esKey: 'check.checkAmount',
+        options: {
+            sort: true,
+            filter: true,
+            customRender: (value) => {
+                return value ? <p>{value ? `${vf_currency_to_fixed(value, 2)}` : ""}</p> : <p style={{ color: "#898989b0" }}>N/A</p>;
+            },
+        },
     },
     {
         name: "source", label: "Source", esKey: 'check.source.keyword', options: { sort: true, filter: true }
@@ -70,7 +81,7 @@ const RevenueStatementHeadCells = [
         name: "sourceId", label: "Source Id", esKey: 'check.sourceId.keyword', options: { sort: true, filter: true }
     },
     {
-        name: "propertyName", label: "Property", esKey: 'property.name.keyword', options: { sort: true, filter: true }
+        name: "number", label: "Payor Property #", esKey: 'property.number.keyword', options: { display: false, sort: false, filter: true, style: { minWidth: 250 }, }
     },
     {
         name: "state", label: "State", esKey: 'property.state.keyword', options: { sort: true, filter: true }
@@ -95,16 +106,48 @@ const RevenueStatementHeadCells = [
         name: "price", label: "Avg Price", esKey: 'price', options: { sort: true, filter: true }
     },
     {
-        name: "grossPropertyVolume", label: "Prop Gross Volume", esKey: 'grossPropertyVolume', options: { sort: true, filter: true }
+        name: "grossPropertyVolume",
+        label: "Prop Gross Volume",
+        esKey: 'grossPropertyVolume',
+        options: {
+            sort: true, filter: true,
+            customRender: (value) => {
+                return value ? <p>{value ? vf_number(value, 2) : ""}</p> : <p style={{ color: "#898989b0" }}>--</p>;
+            },
+        },
     },
     {
-        name: "grossPropertyValue", label: "Prop Gross Revenue", esKey: 'grossPropertyValue', options: { sort: true, filter: true }
+        name: "grossPropertyValue",
+        label: "Prop Gross Revenue",
+        esKey: 'grossPropertyValue',
+        options: {
+            sort: true, filter: true,
+            customRender: (value) => {
+                return value ? <p>{value ? vf_number(value, 2) : ""}</p> : <p style={{ color: "#898989b0" }}>--</p>;
+            },
+        },
     },
     {
-        name: "grossOwnerVolume", label: "Gross Owner Volume", esKey: 'grossOwnerVolume', options: { sort: true, filter: true }
+        name: "grossOwnerVolume",
+        label: "Gross Owner Volume",
+        esKey: 'grossOwnerVolume',
+        options: {
+            sort: true, filter: true,
+            customRender: (value) => {
+                return value ? <p>{value ? vf_number(value, 2) : ""}</p> : <p style={{ color: "#898989b0" }}>--</p>;
+            },
+        },
     },
     {
-        name: "grossOwnerValue", label: "Owner Gross Revenue", esKey: 'grossOwnerValue', options: { sort: true, filter: true }
+        name: "grossOwnerValue",
+        label: "Owner Gross Revenue",
+        esKey: 'grossOwnerValue',
+        options: {
+            sort: true, filter: true,
+            customRender: (value) => {
+                return value ? <p>{value ? vf_number(value, 2) : ""}</p> : <p style={{ color: "#898989b0" }}>--</p>;
+            },
+        },
     },
     {
         name: "ownerTax", label: "Owner Tax Amt", esKey: 'ownerTax', options: { sort: true, filter: true }
