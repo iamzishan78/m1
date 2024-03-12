022e4acbf44d398d202304dc4537b88d0291f5d0
iamzishan78 <zishannaseeb528@gmail.com>
2024-03-12 13:19:41 +0530

    Add the test cases for open property interest detail view

---
+++ cypress/component/DetailPages/Analytics.cy.jsx
@@ -0,0 +1,27 @@
+/* eslint-disable no-undef */
+import { RevenuePropertyDetails } from "components/Revenue/components";
+import MRTTable from 'components/MRTTable';
+import React from 'react';
+
+// Describe block for testing the AgreementFieldsSection
+describe('AnalyticsSection', () => {
+    beforeEach(() => {
+        cy.interceptAndWait(
+          ['getESSimpleSearch'],
+          alias => {
+            cy.viewport(1600, 1200).mount(<MRTTable name="PropertyIntrestTable" />, {
+              spec: 'PropertyIntrestTableSpec',
+            });
+          },
+          { wait: false }
+        );
+      });
+
+    it('Open detail view of property interest', () => {
+      cy.get(`tr.MuiTableRow-root[data-index="${0}"] > td.MuiTableCell-root`)
+      .eq(2)
+      .find('div > div > a').click();
+      cy.mount(<RevenuePropertyDetails />);
+    });
+
+});
