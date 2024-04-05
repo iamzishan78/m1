ac78c57aeb3922c607423923f94675b639ea675b
iamzishan78 <zishannaseeb528@gmail.com>
2024-04-05 11:35:36 +0500

    deal update test case fixed

--- cypress/component/DetailPages/Contact.cy.jsx
+++ cypress/component/DetailPages/Contact.cy.jsx
@@ -1,38 +1,34 @@
-import ContactDetailCard from 'components/ContactDetailCard/ContactDetailCard';
-const { basic_timeouts } = require('../../cypressUtils/data');
+/* eslint-disable no-undef */
 import React from 'react';
+import ContactDetailCard from 'components/ContactDetailCard/ContactDetailCard';
+import { basic_timeouts } from '../../cypressUtils/data';
 
 // Describe block for testing the ContactDetailsSection
 describe('ContactDetailsSection', () => {
   beforeEach(() => {
-    cy.interceptAndWait(
-      ['getContact'],
-      (alias) => {
-        // Mounting the ContactDetailCard component with predefined props
-        cy.viewport(1600, 1200).mount(
-          <ContactDetailCard contactId="65ad9213ede1b8fc69df499f" />,
-          {
-            // Providing additional props for test case execution
-            testCase: {
-              contactId: '65ad9213ede1b8fc69df499f',
-            },
-          }
-        );
-      },
-      // Options for interception
-      { wait: false }
-    );
+    cy.interceptAndWait(['getContact'], () => {
+      // Mounting the ContactDetailCard component with predefined props
+      cy.viewport(1600, 1200).mount(
+        <ContactDetailCard contactId="65ad9213ede1b8fc69df499f" />,
+        {
+          // Providing additional props for test case execution
+          testCase: {
+            contactId: '65ad9213ede1b8fc69df499f',
+          },
+        }
+      );
+    });
   });
 
   const addDealNameString = 'test deal';
-  const updateDealNameString = '123';
+  const updateDealNameString = addDealNameString + ' updated';
 
   // Test case to verify contact deal creation
   it('Add contact deal', () => {
     // Clicking on deals menu to load deals grid
     cy.interceptAndWait(['getContactDeals'], () => {
       cy.get('#Deals', {
-        timeout: basic_timeouts.midTimeout,
+        timeout: basic_timeouts.longTimeout,
       })
         .scrollIntoView()
         .click({ force: true });
@@ -44,19 +40,18 @@ describe('ContactDetailsSection', () => {
     cy.get('textarea[placeholder="Click to enter deal name"]')
       .click()
       .wait(100)
+      .clear()
       .type(addDealNameString);
 
     // Waiting for the creation response and processing the response data
     cy.interceptAndWait(
       ['addDeal'],
-      (alias) => {
+      alias => {
         cy.get('[data-testid="add-deal-icon-button"]').click();
 
-        cy.wait(alias).then((creationResponse) => {
+        cy.wait(alias).then(creationResponse => {
           expect(creationResponse?.response?.statusCode).to.eq(200);
-          expect(
-            creationResponse?.response?.body?.data?.addDeal?.success
-          ).to.eq(true);
+          expect(creationResponse?.response?.body?.data?.addDeal?.success).to.eq(true);
         });
       },
       { wait: false }
@@ -68,38 +63,33 @@ describe('ContactDetailsSection', () => {
     // Clicking on deals menu to load deals grid
     cy.interceptAndWait(['getContactDeals'], () => {
       cy.get('#Deals', {
-        timeout: basic_timeouts.midTimeout,
+        timeout: basic_timeouts.longTimeout,
       })
         .scrollIntoView()
         .click({ force: true });
     });
 
-    cy.get('tr.MuiTableRow-root') // Find all table rows
-      .contains('td.MuiTableCell-root > div', addDealNameString) // Find the cell whoes value equal to deal name
+    cy.get('td[data-colindex="1"]') // Find all table rows 1st column
+      .contains(addDealNameString) // Find the cell whoes value equal to deal name
       .click();
 
     // Selecting the textarea for updating deal name
     cy.get('textarea[placeholder="Click to enter deal name"]')
       .click()
       .wait(100)
+      .clear()
       .type(updateDealNameString);
 
     // Waiting for the creation response and processing the response data
     cy.interceptAndWait(
       ['updateDeal'],
-      (alias) => {
-        cy.get(
-          '.MuiButtonBase-root[data-testid="add-deal-icon-button"]'
-        ).click();
+      alias => {
+        cy.get('.MuiButtonBase-root[data-testid="add-deal-icon-button"]').click();
 
-        cy.wait(alias, { timeout: basic_timeouts.longTimeout }).then(
-          (updateResponse) => {
-            expect(updateResponse?.response?.statusCode).to.eq(200);
-            expect(
-              updateResponse?.response?.body?.data?.updateDeal?.success
-            ).to.eq(true);
-          }
-        );
+        cy.wait(alias, { timeout: basic_timeouts.longTimeout }).then(updateResponse => {
+          expect(updateResponse?.response?.statusCode).to.eq(200);
+          expect(updateResponse?.response?.body?.data?.updateDeal?.success).to.eq(true);
+        });
       },
       { wait: false }
     );
@@ -110,23 +100,18 @@ describe('ContactDetailsSection', () => {
     // Clicking on deals menu to load deals grid
     cy.interceptAndWait(['getContactDeals'], () => {
       cy.get('#Deals', {
-        timeout: basic_timeouts.midTimeout,
+        timeout: basic_timeouts.longTimeout,
       })
         .scrollIntoView()
         .click({ force: true });
     });
 
-    cy.get('tr.MuiTableRow-root') // Find all table rows
-      .contains(
-        'td.MuiTableCell-root > div',
-        addDealNameString + updateDealNameString
-      ) // Find the cell whoes value equal to deal name
+    cy.get('td[data-colindex="1"]') // Find all table rows 1st column
+      .contains(updateDealNameString) // Find the cell whoes value equal to deal name
       .click();
 
     // Click on delete icon button
-    cy.get(
-      '.MuiButtonBase-root[data-testid="delete-deal-icon-button"]'
-    ).click();
+    cy.get('.MuiButtonBase-root[data-testid="delete-deal-icon-button"]').click();
 
     // Click on delete menu item
     cy.get('li[data-testid="delete-confirm"]').click({ force: true });
@@ -134,15 +119,13 @@ describe('ContactDetailsSection', () => {
     // Waiting for the creation response and processing the response data
     cy.interceptAndWait(
       ['updateDeal'],
-      (alias) => {
+      alias => {
         // Click on delete dialog button
         cy.get('#deleteButton').click();
 
-        cy.wait(alias).then((deleteResponse) => {
+        cy.wait(alias).then(deleteResponse => {
           expect(deleteResponse?.response?.statusCode).to.eq(200);
-          expect(
-            deleteResponse?.response?.body?.data?.updateDeal?.success
-          ).to.eq(true);
+          expect(deleteResponse?.response?.body?.data?.updateDeal?.success).to.eq(true);
         });
       },
       { wait: false }
