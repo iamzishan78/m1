162b71de280bb00478e937487d2fff1c01fe0c99
iamzishan78 <zishannaseeb528@gmail.com>
2024-04-08 13:06:05 +0500

    expot purchased button issue also resolved

--- src/components/MRTTable/Common/Dialog/ExportContactsPurchaseAndOwners/index.js
+++ src/components/MRTTable/Common/Dialog/ExportContactsPurchaseAndOwners/index.js
@@ -110,6 +110,11 @@ const ExportContactsAndPurchase = ({
     }
 
     onClose();
+    let sortOrder = {};
+    if (Object.keys(sort).length > 0) {
+      sortOrder = { field: `${sort?.field}.keyword`, order: sort?.order };
+    }
+
     dispatch(execCommonAsyncExportJobAction.STARTED({
       jobType: 'EXPORTCSV',
       client,
@@ -121,7 +126,7 @@ const ExportContactsAndPurchase = ({
         search,
         filters,
         esIndex,
-        sort,
+        sort: sortOrder,
         isSelectAll: isAllRowsSelected,
         contactIdKey,
         datasets,
