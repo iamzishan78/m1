f3880be9cd5f4864711643dc9d023f5449f0a638
iamzishan78 <zishannaseeb528@gmail.com>
2024-04-08 17:05:32 +0530

    Add new column purchased data exists in unit grid

--- src/components/MRTTable/Schema/ownersperunit-schema.js
+++ src/components/MRTTable/Schema/ownersperunit-schema.js
@@ -515,6 +515,23 @@ const OwnersPerUnitMeta = {
 		},
 
 		{
+			...CommonSchema.COMMON_COLUMN,
+			name: 'contact.isPurchased',
+			accessorFn: row => row?.contact?.isPurchased,
+			header: 'Purchased Data Exists',
+			id: 'contact.isPurchased',
+			filterSelectOptions: [
+				{ label: 'Yes', value: 'true' },
+				{ label: 'No', value: 'false' },
+			],
+			Cell: ({ row }) => {
+				const isPurchased = [true, 'true', 'True'].includes(row.getValue('contact.isPurchased'));
+				return <>{isPurchased ? 'Yes' : 'No'}</>;
+			},
+			isSearchField: false
+		},
+
+		{
 			...CommonSchema.TAGS,
 			Cell: ({ row }) => {
 				const targetSourceId = row.getValue('ownerEntity');
@@ -550,13 +567,6 @@ const OwnersPerUnitMeta = {
 
 				return <ContactActionMenu id={id} name={name} esIndex={esIndex} dialogType="dialog" />;
 			},
-		},
-
-		{
-			...CommonSchema.HIDDEN,
-			name: 'isPurchased',
-			accessorFn: row => row?.contact?.isPurchased,
-			id: 'contact.isPurchased',
 		},
 	],
 };
