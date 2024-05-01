9562e3c42e5ee9618533d709d09d27418c4485f3
iamzishan78 <zishannaseeb528@gmail.com>
2024-05-01 14:32:43 +0500

    tract uploader headers updated

--- src/components/BulkUpload/jobHeaders/PARCELINTERESTS.js
+++ src/components/BulkUpload/jobHeaders/PARCELINTERESTS.js
@@ -3,6 +3,7 @@ const CONTACTS_FIELDS = require("./CONTACTS").default
 const fields = JSON.parse(JSON.stringify(CONTACTS_FIELDS))
 fields.splice(CONTACTS_FIELDS.length - 1, 1)
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default [
     ...fields,
     {
@@ -84,6 +85,30 @@ export default [
         actual_key: "parcel.actual_offer_price"
     },
     {
+        label: "Target Offer (NRA)",
+        mapped_key: "",
+        required: false,
+        actual_key: "parcel.offer_price"
+    },
+    {
+        label: "Max Offer (NRA)",
+        mapped_key: "",
+        required: false,
+        actual_key: "parcel.max_offer_price"
+    },
+    {
+        label: "Target Offer (NMA)",
+        mapped_key: "",
+        required: false,
+        actual_key: "parcel.offer_price_nma"
+    },
+    {
+        label: "Max Offer (NMA)",
+        mapped_key: "",
+        required: false,
+        actual_key: "parcel.max_offer_price_nma"
+    },
+    {
         label: "Net Royalty Acres",
         mapped_key: "",
         required: false,
@@ -94,6 +119,18 @@ export default [
         mapped_key: "",
         required: false,
         actual_key: "parcel.sdGrossAcres"
+    },
+    {
+        label: "Tract Campaign",
+        mapped_key: "",
+        required: false,
+        actual_key: "parcel.campaignName"
+    },
+    {
+        label: "Non-Exec Rights Only",
+        mapped_key: "",
+        required: false,
+        actual_key: "parcel.nonExecRightsOnly"
     },
     {
         label: "Depth From",
