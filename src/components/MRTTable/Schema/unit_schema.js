8d5aca005a8e2170d304efa8668369457bf2abd8
iamzishan78 <zishannaseeb528@gmail.com>
2024-06-17 17:05:56 +0530

    Add bulk updatebutton & slideout to unit grid

--- src/components/MRTTable/Schema/unit_schema.js
+++ src/components/MRTTable/Schema/unit_schema.js
@@ -13,6 +13,7 @@ import { globalStateController } from 'hookstate/globalStateController';
 import { UPDATECUSTOMLAYER } from "graphQL/useMutationUpdateCustomLayer";
 import { tableGlobalController } from 'hookstate/tableController';
 import { copy } from "utils/helper";
+import UnitToolbar from 'components/MRTTable/TablesOverride/UnitTable/UnitToolbar';
 
 const esIndex = 'shapes_flat';
 
@@ -58,6 +59,7 @@ const onCustomKeyChange = async (client, row, value, item) => {
 const UnitMeta = {
 	esIndex,
 	pageSize: 50,
+	CustomToolBar: UnitToolbar,
 	pagination: {
 		pageIndex: 0,
 		pageSize: 50,
