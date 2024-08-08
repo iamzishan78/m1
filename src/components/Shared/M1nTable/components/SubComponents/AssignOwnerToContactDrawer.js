36af3b6df8c7b8cdb631f9c6308ed1b98f2ac037
iamzishan78 <zishannaseeb528@gmail.com>
2024-08-08 15:40:36 +0500

    merging issue resolved

--- src/components/Shared/M1nTable/components/SubComponents/AssignOwnerToContactDrawer.js
+++ src/components/Shared/M1nTable/components/SubComponents/AssignOwnerToContactDrawer.js
@@ -100,7 +100,7 @@ const useStyles = makeStyles(styles);
  * @param {string} props.fieldKey - Current field value.
  * @param {Array} props.campaigns - Current campaigns array.
  */
-function SelectedField({ field, setFieldKey, setCampaigns, setContactOwner, contactOwner, fieldKey, campaigns }) {
+function SelectedField({ field, setFieldKey, setCampaigns, setContactOwner, contactOwner, fieldKey, campaigns, classes, publicTags }) {
   let filterKey = ''; // Initialize filterKey variable to determine specific filter criteria
 
   // Switch statement to render different input fields based on the field type
@@ -123,7 +123,6 @@ function SelectedField({ field, setFieldKey, setCampaigns, setContactOwner, cont
       return (
         <CampaignNameField
           value={fieldKey}
-          className={classes.maxWidth} // Uses CSS class 'maxWidth'
           onChange={(values, id) => {
             setFieldKey(values); // Sets the field key value
             setCampaigns([...campaigns, { _id: id, campaignName: values[values.length - 1] }]); // Updates campaigns array
@@ -666,6 +665,8 @@ export default function AssignOwnerToContactDrawer({
                     contactOwner={contactOwner}
                     fieldKey={fieldKey}
                     campaigns={campaigns}
+                    classes={classes}
+                    publicTags={publicTags}
                   />
                 </Grid>
               </Grid>
