f7c82f14e0851a1a9dcf18fba9d21eed7dfbcddf
iamzishan78 <zishannaseeb528@gmail.com>
2024-08-19 14:32:12 +0500

    revert ashish changes

--- src/graphQL/useMutationRunFlattening.js
+++
@@ -1,7 +0,0 @@
-import gql from "graphql-tag";
-
-export const FLATTENNING = gql`
-  mutation runFlattening($models: JSON, $chunkSize: Int) {
-    runFlattening(models: $models, chunkSize: $chunkSize)
-  }
-`;
