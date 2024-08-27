8d7a0cd2b86b1c28dc751b39fe554c08c33594d9
iamzishan78 <zishannaseeb528@gmail.com>
2024-08-27 16:30:58 +0500

    Map position fixed

--- src/components/Transact/components/DealDialog/AddDealDialog.js
+++ src/components/Transact/components/DealDialog/AddDealDialog.js
@@ -124,6 +124,9 @@ const useStyles = makeStyles((theme) => ({
     "& .MuiDialog-paper": {
       overflowY: "hidden",
     },
+    '& div[class^="makeStyles-panelContent-"]': {
+      height: "52%",
+    },
   },
   contentRoot: {
     overflowY: "overlay",
@@ -1880,11 +1883,12 @@ function AddDealDialog(props) {
         </RightDialog>
         {stateApp.transactBarView === "Map" && (
           <div
-            maxWidth="calc(100vw - 28vw)"
             style={{
-              position: "relative",
+              position: "absolute",
+              left: 0,
               "z-index": "9999",
-              width: "calc(100vw - 30vw)",
+              width: "71%",
+              height: "50%"
             }}
           >
             <MapProvider
