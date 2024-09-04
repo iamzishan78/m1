7227102b7bf794ac23fd14401a0d97de3dee8906
iamzishan78 <zishannaseeb528@gmail.com>
2024-09-04 17:51:59 +0530

    Set the correct outcome  index

--- src/components/Activities/components/ActivitiesModal.js
+++ src/components/Activities/components/ActivitiesModal.js
@@ -638,7 +638,7 @@ export default function ActivitiesModal({ events, setSelectedActivityId }) {
                 <AutoCompleteAddNewField
                   ref={outcomeFieldRef}
                   queryParams={{
-                    esIndex: "contacts_flat",
+                    esIndex: "activities_flat",
                     filterKey: "outcome.keyword",
                     size: 50,
                   }}
