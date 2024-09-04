c92a1937f03089b7f9fddd26cbd5447e1bd5f037
iamzishan78 <zishannaseeb528@gmail.com>
2024-09-04 23:28:43 +0500

    fixed getCypressLog for pr id type mismatch

--- src/graphQL/useQueryCypressLog.js
+++ src/graphQL/useQueryCypressLog.js
@@ -1,7 +1,7 @@
 const gql = require('graphql-tag');
 
 const GET_CYPRESS_LOG = gql`
-  query getCypressLog($prId: String!, $sourceBranch: String ) {
+  query getCypressLog($prId: Int!, $sourceBranch: String ) {
     getCypressLog(prId: $prId, sourceBranch: $sourceBranch)   
   }
 `;
