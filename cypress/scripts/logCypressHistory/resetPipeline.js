ea88708a67212628368ae4b83cf24aab60768ae8
iamzishan78 <zishannaseeb528@gmail.com>
2024-09-13 00:56:41 +0500

    testing hardcoded pipeliine modes

--- cypress/scripts/logCypressHistory/resetPipeline.js
+++ cypress/scripts/logCypressHistory/resetPipeline.js
@@ -39,6 +39,7 @@ const ResetPipeline = async ({ isUpdateReset }) => {
       response.data.data.resetPipeline
     ) {
       response = response.data.data.resetPipeline;
+      console.log("resetPipeline responnse: ", response)
       return response;
     } else {
       console.error('Invalid response structure:', response);
--- cypress/scripts/logCypressHistory/utils/constants.js
+++ cypress/scripts/logCypressHistory/utils/constants.js
@@ -1,10 +1,12 @@
 const BUILD_ID = process.env.BUILD_ID;
 const SOURCE_BRANCH = process.env.SOURCE_BRANCH;
-const PIPELINE_RUN_MODE = process.env.PIPELINE_RUN_MODE;
-const PIPELINE_TRIGGER_MODE = process.env.PIPELINE_TRIGGER_MODE;
+// const PIPELINE_RUN_MODE = process.env.PIPELINE_RUN_MODE;
+// const PIPELINE_TRIGGER_MODE = process.env.PIPELINE_TRIGGER_MODE;
+const PIPELINE_RUN_MODE = 'reset';
+const PIPELINE_TRIGGER_MODE = 'Manual';
 
-const MAX_PIPELINE_DURATION = 8 * 60 * 1000; // 55 minutes in milliseconds
-const PIPELINE_WARNING_THRESHOLD = 6 * 60 * 1000; // 50 minutes in milliseconds
+const MAX_PIPELINE_DURATION = 10 * 60 * 1000; // 55 minutes in milliseconds
+const PIPELINE_WARNING_THRESHOLD = 8 * 60 * 1000; // 50 minutes in milliseconds
 const PIPELLINE_CHECK_TIME = 1 * 60 * 1000; // 1 minutes in milliseconds
 
 const TENENT = process.env.TENENT;
