d1e36b019cc77abb6e12b68dee1f52faa9cd6b50
iamzishan78 <zishannaseeb528@gmail.com>
2024-09-16 14:52:44 +0500

    snap grid > tract columns matched with asset > tract columns

--- src/components/MRTTable/Schema/tract_schema.js
+++ src/components/MRTTable/Schema/tract_schema.js
@@ -9,6 +9,7 @@ import { globalStateController } from 'hookstate/globalStateController';
 import { UPDATECUSTOMLAYER } from "graphQL/useMutationUpdateCustomLayer";
 import { tableGlobalController } from 'hookstate/tableController';
 import { copy } from "utils/helper";
+import { formatDate } from 'components/Shared/functions';
 
 const esIndex = 'shapes_flat';
 
@@ -157,6 +158,14 @@ const TractMeta = {
 			id: 'shapeJson.properties.shapeArea',
 			header: 'Calc Acres',
 		},
+		// Tract NRA column
+		{
+			...CommonSchema.COMMON_COLUMN,
+			name: 'shapeJson.properties.netRoyalityAcres.calculatedNra.keyword',
+			accessorFn: row => row?.shapeJson?.properties?.netRoyalityAcres?.calculatedNra,
+			id: 'shapeJson.properties.netRoyalityAcres.calculatedNra',
+			header: 'Tract NRA',
+		},
 		{
 			...CommonSchema.COMMON_COLUMN,
 			name: 'shapeJson.properties.execNetAcres.keyword',
@@ -222,6 +231,42 @@ const TractMeta = {
 			header: 'Campaign Name',
 			size: 270,
 			Cell: ({ renderedCellValue }) => <CampaignNameField value={renderedCellValue} fullWidth disabled />,
+		},
+		// Department column
+		{
+			...CommonSchema.COMMON_COLUMN,
+			name: 'shapeJson.properties.department.keyword',
+			accessorFn: row => row?.shapeJson?.properties?.department,
+			id: 'shapeJson.properties.department',
+			header: 'Department',
+		},
+		{
+			...CommonSchema.COMMON_COLUMN,
+			name: 'createBy.name.keyword',
+			accessorFn: row => row?.createBy?.name,
+			id: 'createBy.name',
+			header: 'Created By',
+		},
+		{
+			...CommonSchema.COMMON_COLUMN,
+			name: 'createAt.keyword',
+			id: 'createAt',
+			header: 'Created At',
+			Cell: ({ row }) => <>{formatDate(row?.original?.createAt)}</>, // format date before showing
+		},
+		{
+			...CommonSchema.COMMON_COLUMN,
+			name: 'lastUpdateBy.name.keyword',
+			accessorFn: row => row?.lastUpdateBy?.name,
+			id: 'lastUpdateBy.name',
+			header: 'Last Updated By',
+		},
+		{
+			...CommonSchema.COMMON_COLUMN,
+			name: 'lastUpdateAt.keyword',
+			id: 'lastUpdateAt',
+			header: 'Last Updated Date',
+			Cell: ({ row }) => <>{formatDate(row?.original?.lastUpdateAt)}</>, // format date before showing
 		},
 		{
 			...CommonSchema.TAGS,
