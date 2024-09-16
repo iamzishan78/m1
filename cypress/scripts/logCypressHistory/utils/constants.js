30547a9e233d5f248ff32fb20edb8433ca49ed6f
iamzishan78 <zishannaseeb528@gmail.com>
2024-09-16 15:33:05 +0500

    used hard coded strings

--- cypress/scripts/logCypressHistory/utils/constants.js
+++ cypress/scripts/logCypressHistory/utils/constants.js
@@ -1,7 +1,9 @@
 const BUILD_ID = process.env.BUILD_ID;
 const SOURCE_BRANCH = process.env.SOURCE_BRANCH;
-const PIPELINE_RUN_MODE = process.env.PIPELINE_RUN_MODE;
-const PIPELINE_TRIGGER_MODE = process.env.PIPELINE_TRIGGER_MODE;
+// const PIPELINE_RUN_MODE = process.env.PIPELINE_RUN_MODE;
+// const PIPELINE_TRIGGER_MODE = process.env.PIPELINE_TRIGGER_MODE;
+const PIPELINE_RUN_MODE = 'passed_only';
+const PIPELINE_TRIGGER_MODE = 'Manual';
 
 const MAX_PIPELINE_DURATION = 10 * 60 * 1000; // 55 minutes in milliseconds
 const PIPELINE_WARNING_THRESHOLD = 8 * 60 * 1000; // 50 minutes in milliseconds
