512ce8e5c79c2fe0a5e9ee83ac11717d835c4364
iamzishan78 <zishannaseeb528@gmail.com>
2024-09-19 13:02:14 +0500

    Null checks added to avoid crashing

--- src/components/Revenue/components/Common/MetadataDrawer.js
+++ src/components/Revenue/components/Common/MetadataDrawer.js
@@ -226,7 +226,7 @@ export default function MetadataDrawer(props) {
   }, [files, uploadedFiles, viewFiles]);
 
   useEffect(() => {
-    const owner = props.data.metaOwner?._id ?? props.data.owner;
+    const owner = props.data?.metaOwner?._id ?? props.data?.owner;
     if (owner) {
       setOwnerId(owner);
     }
@@ -330,7 +330,7 @@ export default function MetadataDrawer(props) {
                             {...params}
                             id="source-simple-select-outlined-label"
                             variant="outlined"
-                            value={data.source || ""}
+                            value={data?.source || ""}
                             fullWidth
                             onChange={(e) => {
                               props.onUpdate({ source: e.target.value });
