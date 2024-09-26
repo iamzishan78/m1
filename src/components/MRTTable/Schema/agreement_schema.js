755ab0c25d21ef3de594859afb2ae617062540f1
iamzishan78 <zishannaseeb528@gmail.com>
2024-09-26 18:51:13 +0500

    date filter fixed

--- src/components/MRTTable/Schema/agreement_schema.js
+++ src/components/MRTTable/Schema/agreement_schema.js
@@ -283,7 +283,6 @@ const AgreementMeta = {
       accessorKey: 'shapeJson.properties.extensionDate',
       header: 'Extension Exp Date',
       isSearchField: false,
-      type: 'date',
       Cell: ({ row }) => {
         return <>{formatDate(row?.original?.shapeJson?.properties?.extensionDate)}</>
       },
