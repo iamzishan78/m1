cdf7a27b1d1f5c520921d5bc420797dc5b5e6141
iamzishan78 <zishannaseeb528@gmail.com>
2024-09-27 12:54:40 +0500

    Fixed file upload ui issue

--- src/components/Shared/SidePanel/compoennts/Panel.js
+++ src/components/Shared/SidePanel/compoennts/Panel.js
@@ -376,7 +376,7 @@ function Panel({ type, title, headerButton, handleToggle, onDragEnd, panelItems 
           maxWidth: "425px",
           left: mapControlsStateValues.expandedPanel ? "0px" : type === "marketplace" ? "-567px" : "0px",
           listStyleType: "none",
-          zIndex: "999999", // Z-index to fix dialog overlapping with searchbar
+          zIndex: "1300", // Z-index to fix dialog overlapping with searchbar
         }}
       >
         <StyledMenu
