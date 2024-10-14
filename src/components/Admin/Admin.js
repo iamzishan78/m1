5ff1e8cc8d7e74d6336b9126bc74bca454df1d05
iamzishan78 <zishannaseeb528@gmail.com>
2024-10-14 16:46:32 +0500

    lint

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,111 +1,110 @@
-import React, { useContext, useEffect, useState } from "react";
-import { Switch, Route, useLocation } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
+import React, { useContext, useEffect, useState } from 'react';
+import { Switch, Route, useLocation } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
 
-import { setActiveModule, toggleQuickActionsPanel } from "store/actions/commonActions";
-import { AppContext } from "AppContext";
-import { FEATURES } from "components/Shared/FeatureFlag/common";
+import { setActiveModule, toggleQuickActionsPanel } from 'store/actions/commonActions';
+import { AppContext } from 'AppContext';
+import { FEATURES } from 'components/Shared/FeatureFlag/common';
 
-import FeatureFlag from "components/Shared/FeatureFlag/FeatureFlagComponent";
-import QuickActionPanel from "components/Land/components/QuickActionPanel";
+import FeatureFlag from 'components/Shared/FeatureFlag/FeatureFlagComponent';
+import QuickActionPanel from 'components/Land/components/QuickActionPanel';
 
-import { AdminManagementRoutes } from "utils/data";
-import Map from "./components/Map";
-import AdminSettings from "components/Shared/AdminSettings";
-import Flatten from "components/Admin/Flatten"
-import Reindex from "components/Admin/Reindex"
-import BulkDataEditing from "components/Admin/components/BulkDataEditing";
+import { AdminManagementRoutes } from 'utils/data';
+import Map from './components/Map';
+import AdminSettings from 'components/Shared/AdminSettings';
+import Flatten from 'components/Admin/Flatten';
+import Reindex from 'components/Admin/Reindex';
+import BulkDataEditing from 'components/Admin/components/BulkDataEditing';
 
 const Components = {
-  Map,
-  AdminSettings,
-  Flatten,
-  Reindex,
-  BulkDataEditing,
+	Map,
+	AdminSettings,
+	Flatten,
+	Reindex,
+	BulkDataEditing,
 };
 
 function isM1neralAddress(email) {
-  return email.endsWith('@m1neral.com');
+	return email.endsWith('@m1neral.com');
 }
 
-
 export default function Admin() {
-  const location = useLocation();
-  const [stateApp] = useContext(AppContext);
-  const dispatch = useDispatch();
-  const [allowedPaths, setAllowablePaths] = useState({});
-  const { quickActionsPanelState, activeModule } = useSelector(({ common }) => common);
+	const location = useLocation();
+	const [stateApp] = useContext(AppContext);
+	const dispatch = useDispatch();
+	const [allowedPaths, setAllowablePaths] = useState({});
+	const { quickActionsPanelState, activeModule } = useSelector(({ common }) => common);
 
-  useEffect(() => {
-    const option = Object.values(AdminManagementRoutes).find((item) => {
-      return item.link === location.pathname;
-    });
-    if (option) {
-      dispatch(setActiveModule(option));
-    }
-  }, [location.pathname]);
+	useEffect(() => {
+		const option = Object.values(AdminManagementRoutes).find(item => {
+			return item.link === location.pathname;
+		});
+		if (option) {
+			dispatch(setActiveModule(option));
+		}
+	}, [location.pathname]);
 
-  const handlePanelStateChange = (state) => {
-    dispatch(toggleQuickActionsPanel(state));
-  };
+	const handlePanelStateChange = state => {
+		dispatch(toggleQuickActionsPanel(state));
+	};
 
-  const sidePanelOptions = React.useMemo(() => {
-    const options = {};
-    Object.keys(allowedPaths).forEach((key) => {
-      if (!allowedPaths[key].isExcluded) {
-        options[key] = allowedPaths[key];
-      }
-    });
-    return options;
-  }, [allowedPaths]);
+	const sidePanelOptions = React.useMemo(() => {
+		const options = {};
+		Object.keys(allowedPaths).forEach(key => {
+			if (!allowedPaths[key].isExcluded) {
+				options[key] = allowedPaths[key];
+			}
+		});
+		return options;
+	}, [allowedPaths]);
 
-  useEffect(() => {
-    const allPaths = JSON.parse(JSON.stringify(AdminManagementRoutes));
-    if (!isM1neralAddress(stateApp.user.email)) {
-      delete allPaths["FLATTENING"]
-      delete allPaths["REINDEX"]
-    }
-    const feature = stateApp.user?.features?.find((feature) => feature.name === FEATURES.CONTACTSUBMENU);
-    // const feature = stateApp.user?.features?.find(feature => feature.name === FEATURES.ANALYTICSSUBMENU);
-    const allAllowedPaths = {};
-    if (feature?.JSON) {
-      const data = JSON.parse(feature.JSON);
-      Object.keys(allPaths).forEach((path) => {
-        if (data.options.includes(allPaths[path].value)) {
-          allAllowedPaths[path] = allPaths[path];
-        }
-      });
-    } else {
-      Object.keys(allPaths).forEach((path) => {
-        if (allPaths[path].isDefault) {
-          allAllowedPaths[path] = allPaths[path];
-        }
-      });
-    }
+	useEffect(() => {
+		const allPaths = JSON.parse(JSON.stringify(AdminManagementRoutes));
+		if (!isM1neralAddress(stateApp.user.email)) {
+			delete allPaths['FLATTENING'];
+			delete allPaths['REINDEX'];
+		}
+		const feature = stateApp.user?.features?.find(feature => feature.name === FEATURES.CONTACTSUBMENU);
+		// const feature = stateApp.user?.features?.find(feature => feature.name === FEATURES.ANALYTICSSUBMENU);
+		const allAllowedPaths = {};
+		if (feature?.JSON) {
+			const data = JSON.parse(feature.JSON);
+			Object.keys(allPaths).forEach(path => {
+				if (data.options.includes(allPaths[path].value)) {
+					allAllowedPaths[path] = allPaths[path];
+				}
+			});
+		} else {
+			Object.keys(allPaths).forEach(path => {
+				if (allPaths[path].isDefault) {
+					allAllowedPaths[path] = allPaths[path];
+				}
+			});
+		}
 
-    Object.keys(allPaths).forEach((path) => {
-      if (allPaths[path].featureFlag === 'AlWAYSSHOW') {
-        allAllowedPaths[path] = allPaths[path];
-      }
-    });
-    setAllowablePaths(allAllowedPaths);
-  }, [stateApp?.user]);
+		Object.keys(allPaths).forEach(path => {
+			if (allPaths[path].featureFlag === 'AlWAYSSHOW') {
+				allAllowedPaths[path] = allPaths[path];
+			}
+		});
+		setAllowablePaths(allAllowedPaths);
+	}, [stateApp?.user]);
 
-  return (
-    <FeatureFlag feature={FEATURES.CONTACTSUBMENU}>
-      <QuickActionPanel
-        title="Admin Settings"
-        handlePanelStateChange={handlePanelStateChange}
-        quickActionsPanelState={quickActionsPanelState}
-        activeModule={activeModule}
-        actions={sidePanelOptions}
-      >
-        {Object.values(allowedPaths).map((option) => (
-          <Switch>
-            <Route exact path={option.link} component={Components[option.component]} />
-          </Switch>
-        ))}
-      </QuickActionPanel>
-    </FeatureFlag>
-  );
+	return (
+		<FeatureFlag feature={FEATURES.CONTACTSUBMENU}>
+			<QuickActionPanel
+				title="Admin Settings"
+				handlePanelStateChange={handlePanelStateChange}
+				quickActionsPanelState={quickActionsPanelState}
+				activeModule={activeModule}
+				actions={sidePanelOptions}
+			>
+				{Object.values(allowedPaths).map(option => (
+					<Switch>
+						<Route exact path={option.link} component={Components[option.component]} />
+					</Switch>
+				))}
+			</QuickActionPanel>
+		</FeatureFlag>
+	);
 }
--- src/components/Table/Jobs/BulkDataTable.js
+++ src/components/Table/Jobs/BulkDataTable.js
@@ -1,70 +1,65 @@
-import React, { useEffect } from "react";
-import { Container } from "@material-ui/core";
-import Table from "components/Shared/M1nTable/components/Table";
-import TableESHOC from "../TableESHOC";
-import TableHeader from "components/Table/constants/bulk-data-header-schema";
-import { CREATE_JOB } from "graphQL/useMutationCreateJob";
-import { useMutation } from "@apollo/client";
+import React, { useEffect } from 'react';
+import { Container } from '@material-ui/core';
+import Table from 'components/Shared/M1nTable/components/Table';
+import TableESHOC from '../TableESHOC';
+import TableHeader from 'components/Table/constants/bulk-data-header-schema';
+import { CREATE_JOB } from 'graphQL/useMutationCreateJob';
+import { useMutation } from '@apollo/client';
 import moment from 'moment';
 
-import { copy, deepEqualObjects } from "components/Shared/functions";
-import { usetableStyles } from "../Styles";
+import { copy, deepEqualObjects } from 'components/Shared/functions';
+import { usetableStyles } from '../Styles';
 
 function BulkDataTable(props) {
-  // const classes = useStyles();
-  const classes = usetableStyles({ isFullHeight: true, isAgreementsTable: true });
-  const [createJob, { data: createJobData }] = useMutation(CREATE_JOB);
+	// const classes = useStyles();
+	const classes = usetableStyles({ isFullHeight: true, isAgreementsTable: true });
+	const [createJob, { data: createJobData }] = useMutation(CREATE_JOB);
 
-  // queries
-  const esIndex = 'jobs_flat'
+	// queries
+	const esIndex = 'jobs_flat';
 
-  const formatHits = (hits) => {
-    hits = hits.map((hit) => {
-      hit.by = hit?.user?.name;
-      hit.progress = `${(hit.progress / hit.totalProgress) * 100} %`;
-      hit.on = moment(hit.createAt).format('MM/DD/YYYY');
+	const formatHits = hits => {
+		hits = hits.map(hit => {
+			hit.by = hit?.user?.name;
+			hit.progress = `${(hit.progress / hit.totalProgress) * 100} %`;
+			hit.on = moment(hit.createAt).format('MM/DD/YYYY');
 
-      return hit;
-    });
-    return hits;
-  };
+			return hit;
+		});
+		return hits;
+	};
 
-  useEffect(() => {
-    props.setTableMeta({
-      TableHeader: copy(TableHeader),
-      esIndex,
-      startPaginationAt: 25,
-      defaultSort: { field: "ts", order: "desc" },
-      formatHits,
-    });
-    // eslint-disable-next-line
-  }, []);
+	useEffect(() => {
+		props.setTableMeta({
+			TableHeader: copy(TableHeader),
+			esIndex,
+			startPaginationAt: 25,
+			defaultSort: { field: 'ts', order: 'desc' },
+			formatHits,
+		});
+		// eslint-disable-next-line
+	}, []);
 
-  useEffect(() => {
-    if (props.clickedRow) {
-      props.setFailedJob(props.clickedRow)
-    }
-  }, [props.clickedRow])
+	useEffect(() => {
+		if (props.clickedRow) {
+			props.setFailedJob(props.clickedRow);
+		}
+	}, [props.clickedRow]);
 
+	const createJobTest = () => {
+		createJob({
+			variables: {
+				jobId: props.rows[0]._id,
+				sendEmail: true,
+			},
+		});
+	};
 
-  const createJobTest = () => {
-    createJob({
-      variables: {
-        jobId: props.rows[0]._id,
-        sendEmail: true,
-      },
-    });
-  }
-
-  delete props.options.customRender
-  return (
-    <>
-      <Container
-        maxWidth={false}
-        className={classes.container}
-        id={props.id ? props.id : props.parent}
-      >
-        {/* <Button
+	delete props.options.customRender;
+	return (
+		<>
+			<Container maxWidth={false} className={classes.container} id={props.id ? props.id : props.parent}>
+				{/* <Button
         id="addSaveButton"
         color="secondary"
         variant="contained"
@@ -74,29 +69,29 @@ function BulkDataTable(props) {
       >
         Edit Data
       </Button> */}
-        <Table
-          style={{ backgroundColor: "#fff" }}
-          header={props.header}
-          columns={props.columns}
-          rows={props.rows}
-          total={false}
-          loading={props.loading}
-          targetLabel={props.targetLabel}
-          uploadIcon={null}
-          dense={props.dense ? props.dense : undefined}
-          orderByTracks={false}
-          startPaginationAt={null}
-          onTableChange={props.onTableChange}
-          options={{
-            ...props.options,
-          }}
-          parent={props.parent}
-          setColumnsBase={[]}
-          {...props.esHocProps}
-        />
-      </Container>
-    </>
-  );
+				<Table
+					style={{ backgroundColor: '#fff' }}
+					header={props.header}
+					columns={props.columns}
+					rows={props.rows}
+					total={false}
+					loading={props.loading}
+					targetLabel={props.targetLabel}
+					uploadIcon={null}
+					dense={props.dense ? props.dense : undefined}
+					orderByTracks={false}
+					startPaginationAt={null}
+					onTableChange={props.onTableChange}
+					options={{
+						...props.options,
+					}}
+					parent={props.parent}
+					setColumnsBase={[]}
+					{...props.esHocProps}
+				/>
+			</Container>
+		</>
+	);
 }
 
 export default React.memo(TableESHOC(BulkDataTable), deepEqualObjects);
