a495000b5f005592a3d0eb6abf94e028a558278b
iamzishan78 <zishannaseeb528@gmail.com>
2024-10-21 16:22:52 +0500

    Moved association implementationn in seperate dialog and moved styles

--- src/components/MRTTable/TablesOverride/AssetCustomEntities/Popups/CustomAssetEntityDialog.js
+++ src/components/MRTTable/TablesOverride/AssetCustomEntities/Popups/CustomAssetEntityDialog.js
@@ -1,87 +1,17 @@
-import React, { useEffect, useState } from 'react';
-import { makeStyles } from '@material-ui/styles';
+import React, { useEffect } from 'react';
 import CloseIcon from '@material-ui/icons/Close';
-import { Grid, Dialog, IconButton, Button, TextField, MenuItem, Chip } from '@material-ui/core';
+import { Grid, Dialog, IconButton, Button, TextField, MenuItem } from '@material-ui/core';
 import Loader from 'components/Loaders';
-import { useLazyQuery, useMutation } from '@apollo/client';
+import { useMutation } from '@apollo/client';
 import { Controller, useForm, useWatch } from 'react-hook-form';
 import { tableGlobalController } from 'hookstate/tableController';
 import { UPSERT_CUSTOM_ASSET_INFO } from 'graphQL/useMutationUpsertCustomAssetInfo';
-import { GET_ALL_MODELS } from 'graphQL/useQueryModels';
 import DynamicForm from '../Forms/DynamicForm';
 import { entityCreationOptions } from 'components/MRTTable/utils/data';
-
-const useStyles = makeStyles(theme => ({
-	header: {
-		display: 'flex',
-		justifyContent: 'space-between',
-		padding: '15px 30px',
-	},
-
-	dialogActions: {
-		display: 'flex',
-		justifyContent: 'flex-end',
-		'& svg': {
-			fill: '#d9d9d9',
-			'&:hover': {
-				fill: '#b5b2b2',
-			},
-		},
-	},
-
-	btnColor: {
-		color: 'white',
-		backgroundColor: '#4576CF',
-	},
-
-	assetsContainer: {
-		marginTop: '20px',
-		display: 'flex',
-		justifyContent: 'center',
-		alignItems: 'center',
-		flexDirection: 'column',
-	},
-
-	tableWrapper: {
-		maxHeight: '500px', // Set the maximum height
-		overflowY: 'auto', // Enable vertical scrolling
-		width: '80%',
-	},
-
-	assetTable: {
-		width: '100%',
-		borderCollapse: 'collapse',
-		'& th, & td': {
-			border: '1px solid #ddd',
-			padding: '8px',
-		},
-		'& th': {
-			backgroundColor: '#f2f2f2',
-			position: 'sticky',
-			top: '0', // Stick to the top of the container
-		},
-	},
-
-	columnContainer: {
-		width: '100%',
-		marginLeft: '80px',
-	},
-
-	entityRow: {
-		display: 'flex',
-		alignItems: 'center',
-		justifyContent: 'flex-start',
-		width: '100%',
-	},
-
-	actionButton: {
-		marginLeft: theme.spacing(1),
-	},
-}));
+import { useStyles } from './styles';
 
 function CustomAssetEntityDialog() {
 	const classes = useStyles();
-	const [modelsOptions, setModelsOptions] = useState([]);
 	const defaultFields = [
 		{ _id: '', mappingKey: '', keyType: '', label: '', isSummaryField: false, isControlColumn: false },
 	];
@@ -90,7 +20,6 @@ function CustomAssetEntityDialog() {
 			table_name: '',
 			fields: defaultFields,
 			creation_place: '',
-      associatedModels: [],
 		},
 	});
 
@@ -102,14 +31,6 @@ function CustomAssetEntityDialog() {
 	const { selectedAsset } = stateValues || {};
 
 	const isCreateMode = type === 'addCustomAsset';
-
-	const [getAllModels, { data: allModels }] = useLazyQuery(GET_ALL_MODELS, {
-		fetchPolicy: 'no-cache',
-		onCompleted: () => {
-			const models = allModels?.getAllModels?.models || [];
-			setModelsOptions(models);
-		},
-	});
 
 	const [storeCustomAsset, { data }] = useMutation(UPSERT_CUSTOM_ASSET_INFO, {
 		onCompleted: () => {
@@ -124,17 +45,10 @@ function CustomAssetEntityDialog() {
 	});
 
 	useEffect(() => {
-		if (isOpen) {
-			getAllModels();
-		}
-	}, [isOpen]);
-
-	useEffect(() => {
 		reset({
 			table_name: selectedAsset?.tableName || '',
 			fields: selectedAsset?.modelKeys || defaultFields,
 			creation_place: selectedAsset?.creationPlace || '',
-			associatedModels: selectedAsset?.associatedModels || [],
 		});
 	}, [selectedAsset, reset]);
 
@@ -154,7 +68,6 @@ function CustomAssetEntityDialog() {
 				tableName: data.table_name,
 				modelKeys: data.fields,
 				creationPlace: data.creation_place,
-        associatedModels: data.associatedModels,
 			},
 		}).then(res => {
 			if (res?.data?.upsertCustomAssetInfo) {
@@ -244,52 +157,11 @@ function CustomAssetEntityDialog() {
 											)}
 										/>
 									</Grid>
-									<Grid item xs={6}>
-										<Controller
-											control={control}
-											name={`associatedModels`}
-											render={props => (
-												<TextField
-													select
-													size="small"
-													type="text"
-													variant="outlined"
-													value={props.value || []}
-													inputRef={props.ref}
-													onWheel={e => e.target.blur()}
-													onChange={e => {
-														props.onChange(e.target.value);
-													}}
-													label="Associations"
-													placeholder="Associations"
-													fullWidth
-													defaultValue=""
-													SelectProps={{
-														multiple: true,
-														renderValue: selected => (
-															<div style={{ display: 'flex', flexWrap: 'wrap' }}>
-																{selected.map(value => (
-																	<Chip key={value} label={value} style={{ margin: 2 }} />
-																))}
-															</div>
-														),
-													}}
-													disabled={!isCreateMode}
-												>
-													{modelsOptions?.map(option => (
-														<MenuItem key={option.value} value={option.value}>
-															{option.label}
-														</MenuItem>
-													))}
-												</TextField>
-											)}
-										/>
-									</Grid>
 								</Grid>
 								<Grid item>
 									<h3>Add Model Keys for this Entity in this table </h3>
 								</Grid>
-								<DynamicForm control={control} setValue={setValue} isCreateMode={isCreateMode} />
+								<DynamicForm control={control} setValue={setValue} />
 							</div>
 
 							<div
