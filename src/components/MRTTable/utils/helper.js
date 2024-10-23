0a4ef601cc3a73b5e360e29e13f62fc5d801352d
iamzishan78 <zishannaseeb528@gmail.com>
2024-10-23 12:47:57 +0500

    Added generic detail page

--- src/components/MRTTable/utils/helper.js
+++ src/components/MRTTable/utils/helper.js
@@ -1,45 +1,54 @@
-export const formatGridViewToMRT = (selectedGridView) => {
-  const tableProperties = {}
-  if (selectedGridView?.columns) {
-    tableProperties.columnVisibility = selectedGridView?.columns.reduce((acc, obj) => {
-      acc[obj.name] = obj.display;
-      return acc;
-    }, {});
-  }
-  if (selectedGridView?.filters?.length) {
-    tableProperties.filters = selectedGridView.filters
-  }
-  if (selectedGridView?.sorting?.length) {
-    tableProperties.sorting = selectedGridView.sorting;
-  }
-  if (selectedGridView?.columnPinning) {
-    tableProperties.columnPinning = selectedGridView.columnPinning;
-  }
-  if (selectedGridView?.columnOrdering) {
-    tableProperties.columnOrdering = selectedGridView.columnOrdering;
-  }
+export const formatGridViewToMRT = selectedGridView => {
+	const tableProperties = {};
+	if (selectedGridView?.columns) {
+		tableProperties.columnVisibility = selectedGridView?.columns.reduce((acc, obj) => {
+			acc[obj.name] = obj.display;
+			return acc;
+		}, {});
+	}
+	if (selectedGridView?.filters?.length) {
+		tableProperties.filters = selectedGridView.filters;
+	}
+	if (selectedGridView?.sorting?.length) {
+		tableProperties.sorting = selectedGridView.sorting;
+	}
+	if (selectedGridView?.columnPinning) {
+		tableProperties.columnPinning = selectedGridView.columnPinning;
+	}
+	if (selectedGridView?.columnOrdering) {
+		tableProperties.columnOrdering = selectedGridView.columnOrdering;
+	}
 
-  return tableProperties
-}
+	return tableProperties;
+};
 
 // Helper for extracting values
-export const extractValueRecursively = (obj) => {
-  if (obj === null || obj === undefined) return undefined;
-  if (obj === 'NaN') return null;
+export const extractValueRecursively = obj => {
+	if (obj === null || obj === undefined) return undefined;
+	if (obj === 'NaN') return null;
 
-  if (typeof obj === 'object' && !Array.isArray(obj)) {
-    return Object.keys(obj).reduce((acc, key) => {
-      const value = extractValueRecursively(obj[key]?.value !== undefined ? obj[key]?.value : obj[key]);
-      acc[key] = value !== undefined ? value : obj[key];
-      return acc;
-    }, {});
-  }
+	if (typeof obj === 'object' && !Array.isArray(obj)) {
+		return Object.keys(obj).reduce((acc, key) => {
+			const value = extractValueRecursively(obj[key]?.value !== undefined ? obj[key]?.value : obj[key]);
+			acc[key] = value !== undefined ? value : obj[key];
+			return acc;
+		}, {});
+	}
 
-  return obj;
+	return obj;
 };
 
 // Helper for replacing spaces with underscore
-export const removeSpacesAndLowercase = (key) => key?.replace(/\s+/g, '_')?.toLowerCase();
+export const removeSpacesAndLowercase = key => key?.replace(/\s+/g, '_')?.toLowerCase();
 
 // Helper for removing spaces
-export const removeSpaces = (key) => key?.replace(/\s+/g, '').toLowerCase();
+export const removeSpaces = key => key?.replace(/\s+/g, '_').toLowerCase();
+
+export const replaceUnderscoreAndCapitalize = str => {
+	return str
+		.toLowerCase()
+		.replace(/_/g, ' ')
+		.split(' ')
+		.map(word => word.charAt(0).toUpperCase() + word.slice(1)) // Capitalize the first letter of each word
+		.join(' ');
+};
