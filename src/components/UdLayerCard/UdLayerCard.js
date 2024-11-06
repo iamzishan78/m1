38b6b0ddff947b9e8fb78de98b83d13868355232
iamzishan78 <zishannaseeb528@gmail.com>
2024-11-06 16:03:26 +0500

    UdLayerCard Add + Sync added

--- src/components/UdLayerCard/UdLayerCard.js
+++ src/components/UdLayerCard/UdLayerCard.js
@@ -1,261 +1,337 @@
-import React, { useContext, useEffect } from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Grid from "@material-ui/core/Grid";
-import Card from "@material-ui/core/Card";
-import CardHeader from "@material-ui/core/CardHeader";
-import CardContent from "@material-ui/core/CardContent";
-import IconButton from "@material-ui/core/IconButton";
-import CloseIcon from "@material-ui/icons/Close";
-import Tooltip from "@material-ui/core/Tooltip";
-import $ from "jquery";
+import React, { useContext, useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardContent from '@material-ui/core/CardContent';
+import IconButton from '@material-ui/core/IconButton';
+import Tooltip from '@material-ui/core/Tooltip';
+import $ from 'jquery';
+import { useLazyQuery } from '@apollo/client';
+import { Close, Layers, Sync } from '@material-ui/icons';
 
-// contexts
-import { AppContext } from "../../AppContext";
-import { clearMapAndCloseShapeActionsPopup } from "components/MapControls/commonHelper";
-import LayerIcon from "@material-ui/icons/Layers";
-import { popupController } from "hookstate/popupStateController";
-import { drawController } from "hookstate/drawStateController";
-import { layerRefs } from "hookstate";
-import { mapControlsController } from "hookstate/mapControlsController";
-import { mapStateController } from "hookstate/mapStateController";
-import FilterAltIcon from "components/Shared/svgIcons/FilterAltIcon";
-import { globalStateController } from "hookstate/globalStateController";
+import { AppContext } from '../../AppContext';
+import { clearMapAndCloseShapeActionsPopup } from 'components/MapControls/commonHelper';
+import { popupController } from 'hookstate/popupStateController';
+import { drawController } from 'hookstate/drawStateController';
+import { layerRefs } from 'hookstate';
+import { mapControlsController } from 'hookstate/mapControlsController';
+import { mapStateController } from 'hookstate/mapStateController';
+import FilterAltIcon from 'components/Shared/svgIcons/FilterAltIcon';
+import M1neral_headers, { getCustomFieldHeaders } from 'components/BulkUpload/jobHeaders';
+import { history } from 'store';
+import { jobController } from 'hookstate/jobStateController';
+import { GET_META_DATA } from 'graphQL/useQueryGetMetaData';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-  },
-  card: {
-    position: (props) => props.position,
-    left: (props) => props.cardLeft,
-    borderRadius: 0,
-    top: (props) => props.cardTop,
-    webkitTransform: "translateZ(0)",
-    transition: "width 0.1s, height 0.1s, left 0.1s, top 0.1s",
-    width: (props) => props.cardWidth,
-    height: (props) => (props.expanded ? props.height : "inherit"),
-    background: "#112040",
-    borderStyle: "solid",
-    borderWidth: "thin",
-    borderColor: "#112040",
-    "& .MuiCardHeader-action": {
-      alignSelf: "left",
-    },
-    zIndex: 1250,
-  },
-  title: {
-    fontFamily: "Poppins",
-    color: "#FFFFFF",
-    fontSize: (props) => (["Contact", "Contact Details", "Add Activity", "Activity Details"].includes(props.title) ? "20px" : "15px"),
-  },
-  headerIcons: {
-    "& .MuiBadge-anchorOriginTopRightRectangle": {
-      right: "10px",
-      top: "5px",
-    },
-  },
-  subheader: {
-    fontFamily: "Poppins",
-    color: "#FFFFFF",
-    fontSize: "11px",
-  },
-  headerContainer: {
-    wordBreak: 'break-word',
-  },
-  content: {
-    backgroundColor: "#fffff",
-    transition: "height 0.1s",
-    background: "#fff",
-    padding: "0 !important",
-    overflowY: "auto",
-    height: "325px",
-    "&::-webkit-scrollbar": {
-      width: "0.75em",
-    },
-    "&::-webkit-scrollbar-thumb": {
-      backgroundColor: "#929292",
-      borderRadius: 10,
-    },
-  },
-  icons: {
-    "&:hover": {
-      backgroundColor: "#031d40",
-    },
-    color: "white",
-  },
-  contentGrid: {
-    padding: 20,
-  }
+const useStyles = makeStyles(theme => ({
+	root: {},
+	card: {
+		position: props => props.position,
+		left: props => props.cardLeft,
+		borderRadius: 0,
+		top: props => props.cardTop,
+		webkitTransform: 'translateZ(0)',
+		transition: 'width 0.1s, height 0.1s, left 0.1s, top 0.1s',
+		width: props => props.cardWidth,
+		height: props => (props.expanded ? props.height : 'inherit'),
+		background: '#112040',
+		borderStyle: 'solid',
+		borderWidth: 'thin',
+		borderColor: '#112040',
+		'& .MuiCardHeader-action': {
+			alignSelf: 'left',
+		},
+		zIndex: 1250,
+	},
+	title: {
+		fontFamily: 'Poppins',
+		color: '#FFFFFF',
+		fontSize: props =>
+			['Contact', 'Contact Details', 'Add Activity', 'Activity Details'].includes(props.title) ? '20px' : '15px',
+	},
+	headerIcons: {
+		'& .MuiBadge-anchorOriginTopRightRectangle': {
+			right: '10px',
+			top: '5px',
+		},
+	},
+	subheader: {
+		fontFamily: 'Poppins',
+		color: '#FFFFFF',
+		fontSize: '11px',
+	},
+	headerContainer: {
+		wordBreak: 'break-word',
+	},
+	content: {
+		backgroundColor: '#fffff',
+		transition: 'height 0.1s',
+		background: '#fff',
+		padding: '0 !important',
+		overflowY: 'auto',
+		height: '325px',
+		'&::-webkit-scrollbar': {
+			width: '0.75em',
+		},
+		'&::-webkit-scrollbar-thumb': {
+			backgroundColor: '#929292',
+			borderRadius: 10,
+		},
+	},
+	icons: {
+		'&:hover': {
+			backgroundColor: '#031d40',
+		},
+		color: 'white',
+	},
+	contentGrid: {
+		padding: 20,
+	},
 }));
 
 export const getUdLayerCardTitle = ({ layer, properties }) => {
-  let { layerName, groupName, id } = layer;
-  layerName = layerName || properties.Unit_Name || id
-  if (!layerName && !groupName) {
-    return "--";
-  }
-  return groupName || layerName;
+	let { layerName, groupName, id } = layer;
+	layerName = layerName || properties.Unit_Name || id;
+	if (!layerName && !groupName) {
+		return '--';
+	}
+	return groupName || layerName;
 };
 
 function UdLayerCard(props) {
-  const classes = useStyles(props);
-  // contexts
-  const [stateApp, setStateApp] = useContext(AppContext);
+	const classes = useStyles(props);
+	// contexts
+	const [stateApp, setStateApp] = useContext(AppContext);
 
-  const handleCloseLeftSidePanel = () => {
-    mapControlsController.setState({ expandedPanel: false })
-  };
+	const [getMetaData, { data: metaDataRes }] = useLazyQuery(GET_META_DATA);
 
-  const handleCloseShapeDrawer = () => {
-    drawController.reset();
+	useEffect(() => {
+		getMetaData({
+			variables: {},
+		});
+	}, [getMetaData]);
 
-    const sourceId = layerRefs.abstract_geo?.get({ noproxy: true })?.sourceId;
+	const handleCloseLeftSidePanel = () => {
+		mapControlsController.setState({ expandedPanel: false });
+	};
 
-    if (!sourceId) return;
+	const handleCloseShapeDrawer = () => {
+		drawController.reset();
 
-    // unselecting the grids
-    const featuresList = window.mapRef?.getSource(sourceId)?._data?.features || [];
-    for (let i = 0; i < featuresList.length; i++) {
-      const id = featuresList[i].properties.Id;
-      window.mapRef?.setFeatureState({ source: sourceId, id: id }, { click: false });
-    }
-  };
+		const sourceId = layerRefs.abstract_geo?.get({ noproxy: true })?.sourceId;
 
-  const handleAddShapeClick = (e, action) => {
-    if (!!popupController.getValue('expandedCard')) {
-      handleCloseLeftSidePanel();
-      handleCloseShapeDrawer();
-    }
+		if (!sourceId) return;
 
-    if (e && action) {
-      if (action === "draw") {
-        mapControlsController.updateState({ selectedMapControl: action })
+		// unselecting the grids
+		const featuresList = window.mapRef?.getSource(sourceId)?._data?.features || [];
+		for (let i = 0; i < featuresList.length; i++) {
+			const id = featuresList[i].properties.Id;
+			window.mapRef?.setFeatureState({ source: sourceId, id: id }, { click: false });
+		}
+	};
 
-        if (!drawController.getValue('editDraw')) {
-          popupController.reset();
+	const handleAddShapeClick = (e, action) => {
+		if (!!popupController.getValue('expandedCard')) {
+			handleCloseLeftSidePanel();
+			handleCloseShapeDrawer();
+		}
 
-          drawController.updateState({ showAddShapePopup: true });
-        } else {
-          clearMapAndCloseShapeActionsPopup(stateApp, setStateApp);
-        }
-      }
-    }
-    const { toggle3d, toggleZoomOut } = mapStateController.getValues(['toggle3d', 'toggleZoomOut'])
-    mapStateController.updateState({
-      toggle3d: action === "threed" ? !toggle3d : toggle3d,
-      toggleZoomOut: action === "zoomout" ? !toggleZoomOut : toggleZoomOut,
-    })
+		if (e && action) {
+			if (action === 'draw') {
+				mapControlsController.updateState({ selectedMapControl: action });
 
-    if (window.drawRef && window.drawRef.getMode() !== "simple_select") {
-      drawController.updateState({
-        editDraw: false,
-      });
-      window.drawRef.changeMode("simple_select");
-    }
-  };
+				if (!drawController.getValue('editDraw')) {
+					popupController.reset();
 
+					drawController.updateState({ showAddShapePopup: true });
+				} else {
+					clearMapAndCloseShapeActionsPopup(stateApp, setStateApp);
+				}
+			}
+		}
+		const { toggle3d, toggleZoomOut } = mapStateController.getValues(['toggle3d', 'toggleZoomOut']);
+		mapStateController.updateState({
+			toggle3d: action === 'threed' ? !toggle3d : toggle3d,
+			toggleZoomOut: action === 'zoomout' ? !toggleZoomOut : toggleZoomOut,
+		});
 
-  if (!props.selectedUserDefinedLayer) {
-    return <></>
-  }
-  const {
-    selectedUserDefinedLayer: { layer, properties },
-    parent,
-  } = props;
+		if (window.drawRef && window.drawRef.getMode() !== 'simple_select') {
+			drawController.updateState({
+				editDraw: false,
+			});
+			window.drawRef.changeMode('simple_select');
+		}
+	};
 
-  const handleClose = () => {
-    if (parent === "map") {
-      if ($("#tempPopupHolder").length) {
-        let popUps = document.getElementsByClassName("mapboxgl-popup");
-        if (popUps[0]) popUps[0].remove();
-      }
+	if (!props.selectedUserDefinedLayer) {
+		return <></>;
+	}
+	const {
+		selectedUserDefinedLayer: { layer, properties },
+		parent,
+	} = props;
 
-      popupController.reset();
-      drawController.reset();
+	const handleClose = () => {
+		if (parent === 'map') {
+			if ($('#tempPopupHolder').length) {
+				let popUps = document.getElementsByClassName('mapboxgl-popup');
+				if (popUps[0]) popUps[0].remove();
+			}
 
-      setStateApp((state) => ({
-        ...state,
-        viewDoc: null,
-      }));
-    }
-  };
+			popupController.reset();
+			drawController.reset();
 
-  const getTitle = () => {
-    return (
-      <div
-        style={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "flex-start",
-          width: "100%",
-          marginRight: "48px",
-        }}
-      >
-        {getUdLayerCardTitle({ layer, properties })}
-      </div>
-    );
-  };
+			setStateApp(state => ({
+				...state,
+				viewDoc: null,
+			}));
+		}
+	};
 
-  return (
-    <React.Fragment>
-      <Card className={classes.card}>
-        <CardHeader
-          data-testid='ud-layer-card-header'
-          classes={{ title: classes.title, subheader: classes.subheader }}
-          className={classes.headerContainer}
-          action={
-            <div className={classes.headerIcons}>
-              {/* Filter support added back */}
-              <Tooltip title="Filter" placement="top">
-                <IconButton
-                  size="small"
-                  onClick={() => {
-                    drawController.updateState({ editDraw: true });
-                    drawController.actionFilter()
-                    popupController.updateState({ popupOpen: false });
-                  }}
-                  aria-label="Filter"
-                  data-testid="filter-on-map"
-                >
-                  <FilterAltIcon color="secondary" />
-                </IconButton>
-              </Tooltip>
+	const getTitle = () => {
+		return (
+			<div
+				style={{
+					display: 'flex',
+					alignItems: 'center',
+					justifyContent: 'flex-start',
+					width: '100%',
+					marginRight: '48px',
+				}}
+			>
+				{getUdLayerCardTitle({ layer, properties })}
+			</div>
+		);
+	};
 
-              <Tooltip title={"Add Shape to Layer"} placement="top">
-                <IconButton size={"small"} onClick={(e) => handleAddShapeClick(e, 'draw')} aria-label="close" className={classes.icons}>
-                  <LayerIcon color="secondary" />
-                </IconButton>
-              </Tooltip>
-              <Tooltip title={"Close"} placement="top">
-                <IconButton size={"small"} onClick={handleClose} aria-label="close" className={classes.icons}>
-                  <CloseIcon color="secondary" />
-                </IconButton>
-              </Tooltip>
-            </div>
-          }
-          // Expandable Card Title
-          title={getTitle()}
-          // Expandable Card Secondary Header
-          subheader={layer.groupName ? layer.layerName : ""}
-        />
-        <CardContent className={classes.content}>
-          <Grid container direction="row" alignItems="center" justify="flex" className={classes.contentGrid}>
-            {Object.keys(properties)
-              .filter((prop) => prop !== "shapeCenter" && prop !== "originalProperties")
-              .map((prop) => (
-                <>
-                  <Grid item xs={5}>
-                    {prop}
-                  </Grid>
-                  <Grid item xs={7} style={{ fontWeight: "bold" }}>
-                    {properties[prop]}
-                  </Grid>
-                </>
-              ))}
-          </Grid>
-        </CardContent>
-      </Card>
-    </React.Fragment>
-  );
+	const handleSync = async () => {
+		let columns = [];
+
+		const hits = [props.selectedUserDefinedLayer];
+
+		hits.forEach(hit => {
+			const currentColumns = Object.keys(hit.properties);
+			if (currentColumns.length > columns.length) columns = currentColumns;
+		});
+
+		const jobType = 'TRACT_SHAPE';
+
+		let m1neralHeaders = M1neral_headers[jobType] || [];
+
+		const customFieldHeaders = getCustomFieldHeaders(jobType, metaDataRes?.getMetaData?.metaData);
+
+		m1neralHeaders = [...m1neralHeaders, ...customFieldHeaders];
+
+		for (let i = 0; i < columns.length; i++) {
+			const columnName = columns[i];
+
+			const matchedKey = m1neralHeaders.find(el => el?.label === columnName);
+
+			const column = {
+				mapped_key: columnName,
+				required: !!matchedKey?.actual_key,
+				actual_key: matchedKey?.actual_key || '',
+				label: matchedKey?.label || '',
+			};
+
+			if (column?.actual_key === matchedKey?.actual_key) {
+				matchedKey.mapped_key = column.mapped_key;
+				matchedKey.required = column.required;
+			}
+
+			columns[i] = column;
+		}
+
+		jobController.updateState({
+			transferData: {
+				selectedSourceCategory: {
+					m1neralHeaders,
+					mappedHeadersFromCSV: columns,
+					hits,
+				},
+				selectedPlatformCategory: {
+					index: 8,
+					value: 'tract',
+					label: 'Tracts',
+					mapGrid: false,
+				},
+			},
+		});
+
+		history.push(`/bulkupload/shape_to_m1_layer`);
+	};
+
+	return (
+		<React.Fragment>
+			<Card className={classes.card}>
+				<CardHeader
+					data-testid="ud-layer-card-header"
+					classes={{ title: classes.title, subheader: classes.subheader }}
+					className={classes.headerContainer}
+					action={
+						<div className={classes.headerIcons}>
+							{/* Filter support added back */}
+							<Tooltip title="Filter" placement="top">
+								<IconButton
+									size="small"
+									onClick={() => {
+										drawController.updateState({ editDraw: true });
+										drawController.actionFilter();
+										popupController.updateState({ popupOpen: false });
+									}}
+									aria-label="Filter"
+									data-testid="filter-on-map"
+								>
+									<FilterAltIcon color="secondary" />
+								</IconButton>
+							</Tooltip>
+
+							<Tooltip title={'Add + Sync'} placement="top">
+								<IconButton size={'small'} onClick={e => handleSync()} aria-label="close" className={classes.icons}>
+									<Sync color="secondary" />
+								</IconButton>
+							</Tooltip>
+							<Tooltip title={'Add Shape to Layer'} placement="top">
+								<IconButton
+									size={'small'}
+									onClick={e => handleAddShapeClick(e, 'draw')}
+									aria-label="close"
+									className={classes.icons}
+								>
+									<Layers color="secondary" />
+								</IconButton>
+							</Tooltip>
+							<Tooltip title={'Close'} placement="top">
+								<IconButton size={'small'} onClick={handleClose} aria-label="close" className={classes.icons}>
+									<Close color="secondary" />
+								</IconButton>
+							</Tooltip>
+						</div>
+					}
+					// Expandable Card Title
+					title={getTitle()}
+					// Expandable Card Secondary Header
+					subheader={layer.groupName ? layer.layerName : ''}
+				/>
+				<CardContent className={classes.content}>
+					<Grid container direction="row" alignItems="center" justify="flex" className={classes.contentGrid}>
+						{Object.keys(properties)
+							.filter(prop => prop !== 'shapeCenter' && prop !== 'originalProperties')
+							.map(prop => (
+								<>
+									<Grid item xs={5}>
+										{prop}
+									</Grid>
+									<Grid item xs={7} style={{ fontWeight: 'bold' }}>
+										{properties[prop]}
+									</Grid>
+								</>
+							))}
+					</Grid>
+				</CardContent>
+			</Card>
+		</React.Fragment>
+	);
 }
 
 UdLayerCard.whyDidYouRender = true;
