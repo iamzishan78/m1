989003541dc40d3fc903419557b2e6467b88918f
iamzishan78 <zishannaseeb528@gmail.com>
2024-11-07 16:36:11 +0500

    isElasticQuery added in gridGenericRemove

--- src/components/MRTTable/Common/Dialog/index.js
+++ src/components/MRTTable/Common/Dialog/index.js
@@ -7,7 +7,7 @@ import { AssignOwnerToContactDrawerContainer, MultipleOwnerToContactDrawerContai
 import RightDialog from 'components/ContactDetailCard/components/RightDialog';
 import BuyContactsInfoDialogContent from 'components/Shared/M1nTable/components/SubComponents/BuyContactsInfoDialogContent';
 import DeleteConfirmationDialogContent from './ConfirmationDialog/DeleteConfirmationDialog';
-import { REMOVECOMMONGRIDFUNCTIONALITY } from 'graphQL/useMutationCommonGridRemove';
+import { GRID_GENERIC_REMOVE } from 'graphQL/useMutationCommonGridRemove';
 import Loader from 'components/Loaders';
 import CommentDialog from './CommentDialog';
 import TagDialog from './TagDialog';
@@ -20,10 +20,10 @@ function AllDialogs(props) {
 	const tableKey = rest?.tableKey;
 
 	const {
-		stateValues: { refetchQueries },
-	} = tableController(props.tableKey).useState(['refetchQueries']);
+		stateValues: { refetchQueries, isElasticQuery },
+	} = tableController(props.tableKey).useState(['refetchQueries', 'isElasticQuery']);
 
-	const [removeCommonDelete] = useMutation(REMOVECOMMONGRIDFUNCTIONALITY, {
+	const [gridGenericRemove] = useMutation(GRID_GENERIC_REMOVE, {
 		awaitRefetchQueries: true,
 		refetchQueries,
 	});
@@ -47,7 +47,7 @@ function AllDialogs(props) {
 		Loader.createToast('deletion', 'Deletion in Progress');
 		const user = globalStateController.getValue('user');
 		const testCase = globalStateController.getValue('testCase');
-		removeCommonDelete({
+		gridGenericRemove({
 			variables: {
 				tableKey,
 				deletedData: dataToDelete,
@@ -55,6 +55,7 @@ function AllDialogs(props) {
 				ESVariables: rest?.ESVariables,
 				isSelectAll: rest?.isSelectAll,
 				cypressDelete: testCase?.cypressDelete,
+				isElasticQuery,
 			},
 		}).then(
 			res => {
