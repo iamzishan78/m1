26d1aa362cca5bf7a388f4c0c4a7d306f7cfc480
iamzishan78 <zishannaseeb528@gmail.com>
2024-11-07 18:46:33 +0500

    Generalised the commonn summary fields component

--- src/components/Shared/components/common/DetailCard/CommonSummaryFields.js
+++ src/components/Shared/components/common/DetailCard/CommonSummaryFields.js
@@ -1,9 +1,6 @@
-import React, { Fragment, memo, useEffect, useState } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 
 import { useLazyQuery } from '@apollo/client';
-
-import { get } from 'lodash';
-import { copy } from 'utils/helper';
 
 import AddIcon from '@material-ui/icons/Add';
 import { Button, Grid } from '@material-ui/core';
@@ -14,23 +11,16 @@ import { GET_META_DATA } from 'graphQL/useQueryGetMetaData';
 import { globalStateController } from 'hookstate/globalStateController';
 import { detailCardController } from 'hookstate/detailCardController';
 
-import DateField from './Fields/DateField';
-import OwnerField from './Fields/OwnerField';
-import SummaryDropdown from './Fields/SummaryDropdown';
-import SummaryTextField from './Fields/SummaryTextField';
-import SummaryUsersList from './Fields/SummaryUsersList';
-import ShapeAutoComplete from './Fields/shapeAutoComplete';
-import SummaryAutoComplete from './Fields/SummaryAutoComplete';
 import MetaField from 'components/Table/helpers/MetaField';
-import SimpleSelectField from 'components/Shared/components/common/DetailCard/Fields/SimpleSelectFIeld';
 import { popupController } from 'hookstate/popupStateController';
+import GenericFields from './GenericFields';
 
 const useStyles = makeStyles(theme => ({
-	container: {
+	container: ({ isBasicInfo }) => ({
 		height: '100%',
-		padding: '10px 30px 15px 5px',
-		marginBottom: '30px',
-	},
+		padding: !isBasicInfo && '10px 30px 15px 5px',
+		marginBottom: !isBasicInfo && '30px',
+	}),
 	gridStyle: {
 		display: 'flex',
 		flexDirection: 'row',
@@ -40,38 +30,6 @@ const useStyles = makeStyles(theme => ({
 	fieldLabel: {
 		fontWeight: 'bold',
 		fontSize: '15px',
-	},
-	datePicker: {
-		'& .MuiIconButton-root': {
-			padding: '12px 0px',
-		},
-		'& .MuiFormControl-marginNormal': {
-			margin: '0px',
-		},
-	},
-	field: {
-		'& .MuiAutocomplete-clearIndicator': {
-			marginRight: '10px',
-		},
-		'& .MuiFormControl-marginNormal': {
-			margin: '0px',
-		},
-		'& .MuiFormControl-marginDense': {
-			margin: '0px',
-		},
-		'& .MuiInputBase-root': {
-			borderRadius: '7px',
-		},
-	},
-	emailAdornment: {
-		cursor: 'pointer',
-	},
-	baseValueChanged: {
-		width: '100%',
-		'& .MuiInputBase-input': {
-			color: 'dodgerblue',
-			fontWeight: 'bold',
-		},
 	},
 	addDataButton: {
 		marginTop: '2rem',
@@ -86,95 +44,8 @@ const useStyles = makeStyles(theme => ({
 	},
 }));
 
-const RenderFieldComponent = memo(({ field: fieldObj, summaryDataValues }) => {
-	const field = copy(fieldObj);
-	const isMetaField = field._id && field.category;
-
-	field.key = (field.mappingKey || field.key)?.replaceAll('.keyword', '');
-	field.type = field.keyType || field.type;
-
-	switch (field.keyType) {
-		case 'text':
-		case 'string':
-		case 'textarea':
-		case 'email':
-		case 'currency':
-		case 'number':
-			return (
-				<SummaryTextField
-					fieldData={get(summaryDataValues, field.key)}
-					field={field}
-					summaryData={summaryDataValues}
-					isMetaField={isMetaField}
-				/>
-			);
-
-		case 'autocomplete':
-			return (
-				<SummaryAutoComplete
-					fieldData={get(summaryDataValues, field.key)}
-					fieldKey={field.key}
-					defaultOptions={field.options || []}
-					payload={field.payload}
-					variant="outlined"
-				/>
-			);
-
-		case 'date':
-			return (
-				<DateField
-					fieldData={get(summaryDataValues, field.key)}
-					field={field}
-					// Add date-specific props here
-				/>
-			);
-
-		case 'simpleSelect':
-			return (
-				<SimpleSelectField
-					fieldData={get(summaryDataValues, field.key)}
-					field={field}
-					// Add simple select-specific props here
-				/>
-			);
-
-		case 'owner':
-			return <OwnerField fieldData={get(summaryDataValues, field.key)} field={field} />;
-
-		case 'user':
-			return <SummaryUsersList fieldData={get(summaryDataValues, field.key)} field={field} />;
-
-		case 'dropdown':
-		case 'multiselect':
-			return (
-				<SummaryDropdown
-					fieldData={get(summaryDataValues, field.key)}
-					field={field}
-					summaryData={summaryDataValues}
-					isMetaField={isMetaField}
-				/>
-			);
-
-		case 'shapeautocomplete':
-			return (
-				<ShapeAutoComplete
-					fieldData={get(summaryDataValues, field.key)}
-					fieldKey={field.key}
-					shapeType={field.shapeType}
-					variant="outlined"
-				/>
-			);
-
-		// Add more cases for other field types
-
-		default:
-			// You can provide a default component or handle unknown field types
-			return <div>{`Unsupported Field Type : ${field.type}`}</div>;
-	}
-});
-
-export default function CommonSummaryFieldsComponent({ metaDataCategory, formFields }) {
-	const classes = useStyles();
+export default function CommonSummaryFieldsComponent({ metaDataCategory, formFields, isBasicInfo = false }) {
+	const classes = useStyles({ isBasicInfo });
 
 	const {
 		stateValues: { expandedCard },
@@ -215,9 +86,9 @@ export default function CommonSummaryFieldsComponent({ metaDataCategory, formFie
 	}, [metaDataRes, currentAsset]);
 
 	return (
-		<Grid container spacing={2} alignItems="center" className={classes.container}>
+		<Grid container spacing={isBasicInfo ? 0 : 2} alignItems="center" className={classes.container}>
 			{fields?.map((field, key) => (
-				<Grid xs={expandedCard ? 12 : 6} item key={key}>
+				<Grid xs={expandedCard || isBasicInfo ? 12 : 6} item key={key}>
 					<Grid container className={classes.gridStyle}>
 						<Grid item xs={4} style={{ display: 'flex' }}>
 							<div id={field.label} className={classes.fieldLabel}>
@@ -226,7 +97,7 @@ export default function CommonSummaryFieldsComponent({ metaDataCategory, formFie
 						</Grid>
 						<Grid item xs={8}>
 							<Fragment>
-								<RenderFieldComponent field={field} summaryDataValues={stateValues.currentAssetRecord} />
+								<GenericFields field={field} summaryDataValues={stateValues.currentAssetRecord} />
 							</Fragment>
 						</Grid>
 					</Grid>
