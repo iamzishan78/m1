c2ae12133dfed4c54d5523c9397cc27a4a373734
iamzishan78 <zishannaseeb528@gmail.com>
2024-11-08 15:59:07 +0500

    revert

--- src/components/ExpandableCard/ExpandableCard.js
+++ src/components/ExpandableCard/ExpandableCard.js
@@ -333,7 +333,7 @@ function ExpandableCard(props) {
 		};
 
 		disableBodyScrollBarIfExpanded();
-		if (document.getElementById('side-panel-pullout-btn')) document.getElementById(' ').style.display = 'none'; // hide pullout button from the sidebar when details card is opened
+		if (document.getElementById('side-panel-pullout-btn')) document.getElementById('side-panel-pullout-btn').style.display = 'none'; // hide pullout button from the sidebar when details card is opened
 		return () => {
 			if (document.getElementById('side-panel-pullout-btn'))
 				document.getElementById('side-panel-pullout-btn').style.display = 'flex'; // Show pullout button from the sidebar when details card get closed
