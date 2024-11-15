54c35e639f8bbc85934158d3791c3b8dedba44f4
iamzishan78 <zishannaseeb528@gmail.com>
2024-11-15 17:35:57 +0500

    [IMPROVEMENT] - Map > On deleting file from layer manager there should be a success message for better user experience

--- src/components/Shared/SidePanel/compoennts/Datasets/Menu.js
+++ src/components/Shared/SidePanel/compoennts/Datasets/Menu.js
@@ -2,100 +2,115 @@ import React from 'react';
 import Menu from '@material-ui/core/Menu';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import { makeStyles, List, ListItem, ListItemText } from '@material-ui/core';
-import DeleteOutlineOutlinedIcon from '@material-ui/icons/DeleteOutlineOutlined';
+import VisibilityOffOutlinedIcon from '@material-ui/icons/VisibilityOffOutlined';
 import ArrowForwardOutlinedIcon from '@material-ui/icons/ArrowForwardOutlined';
 
+const useStyles = makeStyles(theme => ({
+	popover: {
+		'& .MuiPopover-paper': {
+			color: 'rgb(59, 70, 99)',
+			backgroundColor: '#1c2233',
+			marginTop: '45px',
+			marginLeft: '30px',
+			// left: '10% !important',
+		},
+		'& .MuiTabs-indicator': {
+			height: '4px',
+			backgroundColor: 'rgba(23, 170, 221, 1)',
+		},
 
-const useStyles = makeStyles((theme) => ({
-    popover: {
-        "& .MuiPopover-paper": {
-            color: 'rgb(59, 70, 99)',
-            backgroundColor: '#1c2233',
-            marginTop: '45px',
-            marginLeft: '30px',
-            // left: '10% !important',
-        },
-        "& .MuiTabs-indicator": {
-            height: '4px',
-            backgroundColor: "rgba(23, 170, 221, 1)",
-        },
+		'& .MuiFilledInput-root': {
+			backgroundColor: '#252d40',
+		},
+		'& .Mui-disabled': {
+			paddingBottom: '10px',
+			borderBottom: '1px solid lightgrey',
+		},
+		'& .MuiMenuItem-root': {
+			'&:hover': {
+				color: 'rgba(23, 170, 221, 1)',
+			},
+		},
 
-        "& .MuiFilledInput-root": {
-            backgroundColor: '#252d40'
-        },
-        "& .Mui-disabled": {
-            paddingBottom: "10px",
-            borderBottom: "1px solid lightgrey",
-        },
-        "& .MuiMenuItem-root": {
-            "&:hover": {
-                color: "rgba(23, 170, 221, 1)",
-            },
-        },
-
-        '& .MuiCircularProgress-colorPrimary': {
-            color: "rgba(23, 170, 221, 1)",
-        },
-        '& .menu': {
-            paddingRight: "10px",
-            color: '#ffff',
-            '& .MuiTypography-body1': {
-                fontWeight: 500,
-            }
-        }
-    },
-    inputField: {
-        padding: '20px',
-        '& .MuiInputLabel-filled': {
-            color: 'grey'
-        },
-        '& .MuiFilledInput-input': {
-            color: '#fff'
-        }
-    }
+		'& .MuiCircularProgress-colorPrimary': {
+			color: 'rgba(23, 170, 221, 1)',
+		},
+		'& .menu': {
+			paddingRight: '10px',
+			color: '#ffff',
+			'& .MuiTypography-body1': {
+				fontWeight: 500,
+			},
+		},
+	},
+	inputField: {
+		padding: '20px',
+		'& .MuiInputLabel-filled': {
+			color: 'grey',
+		},
+		'& .MuiFilledInput-input': {
+			color: '#fff',
+		},
+	},
 }));
 
-
 export default function DatasetMenu({ dataset, handleRemove, handleTransfer }) {
-    const classes = useStyles();
+	const classes = useStyles();
 
-    const [anchorEl, setAnchorEl] = React.useState(null);
+	const [anchorEl, setAnchorEl] = React.useState(null);
 
-    const handleClick = (event) => {
-        event.stopPropagation();
-        setAnchorEl(event.currentTarget);
-    };
+	const handleClick = event => {
+		event.stopPropagation();
+		setAnchorEl(event.currentTarget);
+	};
 
-    const handleClose = (event) => {
-        event.stopPropagation();
-        setAnchorEl(null);
-    };
+	const handleClose = event => {
+		event.stopPropagation();
+		setAnchorEl(null);
+	};
 
-    return (
-        <div >
-            <MoreVertIcon id={"dataset-morevert-" + dataset.sourceName} aria-controls={"dataset-menu " + dataset.sourceName} className='actionIcon' onClick={handleClick} />
-            <Menu
-                id={"dataset-menu " + dataset.sourceName}
-                anchorEl={anchorEl}
-                keepMounted
-                open={Boolean(anchorEl)}
-                onClose={handleClose}
-                className={classes.popover}
-            >
-                <div className={'menu'}>
-                    <List component="nav" aria-label="main mailbox folders" disablePadding >
-                        <ListItem button onClick={(e) => { handleClose(e); handleTransfer(dataset); }}>
-                            <ArrowForwardOutlinedIcon />
-                            <ListItemText primary="Transfer" />
-                        </ListItem>
-                        <ListItem id={"remove-source-" + dataset.sourceName} button onClick={(e) => { handleClose(e); handleRemove(dataset, false) }}>
-                            <DeleteOutlineOutlinedIcon />
-                            <ListItemText primary="Remove" />
-                        </ListItem>
-                    </List>
-                </div>
-
-            </Menu>
-        </div>
-    );
+	return (
+		<div>
+			<MoreVertIcon
+				id={'dataset-morevert-' + dataset.sourceName}
+				aria-controls={'dataset-menu ' + dataset.sourceName}
+				className="actionIcon"
+				onClick={handleClick}
+			/>
+			<Menu
+				id={'dataset-menu ' + dataset.sourceName}
+				anchorEl={anchorEl}
+				keepMounted
+				open={Boolean(anchorEl)}
+				onClose={handleClose}
+				className={classes.popover}
+			>
+				<div className={'menu'}>
+					<List component="nav" aria-label="main mailbox folders" disablePadding>
+						<ListItem
+							button
+							onClick={e => {
+								handleClose(e);
+								handleTransfer(dataset);
+							}}
+						>
+							<ArrowForwardOutlinedIcon />
+							<ListItemText primary="Transfer" />
+						</ListItem>
+						<ListItem
+							id={'remove-source-' + dataset.sourceName}
+							button
+							onClick={e => {
+								handleClose(e);
+								handleRemove(dataset, false);
+							}}
+						>
+							<VisibilityOffOutlinedIcon />
+							<ListItemText primary="Hide" />
+						</ListItem>
+					</List>
+				</div>
+			</Menu>
+		</div>
+	);
 }
