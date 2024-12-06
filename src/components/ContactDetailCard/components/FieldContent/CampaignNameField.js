d6c564d32fb4bdef4700138995ba22f137a12fb3
iamzishan78 <zishannaseeb528@gmail.com>
2024-12-06 18:17:56 +0500

    Correctly accessing original property from hits  where neede

--- src/components/ContactDetailCard/components/FieldContent/CampaignNameField.js
+++ src/components/ContactDetailCard/components/FieldContent/CampaignNameField.js
@@ -7,7 +7,7 @@ import Grid from '@material-ui/core/Grid';
 import ClearIcon from '@material-ui/icons/Clear';
 import { useLazyQuery, useMutation } from '@apollo/client';
 
-import { GET_ES_FILTER_LIST } from 'graphQL/useQueryESFilterList';
+import { GET_DB_FILTERS } from 'graphQL/useQueryDbQuery';
 import { UPSERT_CAMPAIGN_DESCRIPTORS } from 'graphQL/useMutationCampaign';
 import 'components/Shared/Tagger.css';
 
@@ -104,14 +104,14 @@ export default function CampaignNameField(props) {
 	const [inputValue, setInputValue] = useState([]);
 	const [tFActive, setTFActive] = useState(false);
 
-	const [getCampaignFilters, { data: campaignfiltersData }] = useLazyQuery(GET_ES_FILTER_LIST, {
+	const [getCampaignFilters, { data: campaignfiltersData }] = useLazyQuery(GET_DB_FILTERS, {
 		fetchPolicy: 'no-cache',
 	});
 	const [upsertCampaignDescriptors] = useMutation(UPSERT_CAMPAIGN_DESCRIPTORS);
 
 	useEffect(() => {
-		if (campaignfiltersData?.getESFilterList?.hits) {
-			const allFiltersData = campaignfiltersData.getESFilterList.hits.map(hit => hit.key);
+		if (campaignfiltersData?.getDbFilters?.hits) {
+			const allFiltersData = campaignfiltersData.getDbFilters.hits.map(hit => hit.key);
 			setOptions(allFiltersData.filter(d => d));
 		}
 	}, [campaignfiltersData]);
@@ -129,9 +129,12 @@ export default function CampaignNameField(props) {
 	useEffect(() => {
 		getCampaignFilters({
 			variables: {
-				esIndex: 'campaigns_flat',
-				filterKey: 'name.keyword',
-				size: 1000,
+				index: 'campaigns_flat',
+				filterAggs: {
+					field: 'name.keyword',
+					size: 1000,
+					type: 'withOriginalIds',
+				},
 			},
 		});
 	}, [getCampaignFilters]);
@@ -149,20 +152,22 @@ export default function CampaignNameField(props) {
 				relatedObject: props.targetLabelId,
 				isDeleted: false,
 			};
+
 		if (reason === 'select-option') {
-			campaign = campaignfiltersData.getESFilterList.hits.find(hit => hit.key === values[values.length - 1]);
+			campaign = campaignfiltersData.getDbFilters.hits.find(hit => hit.key === values[values.length - 1]);
 			if (campaign) {
-				payload.descriptorObject = campaign.original.hits.hits[0]._id;
+				payload.descriptorObject = campaign.original[0]?._id;
 			}
 		} else {
-			const deletedCampaign = campaignfiltersData.getESFilterList.hits.find(
+			const deletedCampaign = campaignfiltersData.getDbFilters.hits.find(
 				hit => hit.key === inputValue.find(v => !values.includes(v))
 			);
 			if (deletedCampaign) {
-				payload.descriptorObject = deletedCampaign.original.hits.hits[0]._id;
+				payload.descriptorObject = deletedCampaign.original[0]?._id;
 			}
 			payload.isDeleted = true;
 		}
+
 		props.onChange(values, payload.descriptorObject);
 		if (payload.relatedObject)
 			upsertCampaignDescriptors({
@@ -226,8 +231,3 @@ export default function CampaignNameField(props) {
 		</div>
 	);
 }
-
-CampaignNameField.defaultProps = {
-	type: 'textfield',
-	simpleChips: false,
-};
