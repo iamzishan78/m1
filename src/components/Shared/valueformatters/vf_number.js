53130b87cb8644ce4b14d3e33d07e74f50fbe8f0
iamzishan78 <zishannaseeb528@gmail.com>
2024-12-06 18:38:56 +0500

    debugger removed

--- src/components/Shared/valueformatters/vf_number.js
+++ src/components/Shared/valueformatters/vf_number.js
@@ -1,6 +1,5 @@
 import commaNumber from 'comma-number';
 export default function vf_number(value, toFixed) {
-	debugger;
 	const numberValue = typeof value === 'number' ? value : parseFloat(value);
 
 	const formattedValue = commaNumber(typeof toFixed === 'number' ? numberValue.toFixed(toFixed) : numberValue);
