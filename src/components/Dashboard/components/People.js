050a9be4c7b22f92aa0c57008e9e1b42a4102381
iamzishan78 <zishannaseeb528@gmail.com>
2024-12-11 18:55:54 +0530

    add profile photo to users in organization

--- src/components/Dashboard/components/People.js
+++ src/components/Dashboard/components/People.js
@@ -1,10 +1,11 @@
-import React, { Fragment } from 'react';
-import { useQuery } from '@apollo/client';
+import React, { Fragment, useEffect, useState } from 'react';
+import { useLazyQuery, useQuery } from '@apollo/client';
 import { Box, CardHeader, CircularProgress, Grid, Typography } from '@mui/material';
 import { GETMONGOUSERS } from 'graphQL/useQueryGetUsers';
 import { makeStyles } from '@material-ui/styles';
 import Avatar from 'react-avatar';
 import { getRandomColor } from 'components/Shared/functions/ui';
+import { GET_PROFILES_IMAGES } from 'graphQL/useQueryGetProfile';
 
 const useStyles = makeStyles(theme => ({
 	headerTitle: {
@@ -17,7 +18,36 @@ const useStyles = makeStyles(theme => ({
 
 const People = () => {
 	const classes = useStyles();
+	const [profilesInfo, setProfilesInfo] = useState({});
+	const [usersData, setUsersData] = useState([]);
 	const { data: userLists, loading } = useQuery(GETMONGOUSERS);
+	const [getProfilesImages, profilesData] = useLazyQuery(GET_PROFILES_IMAGES, {
+		fetchPolicy: 'cache-first',
+	});
+
+	useEffect(() => {
+		if (userLists && userLists.allMongoUsers) {
+			const data = userLists.allMongoUsers
+				?.map(user => ({
+					_id: user._id,
+					name: user.name,
+					displayName: user.displayName || user.name,
+					email: user.email,
+				}))
+				.filter(user => user._id && user.name);
+			setUsersData(data);
+			const emails = userLists.allMongoUsers?.map(user => user.email);
+			getProfilesImages({
+				variables: { emails },
+			});
+		}
+	}, [userLists, getProfilesImages]);
+
+	useEffect(() => {
+		if (profilesData?.data?.profileByEmail?.profiles) {
+			setProfilesInfo(profilesData.data.profileByEmail.profiles);
+		}
+	}, [profilesData]);
 
 	const PeopleCardContainer = () => {
 		return (
@@ -28,7 +58,7 @@ const People = () => {
 					rowSpacing={2}
 					sx={{ marginLeft: '8px', marginRight: '8px', maxHeight: 'calc(100% - 70px)', overflow: 'auto' }}
 				>
-					{!userLists && !userLists?.allMongoUsers.length ? (
+					{!usersData.length ? (
 						<Grid item xs={12} md={12} sm={12}>
 							<Typography variant="body1" fontSize={'1rem'}>
 								No People Found
@@ -36,17 +66,21 @@ const People = () => {
 						</Grid>
 					) : (
 						<Fragment>
-							{userLists.allMongoUsers.map(user => {
+							{usersData.map(user => {
 								return (
 									<Grid item xs={6}>
 										<Box display={'flex'} alignItems={'center'}>
-											<Avatar
-												color={getRandomColor(user?.displayName)}
-												fgColor="#fff"
-												name={user?.displayName ? user?.displayName.split(' ').splice(0, 2).join(' ') : ''}
-												size="60"
-												round
-											/>
+											{profilesInfo[user?.email]?.profileImage ? (
+												<Avatar src={profilesInfo[user?.email].profileImage} size="60" round />
+											) : (
+												<Avatar
+													color={getRandomColor(user?.displayName)}
+													fgColor="#fff"
+													name={user?.displayName ? user?.displayName.split(' ').splice(0, 2).join(' ') : ''}
+													size="60"
+													round
+												/>
+											)}
 											<Typography fontSize={'0.8rem'} sx={{ marginLeft: '8px' }}>
 												{user?.displayName ? user?.displayName : ''}
 											</Typography>
