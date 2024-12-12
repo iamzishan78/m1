aa29edf15e2f31a0f9b06b53ded2c7728c5aad43
iamzishan78 <zishannaseeb528@gmail.com>
2024-12-12 23:24:09 +0530

    add fade background image

--- src/components/AzureLogin/hexoCatCanvas.js
+++ src/components/AzureLogin/hexoCatCanvas.js
@@ -10,7 +10,7 @@ const HexocetCanvas = () => {
 			stepCount: 0,
 			birthPeriod: 1,
 			hexSize: 40,
-			targetBounceChance: 0.005,
+			targetBounceChance: 0.02,
 			springStiffness: 0.01,
 			viscosity: 0.8,
 			particleOpacity: 0.7,
@@ -120,39 +120,13 @@ const HexocetCanvas = () => {
 			ctx.closePath();
 		};
 
-		const drawBackgroundImage = () => {
-			const ctx = Hexocet.ctx;
-
-			if (!backgroundImage.complete) return;
-
-			const canvasWidth = Hexocet.canvas.width;
-			const canvasHeight = Hexocet.canvas.height;
-
-			const imageAspectRatio = backgroundImage.width / backgroundImage.height;
-			const canvasAspectRatio = canvasWidth / canvasHeight;
-
-			let drawWidth, drawHeight;
-			if (imageAspectRatio > canvasAspectRatio) {
-				drawWidth = canvasHeight * imageAspectRatio;
-				drawHeight = canvasHeight;
-			} else {
-				drawWidth = canvasWidth;
-				drawHeight = canvasWidth / imageAspectRatio;
-			}
-
-			const drawX = (canvasWidth - drawWidth) / 2;
-			const drawY = (canvasHeight - drawHeight) / 2;
-
-			ctx.drawImage(backgroundImage, drawX, drawY, drawWidth, drawHeight);
-		};
-
 		const draw = () => {
 			const ctx = Hexocet.ctx;
-			ctx.rect(0, 0, Hexocet.canvas.width, Hexocet.canvas.height);
-			ctx.globalAlpha = Hexocet.fadeLayerOpacity;
-
-			drawBackgroundImage();
-			ctx.globalAlpha = 1;
+			if (Hexocet.fade) {
+				ctx.rect(0, 0, Hexocet.canvas.width, Hexocet.canvas.height);
+				ctx.fillStyle = `rgba(0, 0, 0, ${Hexocet.fadeLayerOpacity})`;
+				ctx.fill();
+			}
 
 			// Draw hexagons around seeds
 			Hexocet.seeds.forEach(seed => {
