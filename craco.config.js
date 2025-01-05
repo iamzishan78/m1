2dbb64bc47e3ee4eee44eec3f854da83c9dc7b61
iamzishan78 <zishannaseeb528@gmail.com>
2025-01-05 21:37:22 +0000

    Updated craco.config.js

--- craco.config.js
+++ craco.config.js
@@ -1,61 +1,92 @@
+const presetReact = require('@babel/preset-react').default;
+const presetCRA = require('babel-preset-react-app');
+const CracoEsbuildPlugin = require('craco-esbuild');
+const { ProvidePlugin } = require('webpack');
+
 module.exports = {
+	babel: {
+		loaderOptions: (babelLoaderOptions, { env, paths }) => {
+			const origBabelPresetReactAppIndex = babelLoaderOptions.presets.findIndex(preset => {
+				return preset[0].includes('babel-preset-react-app')
+			})
+
+			if (origBabelPresetReactAppIndex === -1) {
+				return babelLoaderOptions;
+			}
+
+			const overridenBabelPresetReactApp = (...args) => {
+				const babelPresetReactAppResult = presetCRA(...args);
+				const origPresetReact = babelPresetReactAppResult.presets.find(preset => {
+					return preset[0] === presetReact;
+				});
+				Object.assign(origPresetReact[1], {
+					importSource: '@welldone-software/why-did-you-render',
+				});
+				return babelPresetReactAppResult;
+			}
+
+			babelLoaderOptions.presets[origBabelPresetReactAppIndex] = overridenBabelPresetReactApp;
+
+			return babelLoaderOptions;
+		},
+	},
+	eslint: {
+		enable: false
+	},
 	webpack: {
-		configure: webpackConfig => {
-			// Add a Babel loader to handle optional chaining and other modern JavaScript features
+		configure: (webpackConfig, { env, paths, ...rest }) => {
+			webpackConfig.entry = process.env.CYPRESS === 'true' ? './src/cypress.js' : './src/index.js';
+			// Add Babel loader for specific modules
 			webpackConfig.module.rules.push({
-				test: /\.(js|jsx|mjs|cjs|ts|tsx)$/, // Include JS/TS files
-				exclude: /node_modules/, // Exclude node_modules
+				test: /\.js$/,
+				include: [
+					/node_modules\/@mui\/x-date-pickers/, // For previous issue
+					/node_modules\/@tanstack\/virtual-core/, // For this issue
+					/node_modules\/@mui\/utils/, // Include @mui/utils
+				],
 				use: {
 					loader: 'babel-loader',
 					options: {
-						presets: ['@babel/preset-env', ['@babel/preset-react', { runtime: 'automatic' }]],
+						presets: ['@babel/preset-env'],
 						plugins: [
-							['@babel/plugin-proposal-class-properties', { loose: true }], // Add class fields support
-							['@babel/plugin-proposal-private-methods', { loose: true }], // Add private methods support
-							['@babel/plugin-proposal-private-property-in-object', { loose: true }], // Add private properties in objects
 							'@babel/plugin-proposal-optional-chaining',
 							'@babel/plugin-proposal-nullish-coalescing-operator',
-							'@babel/plugin-proposal-logical-assignment-operators',
+							'@babel/plugin-proposal-class-properties',
+							'@babel/plugin-proposal-private-methods',
 						],
 					},
 				},
 			});
+
 			return webpackConfig;
 		},
-	},
-	babel: {
-		// Ensure optional chaining is supported
+
 		plugins: [
-			['@babel/plugin-proposal-class-properties', { loose: true }], // Add class fields support
-			['@babel/plugin-proposal-private-methods', { loose: true }], // Add private methods support
-			['@babel/plugin-proposal-private-property-in-object', { loose: true }], // Add private properties in objects
-			'@babel/plugin-proposal-optional-chaining',
-			'@babel/plugin-proposal-nullish-coalescing-operator',
-			'@babel/plugin-proposal-logical-assignment-operators',
+			new ProvidePlugin({
+				React: 'react',
+			}),
 		],
 	},
-	eslint: {
-		enable: false,
-		mode: 'extends',
-		configure: {
-			rules: {
-				// Example: disable prop-types rule for Material-UI
-				'react/prop-types': 'off',
+	plugins: [{
+		plugin: CracoEsbuildPlugin,
+		options: {
+			esbuildLoaderOptions: {
+				// Optional. Defaults to auto-detect loader.
+				loader: 'jsx', // Set the value to 'tsx' if you use typescript
+				target: 'es2018',
 			},
-		},
-	},
-	// style: {
-	//  postcss: {
-	//      plugins: [
-	//          require('tailwindcss'),
-	//          require('autoprefixer'),
-	//      ],
-	//  },
-	// },
-	resolve: {
-		alias: {
-			// Ensure Material-UI styled-engine works correctly with styled-components if needed
-			'@mui/styled-engine': '@mui/styled-engine-sc',
-		},
-	},
+			esbuildMinimizerOptions: {
+				// Optional. Defaults to:
+				target: 'es2018',
+				css: true, // if true, OptimizeCssAssetsWebpackPlugin will also be replaced by esbuild.
+			},
+			skipEsbuildJest: true, // Optional. Set to true if you want to use babel for jest tests,
+			esbuildJestOptions: {
+				loaders: {
+					'.ts': 'ts',
+					'.tsx': 'tsx',
+				}
+			}
+		}
+	}]
 };
