589548d0e51a6e0cf870869531fbccac1e90d81d
iamzishan78 <zishannaseeb528@gmail.com>
2025-01-09 15:55:39 +0500

    Fixed table key issue

--- src/components/Shared/SidePanel/compoennts/Filters/UserMapFilter.js
+++ src/components/Shared/SidePanel/compoennts/Filters/UserMapFilter.js
@@ -154,11 +154,6 @@ const UserMapFilter = ({ mapView, index, remove, resetForm }) => {
 
 	const layers = globalStateController.getValue('layers');
 
-	const tableKey = Object.keys(tableESState).find(key => {
-		const tableState = tableESState[key].get({ noproxy: true });
-		return tableState?.layerIdentifier === dataSourceName;
-	});
-
 	const [debouncedFilterValues, setDebouncedFilterValues] = useState(filterValues); // New state for debounced filter values
 
 	// Debounce the filter values
@@ -274,6 +269,10 @@ const UserMapFilter = ({ mapView, index, remove, resetForm }) => {
 			const mapViewFilters = getMapViewFilters();
 			// Upsert the map view data to the GraphQL API
 			if (canUpdateMapView) {
+				const tableKey = Object.keys(tableESState).find(key => {
+					const tableState = tableESState[key].get({ noproxy: true });
+					return tableState?.layerIdentifier === dataSourceName;
+				});
 				const tableState = tableESState[tableKey]?.get({ noproxy: true });
 
 				const formattedFilter = getFormattedFilterBasedOnType(
@@ -373,6 +372,10 @@ const UserMapFilter = ({ mapView, index, remove, resetForm }) => {
 		if (dataSourceName && !(customLayersFieldAccessors[dataSourceName]?.keys || layer?.layerSchema)) {
 			return [];
 		}
+		const tableKey = Object.keys(tableESState).find(key => {
+			const tableState = tableESState[key].get({ noproxy: true });
+			return tableState?.layerIdentifier === dataSourceName;
+		});
 
 		const fields = [
 			{
@@ -459,14 +462,17 @@ const UserMapFilter = ({ mapView, index, remove, resetForm }) => {
 
 		mapViewFilters = mapViewFilters.filter((_, i) => i !== index);
 
-		remove(index); // Set the filter cleared state to true
-
+		const tableKey = Object.keys(tableESState).find(key => {
+			const tableState = tableESState[key].get({ noproxy: true });
+			return tableState?.layerIdentifier === dataSourceName;
+		});
 		tableController(tableKey).clearFilter((fieldName?.value || fieldName)?.replace('.keyword', ''), false);
 		tableController(tableKey).setFilterMode(
 			(fieldName?.value || fieldName)?.replace('.keyword', ''),
 			'singleselect',
 			false
 		);
+		remove(index); // Set the filter cleared state to true
 		resetForm({
 			mapViews: mapViewFilters || [],
 		});
