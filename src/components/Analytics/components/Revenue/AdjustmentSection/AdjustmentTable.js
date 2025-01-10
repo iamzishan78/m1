afded9ca0a106639f55ee0bbd978d4a057c088ec
iamzishan78 <zishannaseeb528@gmail.com>
2025-01-10 13:09:26 +0530

    Update the table width same as other table in income statement

--- src/components/Analytics/components/Revenue/AdjustmentSection/AdjustmentTable.js
+++ src/components/Analytics/components/Revenue/AdjustmentSection/AdjustmentTable.js
@@ -126,12 +126,12 @@ export default function AdjustmentTable({ monthsInterval, items, total }) {
 						<Table className={classes.table} aria-label="caption table">
 							<TableHead>
 								<TableRow>
-									<TableCell style={{ paddingLeft: 0 }} component="th" className={`${classes.nameCell} ${classes.headerCell}`}>
+									<TableCell style={{ paddingLeft: 0, width: '-webkit-fill-available' }} component="th" className={`${classes.nameCell} ${classes.headerCell}`}>
 										<CSVDownloader
 											datas={convertAnalyticsDataToCSV(csvItems, monthsInterval)}
 											filename={'Adjustments'}
 											type="link"
-											style={{ width: 'max-content', position: 'relative',  left: '15px'   }}
+											style={{ position: 'relative',  left: '15px', width: '30px'   }}
 										>
 											<IconButton style={{ display: 'flex', padding: '0px',}}>
 												<Tooltip title="Download CSV" aria-label="add">
