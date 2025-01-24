bf150472e42beb063c42b5a41521c8cc6941ff57
iamzishan78 <zishannaseeb528@gmail.com>
2025-01-24 10:41:21 +0530

    Fixed the isPurchased symbol not showing at tract interest owner

--- src/components/MRTTable/Schema/tract_interest_owner_schema.js
+++ src/components/MRTTable/Schema/tract_interest_owner_schema.js
@@ -87,7 +87,7 @@ const TractInterestOwnerMeta = {
 			header: 'Owner Name',
 			Cell: ({ renderedCellValue, row }) => {
 				// Check if the contact is purchased
-				const isPurchased = [true, 'true', 'True'].includes(row.getValue('isPurchased'));
+				const isPurchased = [true, 'true', 'True'].includes(row?.original?.isPurchased);
 				return (
 					<div
 						style={{
