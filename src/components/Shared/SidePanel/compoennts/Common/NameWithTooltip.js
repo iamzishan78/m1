a05899b0a831f153b1831109775ecdb881d8168c
iamzishan78 <zishannaseeb528@gmail.com>
2025-01-27 15:57:04 +0530

    Implemented common typography for showing tooltip

---
+++ src/components/Shared/SidePanel/compoennts/Common/NameWithTooltip.js
@@ -0,0 +1,59 @@
+import React, { useRef, useState, useEffect } from "react";
+import { Typography, Tooltip, Box } from "@mui/material";
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+    iconTooltip: {
+        fontSize: '15px',
+        padding: '10px',
+    },
+}))
+const NameWithTooltip = ({ title, style, index }) => {
+    const classes = useStyles();
+    const textRef = useRef(null); // Reference to Typography element
+    const [isTruncated, setIsTruncated] = useState(false); // State to track if text is truncated
+
+    // Check if the text is truncated by comparing scrollWidth and offsetWidth
+    useEffect(() => {
+        const checkTruncation = () => {
+            if (textRef.current) {
+                const { offsetWidth, scrollWidth } = textRef.current;
+                setIsTruncated(scrollWidth > offsetWidth); // If text is overflowing, it's truncated
+            }
+        };
+
+        checkTruncation();
+        window.addEventListener("resize", checkTruncation); // Recalculate on resize
+        return () => window.removeEventListener("resize", checkTruncation); // Clean up event listener
+    }, [title, index]); // Re-run when the `title` changes
+
+    return (
+        <>
+
+            <Typography
+                ref={textRef}
+                noWrap
+                style={style}
+            >
+                {title}
+            </Typography>
+            {isTruncated && (
+                <Tooltip classes={{ tooltip: classes.iconTooltip }} arrow title={title}>
+                    <Box
+                        sx={{
+                            position: "relative",
+                            top: "0px",
+                            right: "12px",
+                            width: "12px", // Approximate ellipsis width
+                            // height: "15px",
+                            pointerEvents: "auto",
+                            cursor: "pointer",
+                        }}
+                    />
+                </Tooltip>
+            )}
+        </>
+    );
+};
+
+export default NameWithTooltip;
