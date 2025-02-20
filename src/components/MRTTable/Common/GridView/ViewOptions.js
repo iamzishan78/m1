02519da829b227d2c7fe2dd9d7c0082134da0722
iamzishan78 <zishannaseeb528@gmail.com>
2025-02-20 13:52:16 +0500

    Common Views favourite tab bug fixed

--- src/components/MRTTable/Common/GridView/ViewOptions.js
+++ src/components/MRTTable/Common/GridView/ViewOptions.js
@@ -99,7 +99,7 @@ function ViewOptions({ moduleName, buttonRef }) {
 		'isViewOpen',
 		'fetchViewSettings',
 	]);
-	const { isTable, allViews, isViewOpen, fetchViewSettings } = viewStateValues;
+	const { allViews, isViewOpen, fetchViewSettings } = viewStateValues;
 
 	const [selectedTab, setSelectedTab] = useState('views');
 	const [filterViews, setfilterViews] = useState(allViews);
@@ -109,15 +109,8 @@ function ViewOptions({ moduleName, buttonRef }) {
 		if (selectedTab === 'views') {
 			setfilterViews(allViews);
 		} else if (selectedTab === 'favorites') {
-			let data = null;
-
-			if (isTable) {
-				data = allViews.filter(view => view.favouriteBy?.includes(user?._id));
-			} else {
-				data = allViews.filter(view => view.isFavourite === true);
-			}
-
-			setfilterViews(data);
+			const favViews = allViews.filter(view => view.favouriteBy?.includes(user?._id));
+			setfilterViews(favViews);
 		} else {
 			setfilterViews([]);
 		}
