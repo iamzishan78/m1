f2e4dee20fd4090942b49db5f26d15d81b0df287
iamzishan78 <zishannaseeb528@gmail.com>
2025-02-24 19:04:43 +0500

    Files > Add Document / edit issue fixed

--- src/controllers/AddAndEditController.js
+++ src/controllers/AddAndEditController.js
@@ -24,7 +24,7 @@ class CreateViewStateController extends StateController {
 	initialize(tableKey, FieldsValue) {
 		this.updateState({
 			tableKey,
-			FieldsValue,
+			...FieldsValue,
 		});
 	}
 }
