53043c6a53d63179a2e3bda1f73b8b5942c4258b
iamzishan78 <zishannaseeb528@gmail.com>
2025-02-27 17:42:15 +0500

    `Refactor DateField component to use CustomDatePicker`

--- src/components/Shared/components/common/DetailCard/Fields/DateField.js
+++ src/components/Shared/components/common/DetailCard/Fields/DateField.js
@@ -1,40 +1,17 @@
 import React, { useEffect, useState } from 'react';
 
-import { TextField, CircularProgress } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
+import { CircularProgress } from '@mui/material';
 
+import { debounce } from 'lodash';
 import moment from 'moment';
+import PropTypes from 'prop-types';
 
 import * as Pages from 'components/Shared/components/common/DetailCard/pages';
+import CustomDatePicker from 'components/Shared/FormsFieldsData/Fields/CustomDatePicker';
 
 import { detailCardController } from 'controllers/detailCardController';
 
-import { parseDate } from 'utils/helper';
-
-const useStyles = makeStyles({
-	dateRoot: {
-		color: 'grey',
-		'& input': {
-			marginLeft: '20px',
-		},
-	},
-	field: {
-		'& .MuiAutocomplete-clearIndicator': {
-			marginRight: '10px',
-		},
-		'& .MuiFormControl-marginNormal': {
-			margin: '0px',
-		},
-		'& .MuiFormControl-marginDense': {
-			margin: '0px',
-		},
-		'& .MuiInputBase-root': {
-			borderRadius: '7px',
-		},
-	},
-});
 function DateField({ fieldData, field }) {
-	const classes = useStyles();
 	const {
 		stateValues: { page, loadingField },
 	} = detailCardController.useState(['page', 'loadingField']);
@@ -48,50 +25,51 @@ function DateField({ fieldData, field }) {
 		setValue(fieldData || '');
 	}, [fieldData]);
 
-	const handleDateChange = value => {
-		const newValue = parseDate(value);
-		setValue(newValue);
-	};
-
-	const handleBlur = event => {
-		let currValue = event.target.value;
-
-		if (currValue !== fieldData) {
-			callApi({ key: field.key, value: currValue, field, previousValue: fieldData, resetFn: setValue });
+	const handleBlur = debounce(currValue => {
+		if (currValue.toDate().toISOString() !== fieldData) {
+			callApi({ key: field.key, value: currValue.toDate(), field, previousValue: fieldData, resetFn: setValue });
 		}
-	};
+	}, 1000);
 
-	const handleKeyUp = e => {
-		if (e.key === 'Enter') {
-			e.target.blur();
-		}
+	const handleDateChange = newValue => {
+		const formattedValue = newValue ? moment(newValue.toDate()).format('YYYY-MM-DD') : '';
+		setValue(formattedValue);
+		handleBlur(newValue);
 	};
 
 	return (
-		<TextField
-			autoOk
-			type="date"
-			variant="outlined"
-			margin="dense"
-			className={`${classes.field}`}
-			fullWidth
-			value={value ? moment(value).format('YYYY-MM-DD') : ''}
-			onBlur={handleBlur}
-			onKeyUp={handleKeyUp}
-			disabled={field.disabled}
-			onChange={e => handleDateChange(e.target.value)}
-			InputLabelProps={{
-				shrink: true,
+		<CustomDatePicker
+			fieldAttributes={{
+				name: field.key,
+				value,
+				label: field.label || '',
+				placeholder: 'Select a date',
+			}}
+			fieldConfig={{
+				disabled: field.disabled,
+				required: field.required || false,
+				fullWidth: true,
+				variant: 'outlined',
+				hasTime: false,
+			}}
+			fieldEvents={{
+				onChange: handleDateChange,
 			}}
 			InputProps={{
 				endAdornment: loadingField && loadingField === field?.key && <CircularProgress size={22} color="secondary" />,
 			}}
-			disableToolbar
-			KeyboardButtonProps={{ 'aria-label': 'change date' }}
-			format="MM/DD/YYYY"
-			PopoverProps={{ disablePortal: false }}
 		/>
 	);
 }
+
+DateField.propTypes = {
+	fieldData: PropTypes.string,
+	field: PropTypes.shape({
+		key: PropTypes.string.isRequired,
+		label: PropTypes.string,
+		disabled: PropTypes.bool,
+		required: PropTypes.bool,
+	}),
+};
 
 export default DateField;
