5211b6f837b27336d95a8705bef9988ba3ecb0fa
iamzishan78 <zishannaseeb528@gmail.com>
2025-04-08 15:56:32 +0500

    [BUG] - Contacts grid > Created Date and Updated Date filtering not working in all client environments

--- src/components/MRTTable/Hooks/useTableESSimple.js
+++ src/components/MRTTable/Hooks/useTableESSimple.js
@@ -32,7 +32,7 @@ const useTableESSimple = tableKey => {
 
 	const localizationOptions = {
 		filterCustomFilterFn: 'AutoComplete',
-		selectedCountOfRowCountRowsSelected: `${Object.keys(tableStateValues?.rowSelection || {})?.length} of ${tableStateValues?.data.total} row(s) selected`
+		selectedCountOfRowCountRowsSelected: `${Object.keys(tableStateValues?.rowSelection || {})?.length} of ${tableStateValues?.data.total} row(s) selected`,
 	};
 
 	if (tableStateValues.asyncRowSelection && !tableStateValues.isSubSetSelect)
@@ -126,31 +126,32 @@ const useTableESSimple = tableKey => {
 
 				Controller.setColumnPinning(newPinning, tableStateValues?.columnPinning, tableStateValues.TableSchema);
 			},
-			onRowSelectionChange: (checkFunc) => {
+			onRowSelectionChange: checkFunc => {
 				if (typeof checkFunc !== 'function') {
-					Controller.setIsAllRowsSelected(false)
-					Controller.setColumnCheck(checkFunc)
-					return
+					Controller.setIsAllRowsSelected(false);
+					Controller.setColumnCheck(checkFunc);
+					return;
 				}
 
-				let newstate = checkFunc(tableStateValues?.rowSelection)
+				let newstate = checkFunc(tableStateValues?.rowSelection);
 				const allNumbers = _.range(0, tableStateValues?.pageSize);
 				const missingNumbers = _.difference(allNumbers, _.keys(newstate).map(Number));
-				const selectAll = (tableStateValues.data?.rows?.length === Object.keys(newstate)?.length) && !missingNumbers.length;
+				const selectAll =
+					tableStateValues.data?.rows?.length === Object.keys(newstate)?.length && !missingNumbers.length;
 				if (selectAll) {
 					if (tableStateValues.asyncRowSelection) {
 						for (let i = 0; i < tableStateValues?.data?.rows?.length; i++) {
-							newstate[i] = true
+							newstate[i] = true;
 						}
-						Controller.setColumnCheck(newstate)
+						Controller.setColumnCheck(newstate);
 						Controller.updateState({
 							onScrollCheck: true,
-							isSubSetSelect: null
-						})
-						return
+							isSubSetSelect: null,
+						});
+						return;
 					}
 					for (let i = 0; i < tableStateValues.data?.total; i++) {
-						newstate[i] = true
+						newstate[i] = true;
 					}
 				}
 				let unselectAll = true;
@@ -163,18 +164,18 @@ const useTableESSimple = tableKey => {
 				}
 
 				if (unselectAll) {
-					Controller.setIsAllRowsSelected(false)
-					newstate = {}
+					Controller.setIsAllRowsSelected(false);
+					newstate = {};
 					if (tableStateValues?.isSubSetSelect) {
 						Controller.updateState({
 							isSubSetSelect: null,
 						});
 					}
 					Controller.updateState({
-						onScrollCheck: false
+						onScrollCheck: false,
 					});
 				}
-				Controller.setColumnCheck(newstate)
+				Controller.setColumnCheck(newstate);
 			},
 			onColumnFiltersChange: filtersFunc => {
 				const newFilters = filtersFunc(
@@ -204,16 +205,19 @@ const useTableESSimple = tableKey => {
 				Controller.syncFilters(result);
 
 				result.forEach(filter => {
-					const { mode, isKeyword } = tableState?.filterModes?.get({ noproxy: true })?.[filter.id] || {};
-
+					let { mode, isKeyword } = tableState?.filterModes?.get({ noproxy: true })?.[filter.id] || {};
 					let { value } = filter;
 					const { type, oRFilter, columnType } = filter;
 					if (mode && typeof filter.value === 'string' && columnType !== 'date')
 						value = isKeyword ? filter.value : +filter.value || 0;
 					if (mode && tableESSimpleFilterModeOtions.inclusive.includes(mode))
 						value = filter.value.map(value => +value || 0);
-					if (columnType === 'date')
-						value = filter.value
+					if (columnType === 'date') {
+						value = filter.value;
+						mode = ['greaterThanOrEqualTo', 'lessThanOrEqualTo', 'betweenInclusive'].includes(mode)
+							? mode
+							: 'betweenInclusive';
+					}
 
 					Controller.setFilter({
 						field: filter.id,
@@ -222,16 +226,16 @@ const useTableESSimple = tableKey => {
 						oRFilter,
 						...(mode &&
 							!['multiselect', 'singleselect'].includes(mode) && {
-							type: 'advanced',
-							searchType: mode,
-							isKeyword,
-							columnType,
-						}),
+								type: 'advanced',
+								searchType: mode,
+								isKeyword,
+								columnType,
+							}),
 					});
 				});
 			},
 
-			onColumnOrderChange: (ordering) => {
+			onColumnOrderChange: ordering => {
 				Controller.setColumnOrdering(ordering);
 			},
 
@@ -240,7 +244,9 @@ const useTableESSimple = tableKey => {
 					const { className } = e.target;
 					if (
 						tableStateValues?.onClickedRow &&
-						(typeof className === 'object' || className?.includes('MuiTableCell-root') || className?.includes('row-click'))
+						(typeof className === 'object' ||
+							className?.includes('MuiTableCell-root') ||
+							className?.includes('row-click'))
 					) {
 						tableStateValues?.onClickedRow(row?.row?.original);
 					}
@@ -274,9 +280,9 @@ const useTableESSimple = tableKey => {
 			enableRowNumbers: true,
 			muiToolbarAlertBannerProps: tableStateValues?.isError
 				? {
-					color: 'error',
-					children: 'Error loading data',
-				}
+						color: 'error',
+						children: 'Error loading data',
+					}
 				: undefined,
 			muiTableContainerProps: {
 				ref: tableContainerRef, // get access to the table container element
@@ -302,12 +308,12 @@ const useTableESSimple = tableKey => {
 				),
 			renderToolbarInternalActions: tableStateValues.toolbarInternalActions
 				? ({ table }) => (
-					<ToolbarInternalActions
-						table={table}
-						toolbarInternalActions={tableStateValues.toolbarInternalActions}
-						enableHiding={tableStateValues.enableHiding}
-					/>
-				)
+						<ToolbarInternalActions
+							table={table}
+							toolbarInternalActions={tableStateValues.toolbarInternalActions}
+							enableHiding={tableStateValues.enableHiding}
+						/>
+					)
 				: undefined,
 		},
 	};
