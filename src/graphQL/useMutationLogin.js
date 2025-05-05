7e4d62b7d24573366e79309b718113e39fde8bdc
iamzishan78 <zishannaseeb528@gmail.com>
2025-05-05 15:33:26 +0500

    `Removed simpleAuthBypass logic and related GraphQL mutation`

--- src/graphQL/useMutationLogin.js
+++ src/graphQL/useMutationLogin.js
@@ -20,24 +20,3 @@ export const LOGIN_MUTATION = gql`
 		}
 	}
 `;
-
-export const SIMPLE_BYPASS_LOGIN_MUTATION = gql`
-	mutation simpleBypassLogin($email: String) {
-		simpleBypassLogin(email: $email) {
-			success
-			message
-			sessionData
-			user {
-				_id
-				email
-				name
-				displayName
-				adUserId
-				rolePrivileges
-				roles
-				featureSettings
-				dialpad
-			}
-		}
-	}
-`;
