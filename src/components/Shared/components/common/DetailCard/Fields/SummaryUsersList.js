824d5c0d54a0c104d07e3bbda79c3df2a875151e
iamzishan78 <zishannaseeb528@gmail.com>
2025-05-13 14:40:09 +0500

    Fixed styling for user fields

--- src/components/Shared/components/common/DetailCard/Fields/SummaryUsersList.js
+++ src/components/Shared/components/common/DetailCard/Fields/SummaryUsersList.js
@@ -1,11 +1,21 @@
 import React, { useState, useEffect } from 'react';
 
+import { makeStyles } from '@material-ui/core/styles';
+
 import * as Pages from 'components/Shared/components/common/DetailCard/pages';
 import UsersListWithIcon from 'components/Shared/UsersListWithIcon';
 
 import { detailCardController } from 'stateManagement/detailCardController';
 
+const useStyles = makeStyles(() => ({
+	container: {
+		height: '100%',
+		paddingRight: '8px',
+	},
+}));
+
 const SummaryUsersList = ({ fieldData, field }) => {
+	const classes = useStyles();
 	const {
 		stateValues: { page },
 	} = detailCardController.useState(['page']);
@@ -19,7 +29,9 @@ const SummaryUsersList = ({ fieldData, field }) => {
 
 		if (!user) {
 			callApi({ key: field.key, value: null, field, previousValue: fieldData?._id, resetFn: setValue });
-			if (!field?.isRequired) {setValue(updatedvalue);}
+			if (!field?.isRequired) {
+				setValue(updatedvalue);
+			}
 		} else if (user?.value !== fieldData?._id) {
 			callApi({ key: field.key, value: user?.value, field, previousValue: fieldData?._id, resetFn: setValue });
 			setValue(updatedvalue);
@@ -31,12 +43,14 @@ const SummaryUsersList = ({ fieldData, field }) => {
 	}, [fieldData]);
 
 	return (
-		<UsersListWithIcon
-			field={field}
-			placeholder={`Enter ${field?.label}`}
-			selectedUserId={value}
-			onChangeUser={handleChange}
-		/>
+		<div className={classes.container}>
+			<UsersListWithIcon
+				field={field}
+				placeholder={`Enter ${field?.label}`}
+				selectedUserId={value}
+				onChangeUser={handleChange}
+			/>
+		</div>
 	);
 };
 
