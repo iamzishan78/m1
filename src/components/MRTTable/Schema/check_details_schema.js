a74f489b443d574ebf55e7ed37608c71821aa684
iamzishan78 <zishannaseeb528@gmail.com>
2025-05-30 15:18:37 +0500

    [ENHANCEMENT] - Revenue > Revenue Statement Input Mode-Convert all remaining isSelect columns to Autocomplete

--- src/components/MRTTable/Schema/check_details_schema.js
+++ src/components/MRTTable/Schema/check_details_schema.js
@@ -303,10 +303,12 @@ const CheckDetailsMeta = {
 			id: 'product',
 			header: 'Product',
 
-			muiEditTextFieldProps: editFieldProps({
+			Edit: editAutoCompleteField({
 				tableKey: 'CheckDetailsTable',
-				type: 'text',
-				isSelect: true,
+				placeholder: 'Product',
+				index: 'checkdetails_flat',
+				id: 'product',
+				addNewAllowed: false,
 			}),
 		},
 
@@ -331,11 +333,12 @@ const CheckDetailsMeta = {
 			name: 'interestType.keyword',
 			id: 'interestType',
 			header: 'Interest Type',
-
-			muiEditTextFieldProps: editFieldProps({
+			Edit: editAutoCompleteField({
 				tableKey: 'CheckDetailsTable',
-				type: 'text',
-				isSelect: true,
+				placeholder: 'Interest Type',
+				index: 'checkdetails_flat',
+				id: 'interestType',
+				addNewAllowed: false,
 			}),
 		},
 
