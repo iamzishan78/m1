e3ce00b8894805824140e7e9687f4e799b4cc348
iamzishan78 <zishannaseeb528@gmail.com>
2025-06-09 16:39:27 +0500

    feat(dialpad): add contactId to call mutation and improve dialpad UI
    - Add contactId parameter to initiateDialpadCall mutation
    - Improve dialpad call button visibility on hover
    - Add fallback to native phone call when contact not synced
    - Update phone columns to use DIALPAD_COLUMN schema

--- src/components/ContactDetailCard/components/FieldContent/DialpadTableCell.js
+++ src/components/ContactDetailCard/components/FieldContent/DialpadTableCell.js
@@ -1,77 +1,99 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import IconButton from "@material-ui/core/IconButton";
-import Tooltip from "@material-ui/core/Tooltip";
-import AddIcCallIcon from "@material-ui/icons/AddIcCall";
-import {  useMutation } from "@apollo/client";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
+import Tooltip from '@material-ui/core/Tooltip';
+import AddIcCallIcon from '@material-ui/icons/AddIcCall';
+import { useMutation } from '@apollo/client';
 
-import { FEATURES } from "components/Shared/FeatureFlag/common";
-import { globalStateController } from "hookstate/globalStateController";
-import { INITIATE_DIALPAD_CALL } from "graphQL/useMutationInitiateCall";
-import { useDispatch } from "react-redux";
-import { showErrorMessage, showInfoMessage, showSuccessMessage } from "actions";
+import { FEATURES } from 'components/Shared/FeatureFlag/common';
+import { globalStateController } from 'hookstate/globalStateController';
+import { INITIATE_DIALPAD_CALL } from 'graphQL/useMutationInitiateCall';
+import { useDispatch } from 'react-redux';
+import { showErrorMessage, showInfoMessage, showSuccessMessage } from 'actions';
 
 const useStyles = makeStyles((theme) => ({
   phoneContainer: {
     display: "flex",
     alignItems: "center",
     marginTop: theme.spacing(1),
+    position: "relative",
+    "&:hover $callButton": {
+      visibility: "visible",
+      opacity: 1,
+    },
   },
   phoneNumber: {
-    marginRight: theme.spacing(1),
     fontSize: "0.9rem",
+    marginRight: theme.spacing(1),
   },
   callButton: {
     padding: theme.spacing(0.5),
+    visibility: "hidden",
+    opacity: 0,
+    transition: "opacity 0.2s",
   },
 }));
 
+function triggerPhoneCall(phoneNumber) {
+	const a = document.createElement('a');
+	a.href = `tel:${phoneNumber}`;
+	a.style.display = 'none';
+	document.body.appendChild(a);
+	a.click();
+	document.body.removeChild(a);
+}
+
 export default function DialpadTableCell(props) {
-  const classes = useStyles();
+	const classes = useStyles();
 
-  const { globalState } = globalStateController.useState(['user'], 'globalState');
-  const feature = globalState?.user?.features?.find(feature => feature.name === FEATURES.DIALPAD_INTEGRATION);
-  const isDialPad = props.row?.original?.dialpadIds?.length && feature;
+	const { globalState } = globalStateController.useState(['user'], 'globalState');
+	const feature = globalState?.user?.features?.find(f => f.name === FEATURES.DIALPAD_INTEGRATION);
+	const isDialPad = props.row?.original?.dialpadIds?.length && feature;
 
-  const [initiateDialpadCall] = useMutation(INITIATE_DIALPAD_CALL);
-  const dispatch = useDispatch();
+	const [initiateDialpadCall] = useMutation(INITIATE_DIALPAD_CALL);
+	const dispatch = useDispatch();
 
-  const handleCall = () =>{
-    if (isDialPad && globalState.user?.dialpad) {
-      dispatch(showInfoMessage('Initiating call...'));
-      initiateDialpadCall({
-        variables: { phoneNumber: props.value, dialpadUserId: globalState?.user?.dialpad?.id },
-      }).then(({ data }) => {
-        if (data?.initiateDialpadCall?.success) {
-          dispatch(showSuccessMessage('Call initiated successfully'));
-        } else {
-          dispatch(showErrorMessage(data?.initiateDialpadCall?.message));
-        }
-      });
-    } else if (isDialPad && !globalState.user?.dialpad) {
-      dispatch(showErrorMessage('User not found on Dialpad. Please Contact Admin.'));
-    }
+	const handleCall = () => {
+		if (isDialPad && globalState.user?.dialpad) {
+			dispatch(showInfoMessage('Initiating call...'));
+			initiateDialpadCall({
+				variables: {
+					phoneNumber: props.value,
+					dialpadUserId: globalState?.user?.dialpad?.id,
+					contactId: props.row?.original?._id,
+				},
+			}).then(({ data }) => {
+				if (data?.initiateDialpadCall?.success) {
+					dispatch(showSuccessMessage('Call initiated successfully'));
+				} else {
+					console.log(data?.initiateDialpadCall?.message)
+					if (data?.initiateDialpadCall?.message?.includes('is not synced with dialpad')) {
+						dispatch(showInfoMessage(data?.initiateDialpadCall?.message));
+						triggerPhoneCall(props.value);
+					} else dispatch(showErrorMessage(data?.initiateDialpadCall?.message));
+				}
+			});
+		} else if (isDialPad && !globalState.user?.dialpad) {
+			dispatch(showErrorMessage('User not found on Dialpad. Please Contact Admin.'));
+		}
+	};
 
-  }
-  return (
-    <div>
+	return (
+		<>
       {props.value && (
         <div className={classes.phoneContainer}>
           <span className={classes.phoneNumber}>{props.value}</span>
-          <Tooltip title={'Call'} placement="top">
+          <Tooltip title="Call" placement="top">
             <IconButton
               size="small"
-              href={isDialPad ? '' : `tel: ${props.value}`}
               className={classes.callButton}
-              onClick={() => {
-                handleCall()
-              }}
+              onClick={handleCall}
             >
-              <AddIcCallIcon htmlColor="#757575" id={'dialpad'} />
+              <AddIcCallIcon htmlColor="#757575" />
             </IconButton>
           </Tooltip>
         </div>
       )}
-    </div>
-  );
+    </>
+	);
 }
--- src/graphQL/useMutationInitiateCall.js
+++ src/graphQL/useMutationInitiateCall.js
@@ -1,8 +1,8 @@
 import gql from 'graphql-tag';
 
 export const INITIATE_DIALPAD_CALL = gql`
-	mutation initiateDialpadCall($phoneNumber: String, $dialpadUserId: String) {
-		initiateDialpadCall(phoneNumber: $phoneNumber, dialpadUserId: $dialpadUserId) {
+	mutation initiateDialpadCall($phoneNumber: String, $dialpadUserId: String, $contactId: String) {
+		initiateDialpadCall(phoneNumber: $phoneNumber, dialpadUserId: $dialpadUserId, contactId: $contactId) {
 			success
 			message
 		}
